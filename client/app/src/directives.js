84b5bb6be5a6ed325efb4e6c58c11d609a5e9eb8
fix <fx.thoorens@gmail.com>
2017-05-12 22:09:00 +0200

    hotfix on decimal

--- client/app/src/directives.js
+++ client/app/src/directives.js
@@ -18,24 +18,15 @@
             var totalBalance = Number(scope.send.totalBalance * satoshis)
             var remainingBalance = ((totalBalance - num) /satoshis)
             scope.send.remainingBalance = isNaN(remainingBalance) ? totalBalance / satoshis : remainingBalance;
-            
+
             if (typeof num == "number" && num > 0) {
               if (num > Number.MAX_SAFE_INTEGER) {
                 ctrl.$setValidity('validAmount', false);
               } else {
-                var decimal = 8;
-                var index = ('' + value).indexOf('.'); // for check len digits after dot
-                var sufix = ('' + value).substring(index + 1);
-
-                if (index >= 0 && sufix.length > decimal) {
-                  ctrl.$setValidity('validAmount', false);
-                  return;
-                } else {
-                  ctrl.$setValidity('validAmount', true);
-                }
+                ctrl.$setValidity('validAmount', true);
               }
             } else {
-              ctrl.$setValidity('validAmount', false)
+              ctrl.$setValidity('validAmount', false);
             }
             return value;
           }
