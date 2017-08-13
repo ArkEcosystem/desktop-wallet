69858a24c7c84c0092b4b6d1ed9af8bf053f3f34
Peter Ince <pdince@gmail.com>
2017-08-14 00:32:59 +1000

    fix: update broken backgrounds path

--- client/app/src/coreServices/networkService.js
+++ client/app/src/coreServices/networkService.js
@@ -90,7 +90,7 @@
             exchanges: {
               changer: "ark_ARK"
             },
-            background:"url(assets/images/Ark.jpg)"
+            background:"url(assets/images/images/Ark.jpg)"
           },
           devnet:{
             nethash:'578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23',
