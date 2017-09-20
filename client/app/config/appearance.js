565e7c08274089408d5487af6bb6eb1bccd839d0
Lucio Rubens <luciorubeens@gmail.com>
2017-09-20 12:56:46 -0300

    config(appearance): add themes and colors

---
+++ client/app/config/appearance.js
@@ -0,0 +1,48 @@
+module.exports = {
+  themes: {
+    'default': {
+      primary: 'blue',
+      accent: 'green',
+      warn: 'red'
+    },
+    'caetano': {
+      primary: 'teal',
+      accent: 'cyan',
+      warn: 'pink',
+      background: 'blue-grey',
+    },
+    'evelynn': {
+      primary: 'pink',
+      accent: 'blue',
+      warn: 'deep-orange'
+    },
+    'rok': {
+      primary: 'blue',
+      accent: 'amber',
+      warn: 'green'
+    },
+    'solarized': {
+      primary: 'orange',
+      accent: 'yellow',
+      warn: 'brown'
+    },
+    'amazonia': {
+      primary: 'green',
+      accent: 'lime',
+      warn: 'light-green'
+    },
+    'brazil': {
+      primary: 'green',
+      accent: 'yellow',
+      warn: 'blue'
+    },
+    'midnight': {
+      primary: 'indigo',
+      accent: 'deep-purple',
+      warn: 'blue'
+    }
+  },
+  colors: {
+    
+  }
+};
