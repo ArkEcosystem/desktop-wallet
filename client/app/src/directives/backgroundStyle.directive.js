352880ce53636258d7dae0ea1ea166804736e335
jaml <dev@jaml.pro>
2017-09-20 23:29:41 +0200

    Permit that background textures use custom CSS styles

---
+++ client/app/src/directives/backgroundStyle.directive.js
@@ -0,0 +1,69 @@
+(function() {
+  'use strict';
+
+  /*
+   * This directive establishes the body background (color or texture).
+   * If that background is a texture, it could be configured to set a CSS class too.
+   *
+   * Since some background textures have light colors, they could use it to
+   * customize other elements, such as buttons that would not be distinguishable
+   * otherwise. Others could use it for improving the general aesthetics.
+   */
+  angular.module('arkclient.directives')
+    .directive('backgroundStyle', [function() {
+      return {
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, elem, attrs, ctrl) {
+
+          // This is the custom configuration of textures
+          var textures = {
+            'Ahoy.jpg': {
+              cssClass: 'pale-background-texture',
+            },
+            'Alchemy.gif': {
+              cssClass: 'pale-background-texture',
+            }
+          };
+
+          var classes = Object.values(textures).reduce(function(all, texture) {
+            if (all.indexOf(texture.cssClass) === -1)
+              all.push(texture.cssClass);
+            return all;
+          }, []);
+
+          // Used to extract the image filename
+          var textureRe = /url\(.+\/([^/]+).\)/;
+
+          scope.$watch(attrs.backgroundStyle, function(value) {
+            var style = { background: value };
+            var newClass = null;
+
+            if (value.match('textures'))
+              style.backgroundRepeat = 'repeat';
+            else
+              style.backgroundSize = 'cover';
+
+            elem.css(style);
+
+            var matches = value.match(textureRe);
+            if (matches) {
+              var filename = matches[1];
+
+              if (textures[filename]) {
+                newClass = textures[filename].cssClass;
+                elem.addClass(newClass);
+              }
+            }
+
+            classes.forEach(function(textureClass) {
+              if (newClass !== textureClass) {
+                elem.removeClass(textureClass);
+              }
+            })
+          });
+        }
+      }
+    }]);
+    
+})();
