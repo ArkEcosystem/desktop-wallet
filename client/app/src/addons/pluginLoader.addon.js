7f5e4bf23ef9cacc56b3d08425f8287a6cd14074
Lucio Rubens <luciorubeens@gmail.com>
2017-09-20 15:21:15 -0300

    refactor(addons): add plugin loader

--- client/app/src/addons/pluginLoader.addon.js
+++ client/app/src/addons/pluginLoader.addon.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  angular.module('arkclient.services')
+  angular.module('arkclient.addons')
     .service('pluginLoader', ['accountService', PluginLoader]);
 
   /**
--- client/app/src/init.js
+++ client/app/src/init.js
@@ -5,6 +5,7 @@
   angular.module('arkclient.services', []);
   angular.module('arkclient.components', []);
   angular.module('arkclient.directives', []);
-  angular.module('arkclient.accounts', ['ngMaterial', 'arkclient.filters']);
-
+  angular.module('arkclient.addons', []);
+  angular.module('arkclient.accounts', ['ngMaterial', 'arkclient.filters', 'arkclient.addons']);
+  
 })();
