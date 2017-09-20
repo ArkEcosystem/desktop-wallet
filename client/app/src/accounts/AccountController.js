a4e797267e1cb1bb1978045ad88a3ba7212be93a
Lucio Rubens <luciorubeens@gmail.com>
2017-09-20 13:46:05 -0300

    refactor(directives): add copy to clipboard

--- client/app/src/accounts/AccountController.js
+++ client/app/src/accounts/AccountController.js
@@ -51,40 +51,7 @@
         var price = scope.ul.connectedPeer.market.price[scope.ul.currency.name];
         return (amount * price).toFixed(5);
       }
-    }]).directive('copyToClipboard', function($window, $mdToast) {
-      var body = angular.element($window.document.body);
-      var textarea = angular.element('<textarea/>');
-      textarea.css({
-        position: 'fixed',
-        opacity: '0'
-      });
-
-      function copy(toCopy) {
-        textarea.val(toCopy);
-        body.append(textarea);
-        textarea[0].select();
-
-        try {
-          var successful = document.execCommand('copy');
-          if (!successful) throw successful;
-        } catch (err) {
-          console.log("failed to copy", toCopy);
-        }
-        $mdToast.simple()
-          .textContent('Text copied to clipboard!')
-          .hideDelay(2000);
-        textarea.remove();
-      }
-
-      return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-          element.bind('click', function(e) {
-            copy(attrs.copyToClipboard);
-          });
-        }
-      }
-    });
+    }]);
   /**
    * Main Controller for the Angular Material Starter App
    * @param $scope
