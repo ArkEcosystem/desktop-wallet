f8fbcdddf24795de6e7d956e7bc32cdebb93a4e1
Lucio Rubens <luciorubeens@gmail.com>
2017-09-20 13:01:10 -0300

    refactor(app.js): add init modules

--- client/app/src/components/components.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  // Prepare the 'coreutils' module for subsequent registration of controllers and delegates
-  angular.module('arkclient.components', [ ]);
-
-})();
---
+++ client/app/src/init.js
@@ -0,0 +1,4 @@
+angular.module('arkclient.filters', []);
+angular.module('arkclient.services', []);
+angular.module('arkclient.components', []);
+angular.module('arkclient.directives', []);
