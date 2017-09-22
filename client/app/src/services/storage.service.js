ccadad85472e84a921a2efcbfd8057f04b44ddac
Lucio Rubens <luciorubeens@gmail.com>
2017-09-22 11:18:24 -0300

    bug: show the edit icon when contact with same address as selected

--- client/app/src/services/storage.service.js
+++ client/app/src/services/storage.service.js
@@ -5,7 +5,12 @@
     .service('storageService', [StorageService]);
 
   /**
-   * NetworkService
+   * StorageService
+   * 
+   * The storage service is responsible for storing/retrieving persistant values within the application, typically related to the network context.
+   * 
+   * The service itself will not save any data until a 10s timer has passed.
+   * 
    * @constructor
    */
   function StorageService() {
@@ -19,11 +24,20 @@
       switchContext(context);
     }
 
-
+    /**
+     * Retrieves the current network context
+     */
     function getContext() {
       return context;
     }
 
+    /**
+     * Switches the network context.
+     * 
+     * @param {string} contextname 
+     * 
+     * @return {string} The supplied context.
+     */
     function switchContext(contextname) {
       var temp = window.localStorage.getItem("storage-" + contextname);
       saveState();
@@ -38,14 +52,34 @@
       return context;
     }
 
+    /**
+     * Retrieve the value for the supplied key name, from the given context.
+     * 
+     * @param {string} key - The key name to retrieve.
+     * @return {any} The stored value.
+     */
     function get(key) {
       return storage[key];
     }
 
+    /**
+     * Retrieve the value for the supplied global key name.
+     * 
+     * @param {string} key - The key name to retrieve.
+     * @return {any} The stored value.
+     */
     function getGlobal(key) {
       return JSON.parse(window.localStorage.getItem("global-" + key));
     }
 
+    /**
+     * Stores the key/value for persistant storage. Will save the data every 10s by default.
+     * 
+     * @param {string} key - The key to set.
+     * @param {any} value - The value to store.
+     * @param {boolean} forceSave - (optional) Override the default save interval, and save immediately.
+     * @return {any} The value supplied.
+     */
     function set(key, value, forcesave) {
       if (!value) {
         delete storage[key];
@@ -58,6 +92,12 @@
       return value;
     }
 
+    /**
+     * Saves a global value, if supplied, otherwise removes the global value.
+     * @param {string} key - The key that is associated to the stored value.
+     * @param {any} value - The value to be stored.
+     * @return {any} The supplied value.
+     */
     function setGlobal(key, value) {
       if (!value) {
         window.localStorage.removeItem("global-" + key);
@@ -67,16 +107,25 @@
       return value;
     }
 
+    /**
+     * Saves the current storage context state, and updates the lastsaved timestamp.
+     */
     function saveState() {
       window.localStorage.setItem("storage-" + context, JSON.stringify(storage));
       window.localStorage.setItem("lastsaved", JSON.stringify(new Date()));
     }
 
+    /**
+     * Will empty all keys/values from storage
+     */
     function clearData() {
       storage = {};
       window.localStorage.clear();
     }
 
+    /**
+     * Interval to save the current app data state, every 10s. 
+     */
     setInterval(
       function() {
         saveState();
