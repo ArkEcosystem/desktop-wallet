f830f9d8ee3b140703ab3903761ec4a09fdaed3c
Lucio Rubens <luciorubeens@gmail.com>
2017-09-24 13:40:53 -0300

    update: build background and ark logo

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function createWindow () {
     // Create the browser window.t
   var platform = require('os').platform();
   var iconpath = __dirname + "/client/ark.png";
-  if(platform == "linux" || platform == "freebsd" || platform == "sunos") iconpath = __dirname + "/client/ark_linux.png";
+  //if(platform == "linux" || platform == "freebsd" || platform == "sunos") iconpath = __dirname + "/client/ark_linux.png";
   //if(platform == "win32") iconpath = __dirname + "/client/ark_windows.png";
   //if(platform == "darwin") iconpath = __dirname + "/client/ark_mac.png";
   let {width,height} = electron.screen.getPrimaryDisplay().workAreaSize
