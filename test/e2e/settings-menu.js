6939328c144dacc386afa8c2520d2a975c3fef8e
jaml <dev@jaml.pro>
2017-10-01 00:22:22 +0200

    Fix test about "Play sound on transactions" setting and improve label

--- test/e2e/settings-menu.js
+++ test/e2e/settings-menu.js
@@ -15,7 +15,7 @@ describe('Settings menu', function() {
     return hooks.afterBlock.bind(this)()
   })
 
-  describe('option to play sound', function() {
+  describe('setting to play sound', function() {
     const playSoundSelector = 'md-switch[aria-label="Play sound when receiving transactions?"]'
 
     before(function() {
@@ -26,32 +26,38 @@ describe('Settings menu', function() {
           .pause(1000)
       })
 
-      this.app.client.addCommand('getPlaySound', function() {
+      this.app.client.addCommand('getPlaySoundState', function() {
         return this.getAttribute(playSoundSelector, 'aria-checked')
       })
     })
     
-    context('when clicking on', function() {
-      before(function() {
-        return this.app.client.clickPlaySound()
-      })
+    context('when is clicked', function() {
+      let wasEnabled
 
-      it('toggles the button', function() {
+      before(function() {
         return this.app.client.getLocalStorage('storage-mainnet')
           .then( storage => {
-            const option = this.app.client.getPlaySound()
-
-            if (storage.playFundsReceivedSound)
-              return option.should.eventually.equal('true')
-            else
-              return option.should.eventually.not.be
+            wasEnabled = storage.playFundsReceivedSound
+            return this.app.client.clickPlaySound()
           })
       })
 
-      // This is not the current behaviour of the app
-      xit('the new state is stored inmediatly', function() {
+      it('toggles the button', function() {
+        const isEnabled = this.app.client.getPlaySoundState()
+        if (wasEnabled)
+          return (isEnabled).should.not.be
+        else
+          return (isEnabled).should.equal('true')
+      })
+
+      it('stores the new state inmediatly', function() {
         return this.app.client.getLocalStorage('storage-mainnet')
-          .then( storage => (storage.playFundsReceivedSound).should.be.true )
+          .then( storage => {
+            if (wasEnabled)
+              return (storage).should.not.have.key('playFundsReceivedSound')
+            else
+              return (storage.playFundsReceivedSound).should.be.true
+          })
       })
     })
   })
