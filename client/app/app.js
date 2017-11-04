3df47bb5e5a7f78a557f1651d6ee94999a5c2681
jaml <dev@jaml.pro>
2017-11-04 23:39:59 +0100

    Move the HTML of the main navigation bar to a new component

--- client/app/app.js
+++ client/app/app.js
@@ -49,29 +49,31 @@ const electron = require('electron')
 const remote = electron.remote
 const Menu = remote.Menu
 
-const InputMenu = Menu.buildFromTemplate([{
-  label: 'Undo',
-  role: 'undo'
-}, {
-  label: 'Redo',
-  role: 'redo'
-}, {
-  type: 'separator'
-}, {
-  label: 'Cut',
-  role: 'cut'
-}, {
-  label: 'Copy',
-  role: 'copy'
-}, {
-  label: 'Paste',
-  role: 'paste'
-}, {
-  type: 'separator'
-}, {
-  label: 'Select all',
-  role: 'selectall'
-} ])
+const InputMenu = Menu.buildFromTemplate([
+  {
+    label: 'Undo',
+    role: 'undo'
+  }, {
+    label: 'Redo',
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Cut',
+    role: 'cut'
+  }, {
+    label: 'Copy',
+    role: 'copy'
+  }, {
+    label: 'Paste',
+    role: 'paste'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Select all',
+    role: 'selectall'
+  }
+])
 
 document.body.addEventListener('contextmenu', (e) => {
   e.preventDefault()
---
+++ client/app/src/components/layout/main-navbar.controller.js
@@ -0,0 +1,25 @@
+;(function () {
+  'use strict'
+
+  /**
+   * NOTE This component uses the entire AccountController yet: it's the first
+   * step to refactor the `index.html`
+   */
+
+  angular
+    .module('arkclient.components')
+    .component('mainNavbar', {
+      templateUrl: 'src/components/layout/main-navbar.html',
+      bindings: {
+        accountCtrl: '='
+      },
+      controller: MainNavbarController
+    })
+
+  function MainNavbarController ($scope) {
+    this.$onInit = () => {
+      this.ul = this.accountCtrl
+    }
+  }
+
+})()
