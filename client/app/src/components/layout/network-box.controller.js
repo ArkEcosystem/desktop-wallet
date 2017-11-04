420c68fa765f245a8d411ea0a0e27c4bc998c2c7
jaml <dev@jaml.pro>
2017-11-05 00:00:40 +0100

    Move the HTML of the network box (dashboard) to a new component

---
+++ client/app/src/components/layout/network-box.controller.js
@@ -0,0 +1,25 @@
+;(function () {
+  'use strict'
+
+  /**
+   * NOTE This component uses the entire AccountController yet: it's the first
+   * step to refactor the `index.html`
+   */
+
+  angular
+    .module('arkclient.components')
+    .component('networkBox', {
+      templateUrl: 'src/components/layout/network-box.html',
+      bindings: {
+        accountCtrl: '='
+      },
+      controller: NetworkController
+    })
+
+  function NetworkController ($scope) {
+    this.$onInit = () => {
+      this.ul = this.accountCtrl
+    }
+  }
+
+})()
