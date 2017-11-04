a86ba0194e4dbe943eedef773208e068c085ca46
Sam Presnal <boiler9000@gmail.com>
2017-11-03 21:12:20 -0400

    inject stubbed dependencies for unit testing the controller

--- test/components/addressbook/addressbook.controller.js
+++ test/components/addressbook/addressbook.controller.js
@@ -2,23 +2,43 @@
 
 describe('AddressbookController', function() {
 
-  let app = null
-    , $compile = null
-    , $rootScope = null
-    , $scope = null
+  let app = null,
+    ctrl = null,
+    $compile = null, 
+    $rootScope = null, 
+    $scope = null;
 
-  beforeEach(module('arkclient.components'))
+  beforeEach(() => {
+    module('arkclient.components', ($provide) => {
+      let mdDialogStub = {},
+        mdToastStub = {},
+        storageServiceStub = {
+          get: (x) => 'test_contact'
+        },
+        getTextCatalogStub = {},
+        accountServiceStub = {};
 
-  beforeEach( ()=> {
-    inject( (_$compile_, _$rootScope_)=> {
-      $compile = _$compile_
-      $rootScope = _$rootScope_
-      $scope = _$rootScope_.$new()
+      // provide the mock
+      // $provide.value('$scope')
+      $provide.value('$mdDialog', mdDialogStub);
+      $provide.value('$mdToast', mdToastStub);
+      $provide.value('storageService', storageServiceStub);
+      $provide.value('gettextCatalog', getTextCatalogStub);
+      $provide.value('accountService', accountServiceStub);
+    });
+
+    inject( (_$compile_, _$rootScope_, _$controller_)=> {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $scope = _$rootScope_.$new();
+      ctrl = _$controller_('AddressbookController', {$scope:$scope});
     })
-  })
+  });
 
   describe('getContacts', function() {
-    it('loads the contacts')
+    it('loads the contacts', () => {
+      ctrl.getContacts();
+    });
   })
 
 })
