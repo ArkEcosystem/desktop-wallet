ca42cd60cf5c04d6840e164c0b92f30d3e81ef75
jaml <dev@jaml.pro>
2017-11-06 12:57:21 +0100

    Fix and refactor some tests

--- test/components/addressbook/addressbook.controller.js
+++ test/components/addressbook/addressbook.controller.js
@@ -1,78 +1,78 @@
 'use strict'
 
-describe('AddressbookController', function() {
-  const expect = chai.expect;
+describe('AddressbookController', function () {
+  const expect = chai.expect
 
   let ctrl,
-    $compile, 
-    $rootScope, 
-    $scope;
+    $scope
 
   let mdDialogMock,
     mdToastMock,
     storageServiceMock,
     getTextCatalogMock,
-    accountServiceMock;
+    accountServiceMock,
+    toastServiceMock
 
   beforeEach(() => {
     module('arkclient.components', ($provide) => {
       // define mocked services and stubbed calls
-      mdDialogMock = {};
-      mdToastMock = {};
+      mdDialogMock = {}
+      mdToastMock = {}
       storageServiceMock = {
         get: sinon.stub().returns(['test_contact'])
-      };
-      getTextCatalogMock = {};
-      accountServiceMock = {};
+      }
+      getTextCatalogMock = {}
+      accountServiceMock = {}
+      toastServiceMock = {}
 
       // provide mocks to angular controller
-      $provide.value('$mdDialog', mdDialogMock);
-      $provide.value('$mdToast', mdToastMock);
-      $provide.value('storageService', storageServiceMock);
-      $provide.value('gettextCatalog', getTextCatalogMock);
-      $provide.value('accountService', accountServiceMock);
-    });
-
-    inject( (_$compile_, _$rootScope_, _$controller_)=> {
-      $compile = _$compile_;
-      $rootScope = _$rootScope_;
-      $scope = _$rootScope_.$new();
-      ctrl = _$controller_('AddressbookController', {$scope:$scope});
+      $provide.value('$mdDialog', mdDialogMock)
+      $provide.value('$mdToast', mdToastMock)
+      $provide.value('storageService', storageServiceMock)
+      $provide.value('gettextCatalog', getTextCatalogMock)
+      $provide.value('accountService', accountServiceMock)
+      $provide.value('toastService', toastServiceMock)
     })
-  });
+
+    inject( (_$compile_, _$rootScope_, _$controller_) => {
+      $scope = _$rootScope_.$new()
+      ctrl = _$controller_('AddressbookController', { $scope: $scope })
+    })
+  })
 
   describe('initialized state', () => {
     it('retrieves contacts from storage', () => {
-      //assert
-      expect(ctrl.contacts).to.deep.equal(['test_contact']);
-      expect(storageServiceMock.get.calledOnce).to.be.true;
-      expect(storageServiceMock.get.getCall(0).args[0]).to.equal('contacts');
-    });
-  });
+      expect(ctrl.contacts).to.deep.equal(['test_contact'])
+      expect(storageServiceMock.get.calledOnce).to.be.true
+      expect(storageServiceMock.get.getCall(0).args[0]).to.equal('contacts')
+    })
+  })
 
   describe('getContacts', () => {
-    it('sets controller contacts to valid return value', () => {
-      //arrange
-      storageServiceMock.get = sinon.stub().returns(['valid_contact']);
+    context('when contacts are valid', () => {
+      beforeEach(function () {
+        storageServiceMock.get = sinon.stub().returns(['valid_contact'])
+      })
 
-      //act
-      ctrl.getContacts();
+      it('sets controller contacts to valid return value', () => {
+        ctrl.getContacts()
 
-      //assert
-      expect(storageServiceMock.get.calledOnce).to.be.true;
-      expect(ctrl.contacts).to.deep.equal(['valid_contact']);
-    });
+        expect(storageServiceMock.get.calledOnce).to.be.true
+        expect(ctrl.contacts).to.deep.equal(['valid_contact'])
+      })
+    })
 
-    it('sets controller contacts to empty array on invalid return value', () => {
-      //arrange
-      storageServiceMock.get = sinon.stub().returns(null);
+    context("when contacts aren't valid", () => {
+      beforeEach(function () {
+        storageServiceMock.get = sinon.stub().returns(null)
+      })
 
-      //act
-      ctrl.getContacts();
+      it('sets controller contacts to empty array on invalid return value', () => {
+        ctrl.getContacts()
 
-      //assert
-      expect(storageServiceMock.get.calledOnce).to.be.true;
-      expect(ctrl.contacts).to.deep.equal([]);
-    });
-  });
-});
+        expect(storageServiceMock.get.calledOnce).to.be.true
+        expect(ctrl.contacts).to.deep.equal([])
+      })
+    })
+  })
+})
