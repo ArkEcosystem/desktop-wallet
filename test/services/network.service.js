1e7dd4f78d13e323d25d6d705a113d571a0a09dd
ckhatri <chirag.k.khatri@gmail.com>
2017-12-23 20:44:22 -0600

    added create network with same name test

--- test/services/network.service.js
+++ test/services/network.service.js
@@ -25,19 +25,8 @@ describe('networkService', function () {
     themeDark: false
   }
 
-  const MOCK_NETWORK_SECOND = {
-    nethash: 'second_mock_hash',
-    peerseed: 'http://5.39.9.240:4001',
-    forcepeer: false,
-    token: 'ARK-MOCK-2',
-    symbol: 'Ѧ-MOCK-2',
-    version: 1,
-    slip44: 1,
-    explorer: 'mock-explorer',
-    exchanges: {changer: 'ark_ARK'},
-    background: 'url(assets/images/images/Ark.jpg)',
-    theme: 'default',
-    themeDark: false
+  const MOCK_MAINNET_NAME = {
+      name: 'mainnet'
   }
 
   beforeEach(() => {
@@ -51,10 +40,20 @@ describe('networkService', function () {
     })
   })
 
-  describe('getConnection', function () {
-    it('returns a promise', function () {
+  describe('getConnection', () => {
+    it('returns a promise', () =>  {
       let connection = networkService.getConnection()
       sinon.assert.match(true, typeof connection.then === 'function')
+    })
+  })
+
+  describe('createNetwork', () => {
+    context('name already taken', () => {
+      it('should give rejection', () => {
+        let answer = networkService.createNetwork(MOCK_MAINNET_NAME)
+        const REJECTED_PROMISE = 2
+        sinon.assert.match(answer.$$state.status, REJECTED_PROMISE)
+      })
     })
   })
 })
