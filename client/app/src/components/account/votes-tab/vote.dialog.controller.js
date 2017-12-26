387d4690da6bea259f48cac4c39af7f2f0620ca5
Perry <perryjhoffman@gmail.com>
2017-12-26 15:28:31 -0800

    Fixing standard js errors.

--- client/app/src/components/account/votes-tab/vote.dialog.controller.js
+++ client/app/src/components/account/votes-tab/vote.dialog.controller.js
@@ -4,7 +4,7 @@
   const MAXIMUM_VOTE_CNT = 101
   let VoteModalController = function VoteModalController ($mdDialog, accountService, toastService, accountObj, delegateToUnvote, passphrasesArr, activeDelegates, currentTheme) {
     this.account = accountObj
-    this.delegate_to_unvote = delegateToUnvote
+    this.delegateToUnvote = delegateToUnvote
     this.activeDelegates = activeDelegates
     this.delegate = {}
     this.theme = currentTheme
@@ -13,27 +13,27 @@
       second: passphrasesArr[1] ? passphrasesArr[1] : ''
     }
 
-    if (this.delegate_to_unvote && this.delegate_to_unvote.username) {
-      this.delegate.name = this.delegate_to_unvote.username
+    if (this.delegateToUnvote && this.delegateToUnvote.username) {
+      this.delegate.name = this.delegateToUnvote.username
     }
 
-    this.delegateExists = (delegate_list = [], delegate_to_add) => {
-      let found_delegate_index = delegate_list.findIndex(delegate => {
-        return delegate.username === delegate_to_add.username
+    this.delegateExists = (delegateList = [], delegateToAdd) => {
+      let foundDelegateIndex = delegateList.findIndex(delegate => {
+        return delegate.username === delegateToAdd.username
       })
 
-      return (found_delegate_index >= 0)
+      return (foundDelegateIndex >= 0)
     }
 
-    this.removeDelegateVote = (selected_delegate) => {
-      accountService.getDelegateByUsername(selected_delegate.name).then(delegate_obj => {
-        $mdDialog.hide({ new_delegate: delegate_obj, passphrases: this.passphrases })
+    this.removeDelegateVote = (selectedDelegate) => {
+      accountService.getDelegateByUsername(selectedDelegate.name).then(delegateObj => {
+        $mdDialog.hide({ new_delegate: delegateObj, passphrases: this.passphrases })
       })
     }
-    this.addDelegateVote = (selected_delegate) => {
-      accountService.getDelegateByUsername(selected_delegate.name).then(delegate_obj => {
-        if (this.delegate_to_unvote || (this.account.selectedVotes.length < MAXIMUM_VOTE_CNT && !this.delegateExists(this.account.selectedVotes, delegate_obj))) {
-          $mdDialog.hide({ new_delegate: delegate_obj, passphrases: this.passphrases })
+    this.addDelegateVote = (selectedDelegate) => {
+      accountService.getDelegateByUsername(selectedDelegate.name).then(delegateObj => {
+        if (this.delegateToUnvote || (this.account.selectedVotes.length < MAXIMUM_VOTE_CNT && !this.delegateExists(this.account.selectedVotes, delegateObj))) {
+          $mdDialog.hide({ new_delegate: delegateObj, passphrases: this.passphrases })
         } else {
           toastService.error('List full or delegate already voted.')
         }
