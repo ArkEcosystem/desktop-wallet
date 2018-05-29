a70b96cf3ace89db42832c5d9b3ee7624f73b9e5
jaml <dev@jaml.pro>
2018-05-29 14:32:14 +0200

    Add the Changelly widget to the Exchange tab

--- client/app/src/components/account/exchange-tab.controller.js
+++ client/app/src/components/account/exchange-tab.controller.js
@@ -16,9 +16,22 @@
       controller: ExchangeTabController
     })
 
-  function ExchangeTabController ($scope) {
+  function ExchangeTabController ($scope, $sce) {
     this.$onInit = () => {
       this.ul = this.accountCtrl
+
+      const merchantId = '7e77643463d3'
+      const refId = '7e77643463d3'
+      const from = 'BTC'
+      const to = 'ARK'
+      const amount = 1
+      const address = this.ul.selected.address
+      // Ark logo colour: #ED2A2D rgb(237,42,45)
+      const colour = 'ED2A2D'
+
+      this.changellySrc = $sce.trustAsResourceUrl(`https://changelly.com/widget/v1?auth=email&from=${from}&to=${to}&merchant_id=${merchantId}&address=${address}&amount=${amount}&ref_id=${refId}&color=${colour}`)
+
+      // TODO not enabled on devnet
     }
   }
 })()
