a35e5626e17d7f945604096d94ed28f6926baedf
jaml <dev@jaml.pro>
2018-06-18 16:33:45 +0200

    Use the app configuration to infer the options of the Changelly widget

--- client/app/src/components/account/exchange-tab.controller.js
+++ client/app/src/components/account/exchange-tab.controller.js
@@ -22,16 +22,52 @@
 
       const merchantId = 'bab9de3731aa'
       const refId = merchantId
-      const from = 'USD'
-      const to = 'ARK'
-      const amount = 100
-      const address = this.ul.selected.address
+
+      let from
+      let to = 'ARK'
+      let address = ''
+      let amount
+
+      if (this.ul.network.token === 'DARK') {
+        from = 'btc'
+        amount = 0.1
+      } else {
+        to = this.ul.network.token
+
+        // Use the current address to receive the ARKs
+        if (to === 'ARK') {
+          address = this.ul.selected.address
+        }
+
+        let pseudo = 500 * Math.random()
+        pseudo = pseudo < 100 ? 100 : pseudo
+
+        let price
+
+        if (this.ul.btcValueActive) {
+          from = this.ul.bitcoinCurrency.name
+          price = this.ul.connectedPeer.market.price[from]
+          amount = pseudo * price
+        } else {
+          from = this.ul.currency.name
+
+          // If it's not 1 of these currencies, BTC would be used
+          if (['eur', 'usd'].indexOf(from) !== -1) {
+            price = (this.ul.connectedPeer.market.price[from] || 1)
+            amount = pseudo / price
+          } else {
+            price = this.ul.connectedPeer.market.price.btc
+            amount = pseudo * price
+          }
+        }
+      }
+
+      amount = amount.toFixed(2)
+
       // Ark logo colour: #ED2A2D rgb(237,42,45)
       const colour = 'ED2A2D'
 
       this.changellySrc = $sce.trustAsResourceUrl(`https://changelly.com/widget/v1?auth=email&from=${from}&to=${to}&merchant_id=${merchantId}&address=${address}&amount=${amount}&ref_id=${refId}&color=${colour}`)
-
-      // TODO not enabled on devnet
     }
   }
 })()
