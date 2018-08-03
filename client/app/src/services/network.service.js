5b14c1bd2608cb580795c1cab3921fafb3da6fb9
jaml <dev@jaml.pro>
2018-08-03 08:37:29 +0200

    Unify, again, the network service, adapting the methods to the version

--- client/app/src/services/network.service.js
+++ client/app/src/services/network.service.js
@@ -2,13 +2,13 @@
   'use strict'
 
   angular.module('arkclient.services')
-    .service('networkService', ['networkV1Service', 'networkV2Service', '$q', '$http', 'storageService', NetworkService])
+    .service('networkService', ['$q', '$http', '$timeout', 'timeService', 'storageService', NetworkService])
 
   /**
    * NetworkService
    * @constructor
    */
-  function NetworkService (networkV1, networkV2, $q, $http, storageService) {
+  function NetworkService ($q, $http, $timeout, timeService, storageService) {
     const path = require('path')
     const packageJson = require(path.resolve(__dirname, '../../package.json'))
     const clientVersion = packageJson.version
@@ -30,13 +30,59 @@
       height: 0,
       lastConnection: null,
       price: storageService.getGlobal('peerCurrencies') || { btc: '0.0' },
-      version: '1' // TODO
+      version: network && network.ports && network.ports['@arkecosystem/core-api'] ? '2' : '1'
     }
+
+    const isV1 = () => currentPeer.version.startsWith('1')
+
+    const httpGet = (url, headers) => {
+      if (!headers) {
+        headers = httpHeaders()
+      }
+
+      return $http({
+        url,
+        method: 'GET',
+        timeout: 5000,
+        headers
+      })
+    }
+
+    const httpHeaders = (override = {}) => {
+      const headers = {
+        // 'Content-Type': 'application/json',
+        os: 'ark-desktop',
+        version: clientVersion,
+        // port: 1,
+        // nethash: network.nethash
+      }
+
+      if (!isV1()) {
+        headers['API-version'] = 2
+      }
+
+      return Object.assign({}, headers, override)
+    }
+
+    /*
+     * Connect to the initial peer to ensure if it's from V1 or V2
+     */
+    const ensurePeerVersion = () => {
+      // Try V2 first and V1 as fallback
+      httpGet(`${currentPeer.ip}/api/node/configuration`).then(
+        _ => currentPeer.version = '2',
+        _error => {
+          httpGet(`${currentPeer.ip}/api/loader/autoconfigure`).then(
+            _ => currentPeer.version = '1',
+            _error => reject('Cannot connect to peer')
+          )
+        })
+    }
+
+    ensurePeerVersion()
 
     const connection = $q.defer()
     connection.notify(currentPeer)
-
-    /* Methods */
 
     const getNetwork = () => network
     const getNetworkName = () => storageService.getContext()
@@ -87,19 +133,11 @@
             resolve({ name: data.name, network: newNetwork })
           }
 
-          const request = endpoint => {
-            return $http({
-              url: `${data.peerseed}${endpoint}`,
-              method: 'GET',
-              timeout: 5000
-            })
-          }
-
           // Try V2 first and V1 as fallback
-          request('/api/node/configuration').then(
+          httpGet(`${data.peerseed}/api/node/configuration`, httpHeaders({ 'API-version': 2 })).then(
             success,
             _error => {
-              request('/api/loader/autoconfigure').then(
+              httpGet(`${data.peerseed}/api/loader/autoconfigure`).then(
                 success,
                 _ => reject('Cannot connect to peer to autoconfigure the network')
               )
@@ -161,30 +199,155 @@
       return network[newNetwork]
     }
 
-    const isV1 = () => currentPeer.version.startsWith('1')
+    const tryGetPeersFromArkJs = network => {
+      if (!network.arkJsKey) {
+        return
+      }
 
-    const listenNetworkHeight = () => {
+      const arkjsNetwork = ark.networks[network.arkJsKey]
+      if (!arkjsNetwork) {
+        return
+      }
+
+      return arkjsNetwork.peers
+    }
+
+    const extractHeight = response => {
       if (isV1()) {
-        networkV1.listenNetworkHeight(connection, network, currentPeer)
+        return response.height || response.data.height
       } else {
-        networkV2.listenNetworkHeight(connection, network, currentPeer)
+        return response.data.now || response.data.data.now
       }
     }
 
-    const pickRandomPeer = () => {
+    const listenNetworkHeight = () => {
+      let endpoint
       if (isV1()) {
-        networkV1.pickRandomPeer(connection, network, currentPeer)
+        endpoint = '/api/blocks/getHeight'
       } else {
-        networkV2.pickRandomPeer(connection, network, currentPeer)
+        endpoint = '/api/node/status'
       }
+
+      httpGet(`${currentPeer.ip}${endpoint}`, httpHeaders()).then(resp => {
+        timeService.getTimestamp().then(timestamp => {
+          currentPeer.lastConnection = timestamp
+
+          if (resp.data && (resp.data.success || resp.data.data)) {
+            const height = extractHeight(resp)
+
+            if (currentPeer.height === height) {
+              currentPeer.isConnected = false
+              currentPeer.error = 'Node is experiencing sychronisation issues'
+              connection.notify(currentPeer)
+              pickRandomPeer()
+            } else {
+              currentPeer.height = height
+              currentPeer.isConnected = true
+              connection.notify(currentPeer)
+            }
+          } else {
+            currentPeer.isConnected = false
+            currentPeer.error = resp.statusText || 'Peer Timeout after 5s'
+            connection.notify(currentPeer)
+          }
+        })
+      })
+
+      $timeout(() => listenNetworkHeight(), 60000)
     }
 
     const getFromPeer = api => {
-      if (isV1()) {
-        return networkV1.getFromPeer(connection, network, currentPeer, api)
-      } else {
-        return networkV2.getFromPeer(connection, network, currentPeer, api)
+      currentPeer.lastConnection = new Date()
+
+      return new Promise((resolve, reject) => {
+        let ip
+        if (isV1()) {
+          ip  = currentPeer.ip
+        } else {
+          // core-api: 4003, core-p2p: 4002
+          ip  = currentPeer.ip.replace('4002', '4003')
+        }
+
+        httpGet(ip + api, httpHeaders()).then(
+          resp => {
+            resolve(resp.data)
+            currentPeer.isConnected = true
+            currentPeer.delay = new Date().getTime() - currentPeer.lastConnection.getTime()
+            connection.notify(currentPeer)
+          },
+          _ => {
+            reject('Peer disconnected')
+            currentPeer.isConnected = false
+            currentPeer.error = resp.statusText || 'Peer Timeout after 5s'
+            connection.notify(currentPeer)
+          }
+        )
+      })
+    }
+
+    const pickRandomPeer = () => {
+      if (network.forcepeer) {
+        return
       }
+      getFromPeer('/api/peers')
+        .then(response => {
+
+          if (response.success) {
+            const regex127 = RegExp(/^(?!127\.).*/) // does not start with '127.'
+            const peers = response.peers.filter(peer => {
+              return peer.status === 'OK' && regex127.test(peer.ip)
+            })
+            storageService.set('peers', peers)
+            findGoodPeer(peers, 0)
+          } else {
+            findGoodPeer(storageService.get('peers'), 0)
+          }
+        }, () => findGoodPeer(storageService.get('peers'), 0))
+    }
+
+    const findGoodPeer = (peers, index, isStaticPeerList) => {
+      const isPeerListValid = () => peers && index <= peers.length - 1
+
+      if (!isStaticPeerList && !isPeerListValid()) {
+        // we don't have any peers, that means the app is probably started for the first time
+        // (and therefore we do not have a peer list in our storage)
+        // and getting a peer list failed (the peerseed server may be down)
+        // in this case we try to get a peer from the hardcoded list in the arkjs config
+        peers = tryGetPeersFromArkJs(network)
+        isStaticPeerList = true
+      } else if (index === 0) {
+        peers = peers.sort((a, b) => b.height - a.height || a.delay - b.delay).filter(p => p.ip !== '127.0.0.1')
+      }
+
+      // check again or we may have an exception in the case when we couldn't get the static peer list from arkjs
+      if (!isPeerListValid()) {
+        return
+      }
+
+      currentPeer.ip = `http://${peers[index].ip}:${peers[index].port}`
+
+      let endpoint
+      if (isV1()) {
+        endpoint = '/api/blocks/getHeight'
+      } else {
+        endpoint = '/api/node/status'
+      }
+
+      getFromPeer(endpoint)
+        .then(response => {
+          const height = extractHeight(response)
+
+          if ((response.success || response.data) && height < currentPeer.height) {
+            findGoodPeer(peers, index + 1, isStaticPeerList)
+          } else {
+            currentPeer.height = height
+            // if we had a static peer list, we now try to get a dynamic peer list
+            // because now we know the current peer does work and we don't want to keep the hardcoded peers
+            if (isStaticPeerList) {
+              pickRandomPeer()
+            }
+          }
+        }, () => findGoodPeer(peers, index + 1, isStaticPeerList))
     }
 
     const broadcastTransaction = (transaction, max) => {
@@ -203,7 +366,6 @@
     }
 
     const postTransaction = (transaction, url) => {
-      const deferred = $q.defer()
       let peerUrl = url
       if (!peerUrl) {
         peerUrl = currentPeer.ip
@@ -216,13 +378,7 @@
           url: `${peerUrl}/${endpoint}`,
           data: { transactions: [transaction] },
           method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            os: 'ark-desktop',
-            version: clientVersion,
-            port: 1,
-            nethash: network.nethash
-          }
+          headers: httpHeaders()
         }).then(response => {
           if (response.data.success) {
             // we make sure that tx is well broadcasted
