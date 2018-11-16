62b7c52b1c5264e70b3fdd55a8a471cf72b6b2f3
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2018-11-16 20:24:44 +0100

    fix: ask for encryption pass only if encryption enabled (#616)

--- src/renderer/components/Transaction/TransactionForm/TransactionFormSecondSignature.vue
+++ src/renderer/components/Transaction/TransactionForm/TransactionFormSecondSignature.vue
@@ -36,7 +36,7 @@
           {{ $t('TRANSACTION.LEDGER_SIGN_NOTICE') }}
         </div>
         <InputPassword
-          v-else-if="!currentWallet.passphrase"
+          v-else-if="currentWallet.passphrase"
           ref="password"
           v-model="$v.form.walletPassword.$model"
           :label="$t('TRANSACTION.PASSWORD')"
