fa3f2bd6a813e5382c4aff3f2144a2a195b63271
Edgar Goetzendorff <hello@dated.fun>
2018-12-06 02:43:02 +0100

    fix: wallet actions button alignment

--- src/renderer/components/Button/ButtonReload.vue
+++ src/renderer/components/Button/ButtonReload.vue
@@ -2,9 +2,9 @@
   <button
     v-tooltip="{ content: title, trigger:'hover' }"
     :class="[
-      withoutBackground ? 'hover:bg-transparent' : `py-2 px-4 rounded ${colorClass ? colorClass : 'bg-theme-button-light text-theme-button-light-text'}`
+      withoutBackground ? 'hover:bg-transparent' : `py-2 px-4 ${colorClass ? colorClass : 'bg-theme-button-light text-theme-button-light-text'}`
     ]"
-    class="ButtonReload cursor-pointer"
+    class="ButtonReload cursor-pointer flex items-center self-stretch"
     @click="emitClick"
   >
     <SvgIcon
