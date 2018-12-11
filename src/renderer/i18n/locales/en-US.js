fa87ffde8fc52ffc23e5dce1595cf7a6e73defe6
kalgoop <17824093+kalgoop@users.noreply.github.com>
2018-12-11 13:48:07 +0530

    rebase develop and add changes to en-US

--- src/renderer/i18n/locales/en-US.js
+++ src/renderer/i18n/locales/en-US.js
@@ -600,6 +600,10 @@ export default {
     MODAL_HEADER: 'Networks'
   },
 
+  NETWORK: {
+    FAILED_CONFIG_UPDATE: 'Failed to update network configuration for {network}'
+  },
+
   TRANSACTION: {
     TYPE: {
       TRANSFER: 'Transfer',
@@ -660,6 +664,8 @@ export default {
     CONFIRMATION_COUNT: '{0} Confirmations',
     CONFIRMATIONS: 'Confirmations',
     CREATE_TRANSFER: 'Create Transfer',
+    DISCARD: 'Discard',
+    EXPIRED: 'Expired',
     FEE: 'Transaction fee',
     ID: 'ID',
     LEDGER_SIGN_NOTICE: 'Next you will confirm the transaction with your Ledger',
@@ -671,6 +677,8 @@ export default {
     PASSPHRASE: 'Passphrase',
     PASSWORD: 'Encryption Password',
     RECIPIENT: 'Recipient',
+    RESEND: 'Resend',
+    RESENT_NOTICE: 'Transaction {transactionId} has been resent',
     SAVE_OFFLINE: 'Save transaction offline',
     SECOND_PASSPHRASE: 'Second Passphrase',
     SEND: 'Send',
@@ -751,6 +759,7 @@ export default {
     RANK: 'Rank',
     USERNAME: 'Username',
     PRODUCTIVITY: 'Productivity',
+    RANK_BANNER: 'Rank: {rank}',
     PRODUCTIVITY_BANNER: 'Productivity: {productivity}',
     APPROVAL: 'Approval',
     FORGED: 'Forged',
