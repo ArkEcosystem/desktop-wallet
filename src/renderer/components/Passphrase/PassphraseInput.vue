73d18c0a060eb9c8c8ca3af2b7df8832b6c1a1ab
jeremiG <gendronjeremi@gmail.com>
2018-12-13 13:10:43 -0500

    if passphrase is still invalid when toggling visibility, focus back on the input field

--- src/renderer/components/Passphrase/PassphraseInput.vue
+++ src/renderer/components/Passphrase/PassphraseInput.vue
@@ -208,6 +208,8 @@ export default {
 
     toggleVisible () {
       this.passphraseIsVisible = !this.passphraseIsVisible
+      // Resume focus on the input field unless passphrase is valid
+      if (!this.$v.model.isValid) this.$refs.input.focus()
     }
   },
 
