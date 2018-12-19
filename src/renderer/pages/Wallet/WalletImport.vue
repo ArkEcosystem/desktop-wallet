6eb9f81af1eb437aed936ca8c80adf1f60f3c89e
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2018-12-19 02:12:45 +0100

    test: normalizing passphrase with NFD option (#786)

--- src/renderer/pages/Wallet/WalletImport.vue
+++ src/renderer/pages/Wallet/WalletImport.vue
@@ -217,6 +217,8 @@ export default {
      */
     step () {
       if (this.step === 2 && !this.useOnlyAddress) {
+        // Important: .normalize('NFD') is needed to properly work with Korean bip39 words
+        // It alters the passphrase string, so no need to normalize again in the importWallet function
         this.schema.address = WalletService.getAddress(this.schema.passphrase.normalize('NFD'), this.session_network.version)
       }
     }
