2dc8bf62baaa99a8248cfff041c20e0ea40ed5a7
ItsANameToo <itsanametoo@protonmail.com>
2018-12-31 12:37:51 +0100

    fix: make time consistent between explorer and desktop

--- src/renderer/mixins/formatter.js
+++ src/renderer/mixins/formatter.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 export default {
   methods: {
     formatter_percentage (value) {
@@ -10,6 +12,11 @@ export default {
 
     formatter_votes (value) {
       return this.$n(this.currency_subToUnit(value), { maximumFractionDigits: 2 })
+    },
+
+    formatter_date (value, format) {
+      moment.locale(window.navigator.userLanguage || window.navigator.language)
+      return format ? moment(value).format(format) : moment(value).format('L LTS')
     }
   }
 }
