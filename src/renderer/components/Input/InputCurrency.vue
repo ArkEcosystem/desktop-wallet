370d6b0f73e83403ea2d5996145c04b762f58d1d
ItsANameToo <itsanametoo@protonmail.com>
2018-12-31 12:09:14 +0100

    fix: do not reset inputcurrency to improve UX

--- src/renderer/components/Input/InputCurrency.vue
+++ src/renderer/components/Input/InputCurrency.vue
@@ -265,11 +265,11 @@ export default {
      */
     updateInputValue (value) {
       // Ignore invalid values
-      if (!value || this.checkAmount(value)) {
+      if (value && this.checkAmount(value)) {
         let number = Number(value.toString().replace(',', '.'))
         number.toString().includes('-')
           ? this.inputValue = number.toFixed(number.toString().split('-')[1]) // Small numbers will be like 1e-7 so we use the number after '-' for toFixed()
-          : this.inputValue = value ? value.toString().replace(',', '.') : ''
+          : this.inputValue = value.toString().replace(',', '.')
         // Inform Vuelidate that the value changed
         this.$v.model.$touch()
         return true
