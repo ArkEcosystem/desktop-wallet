54c02c0a2a849fc73fbb788cb0308ee3e43f0995
Lucio Rubens <luciorubeens@gmail.com>
2019-01-07 16:01:46 -0300

    feat: add button letter component

---
+++ src/renderer/components/Button/ButtonLetter.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    type="button"
+    class="ButtonLetter"
+  >
+    {{ letter }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonLetter',
+
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    letter () {
+      return this.value.charAt(0)
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.ButtonLetter {
+  @apply rounded-full flex items-center justify-center uppercase font-semibold w-5 h-5
+}
+</style>
