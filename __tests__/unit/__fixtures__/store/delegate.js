56d5c4650498ecdd3daa16770b79c2f14543265c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2019-01-18 21:24:57 +0000

    test: additional delegate store tests

--- __tests__/unit/__fixtures__/store/delegate.js
+++ __tests__/unit/__fixtures__/store/delegate.js
@@ -1,12 +1,24 @@
 const delegate1 = {
   username: 'delegate1',
   address: 'AKdr5d9AMEnsKYxpDcoHdyyjSCKVx3r9Nj',
-  publicKey: '020431436cf94f3c6a6ba566fe9e42678db8486590c732ca6c3803a10a86f50b92'
+  publicKey: '020431436cf94f3c6a6ba566fe9e42678db8486590c732ca6c3803a10a86f50b92',
+  voteWeight: 4202880284636,
+  producedBlocks: 108940,
+  missedBlocks: 676,
+  rank: 59,
+  productivity: 99.38,
+  approval: 0.03
 }
 const delegate2 = {
   username: 'delegate2',
   address: 'AU1TGCYoWzBnGMyMK9GTS8BG6EFbgc5xxC',
-  publicKey: '02bf72c578a12c35a97ca1230b93017161ee42c3f0ab82f6fe7c95b3b43561a076'
+  publicKey: '02bf72c578a12c35a97ca1230b93017161ee42c3f0ab82f6fe7c95b3b43561a076',
+  voteWeight: 2500294939144,
+  producedBlocks: 2497,
+  missedBlocks: 885,
+  rank: 62,
+  productivity: 73.83,
+  approval: 0.02
 }
 
 export default [
--- __tests__/unit/store/modules/delegate.spec.js
+++ __tests__/unit/store/modules/delegate.spec.js
@@ -1,9 +1,15 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 import Vue from 'vue'
 import Vuex from 'vuex'
+import apiClient, { client as ClientService } from '@/plugins/api-client'
 import DelegateModule from '@/store/modules/delegate'
 import delegates, { delegate1, delegate2 } from '../../__fixtures__/store/delegate'
 
 Vue.use(Vuex)
+Vue.use(apiClient)
+
+const axiosMock = new MockAdapter(axios)
 
 const store = new Vuex.Store({
   modules: {
@@ -13,7 +19,10 @@ const store = new Vuex.Store({
       getters: {
         network () {
           return {
-            id: 'abc'
+            id: 'abc',
+            constants: {
+              activeDelegates: 51
+            }
           }
         }
       }
@@ -23,6 +32,8 @@ const store = new Vuex.Store({
 })
 
 beforeEach(() => {
+  ClientService.version = 1
+  ClientService.host = 'http://127.0.0.1'
   store.dispatch('delegate/set', delegates)
 })
 
@@ -41,11 +52,78 @@ describe('delegate store module', () => {
     expect(store.getters['delegate/byAddress']('wrong address')).toBe(false)
   })
 
+  it('should return false when no address is given', () => {
+    expect(store.getters['delegate/byAddress']()).toBe(false)
+  })
+
   it('should get a single delegate by its public key', () => {
     expect(store.getters['delegate/byPublicKey']('02bf72c578a12c35a97ca1230b93017161ee42c3f0ab82f6fe7c95b3b43561a076')).toEqual(delegate2)
   })
 
+  it('should return false when delegate with public key does not exist', () => {
+    expect(store.getters['delegate/byPublicKey']('wrong public key')).toBe(false)
+  })
+
   it('should return false when no public key is given', () => {
     expect(store.getters['delegate/byPublicKey']()).toBe(false)
+  })
+
+  it('should fetch delegates from v1 api should store the same as v2 api', async () => {
+    const v1DelegateData = delegates.map(delegate => {
+      return {
+        username: delegate.username,
+        address: delegate.address,
+        publicKey: delegate.publicKey,
+        vote: delegate.voteWeight,
+        producedblocks: delegate.producedBlocks,
+        missedblocks: delegate.missedBlocks,
+        rate: delegate.rank,
+        approval: delegate.approval,
+        productivity: delegate.productivity
+      }
+    })
+    const v2DelegateData = delegates.map(delegate => {
+      return {
+        username: delegate.username,
+        address: delegate.address,
+        publicKey: delegate.publicKey,
+        votes: delegate.voteWeight,
+        blocks: {
+          produced: delegate.producedBlocks,
+          missed: delegate.missedBlocks
+        },
+        rank: delegate.rank,
+        production: {
+          approval: delegate.approval,
+          productivity: delegate.productivity
+        }
+      }
+    })
+
+    axiosMock
+      .onGet(`http://127.0.0.1/api/delegates`, { params: { offset: 0, limit: 51 } })
+      .reply(200, {
+        totalCount: 2,
+        delegates: v1DelegateData
+      })
+
+    await store.dispatch('delegate/load')
+    const v1Delegates = store.getters['delegate/all']
+
+    ClientService.version = 2
+
+    axiosMock
+      .onGet(`http://127.0.0.1/api/delegates`, { params: { page: 1, limit: 100 } })
+      .reply(200, {
+        data: v2DelegateData,
+        meta: {
+          totalCount: 2
+        }
+      })
+
+    await store.dispatch('delegate/load')
+    const v2Delegates = store.getters['delegate/all']
+
+    expect(v1Delegates).toEqual(v2Delegates)
   })
 })
