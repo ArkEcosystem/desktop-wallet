a4f919e7f0a1d11c6164cd503190ee9805e15d01
jaml <dev@jaml.pro>
2019-01-24 18:12:00 +0100

    refactor: extract the `ProfileAvatar` component from the main sidemenu

---
+++ src/renderer/components/Profile/ProfileAvatar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="ProfileAvatar"
+  >
+    <div
+      v-if="profile.avatar"
+      :style="profile.avatar ? `backgroundImage: url('${assets_loadImage(profile.avatar)}')` : ''"
+      :to="{ name: 'profiles' }"
+      class="background-image bg-center bg-no-repeat border-none"
+    >
+      <slot />
+    </div>
+
+    <ButtonLetter
+      v-if="!profile.avatar"
+      :value="profile.name"
+      :has-custom-style="true"
+      size="xl"
+      class="bg-theme-feature-item-selected text-theme-feature-item-selected-text"
+    />
+
+    <slot />
+  </div>
+</template>
+
+<script>
+import { ButtonLetter } from '@/components/Button'
+
+export default {
+  name: 'ProfileAvatar',
+
+  components: {
+    ButtonLetter
+  },
+
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
