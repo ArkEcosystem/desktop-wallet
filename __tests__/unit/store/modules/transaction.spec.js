aecbddd0d1cd35e5cf845841fee0972bce652046
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2019-01-25 15:16:10 +0000

    test: transaction store with static fees

--- __tests__/unit/store/modules/transaction.spec.js
+++ __tests__/unit/store/modules/transaction.spec.js
@@ -1,4 +1,25 @@
-import store from '@/store'
+import axios from 'axios'
+import AxiosMockAdapter from 'axios-mock-adapter'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import apiClient, { client as ClientService } from '@/plugins/api-client'
+import TransactionModule from '@/store/modules/transaction'
+import SessionModule from '@/store/modules/session'
+import WalletModule from '@/store/modules/wallet'
+
+Vue.use(Vuex)
+Vue.use(apiClient)
+
+const axiosMock = new AxiosMockAdapter(axios)
+
+const store = new Vuex.Store({
+  modules: {
+    transaction: TransactionModule,
+    session: SessionModule,
+    wallet: WalletModule
+  },
+  strict: true
+})
 
 describe('TransactionModule', () => {
   const models = [
@@ -20,6 +41,9 @@ describe('TransactionModule', () => {
     store.commit('session/SET_PROFILE_ID', 'exampleId')
     models.forEach(model => store.commit('transaction/STORE', model))
     wallets.forEach(wallet => store.commit('wallet/STORE', wallet))
+    ClientService.version = 1
+    ClientService.host = `http://127.0.0.1:4003`
+    axiosMock.reset()
   })
 
   describe('getters byAddress', () => {
@@ -103,6 +127,78 @@ describe('TransactionModule', () => {
           expect(transaction).toHaveProperty('isReceiver')
         })
       })
+    })
+  })
+
+  describe('getters staticFee', () => {
+    it('should return a single fee', () => {
+      store.commit('transaction/SET_STATIC_FEES', {
+        profileId: 'exampleId',
+        staticFees: [ 1, 2, 3, 4, 5 ]
+      })
+
+      expect(store.getters['transaction/staticFee'](0)).toEqual(1)
+      expect(store.getters['transaction/staticFee'](1)).toEqual(2)
+      expect(store.getters['transaction/staticFee'](2)).toEqual(3)
+      expect(store.getters['transaction/staticFee'](3)).toEqual(4)
+      expect(store.getters['transaction/staticFee'](4)).toEqual(5)
+    })
+
+    it('should return null if no fee', () => {
+      store.commit('transaction/SET_STATIC_FEES', {
+        profileId: 'exampleId',
+        staticFees: []
+      })
+
+      expect(store.getters['transaction/staticFee'](0)).not.toBe(expect.anything())
+    })
+  })
+
+  describe('dispatch updateStaticFees', () => {
+    it('should return update all fees on v1', async () => {
+      axiosMock
+        .onGet(`http://127.0.0.1:4003/api/blocks/getFees`)
+        .reply(200, {
+          fees: {
+            send: 1,
+            secondsignature: 2,
+            delegate: 3,
+            vote: 4,
+            multisignature: 5
+          }
+        })
+
+      await store.dispatch('transaction/updateStaticFees')
+
+      expect(store.getters['transaction/staticFee'](0)).toEqual(1)
+      expect(store.getters['transaction/staticFee'](1)).toEqual(2)
+      expect(store.getters['transaction/staticFee'](2)).toEqual(3)
+      expect(store.getters['transaction/staticFee'](3)).toEqual(4)
+      expect(store.getters['transaction/staticFee'](4)).toEqual(5)
+    })
+
+    it('should return update all fees on v2', async () => {
+      ClientService.version = 2
+
+      axiosMock
+        .onGet(`http://127.0.0.1:4003/api/transactions/fees`)
+        .reply(200, {
+          data: {
+            transfer: 1,
+            secondSignature: 2,
+            delegateRegistration: 3,
+            vote: 4,
+            multiSignature: 5
+          }
+        })
+
+      await store.dispatch('transaction/updateStaticFees')
+
+      expect(store.getters['transaction/staticFee'](0)).toEqual(1)
+      expect(store.getters['transaction/staticFee'](1)).toEqual(2)
+      expect(store.getters['transaction/staticFee'](2)).toEqual(3)
+      expect(store.getters['transaction/staticFee'](3)).toEqual(4)
+      expect(store.getters['transaction/staticFee'](4)).toEqual(5)
     })
   })
 })
