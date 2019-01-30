683a76dc40ac356435b0fd91894fe84441bf2337
Juan <j-a-m-l@users.noreply.github.com>
2019-01-30 16:50:59 +0100

    feat: db migration system (#967)
    * feat: create a Vuex plugin to run migrations
    
    * * latest applied migration
    
    * * add templates
    
    * test: the migration Vuex plugin
    
    * * refactor
    
    * * test
    
    * feat: create an initial migration as example

---
+++ __tests__/unit/store/plugins/vuex-persist-migrations.spec.js
@@ -0,0 +1,64 @@
+import MigrationsPlugin from '@/store/plugins/vuex-persist-migrations'
+
+describe('Migrations plugin', () => {
+  let plugin
+  let fromVersion = '1.0.0'
+  let untilVersion = '2.0.2'
+
+  beforeAll(() => {
+    plugin = new MigrationsPlugin({ fromVersion, untilVersion })
+    plugin.migrations = []
+  })
+
+  describe('checkVersion', () => {
+    describe('when the version does not follow semver rules', () => {
+      it('should throw an Error', () => {
+        expect(() => plugin.checkVersion('no')).toThrow(/semver/)
+      })
+    })
+
+    describe('when the version is between the `fromVersion` (not included) and `untilVersion` (included)', () => {
+      it('should return `true`', () => {
+        plugin = new MigrationsPlugin({ fromVersion: '1.9.9', untilVersion: '2.0.0' })
+        expect(plugin.checkVersion('2.0.0')).toBeTrue()
+
+        plugin = new MigrationsPlugin({ fromVersion: '1.0.0', untilVersion: '3.0.0' })
+        expect(plugin.checkVersion('2.0.0')).toBeTrue()
+
+        plugin = new MigrationsPlugin({ fromVersion: () => '1.0.0', untilVersion: () => '3.0.0' })
+        expect(plugin.checkVersion('2.0.0')).toBeTrue()
+      })
+    })
+
+    describe('when the version is not between the `fromVersion` (not included) and `untilVersion` (included)', () => {
+      it('should return `true`', () => {
+        plugin = new MigrationsPlugin({ fromVersion: '2.0.0', untilVersion: '2.0.0' })
+        expect(plugin.checkVersion('2.0.0')).toBeFalse()
+
+        plugin = new MigrationsPlugin({ fromVersion: '2.0.0', untilVersion: '2.0.1' })
+        expect(plugin.checkVersion('2.0.0')).toBeFalse()
+
+        plugin = new MigrationsPlugin({ fromVersion: '1.0.1', untilVersion: '1.9.3' })
+        expect(plugin.checkVersion('2.0.0')).toBeFalse()
+
+        plugin = new MigrationsPlugin({ fromVersion: () => '1.0.0', untilVersion: () => '1.0.0' })
+        expect(plugin.checkVersion('2.0.0')).toBeFalse()
+      })
+    })
+  })
+
+  describe('apply', () => {
+    it('should call the migration handler', () => {
+      plugin.store = 'store'
+      plugin.migrations = [{
+        version: '1.8.4',
+        title: 'example',
+        handler: jest.fn()
+      }]
+
+      plugin.apply()
+
+      expect(plugin.migrations[0].handler).toHaveBeenCalledWith(plugin.store)
+    })
+  })
+})
---
+++ src/renderer/store/plugins/vuex-persist-migrations.js
@@ -0,0 +1,67 @@
+import logger from 'electron-log'
+import semver from 'semver'
+import { isFunction } from 'lodash'
+
+export default class VuexMigrations {
+  /**
+   * @param {(String|Function)} fromVersion - From this migration version (included)
+   * @param {(String|Function)} untilVersion - Until this migration version (included)
+   */
+  constructor ({ fromVersion, untilVersion }) {
+    this.fromVersion = fromVersion
+    this.untilVersion = untilVersion
+  }
+
+  /**
+   * Webpack does not admit using a variable to indicate the path of the directory
+   * @see https://webpack.js.org/guides/dependency-management/#require-context
+   */
+  get migrationsContext () {
+    return require.context('../migrations', true, /\.js$/)
+  }
+
+  checkVersion (version) {
+    if (!semver.valid(version)) {
+      throw new Error(`The version "${version}" does not comply with semver`)
+    }
+
+    const from = isFunction(this.fromVersion) ? this.fromVersion(this.store) : this.fromVersion
+    const until = isFunction(this.untilVersion) ? this.untilVersion(this.store) : this.untilVersion
+
+    return semver.gt(version, from) && semver.lte(version, until)
+  }
+
+  collect () {
+    this.migrations = []
+
+    this.migrationsContext.keys().sort().forEach(key => {
+      const [filename, version, title] = key.match(/\.\/(.*) - (.*)\.js/)
+
+      if (this.checkVersion(version)) {
+        const handler = this.migrationsContext(filename).default
+        this.migrations.push({ version, title, handler })
+      }
+    })
+  }
+
+  apply () {
+    this.migrations.forEach(migration => {
+      logger.info(`Appliying migration ${migration.version}: ${migration.title}`)
+      migration.handler(this.store)
+    })
+  }
+
+  /**
+   * Integrates with `vuex-persist` and `vuex-persist-ready`
+   */
+  get plugin () {
+    return store => {
+      this.store = store
+
+      this.store._vm.$root.$on('vuex-persist:ready', () => {
+        this.collect()
+        this.apply()
+      })
+    }
+  }
+}
