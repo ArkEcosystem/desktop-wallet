18ab32332a3140d3fc8ccbff4cd669757fb2d98d
Edgar Goetzendorff <hello@dated.fun>
2019-01-30 16:50:43 +0100

    refactor: support for multiple props in sorting util

--- src/renderer/components/utils/Sorting.js
+++ src/renderer/components/utils/Sorting.js
@@ -1,5 +1,20 @@
 
 // Returns a sort function for the given prop
-export function sortByProp (prop) {
-  return (a, b) => a[prop].localeCompare(b[prop])
+export function sortByProps (props, locale = undefined, options = null) {
+  locale = locale || undefined
+  options = options || { sensitivity: 'base', numeric: 'true' }
+
+  return (a, b) => {
+    if (!Array.isArray(props)) {
+      props = [props]
+    }
+
+    for (let i = 0; i < props.length; i++) {
+      const order = a[props[i]].localeCompare(b[props[i]], locale, options)
+
+      if (order !== 0) {
+        return order
+      }
+    }
+  }
 }
