653afd18f555496eb2abc0fd836979ae1cc1d64a
Edgar Goetzendorff <hello@dated.fun>
2019-01-31 08:35:52 +0100

    misc: move array check

--- src/renderer/components/utils/Sorting.js
+++ src/renderer/components/utils/Sorting.js
@@ -4,11 +4,11 @@ export function sortByProps (props, locale = undefined, options = null) {
   locale = locale || undefined
   options = options || { sensitivity: 'base', numeric: 'true' }
 
-  return (a, b) => {
-    if (!Array.isArray(props)) {
-      props = [props]
-    }
+  if (!Array.isArray(props)) {
+    props = [props]
+  }
 
+  return (a, b) => {
     for (let i = 0; i < props.length; i++) {
       const order = a[props[i]].localeCompare(b[props[i]], locale, options)
 
