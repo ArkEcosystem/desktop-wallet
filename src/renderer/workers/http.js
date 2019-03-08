c0278d5b42fdc23a445c7152542a2245d5529fd6
jaml <dev@jaml.pro>
2019-03-08 19:05:37 +0100

    refactor: send NEO address request on the background

--- src/renderer/workers/http.js
+++ src/renderer/workers/http.js
@@ -6,10 +6,10 @@ module.exports = message => {
   return new Promise(async (resolve, reject) => {
     if (message.url) {
       try {
-        const { data } = await axios(message)
+        const { data, status } = await axios(message)
         // Sending through processes involves serializing and parsing the data,
         // so it is better to use only the useful parts of the response
-        resolve({ data })
+        resolve({ data, status })
       } catch (error) {
         reject(error)
       }
