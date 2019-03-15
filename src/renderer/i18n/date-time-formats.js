c96195618ea62d039acfb5a7d86b11a1eae10c61
OCKHAM <ceo@ockham.consulting>
2019-03-15 18:13:19 +0100

    Update date-time-formats.js

--- src/renderer/i18n/date-time-formats.js
+++ src/renderer/i18n/date-time-formats.js
@@ -18,6 +18,17 @@ export default {
       year: 'numeric', month: 'short', day: 'numeric', weekday: 'short', hour: 'numeric', minute: 'numeric'
     }
   },
+  'fr-FR': {
+    short: {
+      year: 'numeric', month: 'short', day: 'numeric'
+    },
+    long: {
+      year: 'numeric', month: 'short', day: 'numeric', weekday: 'short', hour: 'numeric', minute: 'numeric'
+    },
+    shortTime: {
+      hour: 'numeric', minute: 'numeric'
+    }
+  },
   'it-IT': {
     short: {
       year: 'numeric', month: 'short', day: 'numeric'
