0d394c6abf95bffb8063c77ac6f84cf85241cba1
jaml <dev@jaml.pro>
2019-03-22 13:44:08 +0100

    fix: hide the tooltip when clicking on a wallet address

--- src/renderer/components/Wallet/WalletAddress.vue
+++ src/renderer/components/Wallet/WalletAddress.vue
@@ -4,12 +4,17 @@
       v-if="!type"
       v-tooltip="{
         content: address,
-        container: tooltipContainer
+        container: tooltipContainer,
+        delay: { show: 300, hide: 0 },
+        show: showTooltip,
+        trigger: 'manual'
       }"
     >
       <a
         href="#"
         @click.stop="onClick"
+        @mouseover="onMouseOver"
+        @mouseout="onMouseOut"
       >
         {{ wallet_formatAddress(address, addressLength) }}
       </a>
@@ -24,13 +29,18 @@
       v-else-if="type === 3"
       v-tooltip="{
         content: votedDelegateAddress,
-        container: tooltipContainer
+        container: tooltipContainer,
+        delay: { show: 300, hide: 0 },
+        show: showTooltip,
+        trigger: 'manual'
       }"
     >
       <a
         :class="[isUnvote ? 'text-red' : 'text-green']"
         href="#"
         @click.stop="onClick"
+        @mouseover="onMouseOver"
+        @mouseout="onMouseOut"
       >
         {{ isUnvote ? $t("TRANSACTION.TYPE.UNVOTE") : $t("TRANSACTION.TYPE.VOTE") }}
         <span
@@ -105,6 +115,10 @@ export default {
     }
   },
 
+  data: () => ({
+    showTooltip: false
+  }),
+
   computed: {
     isUnvote () {
       if (this.asset && this.asset.votes) {
@@ -155,8 +169,17 @@ export default {
     },
 
     onClick () {
-      this.openAddress()
+      this.showTooltip = false
       this.$emit('click')
+      this.openAddress()
+    },
+
+    onMouseOver () {
+      this.showTooltip = true
+    },
+
+    onMouseOut () {
+      this.showTooltip = false
     },
 
     openAddress () {
