6d99a943df5ab6b1e6bf5a250d56106642ce1253
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-04-10 15:44:37 -0300

    feat(plugins): implement ability to overwrite elements with slots

---
+++ src/renderer/components/Plugin/PluginWrapper.vue
@@ -0,0 +1,26 @@
+<script>
+import { Wormhole } from 'portal-vue'
+export default {
+  name: 'PluginWrapper',
+
+  mounted () {
+    const footerSlot = this.$slots.footer
+    if (footerSlot) {
+      this.$nextTick(() => {
+        Wormhole.open({
+          to: 'footer',
+          from: 'plugin-wrapper',
+          passengers: footerSlot
+        })
+      })
+    }
+  },
+
+  render (h) {
+    const children = this.$slots.default
+    return children
+      ? h('div', children)
+      : h()
+  }
+}
+</script>
