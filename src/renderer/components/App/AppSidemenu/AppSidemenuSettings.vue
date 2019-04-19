0f265819730624c21673496eaa788a5bc41cd5a0
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-04-19 16:16:48 +0200

    fix: remove market item from sidemenu settings

--- src/renderer/components/App/AppSidemenu/AppSidemenuSettings.vue
+++ src/renderer/components/App/AppSidemenu/AppSidemenuSettings.vue
@@ -100,25 +100,6 @@
       </MenuOptionsItem>
 
       <MenuOptionsItem
-        v-if="isMarketEnabled"
-        :title="$t('APP_SIDEMENU.SETTINGS.IS_MARKET_CHART_ENABLED')"
-        @click="toggleSelect('show-market-chart')"
-      >
-        <div
-          slot="controls"
-          class="pointer-events-none"
-        >
-          <ButtonSwitch
-            ref="show-market-chart"
-            :is-active="sessionIsMarketChartEnabled"
-            class="theme-dark"
-            background-color="#414767"
-            @change="setIsMarketChartEnabled"
-          />
-        </div>
-      </MenuOptionsItem>
-
-      <MenuOptionsItem
         :title="$t('APP_SIDEMENU.SETTINGS.PLUGINS')"
         class="text-grey-light"
         @click="goToPlugins"
