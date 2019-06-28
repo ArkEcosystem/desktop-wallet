5b53de666329f0e3229ef3d5d10e69a39a0a2313
Juan <j-a-m-l@users.noreply.github.com>
2019-06-28 16:46:57 +0200

    feat: add a button to navigate back to the previous wallet page (#1273)
    * feat: add a button to the main sidebar to navigate back to the previous page
    
    * fix: typo in CSS class
    
    * feat: create `AppBackButton`
    
    * fix: do not push a useless history state when clicking on a wallet address
    
    * refactor: check if it is possible to go back first
    
    * refactor: use the route name instead of the component name

--- src/renderer/components/App/index.js
+++ src/renderer/components/App/index.js
@@ -1,8 +1,10 @@
+import AppBackButton from './AppBackButton'
 import AppSidemenu from './AppSidemenu/AppSidemenu'
 import AppFooter from './AppFooter'
 import AppIntro from './AppIntro/AppIntro'
 
 export {
+  AppBackButton,
   AppSidemenu,
   AppFooter,
   AppIntro
