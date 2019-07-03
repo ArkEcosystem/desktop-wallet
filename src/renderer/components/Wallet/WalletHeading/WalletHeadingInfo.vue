aa414dc49578bd3fd8bb608bd288dac7877d63bf
kalgoop <17824093+kalgoop@users.noreply.github.com>
2019-07-03 20:04:29 +0530

    refactor: add tooltip to wallet name (#1309)

--- src/renderer/components/Wallet/WalletHeading/WalletHeadingInfo.vue
+++ src/renderer/components/Wallet/WalletHeading/WalletHeadingInfo.vue
@@ -30,10 +30,16 @@
         v-if="name"
         class="flex flex-row text-lg font-semibold text-theme-heading-text"
       >
-        <span class="block xl:hidden">
+        <span
+          class="block xl:hidden"
+          v-tooltip="name"
+        >
           {{ name | truncate(12) }}
         </span>
-        <span class="hidden xl:block">
+        <span
+          class="hidden xl:block"
+          v-tooltip="name"
+        >
           {{ name | truncate(30) }}
         </span>
         <SvgIcon
