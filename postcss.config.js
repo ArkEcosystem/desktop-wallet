92ea6726b189970fc8f63c9e0843bb74f495893d
Sebastian Seijo <sebastiangseijo@gmail.com>
2019-07-05 18:49:05 +0200

    chore(tailwindcss): postcss config
    modification to match new tailwind configuration name

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import')(),
-    require('tailwindcss')('./tailwind.js'),
+    require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer')
   ]
 }
