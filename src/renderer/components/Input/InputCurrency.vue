25f5997639e51a51f2bbbcc5bbd159601afc40dd
CetinaLuka <luka.cetina@student.um.si>
2019-08-07 09:31:28 +0200

    fix: amount and fee warnings not displaying properly

--- src/renderer/components/Input/InputCurrency.vue
+++ src/renderer/components/Input/InputCurrency.vue
@@ -383,10 +383,10 @@ export default {
         return this.inputValue && this.checkAmount(this.inputValue)
       },
       isMoreThanMinimum (value) {
-        return !this.minimumAmount.isGreaterThan(value)
+        return !this.minimumAmount.isGreaterThan(this.inputValue)
       },
       isLessThanMaximum (value) {
-        return !this.maximumAmount.isLessThan(value)
+        return !this.maximumAmount.isLessThan(this.inputValue)
       },
       isRequired (value) {
         if (this.required) {
