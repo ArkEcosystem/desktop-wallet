30e8a8716106b3de178125ef1ae0b40b995d37d5
dated <hello@dated.fun>
2019-10-01 15:20:01 +0200

    fix: change BigNumber import

--- src/renderer/components/utils/merge-table-transactions.js
+++ src/renderer/components/utils/merge-table-transactions.js
@@ -1,5 +1,5 @@
 import { orderBy, uniqBy } from 'lodash'
-import BigNumber from '@/plugins/bignumber'
+import BigNumber from 'bignumber.js'
 
 /**
  * This utility is used to merge the transactions that have been fetched using
