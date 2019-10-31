9cf8b56527b71f3f4a44af0b2a2f4081e8e38724
Edgar Goetzendorff <hello@dated.fun>
2019-10-31 12:42:38 +0100

    deps: update vue-good-table (#1494)
    * deps: update vue-good-table
    
    * fix: table wallet sorting by vote
    
    * fix: delegate table sort params
    
    * fix: table header text alignment
    
    * fix: wallet and contact table sort params
    
    * refactor: get correct sort options in transactions table
    
    * refactor: properly pluralize new transactions notice
    
    * fix: transaction table sort params
    
    * refactor: call mergeTableTransactions with sort params
    
    * refactor: compare bignums in mergeTableTransactions
    
    * refactor: set totalAmount as string
    
    * fix: change BigNumber import
    
    * fix: programmatically set current page

--- src/renderer/components/utils/merge-table-transactions.js
+++ src/renderer/components/utils/merge-table-transactions.js
@@ -1,4 +1,5 @@
 import { orderBy, uniqBy } from 'lodash'
+import BigNumber from 'bignumber.js'
 
 /**
  * This utility is used to merge the transactions that have been fetched using
@@ -6,15 +7,26 @@ import { orderBy, uniqBy } from 'lodash'
  *
  * @param {Array} a
  * @param {Array} b
- * @param {Number} number - of transactions to return
+ * @param {Object} order - of transactions to return
  * @return {Array}
  */
-export default (a, b, number) => {
+export default (a, b, order = null) => {
+  const { field, type } = order || { field: 'timestamp', type: 'desc' }
+
   // The order is important: the fetched transactions should override the stored
   const transactions = uniqBy([
     ...a,
     ...b
   ], 'id')
 
-  return orderBy(transactions, 'timestamp', 'desc').slice(0, number)
+  if (['amount', 'fee'].includes(field)) {
+    return transactions.sort((a, b) => {
+      const bignumA = new BigNumber(a[field])
+      const bignumB = new BigNumber(b[field])
+
+      return type === 'asc' ? bignumA.comparedTo(bignumB) : bignumB.comparedTo(bignumA)
+    })
+  }
+
+  return orderBy(transactions, field, type)
 }
