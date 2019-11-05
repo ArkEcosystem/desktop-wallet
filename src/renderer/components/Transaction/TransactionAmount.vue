7fa37002e27ad9e78f4c4f56ed1e16af6d7f80b5
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2019-11-05 22:08:28 +0000

    fix: multi-payment amount when viewing past tx

--- src/renderer/components/Transaction/TransactionAmount.vue
+++ src/renderer/components/Transaction/TransactionAmount.vue
@@ -1,12 +1,12 @@
 <template>
   <span
     :class="{
-      'text-red': transaction.isSender && transaction.amount,
+      'text-red': transaction.isSender && totalAmount,
       'text-green': !transaction.isSender && isTransfer,
     }"
   >
     {{ transaction.isSender ? '-' : '+' }}
-    {{ formatter_networkCurrency(transaction.amount) }}
+    {{ formatter_networkCurrency(totalAmount) }}
   </span>
 </template>
 
@@ -23,6 +23,19 @@ export default {
   },
 
   computed: {
+    totalAmount () {
+      const amount = this.currency_toBuilder(0)
+      if (this.transaction.asset && this.transaction.asset.payments) {
+        for (const payment of this.transaction.asset.payments) {
+          amount.add(payment.amount)
+        }
+      } else if (this.transaction.amount) {
+        amount.add(this.transaction.amount)
+      }
+
+      return amount.value
+    },
+
     isTransfer () {
       if (this.transaction.type !== undefined) {
         // 0 = transfer, 6 = timelock transfer, 7 = multipayment
