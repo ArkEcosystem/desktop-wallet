1b7889519a2485f2d2fb547c1e03c6e9c85f8e39
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2019-11-14 21:17:20 +0000

    fix: business resignation tx form

--- src/renderer/components/Transaction/TransactionForm/TransactionFormBusiness/TransactionFormBusinessResignation.vue
+++ src/renderer/components/Transaction/TransactionForm/TransactionFormBusiness/TransactionFormBusinessResignation.vue
@@ -23,6 +23,7 @@
       <InputFee
         ref="fee"
         :currency="walletNetwork.token"
+        :transaction-group="$options.transactionGroup"
         :transaction-type="$options.transactionType"
         :show-insufficient-funds="true"
         @input="onFee"
@@ -146,7 +147,7 @@ export default {
     },
 
     async buildTransaction (transactionData, isAdvancedFee = false, returnObject = false) {
-      return this.$client.buildDelegateResignation(transactionData, isAdvancedFee, returnObject)
+      return this.$client.buildBusinessResignation(transactionData, isAdvancedFee, returnObject)
     },
 
     transactionError () {
