1c2ce8d9adb87b191f89f6b9ce8e70a6e2e0f0ac
alessiodf <35549818+alessiodf@users.noreply.github.com>
2019-11-19 03:50:54 +0000

    refactor: codeclimate recommendation

--- src/renderer/services/plugin-manager/sandbox/storage-sandbox.js
+++ src/renderer/services/plugin-manager/sandbox/storage-sandbox.js
@@ -2,19 +2,17 @@ export function createStorageSandbox (walletApi, app, plugin) {
   return () => {
     walletApi.storage = {
       get: (key, global = false) => {
-        const profile = global ? 'global' : app.$store.getters['session/profileId']
         const options = app.$store.getters['plugin/pluginOptions'](
           plugin.config.id,
-          profile
+          global ? 'global' : app.$store.getters['session/profileId']
         )
 
         return options && options[key]
       },
 
       set: (key, value, global = false) => {
-        const profileId = global ? 'global' : app.$store.getters['session/profileId']
         app.$store.dispatch('plugin/setPluginOption', {
-          profileId,
+          profileId: global ? 'global' : app.$store.getters['session/profileId'],
           pluginId: plugin.config.id,
           key,
           value
