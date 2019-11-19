ca0ae8f6d58ab5dc4e01e3784f0be672507a657b
alessiodf <35549818+alessiodf@users.noreply.github.com>
2019-11-19 03:44:02 +0000

    feat(plugins): global storage

--- src/renderer/services/plugin-manager/sandbox/storage-sandbox.js
+++ src/renderer/services/plugin-manager/sandbox/storage-sandbox.js
@@ -1,18 +1,20 @@
 export function createStorageSandbox (walletApi, app, plugin) {
   return () => {
     walletApi.storage = {
-      get: (key) => {
+      get: (key, global = false) => {
+        const profile = global ? 'global' : app.$store.getters['session/profileId']
         const options = app.$store.getters['plugin/pluginOptions'](
           plugin.config.id,
-          app.$store.getters['session/profileId']
+          profile
         )
 
         return options && options[key]
       },
 
-      set: (key, value) => {
+      set: (key, value, global = false) => {
+        const profileId = global ? 'global' : app.$store.getters['session/profileId']
         app.$store.dispatch('plugin/setPluginOption', {
-          profileId: app.$store.getters['session/profileId'],
+          profileId,
           pluginId: plugin.config.id,
           key,
           value
