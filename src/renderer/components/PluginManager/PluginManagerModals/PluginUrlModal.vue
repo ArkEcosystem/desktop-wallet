6e34a55ab6587a598589ff12cd05bd3b50c1329b
Edgar Goetzendorff <hello@dated.fun>
2019-12-07 01:21:13 +0100

    refactor: github url regex

--- src/renderer/components/PluginManager/PluginManagerModals/PluginUrlModal.vue
+++ src/renderer/components/PluginManager/PluginManagerModals/PluginUrlModal.vue
@@ -93,10 +93,10 @@ export default {
         required,
         url,
         isGitHubUrl (value) {
-          return /github\.com/.test(value.toLowerCase())
+          return /^((https?:\/\/)?(www.)?)?github\.com/.test(value.toLowerCase())
         },
         isAllowed (value) {
-          const regex = RegExp('/github.com/([A-Za-z0-9_.-]+)/([A-Za-z0-9_.-]+)[/]?$')
+          const regex = RegExp('^((https?://)?(www.)?)?github.com/([A-Za-z0-9_.-]+)/([A-Za-z0-9_.-]+)[/]?$')
           return regex.test(value.toLowerCase())
         }
       }
