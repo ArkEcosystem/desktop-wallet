103a21e8a91dbf07a3a8e6236cd4d45a0fd4fe12
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-02-04 21:22:04 +0000

    test: add more for pricing api

--- __tests__/unit/services/price-api.spec.js
+++ __tests__/unit/services/price-api.spec.js
@@ -26,6 +26,27 @@ describe('PriceApi', () => {
           .reply(200, require('../__fixtures__/services/price-api/crypto-compare/historical.json'))
       } else {
         nock(MARKET.source[adapterName])
+          .get('/coins/list')
+          .reply(200, [{
+            id: 'ark',
+            symbol: 'ark',
+            name: 'ark'
+          }, {
+            id: 'dark',
+            symbol: 'dark',
+            name: 'dark'
+          }])
+
+        nock(MARKET.source[adapterName])
+          .get('/simple/price')
+          .query(true)
+          .reply(200, {
+            ark: {
+              btc: 0.0000207
+            }
+          })
+
+        nock(MARKET.source[adapterName])
           .get('/coins/ark')
           .reply(200, require('../__fixtures__/services/price-api/coin-gecko/market.json'))
 
@@ -48,6 +69,50 @@ describe('PriceApi', () => {
         expect(response.USD.price).toBe(0.176829)
       }
     })
+
+    describe('checkTradeable', () => {
+      it('should return true if found', async () => {
+        if (adapterName === 'cryptoCompare') {
+          nock(MARKET.source[adapterName])
+            .get('/data/price')
+            .query(true)
+            .reply(200, {
+              BTC: 0.00002073
+            })
+        }
+
+        expect(await priceApi.checkTradeable('ark')).toBe(true)
+      })
+
+      it('should return false if not found', async () => {
+        if (adapterName === 'cryptoCompare') {
+          nock(MARKET.source[adapterName])
+            .get('/data/price')
+            .query(true)
+            .reply(200, {
+              Response: 'Error'
+            })
+        }
+
+        expect(await priceApi.checkTradeable('not-ark')).toBe(false)
+      })
+    })
+
+    if (adapterName === 'coinGecko') {
+      it('should get token ids from api', async () => {
+        const spy = jest.spyOn(CoinGeckoAdapter, 'getTokenId')
+
+        await priceApi.checkTradeable('ARK')
+
+        expect(adapter.tokenLookup).toEqual({
+          ARK: 'ark',
+          DARK: 'dark'
+        })
+        expect(spy).toHaveBeenCalledWith('ARK')
+
+        spy.mockRestore()
+      })
+    }
 
     it('should return historic day values', async () => {
       const response = await priceApi.historicPerDay(token, currency)
