f4a794ab7e2fdd36a9a242006d656a5f9b51f87b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-02-18 19:38:54 +0000

    refactor: omit themes from list of all plugins (#1679)
    * refactor: omit themes from list of all plugins
    
    * test: fix failing
    
    * fix: clear plugin logo if fetch fails
    
    * chore: add individual plugin menu items
    
    * refactor: pluralise theme category

--- src/renderer/i18n/locales/en-US.js
+++ src/renderer/i18n/locales/en-US.js
@@ -658,7 +658,7 @@ export default {
         ALL: 'All',
         GAMING: 'Gaming',
         UTILITY: 'Utility',
-        THEME: 'Theme',
+        THEME: 'Themes',
         OTHER: 'Other'
       },
       FILTERS: {
