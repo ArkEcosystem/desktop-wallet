67cea93be0747aab79256f09919fde6b9d6cde11
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-02-20 20:40:44 +0000

    fix: disable text selection

--- src/renderer/SplashScreen.vue
+++ src/renderer/SplashScreen.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute h-full w-full flex flex-col text-center">
+  <div class="absolute h-full w-full flex flex-col text-center select-none">
     <div class="flex flex-1 justify-center">
       <div class="rounded-lg mb-3 p-5 bg-red m-auto">
         <img
