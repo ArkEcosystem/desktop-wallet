5489bf6d5a4e60c51fa705748750c5b54044cc0b
Edgar Goetzendorff <hello@dated.fun>
2020-02-28 16:29:49 +0100

    fix: add missing items in bridgechain update confirmation modal

--- src/renderer/components/Transaction/TransactionConfirm/TransactionConfirmBridgechain/TransactionConfirmBridgechainUpdate.vue
+++ src/renderer/components/Transaction/TransactionConfirm/TransactionConfirmBridgechain/TransactionConfirmBridgechainUpdate.vue
@@ -17,6 +17,22 @@
     </ListDividedItem>
 
     <ListDividedItem
+      v-if="transaction.asset.bridgechainUpdate.bridgechainRepository"
+      class="TransactionConfirmBridgechainUpdate__bridgechain-repo"
+      :label="$t('WALLET_BUSINESS.BRIDGECHAIN.BRIDGECHAIN_REPOSITORY')"
+    >
+      {{ transaction.asset.bridgechainUpdate.bridgechainRepository }}
+    </ListDividedItem>
+
+    <ListDividedItem
+      v-if="transaction.asset.bridgechainUpdate.bridgechainAssetRepository"
+      class="TransactionConfirmBridgechainUpdate__bridgechain-asset-repo"
+      :label="$t('WALLET_BUSINESS.BRIDGECHAIN.BRIDGECHAIN_ASSET_REPOSITORY')"
+    >
+      {{ transaction.asset.bridgechainUpdate.bridgechainAssetRepository }}
+    </ListDividedItem>
+
+    <ListDividedItem
       v-if="transaction.asset.bridgechainUpdate.seedNodes"
       class="TransactionConfirmBridgechainUpdate__seed-nodes"
       :label="$t('WALLET_BUSINESS.BRIDGECHAIN.SEED_NODES')"
