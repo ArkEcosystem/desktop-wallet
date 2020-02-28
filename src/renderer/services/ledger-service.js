853bef22f94f1fab7da5fda907fff40c2d0e6375
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-02-28 16:22:12 +0000

    Revert "chore: use different ledger package"
    This reverts commit 393132966581d43ce3c7e486befb89778a645220.

--- src/renderer/services/ledger-service.js
+++ src/renderer/services/ledger-service.js
@@ -1,4 +1,4 @@
-import LedgerTransport from '@ledgerhq/hw-transport-node-hid'
+import LedgerTransport from '@ledgerhq/hw-transport-node-hid-singleton'
 import { ARKTransport } from '@arkecosystem/ledger-transport'
 import queue from 'async/queue'
 import logger from 'electron-log'
