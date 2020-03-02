e3a58a0d0251aff35c59cdae0f555bc0a4c8fc6f
Brian Faust <hello@basecode.sh>
2020-03-02 12:53:45 +0200

    Revert "fix: default to local time"
    This reverts commit d0e94d4db8a930bf16e1cd73bda5561cf5ae1810.

--- src/renderer/services/datetime.js
+++ src/renderer/services/datetime.js
@@ -1,6 +1,9 @@
 import dayjs from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
+import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(advancedFormat)
+// todo: do we default to UTC or local time?
+dayjs.extend(utc)
 
 export { dayjs }
