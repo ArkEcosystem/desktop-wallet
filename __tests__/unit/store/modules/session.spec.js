7bd8dd68135653bd4150070bf78e196acbafb8ca
Edgar Goetzendorff <hello@dated.fun>
2020-03-02 15:00:10 +0100

    test: add unit test for setLastFeeByType

--- __tests__/unit/store/modules/session.spec.js
+++ __tests__/unit/store/modules/session.spec.js
@@ -81,4 +81,16 @@ describe('SessionModule', () => {
       expect(store.getters['session/priceApi']).toEqual('coingecko')
     })
   })
+
+  describe('actions > setLastFeeByType', () => {
+    it('should set the value for the last fee by type', () => {
+      store.dispatch('session/setLastFeeByType', {
+        fee: '1000',
+        type: 0
+      })
+      expect(store.getters['session/lastFees']).toEqual({
+        0: '1000'
+      })
+    })
+  })
 })
