296d9cb85a28b81eafe02cbb6eb1b5ce9ad14b47
Edgar Goetzendorff <hello@dated.fun>
2020-03-02 14:41:39 +0100

    fix: allow summary to be null

--- src/renderer/components/Announcements/AnnouncementsPost.vue
+++ src/renderer/components/Announcements/AnnouncementsPost.vue
@@ -89,7 +89,8 @@ export default {
     },
     summary: {
       type: String,
-      required: true
+      required: true,
+      validator: value => typeof value === 'string' || value === null
     },
     url: {
       type: String,
