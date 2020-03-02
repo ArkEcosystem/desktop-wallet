d0e94d4db8a930bf16e1cd73bda5561cf5ae1810
Brian Faust <hello@basecode.sh>
2020-03-02 12:01:33 +0200

    fix: default to local time

--- src/renderer/services/datetime.js
+++ src/renderer/services/datetime.js
@@ -1,9 +1,6 @@
 import dayjs from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
-import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(advancedFormat)
-// todo: do we default to UTC or local time?
-dayjs.extend(utc)
 
 export { dayjs }
