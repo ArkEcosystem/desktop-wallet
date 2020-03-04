7479bf35d25a286111f699c8408c146bc47ebc70
Brian Faust <faustbrian@users.noreply.github.com>
2020-03-04 13:23:42 +0200

    refactor: replace moment with dayjs (#1753)
    * refactor: replace moment with dayjs
    
    * fix: require dayjs locale and advanced formatting rules
    
    * fix: default to local time
    
    * Revert "fix: default to local time"
    
    This reverts commit d0e94d4db8a930bf16e1cd73bda5561cf5ae1810.
    
    * refactor: replace moment call with day.js
    
    * wip
    
    * wip
    
    Co-authored-by: Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>

--- src/renderer/mixins/formatter.js
+++ src/renderer/mixins/formatter.js
@@ -1,4 +1,4 @@
-import moment from 'moment'
+import { dayjs } from '@/services/datetime'
 import prettyBytes from 'pretty-bytes'
 
 export default {
@@ -34,7 +34,20 @@ export default {
      * @return {String}
      */
     formatter_date (value, format = null) {
-      moment.locale(window.navigator.userLanguage || window.navigator.language)
+      let userLanguage =
+        (window.navigator.userLanguage || window.navigator.language).toLowerCase()
+
+      if (userLanguage === 'en-us') {
+        userLanguage = 'en'
+      }
+
+      try {
+        require(`dayjs/locale/${userLanguage}`)
+      } catch {
+        userLanguage = 'en'
+      }
+
+      dayjs.locale(userLanguage)
 
       if (!format) {
         const sessionFormat = this.session_profile.timeFormat
@@ -46,7 +59,8 @@ export default {
           format = 'L LTS'
         }
       }
-      return moment(value).format(format)
+
+      return dayjs(value).format(format)
     }
   }
 }
