6adb03cd085ef26162edbf96f76aabc819b8a053
Brian Faust <hello@basecode.sh>
2020-03-04 12:13:42 +0200

    wip

--- src/renderer/services/datetime.js
+++ src/renderer/services/datetime.js
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
 import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(advancedFormat)
+dayjs.extend(localizedFormat)
 // todo: do we default to UTC or local time?
 dayjs.extend(utc)
 
