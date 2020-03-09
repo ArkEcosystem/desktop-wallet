c5ca8eb76b9cdc2c5c392d82633191a53c3a9e45
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-03-09 12:11:45 +0000

    refactor: change wording for contact delete button (#1774)

--- src/renderer/components/Wallet/WalletHeading/WalletHeadingSecondaryActions.vue
+++ src/renderer/components/Wallet/WalletHeading/WalletHeadingSecondaryActions.vue
@@ -58,7 +58,7 @@
     <ButtonModal
       v-show="!currentWallet.isLedger"
       :class="buttonStyle"
-      :label="$t('WALLET_HEADING.ACTIONS.DELETE_WALLET')"
+      :label="currentWallet.isContact ? $t('WALLET_HEADING.ACTIONS.DELETE_CONTACT') : $t('WALLET_HEADING.ACTIONS.DELETE_WALLET')"
       icon="delete-wallet"
     >
       <template slot-scope="{ toggle, isOpen }">
@@ -94,7 +94,8 @@ export default {
   },
 
   data: () => ({
-    registrationTypes: []
+    registrationTypes: [],
+    isContact: false
   }),
 
   computed: {
@@ -111,13 +112,24 @@ export default {
     }
   },
 
+  watch: {
+    currentWallet (wallet) {
+      this.isContact = wallet.isContact
+    }
+  },
+
   async mounted () {
     this.registrationTypes = await this.getRegistrationTypes()
+    this.isContact = this.currentWallet.isContact
   },
 
   methods: {
     async onRemoval () {
-      this.$router.push({ name: 'wallets' })
+      if (this.isContact) {
+        this.$router.push({ name: 'contacts' })
+      } else {
+        this.$router.push({ name: 'wallets' })
+      }
     },
 
     closeTransactionModal (toggleMethod, isOpen) {
