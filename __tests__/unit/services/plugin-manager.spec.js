4315524f4ca90b6f3e0b98645848b63c1e3f2ce3
Breno Polanski <breno.polanski@gmail.com>
2020-03-24 19:49:15 -0300

    refactor: grant icon & fetching plugin config (#1860)
    * wip: add method to fetch plugins from the list
    
    * feat: add icons
    
    * fix: update url of plugins
    
    * feat: set value for isGrant
    
    * feat: add grant icon component
    
    * feat: add grant icon
    
    * chore: add description for tooltip
    
    * test: update tests to use the new json format
    
    * test: add test for fetchPluginsList
    
    * test: add tests for isGrant
    
    * fix: change value for grants property
    
    * fix: update plugins list url
    
    Co-authored-by: Brian Faust <faustbrian@users.noreply.github.com>
    Co-authored-by: Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>

--- __tests__/unit/services/plugin-manager.spec.js
+++ __tests__/unit/services/plugin-manager.spec.js
@@ -48,6 +48,7 @@ const app = {
     getters: {
       'plugin/isEnabled': jest.fn((pluginId) => pluginId === 'plugin-test'),
       'plugin/isInstalledSupported': jest.fn(() => true),
+      'plugin/isGrant': jest.fn(() => true),
       'plugin/lastFetched': jest.fn(() => 0),
       'profile/byId': jest.fn(() => {}),
       'session/pluginAdapter': 'npm'
@@ -241,40 +242,19 @@ describe('Plugin Manager', () => {
     })
   })
 
-  describe('fetchBlacklist', () => {
+  describe('fetchPluginsList', () => {
     it('should fetch using cache-busted url', async () => {
       const spy = jest.spyOn(Date.prototype, 'getTime').mockReturnValue(1234)
       const spyError = jest.spyOn(console, 'error')
 
       nock('https://raw.githubusercontent.com')
-        .get('/ark-ecosystem-desktop-plugins/config/master/blacklist.json')
+        .get('/ark-ecosystem-desktop-plugins/config/master/plugins.json')
         .query({
           ts: 1234
         })
         .reply(200, [])
 
-      await pluginManager.fetchBlacklist()
-
-      expect(spyError).not.toHaveBeenCalled()
-
-      spy.mockRestore()
-      spyError.mockRestore()
-    })
-  })
-
-  describe('fetchWhitelist', () => {
-    it('should fetch using cache-busted url', async () => {
-      const spy = jest.spyOn(Date.prototype, 'getTime').mockReturnValue(1234)
-      const spyError = jest.spyOn(console, 'error')
-
-      nock('https://raw.githubusercontent.com')
-        .get('/ark-ecosystem-desktop-plugins/config/master/whitelist.json')
-        .query({
-          ts: 1234
-        })
-        .reply(200, [])
-
-      await pluginManager.fetchWhitelist()
+      await pluginManager.fetchPluginsList()
 
       expect(spyError).not.toHaveBeenCalled()
 
