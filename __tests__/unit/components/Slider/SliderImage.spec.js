38e3c7895eed8555771f659bb3de007823e897d8
brenopolanski <breno.polanski@gmail.com>
2020-04-04 20:20:27 -0300

    test: add tests for SliderImage component

--- __tests__/unit/components/Slider/SliderImage.spec.js
+++ __tests__/unit/components/Slider/SliderImage.spec.js
@@ -29,4 +29,94 @@ describe('SliderImage', () => {
   it('should render', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
+
+  it('should render images in row (thumbnails)', () => {
+    const sliderImage = wrapper.find('.SliderImage__container')
+    expect(sliderImage.contains('.SliderImage__container--row')).toBeTruthy()
+  })
+
+  it('should render images in stack', () => {
+    createWrapper(SliderImage, {
+      isRow: false,
+      images
+    })
+    const sliderImage = wrapper.find('.SliderImage__container')
+    expect(sliderImage.contains('.SliderImage__container--stack')).toBeTruthy()
+  })
+
+  it('should render if has images', () => {
+    const sliderImage = wrapper.find('.SliderImage')
+    expect(sliderImage.vm.hasImages).toEqual(true)
+  })
+
+  it('should show navigation if "showNavigation" is true', () => {
+    createWrapper(SliderImage, {
+      isRow: true,
+      showNavigation: true,
+      images
+    })
+
+    const sliderImage = wrapper.find('.SliderImage')
+    const { showNavigation, pageCount } = sliderImage.vm
+    expect(showNavigation && pageCount > 1).toEqual(true)
+  })
+
+  it('should not show navigation if "showNavigation" is false', () => {
+    createWrapper(SliderImage, {
+      isRow: true,
+      showNavigation: false,
+      images
+    })
+
+    const sliderImage = wrapper.find('.SliderImage')
+    const { showNavigation, pageCount } = sliderImage.vm
+    expect(showNavigation && pageCount > 1).toEqual(false)
+  })
+
+  it('should show pagination if "showPagination" is true', () => {
+    createWrapper(SliderImage, {
+      isRow: true,
+      showPagination: true,
+      images
+    })
+
+    const sliderImage = wrapper.find('.SliderImage')
+    const { showPagination, pageCount } = sliderImage.vm
+    expect(showPagination && pageCount > 1).toEqual(true)
+  })
+
+  it('should not show pagination if "showPagination" is false', () => {
+    createWrapper(SliderImage, {
+      isRow: true,
+      showPagination: false,
+      images
+    })
+
+    const sliderImage = wrapper.find('.SliderImage')
+    const { showPagination, pageCount } = sliderImage.vm
+    expect(showPagination && pageCount > 1).toEqual(false)
+  })
+
+  it('should not show navigation and pagination if page count is less than 1', () => {
+    createWrapper(SliderImage, {
+      isRow: true,
+      images: [
+        'preview-1.png'
+      ]
+    })
+
+    const sliderImage = wrapper.find('.SliderImage')
+    expect(sliderImage.vm.pageCount > 1).toEqual(false)
+  })
+
+  it('should show two images per page', () => {
+    createWrapper(SliderImage, {
+      isRow: true,
+      perPage: 2,
+      images
+    })
+
+    const sliderImage = wrapper.find('.SliderImage')
+    expect(sliderImage.vm.perPage).toBe(2)
+  })
 })
