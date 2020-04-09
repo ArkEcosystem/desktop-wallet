682a6fb06ca5ba4f9837b6ef3c33683ab73b514c
Breno Polanski <breno.polanski@gmail.com>
2020-04-09 00:06:28 -0300

    chore: adjust the window size to its content not to show scroll bar (#1883)

--- src/main/menu.js
+++ src/main/menu.js
@@ -12,12 +12,13 @@ export default function ({ createLoadingWindow }) {
   const about = {
     label: 'About',
     click: () => aboutWindow({
+      adjust_window_size: true,
       icon_path: isProduction
         ? path.resolve(__dirname, './static/128x128.png')
         : path.resolve(__dirname, '../../build/icons/128x128.png'),
       copyright: [
         `<p style="text-align: center">Distributed under ${packageJson.license} license</p>`,
-        '<p>Flag icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/"  title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>'
+        '<p>Flag icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">flaticon.com</a> are licensed by <a href="https://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></p>'
       ],
       package_json_dir: path.resolve(__dirname, '../../'),
       css_path: isProduction ? path.resolve(__dirname, 'styles.css') : null,
@@ -30,12 +31,12 @@ export default function ({ createLoadingWindow }) {
     submenu: [
       {
         label: 'Settings',
+        accelerator: 'CmdOrCtrl+,',
         click: (_, focusedWindow) => {
           if (focusedWindow && focusedWindow.webContents) {
             focusedWindow.webContents.send('app:preferences')
           }
-        },
-        accelerator: 'CommandOrControl+,'
+        }
       }
     ]
   }
