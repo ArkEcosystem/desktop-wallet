708da614cb0bf6e2df812a4c4d67c0497ee264c6
brenopolanski <breno.polanski@gmail.com>
2020-04-09 15:44:25 -0300

    refactor: update code to install vue-devtools

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -14,9 +14,9 @@ require('electron-debug')({ showDevTools: true })
 require('electron-log')
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
+require('electron').app.whenReady().then(() => {
+  const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer')
+  installExtension(VUEJS_DEVTOOLS)
     .then(() => {})
     .catch(err => {
       console.log('Unable to install `vue-devtools`: \n', err)
