4cec5f7e99c4694f39114020c9b35ad7e0883f26
brenopolanski <breno.polanski@gmail.com>
2020-04-09 21:11:49 -0300

    feat: add isEmpty native function

--- src/renderer/utils/index.js
+++ src/renderer/utils/index.js
@@ -1,13 +1,6 @@
 import sortByProps from './sort-by-props'
 
 /**
- * Converts the first character of `string` to upper case
- * @param {String} str
- * @return {String} Return the converted string
- */
-const upperFirst = str => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
-
-/**
  * Converts the first character of `string` to upper case and the remaining to lower case
  * @param {String} str
  * @return {String} Return the capitalized string
@@ -15,11 +8,29 @@ const upperFirst = str => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
 const capitalize = str => `${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`
 
 /**
+ * Checks if `value` is an empty object, collection, map, or set
+ * @param {*} val
+ * @return {Boolean} Returns `true` if `value` is empty, else `false`
+ */
+const isEmpty = val => [Object, Array].includes((val || {}).constructor) && !Object.entries((val || {})).length
+
+/**
  * Checks if `value` is `null` or `undefined`
  * @param {*} val
  * @return {Boolean} Return `true` if `value` is nullish, else `false`
  */
 const isNil = val => val === null || val === undefined
+
+/**
+ * Computes the maximum value of `array`
+ * @param {Array} nums
+ * @return {*} Return the maximum value
+ */
+const max = nums => {
+  if (nums.length) {
+    return Math.max(...nums)
+  }
+}
 
 /**
  * Computes the minimum value of `array`
@@ -33,21 +44,18 @@ const min = nums => {
 }
 
 /**
- * Computes the maximum value of `array`
- * @param {Array} nums
- * @return {*} Return the maximum value
+ * Converts the first character of `string` to upper case
+ * @param {String} str
+ * @return {String} Return the converted string
  */
-const max = nums => {
-  if (nums.length) {
-    return Math.max(...nums)
-  }
-}
+const upperFirst = str => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
 
 export {
-  upperFirst,
   capitalize,
+  isEmpty,
   isNil,
-  min,
   max,
-  sortByProps
+  min,
+  sortByProps,
+  upperFirst
 }
