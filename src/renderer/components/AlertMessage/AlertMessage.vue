45c249af31ee77a47b07c3d04292a90124cb881c
brenopolanski <breno.polanski@gmail.com>
2020-05-25 17:11:47 -0300

    refactor(tailwindcss): migrate AlertMessage to typescript

--- src/renderer/components/AlertMessage/AlertMessage.vue
+++ src/renderer/components/AlertMessage/AlertMessage.vue
@@ -5,70 +5,63 @@
 	>
 		<div class="text-left message font-bold">
 			<ButtonClose icon-class="text-white" @click="showNext" />
-			<span class="break-words">
-				{{ alert ? alert.message : "&nbsp;" }}
-			</span>
+			<span class="break-words">{{ alert ? alert.message : "&nbsp;" }}</span>
 		</div>
 	</button>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
 import { ButtonClose } from "@/components/Button";
 import { AppEvent } from "@/enums";
 
-export default {
-	name: "AlertMessage",
-
+@Component({
 	components: {
 		ButtonClose,
 	},
+})
+export default class AlertMessage extends Vue {
+	@Prop({ default: 5000 }) public duration!: number;
 
-	props: {
-		duration: {
-			type: Number,
-			default: 5000,
-		},
-	},
+	private queue: object[] = [];
+	private timer: number | undefined = undefined;
+	private alert: { [key: string]: any } | null = null;
 
-	data: () => ({
-		queue: [],
-		timer: null,
-		alert: null,
-	}),
-
-	mounted() {
+	public mounted() {
 		this.$eventBus.on(AppEvent.Alert, this.queueAlert);
-	},
+	}
 
-	methods: {
-		queueAlert(alert) {
-			this.queue.push(alert);
+	private queueAlert(alert: object) {
+		this.queue.push(alert);
 
-			if (this.queue.length === 1) {
-				this.showNext();
+		if (this.queue.length === 1) {
+			this.showNext();
+		}
+	}
+
+	private showNext() {
+		clearTimeout(this.timer);
+
+		if (this.alert) {
+			this.queue.shift();
+		}
+
+		let duration = this.duration;
+
+		if (this.queue.length) {
+			this.alert = this.queue[0];
+
+			if (this.alert.duration && Number.isFinite(this.alert.duration)) {
+				duration = this.alert.duration;
 			}
-		},
+		} else {
+			this.alert = null;
+		}
 
-		showNext() {
-			clearTimeout(this.timer);
-
-			if (this.alert) {
-				this.queue.shift();
-			}
-			let duration = this.duration;
-			if (this.queue.length) {
-				this.alert = this.queue[0];
-				if (this.alert.duration && Number.isFinite(this.alert.duration)) {
-					duration = this.alert.duration;
-				}
-			} else {
-				this.alert = null;
-			}
-
-			this.timer = setTimeout(this.showNext, duration);
-		},
-	},
-};
+		this.timer = setTimeout(this.showNext, duration);
+	}
+}
 </script>
 
 <style scoped>
@@ -77,19 +70,19 @@ export default {
 }
 .AlertMessage--error {
 	@apply bg-theme-error;
-	box-shadow: 0px 0px 15px var(--theme-error-shadow);
+	box-shadow: 0 0 15px var(--theme-error-shadow);
 }
 .AlertMessage--success {
 	@apply bg-theme-success;
-	box-shadow: 0px 0px 15px var(--theme-success-shadow);
+	box-shadow: 0 0 15px var(--theme-success-shadow);
 }
 .AlertMessage--info {
 	@apply bg-theme-info;
-	box-shadow: 0px 0px 15px var(--theme-info-shadow);
+	box-shadow: 0 0 15px var(--theme-info-shadow);
 }
 .AlertMessage--warn {
 	@apply bg-theme-warn text-theme-warn-text;
-	box-shadow: 0px 0px 15px var(--theme-warn-shadow);
+	box-shadow: 0 0 15px var(--theme-warn-shadow);
 }
 .AlertMessage .ButtonClose {
 	@apply float-right cursor-pointer select-none ml-4;
