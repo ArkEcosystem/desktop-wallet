3f88d1316faece2753643b7e7c12a90065fc2c92
Brian Faust <hello@basecode.sh>
2020-05-26 09:31:48 +0300

    refactor: migrate `renderer/components/utils`

--- src/renderer/components/utils/Identicon.vue
+++ src/renderer/components/utils/Identicon.vue
@@ -42,141 +42,147 @@
 </template>
 
 <script>
+import { Vue, Component, Prop } from "vue-property-decorator";
 import Color from "color";
 import crypto from "crypto";
 import MersenneTwister from "mersenne-twister";
 
 import colors from "@/components/utils/IdenticonColors";
 
-export default {
-	name: "Identicon",
+@Component({
+    name: "Identicon",
 
-	props: {
-		value: {
-			type: String,
-			required: true,
-		},
-		size: {
-			type: Number,
-			required: true,
-		},
-		shape: {
-			type: String,
-			required: false,
-			default: () => "circle",
-		},
-		shapeCount: {
-			type: Number,
-			required: false,
-			default: 3,
-		},
-		isDark: {
-			type: Boolean,
-			required: false,
-			default: false,
-		},
-		showNetworkSymbol: {
-			type: Boolean,
-			required: false,
-			default: true,
-		},
-	},
-
-	data: () => ({
-		rectangles: [],
-		logo: null,
-		backgroundColor: "",
-	}),
-
-	computed: {
-		networkSymbol() {
-			return this.session_network && this.session_network.symbol.length ? this.session_network.symbol[0] : "";
-		},
-		maskId() {
-			return Math.random().toString(36).substring(9);
-		},
-	},
-
-	watch: {
+    watch: {
 		value() {
 			this.generate(this.value);
 		},
-	},
+	}
+})
+export default class Identicon extends Vue {
+    @Prop({
+        type: String,
+        required: true,
+    })
+    value;
 
-	created() {
+    @Prop({
+        type: Number,
+        required: true,
+    })
+    size;
+
+    @Prop({
+        type: String,
+        required: false,
+        default: () => "circle",
+    })
+    shape;
+
+    @Prop({
+        type: Number,
+        required: false,
+        default: 3,
+    })
+    shapeCount;
+
+    @Prop({
+        type: Boolean,
+        required: false,
+        default: false,
+    })
+    isDark;
+
+    @Prop({
+        type: Boolean,
+        required: false,
+        default: true,
+    })
+    showNetworkSymbol;
+
+    rectangles = [];
+    logo = null;
+    backgroundColor = "";
+
+    get networkSymbol() {
+        return this.session_network && this.session_network.symbol.length ? this.session_network.symbol[0] : "";
+    }
+
+    get maskId() {
+        return Math.random().toString(36).substring(9);
+    }
+
+    created() {
 		this.generate(this.value);
-	},
+	}
 
-	methods: {
-		generate(value) {
-			this.rectangles = [];
+    generate(value) {
+        this.rectangles = [];
 
-			// Change string to a hash
-			const hash = crypto.createHash("sha1").update(Buffer.from(value, "utf8")).digest("binary");
-			// Change hash into a 32 bit int for our seed
-			const seed =
-				(hash.charCodeAt(0) << 24) |
-				(hash.charCodeAt(1) << 16) |
-				(hash.charCodeAt(2) << 8) |
-				hash.charCodeAt(3);
-			const generator = new MersenneTwister(seed);
-			const remainingColors = this.hueShift(colors.slice(), generator);
-			this.backgroundColor = this.genColor(remainingColors, generator);
+        // Change string to a hash
+        const hash = crypto.createHash("sha1").update(Buffer.from(value, "utf8")).digest("binary");
+        // Change hash into a 32 bit int for our seed
+        const seed =
+            (hash.charCodeAt(0) << 24) |
+            (hash.charCodeAt(1) << 16) |
+            (hash.charCodeAt(2) << 8) |
+            hash.charCodeAt(3);
+        const generator = new MersenneTwister(seed);
+        const remainingColors = this.hueShift(colors.slice(), generator);
+        this.backgroundColor = this.genColor(remainingColors, generator);
 
-			for (let i = 0; i < this.shapeCount; i++) {
-				this.rectangles.push(this.genShape(generator, remainingColors, 100, i, this.shapeCount));
-			}
-			this.logo = this.genShape(generator, remainingColors, 100, 1, this.shapeCount);
-		},
+        for (let i = 0; i < this.shapeCount; i++) {
+            this.rectangles.push(this.genShape(generator, remainingColors, 100, i, this.shapeCount));
+        }
+        this.logo = this.genShape(generator, remainingColors, 100, 1, this.shapeCount);
+    }
 
-		genShape(generator, remainingColors, diameter, i, total) {
-			const center = diameter / 2;
-			const width = diameter;
-			const height = diameter;
+    genShape(generator, remainingColors, diameter, i, total) {
+        const center = diameter / 2;
+        const width = diameter;
+        const height = diameter;
 
-			const firstRot = generator.random();
-			const angle = Math.PI * 2 * firstRot;
-			const velocity = (diameter / total) * generator.random() + (i * diameter) / total;
+        const firstRot = generator.random();
+        const angle = Math.PI * 2 * firstRot;
+        const velocity = (diameter / total) * generator.random() + (i * diameter) / total;
 
-			const tx = Math.cos(angle) * velocity;
-			const ty = Math.sin(angle) * velocity;
+        const tx = Math.cos(angle) * velocity;
+        const ty = Math.sin(angle) * velocity;
 
-			const translate = "translate(" + tx + " " + ty + ")";
+        const translate = "translate(" + tx + " " + ty + ")";
 
-			// Third random is a shape rotation on top of all of that.
-			const secondRot = generator.random();
-			const rot = firstRot * 360 + secondRot * 180;
-			const rotate = "rotate(" + rot.toFixed(1) + " " + center + " " + center + ")";
-			const transform = translate + " " + rotate;
+        // Third random is a shape rotation on top of all of that.
+        const secondRot = generator.random();
+        const rot = firstRot * 360 + secondRot * 180;
+        const rotate = "rotate(" + rot.toFixed(1) + " " + center + " " + center + ")";
+        const transform = translate + " " + rotate;
 
-			const fill = this.genColor(remainingColors, generator);
+        const fill = this.genColor(remainingColors, generator);
 
-			return {
-				id: i,
-				width,
-				height,
-				transform,
-				fill,
-			};
-		},
+        return {
+            id: i,
+            width,
+            height,
+            transform,
+            fill,
+        };
+    }
 
-		genColor(colors, generator) {
-			const idx = Math.floor(colors.length * generator.random());
-			const color = colors.splice(idx, 1)[0];
-			return color;
-		},
+    genColor(colors, generator) {
+        const idx = Math.floor(colors.length * generator.random());
+        const color = colors.splice(idx, 1)[0];
+        return color;
+    }
 
-		hueShift(colors, generator) {
-			const wobble = 30;
-			const amount = generator.random() * 30 - wobble / 2;
-			return colors.map(function (hex) {
-				const color = Color(hex);
-				color.rotate(amount);
-				return color.hex();
-			});
-		},
-	},
-};
+    hueShift(colors, generator) {
+        const wobble = 30;
+        const amount = generator.random() * 30 - wobble / 2;
+        return colors.map(function (hex) {
+            const color = Color(hex);
+            color.rotate(amount);
+            return color.hex();
+        });
+    }
+}
 </script>
 
 <style>
