72fe5317c205376835c78ee4ed590b36f5de2283
brenopolanski <breno.polanski@gmail.com>
2020-05-31 16:05:11 -0300

    chore(tailwindcss): add negative insets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,24 @@ module.exports = {
 	theme: {
 		extend: {
 			inset: {
+				full: "100%",
 				"-1": "-0.25rem",
 				"-2": "-0.5rem",
 				"-3": "-0.75rem",
 				"-4": "-1rem",
-				full: "100%",
+				"-5": "-1.25rem",
+				"-6": "-1.5rem",
+				"-8": "-2rem",
+				"-10": "-2.5rem",
+				"-12": "-3rem",
+				"-16": "-4rem",
+				"-20": "-5rem",
+				"-24": "-6rem",
+				"-32": "-8rem",
+				"-40": -"10rem",
+				"-48": "-12rem",
+				"-56": "-14rem",
+				"-64": "-16rem",
 			},
 			colors: {
         "theme-page-text": "var(--theme-page-text)",
