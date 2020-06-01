5cab223e41e902cbbd2a1561bf0e6616140809e4
Brian Faust <hello@basecode.sh>
2020-06-01 14:48:01 +0300

    wip

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 import { configure, addParameters } from "@storybook/vue";
 
 import "reflect-metadata";
-import "../src/renderer/styles/style.css";
+import "../src/renderer/app/styles/style.css";
 
 addParameters({
 	options: {
