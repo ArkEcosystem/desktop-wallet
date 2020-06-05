8930a18a3a8690ed64d5697be8a97466019d6eba
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-06-05 13:39:53 -0300

    test: add select component tests (#2028)

--- src/app/components/Select/Select.tsx
+++ src/app/components/Select/Select.tsx
@@ -3,7 +3,14 @@ import React from "react";
 type SelectProps = React.SelectHTMLAttributes<any>;
 
 export const Select = React.forwardRef<HTMLSelectElement, SelectProps>((props: SelectProps, ref) => {
-	return <select ref={ref} className="appearance-none block w-full form-select transition-colors duration-200" {...props} />
+	return (
+		<select
+			data-testid="select-input"
+			ref={ref}
+			className="appearance-none block w-full form-select transition-colors duration-200"
+			{...props}
+		/>
+	);
 });
 
 Select.displayName = "Select";
