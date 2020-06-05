a78c3cc9a934c3a9bf2910526fb76b2e0fdbf8ae
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-05 13:41:59 -0300

    tests: add step indicator specs

---
+++ src/app/components/StepIndicator/__tests__/StepIndicator.spec.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { StepIndicator } from "../";
+
+describe("StepIndicator", () => {
+	it("should render", () => {
+		const { container, asFragment } = render(<StepIndicator />);
+
+		expect(container).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
