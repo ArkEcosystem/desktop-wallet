cd1f5c38bc4beee1acab043cbaca7a87054cab87
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-05 14:55:15 -0300

    tests: add toggle specs

--- src/app/components/Toggle/Toggle.tsx
+++ src/app/components/Toggle/Toggle.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import tw, { styled } from 'twin.macro';
+import tw, { styled } from "twin.macro";
 
 const Input = styled.input`
-    ${tw`sr-only`}
-`
+	${tw`sr-only`}
+`;
 
 const Handle = styled.div`
-    ${tw`inline-flex rounded-full relative h-2 w-10 cursor-pointer bg-theme-neutral-light`}
-`
+	${tw`inline-flex rounded-full relative h-2 w-10 cursor-pointer bg-theme-neutral-light`}
+`;
 
 const HandleInner = styled.span`
     ${tw`h-5 w-5 bg-theme-neutral-light rounded-full absolute transform -translate-y-1/2 mt-1 transition transition-colors transition-transform ease-in-out duration-200`}
@@ -19,17 +19,19 @@ const HandleInner = styled.span`
     ${Input}:focus ~ ${Handle} & {
         ${tw`shadow-outline`}
     }
-`
+`;
 
 type ToggleProps = React.InputHTMLAttributes<any>;
 
 export const Toggle = React.forwardRef<HTMLInputElement, ToggleProps>((props, ref) => {
-    return <label>
-        <Input type="checkbox" ref={ref} {...props} />
-        <Handle>
-            <HandleInner/>
-        </Handle>
-    </label>
-})
+	return (
+		<label>
+			<Input type="checkbox" ref={ref} {...props} />
+			<Handle>
+				<HandleInner />
+			</Handle>
+		</label>
+	);
+});
 
 Toggle.displayName = "Toggle";
