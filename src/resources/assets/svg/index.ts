9e9935d65fced98c264da70c5cea2d11525f4f73
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-06-06 08:10:29 -0300

    refactor: improve svg icon (#2034)

---
+++ src/resources/assets/svg/index.ts
@@ -0,0 +1,19 @@
+import ark from "./ark.svg";
+import btc from "./btc.svg";
+import close from "./close.svg";
+import eth from "./eth.svg";
+import qrcode from "./qrcode.svg";
+import settings from "./settings.svg";
+import upload from "./upload.svg";
+
+const SvgCollection: any = {
+	ark,
+	btc,
+	close,
+	eth,
+	qrcode,
+	settings,
+	upload,
+};
+
+export default SvgCollection;
