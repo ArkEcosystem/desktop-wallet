3de6080dab5b0c4cee403b82db35430553eee7d5
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-06 10:00:04 -0300

    feat: add a form id to handle button interaction

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -4,10 +4,15 @@ import { useForm } from "react-hook-form";
 type Props = {
 	handleOnSubmit: any;
 	children: React.ReactNode;
+	id?: string;
 };
 
-export const Form = ({ handleOnSubmit, children }: Props) => {
+export const Form = ({ handleOnSubmit, children, id }: Props) => {
 	const { handleSubmit } = useForm();
 
-	return <form onSubmit={handleSubmit(handleOnSubmit)}>{children}</form>;
+	return (
+		<form id={id} onSubmit={handleSubmit(handleOnSubmit)}>
+			{children}
+		</form>
+	);
 };
