e005247df2bc1e015a0e8c409247f2cea3edc20c
brenopolanski <breno.polanski@gmail.com>
2020-06-08 09:13:02 -0300

    feat: add styles for the divider component

--- src/app/components/Divider/Divider.tsx
+++ src/app/components/Divider/Divider.tsx
@@ -1,2 +1,13 @@
 import React from "react";
 import { styled } from "twin.macro";
+import { getStyles } from "./style";
+
+type DividerProps = {
+	type?: "horizontal" | "vertical";
+};
+
+export const Divider = styled.div<DividerProps>(getStyles);
+
+Divider.defaultProps = {
+	type: "horizontal",
+};
--- src/app/components/Divider/style.ts
+++ src/app/components/Divider/style.ts
@@ -1,8 +1,33 @@
 import tw, { css } from "twin.macro";
 
 const baseStyle = [
-	tw`flex clear-both w-full min-w-full`,
 	css`
-		border-top: 1px solid var(theme-color-neutral-300);
+		border-top: 1px solid var(--theme-color-neutral-300);
 	`,
 ];
+
+const getType = (type: string): any => {
+	switch (type) {
+		case "horizontal":
+			return [
+				tw`flex clear-both w-full min-w-full`,
+				css`
+					margin: 24px 0;
+				`,
+			];
+		case "vertical":
+			return [
+				tw`relative inline-block align-middle border-t-0`,
+				css`
+					top: -0.06em;
+					height: 0.9rem;
+					margin: 0 8px;
+					border-left: 1px solid var(--theme-color-neutral-300);
+				`,
+			];
+	}
+};
+
+export const getStyles = ({ type }: { type?: string }) => {
+	return [getType(type!), ...baseStyle];
+};
