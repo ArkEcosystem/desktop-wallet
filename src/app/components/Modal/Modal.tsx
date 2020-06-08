b891d7f2a8cddcb0fb286e8af7eb63076834be63
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-08 22:44:01 +0100

    refactor: add y overflow for content

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -20,7 +20,7 @@ const ModalContent = (props: ModalContentProps) => {
 	return (
 		<div
 			data-testid="modal__inner"
-			className="fixed z-10 left-0 right-0 max-w-lg max-h-1/2 rounded-xl bg-theme-background mx-auto mt-24 pt-6 pb-8 px-10"
+			className="fixed flex flex-col z-10 left-0 right-0 max-w-lg max-h-1/2 rounded-xl bg-theme-background mx-auto mt-24 pt-6 pb-8 px-10"
 		>
 			<div className="absolute right-0 top-0 mt-4 mr-4">
 				<Button color="neutral" variant="plain" size="icon" onClick={props.onClick}>
@@ -29,9 +29,12 @@ const ModalContent = (props: ModalContentProps) => {
 			</div>
 
 			<h2>{props.title}</h2>
-			{props.description ? <div className="text-theme-neutral-500">{props.description}</div> : ""}
 
-			{props.children}
+			<div className="flex-1 overflow-auto">
+				{props.description ? <div className="text-theme-neutral-500">{props.description}</div> : ""}
+
+				{props.children}
+			</div>
 		</div>
 	);
 };
