927e71deee7d5c4d95b530abbff3f6bbb8a25a96
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-08 18:36:50 -0300

    chore: update heade UI

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Header = ({ title, subheader }: Props) => (
 	<div className="flex flex-col items-start">
-		<h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
+		<h1 className="text-3xl font-bold md:text-4xl">{title}</h1>
 		{subheader && <div className="mt-2 text-theme-neutral-dark">{subheader}</div>}
 	</div>
 );
