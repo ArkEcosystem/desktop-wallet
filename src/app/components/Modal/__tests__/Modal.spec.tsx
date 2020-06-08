fd52e075c4c642c7d75c1fd44b5f5dc72ad670b5
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-08 23:02:36 +0100

    wip

--- src/app/components/Modal/__tests__/Modal.spec.tsx
+++ src/app/components/Modal/__tests__/Modal.spec.tsx
@@ -14,28 +14,38 @@ describe("Modal", () => {
 	it("should not render if not open", () => {
 		const { asFragment, getByTestId } = render(<Modal title="ark" isOpen={false} />);
 
-		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/)
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should render a modal with overlay", () => {
-		const { asFragment, getByTestId } = render(<Modal title="ark" isOpen={true}>This is the Modal content</Modal>);
+		const { asFragment, getByTestId } = render(
+			<Modal title="ark" isOpen={true}>
+				This is the Modal content
+			</Modal>,
+		);
 
 		expect(getByTestId("modal__overlay")).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should render a modal with description", () => {
-		const { asFragment, getByTestId } = render(<Modal title="ark" description="This is the Modal description" isOpen={true} />);
+		const { asFragment, getByTestId } = render(
+			<Modal title="ark" description="This is the Modal description" isOpen={true} />,
+		);
 
-		expect(getByTestId("modal__inner")).toHaveTextContent("This is the Modal description");
 		expect(asFragment()).toMatchSnapshot();
+		expect(getByTestId("modal__inner")).toHaveTextContent("This is the Modal description");
 	});
 
 	it("should render a modal with content", () => {
-		const { asFragment, getByTestId } = render(<Modal title="ark" isOpen={true}>This is the Modal content</Modal>);
+		const { asFragment, getByTestId } = render(
+			<Modal title="ark" isOpen={true}>
+				This is the Modal content
+			</Modal>,
+		);
 
-		expect(getByTestId("modal__inner")).toHaveTextContent("This is the Modal content");
 		expect(asFragment()).toMatchSnapshot();
+		expect(getByTestId("modal__inner")).toHaveTextContent("This is the Modal content");
 	});
 });
