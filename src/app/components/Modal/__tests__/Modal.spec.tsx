25c0c775ceea8bd4a4cd9d529299d1655c4c0744
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-08 21:12:03 +0100

    test: initial modal test

---
+++ src/app/components/Modal/__tests__/Modal.spec.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { Modal } from "../";
+
+describe("Modal", () => {
+	it("should render", () => {
+		const { container, asFragment } = render(<Modal title="ark" />);
+
+		expect(container).toBeTruthy();
+		// expect(asFragment()).toMatchSnapshot();
+	});
+});
