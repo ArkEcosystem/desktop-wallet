6e597f5726d5001aebc8b316f86a7090772d063e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-08 20:46:19 +0100

    chore: add cross svg icon

--- src/resources/assets/svg/index.ts
+++ src/resources/assets/svg/index.ts
@@ -1,6 +1,7 @@
 import ark from "./ark.svg";
 import btc from "./btc.svg";
 import close from "./close.svg";
+import crossSlim from "./cross-slim.svg";
 import eth from "./eth.svg";
 import qrcode from "./qrcode.svg";
 import settings from "./settings.svg";
@@ -10,6 +11,7 @@ export const SvgCollection: any = {
 	ark,
 	btc,
 	close,
+	crossSlim,
 	eth,
 	qrcode,
 	settings,
