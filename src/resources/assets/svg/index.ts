290f46556c2e94c8c5e222da0f1f2e219926219d
brenopolanski <breno.polanski@gmail.com>
2020-06-09 17:01:01 -0300

    chore: add msq icon

--- src/resources/assets/svg/index.ts
+++ src/resources/assets/svg/index.ts
@@ -1,31 +1,33 @@
 import ark from "./ark.svg";
 import btc from "./btc.svg";
+import chevronDown from "./chevron-down.svg";
 import close from "./close.svg";
 import crossSlim from "./cross-slim.svg";
 import eth from "./eth.svg";
-import qrcode from "./qrcode.svg";
-import settings from "./settings.svg";
-import upload from "./upload.svg";
+import eye from "./eye.svg";
+import eyeOff from "./eye-off.svg";
 import general from "./general.svg";
+import msq from "./msq.svg";
+import qrcode from "./qrcode.svg";
 import peer from "./peer.svg";
 import plugin from "./plugin.svg";
-import eyeOff from "./eye-off.svg";
-import eye from "./eye.svg";
-import chevronDown from "./chevron-down.svg";
+import settings from "./settings.svg";
+import upload from "./upload.svg";
 
 export const SvgCollection: any = {
 	ark,
 	btc,
+	chevronDown,
 	close,
 	crossSlim,
 	eth,
-	qrcode,
-	settings,
-	upload,
+	eye,
+	eyeOff,
 	general,
+	msq,
+	qrcode,
 	peer,
 	plugin,
-	eyeOff,
-	eye,
-	chevronDown,
+	settings,
+	upload,
 };
