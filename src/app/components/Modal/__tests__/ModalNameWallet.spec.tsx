37bd8500397459297eadcdf02308f11bb78995a6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-09 16:47:09 +0100

    test: name wallet modal

---
+++ src/app/components/Modal/__tests__/ModalNameWallet.spec.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { IntlProvider } from "react-intl";
+import { render } from "@testing-library/react";
+
+import { ModalNameWallet } from "../";
+// i18n
+import { locales } from "i18n/locales";
+
+describe("ModalNameWallet", () => {
+	beforeEach(() => {
+		jest.spyOn(console, "error").mockImplementation(() => null);
+	});
+
+	it("should not render if not open", () => {
+		const { asFragment, getByTestId } = render(
+			<IntlProvider locale="en-US" messages={locales["en-US"].messages}>
+				<ModalNameWallet isOpen={false} />
+			</IntlProvider>,
+		);
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal", () => {
+		const { asFragment, getByTestId } = render(
+			<IntlProvider locale="en-US" messages={locales["en-US"].messages}>
+				<ModalNameWallet isOpen={true} />,
+			</IntlProvider>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent("WALLET.MODAL_NAME_WALLET.TITLE");
+		expect(getByTestId("modal__inner")).toHaveTextContent("WALLET.MODAL_NAME_WALLET.DESCRIPTION");
+		expect(getByTestId("modal__inner")).toHaveTextContent("WALLET.MODAL_NAME_WALLET.FIELD_NAME");
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
