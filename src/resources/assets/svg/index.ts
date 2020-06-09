d1b3724fee087be63385ae27fbf25c6840ba8be6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-09 21:23:23 +0100

    chore: update svg export

--- src/resources/assets/svg/index.ts
+++ src/resources/assets/svg/index.ts
@@ -1,31 +1,39 @@
 import ark from "./ark.svg";
 import btc from "./btc.svg";
+import chevronDown from "./chevron-down.svg";
 import close from "./close.svg";
 import crossSlim from "./cross-slim.svg";
+import discord from "./discord.svg";
 import eth from "./eth.svg";
-import qrcode from "./qrcode.svg";
-import settings from "./settings.svg";
-import upload from "./upload.svg";
+import eyeOff from "./eye-off.svg";
+import eye from "./eye.svg";
 import general from "./general.svg";
 import peer from "./peer.svg";
 import plugin from "./plugin.svg";
-import eyeOff from "./eye-off.svg";
-import eye from "./eye.svg";
-import chevronDown from "./chevron-down.svg";
+import qrcode from "./qrcode.svg";
+import send from "./send.svg";
+import settings from "./settings.svg";
+import slack from "./slack.svg";
+import twitter from "./twitter.svg";
+import upload from "./upload.svg";
 
 export const SvgCollection: any = {
 	ark,
 	btc,
+	chevronDown,
 	close,
 	crossSlim,
+	discord,
 	eth,
-	qrcode,
-	settings,
-	upload,
+	eyeOff,
+	eye,
 	general,
 	peer,
 	plugin,
-	eyeOff,
-	eye,
-	chevronDown,
+	qrcode,
+	send,
+	settings,
+	slack,
+	twitter,
+	upload,
 };
