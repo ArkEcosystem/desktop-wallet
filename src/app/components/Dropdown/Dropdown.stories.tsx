92a257c3ee4ebc9352decaf7d28f9f3b0f1c14ab
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-06-09 10:54:01 +0300

    feat: component dropdown (#2064)

---
+++ src/app/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Dropdown } from "./Dropdown";
+
+export default {
+	title: "Components / Dropdown",
+};
+
+export const Default = () => {
+	const options = [
+		{ label: "Option 1", value: "1" },
+		{ label: "Option 2", value: "2" },
+		{ label: "Option 3", value: "4" },
+		{ label: "Option 4", value: "4" },
+	];
+
+	return (
+		<div className="">
+			Default with options array
+			<div className="w-20 ml-40 mt-10">
+				<Dropdown
+					toggleIcon="settings"
+					options={options}
+					onSelect={(option: any) => console.log(option)}
+				></Dropdown>
+			</div>
+		</div>
+	);
+};
+
+export const ContentAsSlot = () => {
+	return (
+		<div className="">
+			Custom html dropdown content
+			<div className="w-20 ml-40 mt-10">
+				<Dropdown toggleIcon="settings" onSelect={(option: any) => console.log(option)}>
+					<div className="p-5">Custom content (default slot)</div>
+				</Dropdown>
+			</div>
+		</div>
+	);
+};
+
+export const CustomToggleIcon = () => {
+	return (
+		<div className="">
+			Custom toggle svg icon:
+			<div className="w-20 ml-40 mt-10">
+				<Dropdown toggleIcon="upload" onSelect={(option: any) => console.log(option)}>
+					<div className="p-5">Custom content (default slot)</div>
+				</Dropdown>
+			</div>
+		</div>
+	);
+};
