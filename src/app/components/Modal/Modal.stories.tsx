4b8e3eb767f7906a4c4dac308fcee048fadcd95a
Breno Polanski <breno.polanski@gmail.com>
2020-06-08 23:30:29 -0300

    fix: responsive welcome page and some tweaks (#2063)

--- src/app/components/Modal/Modal.stories.tsx
+++ src/app/components/Modal/Modal.stories.tsx
@@ -8,21 +8,23 @@ export default {
 	decorators: [withKnobs],
 };
 
-export const Default = () => <Modal
-	title={text("Title", "Title")}
-	description={text("Description", "Description")}
-	isOpen={boolean("Is Open", true)}
-	onClick={() => alert('closed')}
->
-	<div>{text("Content", "Modal Content")}</div>
+export const Default = () => (
+	<Modal
+		title={text("Title", "Title")}
+		description={text("Description", "Description")}
+		isOpen={boolean("Is Open", true)}
+		onClick={() => alert("closed")}
+	>
+		<div>{text("Content", "Modal Content")}</div>
 
-	<div className="mt-4">
-		<Button color="primary" variant="plain" onClick={() => alert("cancel")} className="mr-2">
-			Cancel
-		</Button>
+		<div className="mt-4">
+			<Button color="primary" variant="plain" onClick={() => alert("cancel")} className="mr-2">
+				Cancel
+			</Button>
 
-		<Button color="primary" variant="solid" onClick={() => alert("continue")}>
-			Continue
-		</Button>
-	</div>
-</Modal>;
+			<Button color="primary" variant="solid" onClick={() => alert("continue")}>
+				Continue
+			</Button>
+		</div>
+	</Modal>
+);
