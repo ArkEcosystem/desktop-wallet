886c3f2e67ffa2f7f611127683f5bb879cb98635
brenopolanski <breno.polanski@gmail.com>
2020-06-10 17:43:21 -0300

    feat: add breadcrumbs and arrow-back icon

---
+++ src/app/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type BreadcrumbsProps = {
+	crumbs: any;
+};
+
+export const Breadcrumbs = ({ crumbs }: BreadcrumbsProps) => {
+	return <div></div>;
+};
+
+Breadcrumbs.defaultProps = {
+	crumbs: [],
+};
