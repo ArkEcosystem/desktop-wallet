8eb8f4fc7c963b975f95246db6a79fa6770691c5
Breno Polanski <breno.polanski@gmail.com>
2020-06-09 22:20:25 -0300

    fix: update create profile page (#2081)

--- src/domains/profile/pages/CreateProfile/__tests__/CreateProfile.spec.tsx
+++ src/domains/profile/pages/CreateProfile/__tests__/CreateProfile.spec.tsx
@@ -1,21 +1,13 @@
 import React from "react";
-import { IntlProvider } from "react-intl";
 import { render } from "@testing-library/react";
 
 import { CreateProfile } from "../";
 
 describe("CreateProfile", () => {
 	it("should render", () => {
-		const { container, asFragment, getByTestId, getAllByTestId } = render(
-			<IntlProvider locale="en-US">
-				<CreateProfile />
-			</IntlProvider>,
-		);
+		const { container, asFragment } = render(<CreateProfile />);
 
 		expect(container).toBeTruthy();
-		expect(getByTestId("list-divided__items")).toBeTruthy();
-		expect(getByTestId("create-profile__form")).toBeTruthy();
-		expect(getAllByTestId("list-divided-item__inner-wrapper").length).toEqual(1);
 		expect(asFragment()).toMatchSnapshot();
 	});
 });
