7ce8c494b05c5dead820ecdf03cba6a6fecfb04e
brenopolanski <breno.polanski@gmail.com>
2020-06-10 14:50:06 -0300

    chore: add image for profile created modal

--- src/domains/profile/images/index.tsx
+++ src/domains/profile/images/index.tsx
@@ -1,6 +1,12 @@
 import { ReactComponent as WelcomeBanner } from "./welcome-banner.svg";
+import { ReactComponent as ProfileCreated } from "./profile-created.svg";
 
 export const profile = {
+	components: {
+		profileCreated: {
+			ProfileCreated,
+		},
+	},
 	pages: {
 		welcome: {
 			WelcomeBanner,
