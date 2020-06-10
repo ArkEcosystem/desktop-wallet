8a71be6661a6ce5c666ea75975fb1f3c8c0e8148
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-06-10 04:22:54 +0300

    feat: add wallet card component (#2076)

---
+++ src/app/components/WalletCard/WalletCard.stories.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { WalletCard } from "./WalletCard";
+
+export default {
+	title: "Components / Wallet Card",
+};
+
+export const Default = () => {
+	return (
+		<div className="">
+			<div className="mb-10">
+				<WalletCard
+					coinIcon="eth"
+					coinIconClass="border-theme-neutral-800"
+					avatarId="test"
+					walletName="Primary"
+					address="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
+					balance="100,000 ETH"
+					actions={[
+						{ label: "Action 1", value: "1" },
+						{ label: "Action 1", value: "1" },
+					]}
+					onSelect={(selected: any) => console.log(selected)}
+				/>
+			</div>
+			<div className="mb-10">
+				<WalletCard
+					className="mr-5"
+					coinIcon="eth"
+					coinIconClass="border-theme-neutral-800"
+					avatarId="test"
+					walletName="Secondary"
+					address="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
+					balance="100,000 ETH"
+					actions={[
+						{ label: "Action 1", value: "1" },
+						{ label: "Action 1", value: "1" },
+					]}
+				/>
+				<WalletCard
+					coinIcon="eth"
+					coinIconClass="border-theme-neutral-800"
+					avatarId="test"
+					walletName="My wallet"
+					address="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
+					balance="100,000 ETH"
+					actions={[
+						{ label: "Action 1", value: "1" },
+						{ label: "Action 1", value: "1" },
+					]}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export const Blank = () => {
+	return (
+		<div className="">
+			<div className="mb-5">
+				<WalletCard isBlank={true} className="mr-4" />
+			</div>
+			<div className="mb-10">
+				<WalletCard isBlank={true} className="mr-4" />
+				<WalletCard isBlank={true} />
+			</div>
+		</div>
+	);
+};
