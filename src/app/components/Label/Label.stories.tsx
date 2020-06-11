878ce3690b04e9848736f1aa1d41df0233fe06e3
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-11 23:30:43 +0100

    feat: label component

---
+++ src/app/components/Label/Label.stories.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Label } from "./Label";
+
+export default {
+	title: "Basic / Label",
+};
+
+export const Primary = () => (
+	<div className="space-x-4">
+		<Label color="primary">Label</Label>
+
+		<Label color="primary" size="large">
+			Large Label
+		</Label>
+	</div>
+);
+
+export const Success = () => (
+	<div className="space-x-4">
+		<Label color="success">Label</Label>
+
+		<Label color="success" size="large">
+			Large Label
+		</Label>
+	</div>
+);
+
+export const Danger = () => (
+	<div className="space-x-4">
+		<Label color="danger">Label</Label>
+
+		<Label color="danger" size="large">
+			Large Label
+		</Label>
+	</div>
+);
+
+export const Warning = () => (
+	<div className="space-x-4">
+		<Label color="warning">Label</Label>
+
+		<Label color="warning" size="large">
+			Large Label
+		</Label>
+	</div>
+);
+
+export const Neutral = () => (
+	<div className="space-x-4">
+		<Label color="neutral">Label</Label>
+
+		<Label color="neutral" size="large">
+			Large Label
+		</Label>
+	</div>
+);
