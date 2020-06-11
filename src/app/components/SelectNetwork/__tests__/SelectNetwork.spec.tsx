53d34a51c3c365839d3320299cfe75c03bafd7f9
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-06-11 04:48:15 +0300

    feat: add SelectNetwork component (#2092)

---
+++ src/app/components/SelectNetwork/__tests__/SelectNetwork.spec.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { act } from "@testing-library/react-hooks";
+
+import { SelectNetwork } from "../SelectNetwork";
+
+describe("SelectNetwork", () => {
+	const networks = [
+		{
+			name: "Ark",
+			isSelected: false,
+		},
+		{
+			name: "Eth",
+			isSelected: true,
+		},
+		{
+			name: "Btc",
+			isSelected: false,
+		},
+	];
+
+	it("should render", () => {
+		const { container } = render(<SelectNetwork />);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("should render with icon", () => {
+		const { container } = render(<SelectNetwork networks={networks} />);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("should call onChange callback upon change", () => {
+		const onChange = jest.fn();
+		const { getByTestId } = render(<SelectNetwork networks={networks} onChange={onChange}></SelectNetwork>);
+		const firstNetwork = getByTestId("network__option--0");
+
+		act(() => {
+			fireEvent.click(firstNetwork);
+		});
+
+		expect(onChange).toBeCalled();
+	});
+
+	it("should ignore onChange callback upon selection if not provided", () => {
+		const onChange = jest.fn();
+		const { getByTestId } = render(<SelectNetwork networks={networks}></SelectNetwork>);
+		const firstNetwork = getByTestId("network__option--0");
+
+		act(() => {
+			fireEvent.click(firstNetwork);
+		});
+
+		expect(onChange).not.toHaveBeenCalled();
+	});
+});
