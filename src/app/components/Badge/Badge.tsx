f53b4b136874d77ddda910f02bc09df63157c34a
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-06-11 04:38:31 +0300

    feat: add Badge component (#2085)

---
+++ src/app/components/Badge/Badge.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Icon } from "../Icon";
+import { Wrapper, defaultClasses } from "./style";
+
+type BadgeProps = {
+	className?: string;
+	children?: React.ReactNode;
+	icon?: string;
+	iconWidth?: number;
+	iconHeight?: number;
+};
+
+export const Badge = ({ className, children, icon, iconWidth, iconHeight }: BadgeProps) => {
+	return (
+		<Wrapper className={`${defaultClasses} ${className}`}>
+			{!!icon && <Icon name={icon} width={iconWidth} height={iconHeight}></Icon>}
+			<span>{children}</span>
+		</Wrapper>
+	);
+};
+
+Badge.defaultProps = {
+	iconWidth: 12,
+	iconHeight: 12,
+};
