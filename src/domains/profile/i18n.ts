4f65e718f1531f7a14e32cce3fa211b9f69ba606
brenopolanski <breno.polanski@gmail.com>
2020-06-11 10:22:27 -0300

    chore: update i18n

---
+++ src/domains/profile/i18n.ts
@@ -0,0 +1,9 @@
+export const translations: { [key: string]: any } = {
+	MODAL_PROFILE_CREATED: {
+		TITLE: "Profile Created!",
+		DESCRIPTION_1: "If you are new to the ARK Desktop Wallet, view our tutorial to get started.",
+		DESCRIPTION_2: "If not, you can skip the tutorial to go directly to your portfolio.",
+		START_TUTORIAL: "Start Tutorial",
+		SKIP_TUTORIAL: "Skip Tutorial",
+	},
+};
