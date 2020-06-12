4a1efe15b4a0469685800fcca816d3983f18665b
brenopolanski <breno.polanski@gmail.com>
2020-06-12 18:07:28 -0300

    refactor: update story

--- src/domains/wallets/components/WalletUpdateModal/WalletUpdate.stories.tsx
+++ src/domains/wallets/components/WalletUpdateModal/WalletUpdate.stories.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { boolean } from "@storybook/addon-knobs";
 import { WalletUpdate } from "./";
 
 export default { title: "Wallets / Components / Wallet Update" };
 
-export const Default = () => (
-	<WalletUpdate
-		isOpen={boolean("Is Open", true)}
-		onClose={() => alert("closed")}
-		onCancel={() => alert("cancelled")}
-		onUpdate={() => alert("updated")}
-	/>
-);
+export const Default = () => {
+	const [isUpdate, setIsUpdate] = useState(false);
+	const [isReady, setIsReady] = useState(false);
+
+	useEffect(() => {
+		if (isUpdate) {
+			setTimeout(() => {
+				setIsReady(true);
+				setIsUpdate(false);
+			}, 2000);
+		}
+	}, [isUpdate]);
+
+	return (
+		<WalletUpdate
+			isOpen={boolean("Is Open", true)}
+			isUpdate={isUpdate}
+			isReady={isReady}
+			onClose={() => alert("closed")}
+			onCancel={() => alert("cancelled")}
+			onUpdate={() => setIsUpdate(true)}
+		/>
+	);
+};
