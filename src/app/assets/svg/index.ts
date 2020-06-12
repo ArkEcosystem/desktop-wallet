ddcd25404cc04fe74a057f507a48a24bcd47d25a
Brian Faust <hello@basecode.sh>
2020-06-12 14:12:18 +0300

    wip

--- src/app/assets/svg/index.ts
+++ src/app/assets/svg/index.ts
@@ -1,10 +1,12 @@
 import { ReactComponent as Ark } from "./ark.svg";
 import { ReactComponent as ArrowBack } from "./arrow-back.svg";
+import { ReactComponent as ArrowDown } from "./arrowdown.svg";
+import { ReactComponent as ArrowUp } from "./arrowup.svg";
 import { ReactComponent as Btc } from "./btc.svg";
-import { ReactComponent as Confirmed } from "./confirmed.svg";
 import { ReactComponent as Checkmark } from "./checkmark.svg";
 import { ReactComponent as ChevronDown } from "./chevron-down.svg";
 import { ReactComponent as Close } from "./close.svg";
+import { ReactComponent as Confirmed } from "./confirmed.svg";
 import { ReactComponent as Copy } from "./copy.svg";
 import { ReactComponent as CrossSlim } from "./cross-slim.svg";
 import { ReactComponent as Delegate } from "./delegate.svg";
@@ -14,43 +16,42 @@ import { ReactComponent as Eth } from "./eth.svg";
 import { ReactComponent as Eye } from "./eye.svg";
 import { ReactComponent as EyeOff } from "./eye-off.svg";
 import { ReactComponent as FilePassword } from "./file-password.svg";
+import { ReactComponent as Filters } from "./filters.svg";
 import { ReactComponent as General } from "./general.svg";
+import { ReactComponent as Grid } from "./grid.svg";
+import { ReactComponent as Import } from "./import.svg";
+import { ReactComponent as Ledger } from "./ledger.svg";
+import { ReactComponent as List } from "./list.svg";
 import { ReactComponent as Mistake } from "./mistake.svg";
 import { ReactComponent as Msq } from "./msq.svg";
+import { ReactComponent as Multisig } from "./multisig.svg";
 import { ReactComponent as Peer } from "./peer.svg";
 import { ReactComponent as Plugin } from "./plugin.svg";
+import { ReactComponent as Plus } from "./plus.svg";
 import { ReactComponent as Qrcode } from "./qrcode.svg";
 import { ReactComponent as Received } from "./received.svg";
 import { ReactComponent as Reset } from "./reset.svg";
 import { ReactComponent as Send } from "./send.svg";
+import { ReactComponent as Sent } from "./sent.svg";
 import { ReactComponent as Settings } from "./settings.svg";
 import { ReactComponent as Slack } from "./slack.svg";
 import { ReactComponent as Smartbridge } from "./smartbridge.svg";
+import { ReactComponent as Sort } from "./sort.svg";
+import { ReactComponent as Star } from "./star.svg";
 import { ReactComponent as Trash } from "./trash.svg";
 import { ReactComponent as Twitter } from "./twitter.svg";
 import { ReactComponent as Upload } from "./upload.svg";
-import { ReactComponent as Plus } from "./plus.svg";
-import { ReactComponent as Grid } from "./grid.svg";
-import { ReactComponent as List } from "./list.svg";
-import { ReactComponent as Filters } from "./filters.svg";
-import { ReactComponent as Import } from "./import.svg";
-import { ReactComponent as Sort } from "./sort.svg";
-import { ReactComponent as ArrowUp } from "./arrowup.svg";
-import { ReactComponent as ArrowDown } from "./arrowdown.svg";
-import { ReactComponent as Ledger } from "./ledger.svg";
-import { ReactComponent as Multisig } from "./multisig.svg";
-import { ReactComponent as Star } from "./star.svg";
-import { ReactComponent as Sent } from "./sent.svg";
-import { ReactComponent as Received } from "./received.svg";
 
 export const SvgCollection: any = {
 	Ark,
 	ArrowBack,
+	ArrowDown,
+	ArrowUp,
 	Btc,
-	Confirmed,
 	Checkmark,
 	ChevronDown,
 	Close,
+	Confirmed,
 	Copy,
 	CrossSlim,
 	Delegate,
@@ -60,32 +61,29 @@ export const SvgCollection: any = {
 	Eye,
 	EyeOff,
 	FilePassword,
+	Filters,
 	General,
+	Grid,
+	Import,
+	Ledger,
+	List,
 	Mistake,
 	Msq,
+	Multisig,
 	Peer,
 	Plugin,
+	Plus,
 	Qrcode,
 	Received,
 	Reset,
 	Send,
+	Sent,
 	Settings,
 	Slack,
 	Smartbridge,
+	Sort,
+	Star,
 	Trash,
 	Twitter,
 	Upload,
-	Plus,
-	Grid,
-	List,
-	Filters,
-	Import,
-	Sort,
-	ArrowUp,
-	ArrowDown,
-	Ledger,
-	Multisig,
-	Star,
-	Sent,
-	Received,
 };
