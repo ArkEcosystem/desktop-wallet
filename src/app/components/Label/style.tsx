2a88beae70dc6e9a8eb07a5867ec1178f7485352
goga-m <goga.mamoulasvili@gmail.com>
2020-06-12 11:17:38 +0300

    chore: modify `Label` styling

--- src/app/components/Label/style.tsx
+++ src/app/components/Label/style.tsx
@@ -3,16 +3,22 @@ import tw, { css } from "twin.macro";
 const baseStyle = tw`inline-block font-semibold`;
 
 const getColors = (name: string): any => {
+	const color = `var(--theme-color-${name}-500)`;
+	const bg = `var(--theme-color-${name}-200)`;
+
 	return css`
-		color: var(--theme-color- ${name}-700);
-		background-color: var(--theme-color- ${name}-300);
+		color: ${color};
+		background-color: ${bg};
+		border-radius: 3px;
 	`;
 };
 
 const getSize = (size: string): any => {
 	switch (size) {
-		case "default":
+		case "small":
 			return tw`px-1 text-base rounded-sm`;
+		case "default":
+			return tw`px-2 text-base rounded-sm`;
 		case "large":
 			return tw`px-2 text-lg rounded`;
 	}
