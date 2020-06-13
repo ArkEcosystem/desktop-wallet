3fe0183515ca9632e3d50cfcec2e18ef1b60bf76
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-06-13 15:05:38 -0300

    feat: add `Range` component

---
+++ src/app/components/Range/Range.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Range } from "./Range";
+
+export default { title: "Basic / Range" };
+
+export const Default = () => {
+	const [values, setValues] = React.useState([10]);
+	return (
+		<div className="max-w-lg">
+			<Range values={values} min={0} max={100} step={0.1} onChange={setValues} />
+		</div>
+	);
+};
