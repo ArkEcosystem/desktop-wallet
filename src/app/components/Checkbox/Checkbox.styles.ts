2744694cb90358c66a7760d67247edc5e1270c14
brenopolanski <breno.polanski@gmail.com>
2020-06-14 15:06:46 -0300

    test: add error boundary and tests

--- src/app/components/Checkbox/Checkbox.styles.ts
+++ src/app/components/Checkbox/Checkbox.styles.ts
@@ -5,6 +5,10 @@ const baseStyle = [tw`w-4 h-4 transition duration-150 ease-in-out form-checkbox`
 const getColor = (color: string) => {
 	const colorBase = `var(--theme-color-${color})`;
 
+	if (!["primary", "success", "danger", "warning"].includes(color)) {
+		throw new Error(`Failed to find a color for "${color}"`);
+	}
+
 	return css`
 		color: ${colorBase};
 	`;
--- src/app/components/Checkbox/__tests__/Checkbox.test.tsx
+++ src/app/components/Checkbox/__tests__/Checkbox.test.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { render } from "@testing-library/react";
-
+import { ErrorBoundary } from "react-error-boundary";
 import { Checkbox } from "../Checkbox";
 
 describe("Checkbox", () => {
+	beforeAll(() => {
+		jest.spyOn(console, "error").mockImplementation(() => null);
+	});
+
 	it("should render", () => {
 		const { container } = render(<Checkbox />);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	it("should render if something went wrong", () => {
+		const ErrorFallback = ({ error }: any) => {
+			return (
+				<div role="alert">
+					<p>Something went wrong: </p>
+					<pre>{error.message}</pre>
+				</div>
+			);
+		};
+
+		const { getByRole } = render(
+			<ErrorBoundary FallbackComponent={ErrorFallback}>
+				<Checkbox color="foo" />
+			</ErrorBoundary>,
+		);
+
+		expect(getByRole("alert").textContent).toMatchInlineSnapshot(
+			`"Something went wrong: Failed to find a color for \\"foo\\""`,
+		);
 	});
 
 	it("should render a primary color", () => {
