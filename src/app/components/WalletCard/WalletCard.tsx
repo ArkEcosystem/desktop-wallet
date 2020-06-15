baf6ed1f73d0a1c6c8d7596f435abcfca9452686
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-06-15 09:25:53 +0300

    feat: add dashboard `Wallets` component (#2133)

--- src/app/components/WalletCard/WalletCard.tsx
+++ src/app/components/WalletCard/WalletCard.tsx
@@ -17,6 +17,7 @@ type WalletCardProps = {
 	address?: string;
 	balance?: string;
 	actions?: any;
+	walletTypeIcons?: any[];
 	onSelect?: any;
 };
 
@@ -38,18 +39,26 @@ export const WalletCard = ({
 	coinIconClass,
 	actions,
 	onSelect,
+	walletTypeIcons,
 }: WalletCardProps) => {
+	const getIconTypeClass = (icon: string) => {
+		if (icon === "Star") return "text-theme-warning-400";
+		return "text-theme-primary-300";
+	};
+
 	if (isBlank) {
 		return (
 			<div className={`w-64 inline-block ${className}`}>
 				<Card>
-					<div>
-						<Circle className="-mr-2 bg-theme-background border-theme-primary-contrast"></Circle>
-						<Circle className="border-theme-primary-contrast bg-theme-background"></Circle>
-					</div>
+					<div className="px-1 py-2">
+						<div className="mt-1">
+							<Circle className="-mr-2 border-theme-primary-contrast"></Circle>
+							<Circle className="border-theme-primary-contrast"></Circle>
+						</div>
 
-					<div className="mt-4 text-sm text-theme-primary-contrast">{blankTitle}</div>
-					<div className="mt-1 text-sm text-theme-primary-contrast">{blankSubtitle}</div>
+						<div className="mt-4 text-md text-theme-primary-contrast">{blankTitle}</div>
+						<div className="mt-1 text-md text-theme-primary-contrast">{blankSubtitle}</div>
+					</div>
 				</Card>
 			</div>
 		);
@@ -58,21 +67,32 @@ export const WalletCard = ({
 	return (
 		<div className={`w-64 inline-block ${className}`}>
 			<Card>
-				<div>
-					<Dropdown options={actions} onSelect={onSelect}></Dropdown>
-					<div></div>
-				</div>
-				<div>
-					<Circle className={`border-theme-primary-contrast -mr-2 bg-theme-background ${coinIconClass}`}>
-						{renderCoin(coinIcon)}
-					</Circle>
-					<Circle avatarId={avatarId} className="border-theme-primary-contrast"></Circle>
-				</div>
+				<div className="relative px-1 py-2">
+					<div className="-mr-2">
+						<Dropdown options={actions} onSelect={onSelect}></Dropdown>
+					</div>
+					<div className="absolute right-3">
+						{walletTypeIcons &&
+							walletTypeIcons.map((type: string, index: number) => {
+								return (
+									<div key={index} className={`inline-block mr-2 text ${getIconTypeClass(type)}`}>
+										<Icon name={type} />
+									</div>
+								);
+							})}
+					</div>
+					<div className="mt-1">
+						<Circle className={`border-theme-primary-contrast -mr-2 ${coinIconClass}`}>
+							{renderCoin(coinIcon)}
+						</Circle>
+						<Circle avatarId={avatarId} className="border-theme-primary-contrast"></Circle>
+					</div>
 
-				<div className="mt-4 truncate max-w-12">
-					<Address walletName={walletName} address={address} maxChars={13}></Address>
+					<div className="mt-4 truncate max-w-12">
+						<Address walletName={walletName} address={address} maxChars={13}></Address>
+					</div>
+					<div className="font-bold text-theme-neutral-800">{balance}</div>
 				</div>
-				<div className="font-bold text-theme-neutral-800">{balance}</div>
 			</Card>
 		</div>
 	);
---
+++ src/domains/dashboard/components/Wallets/Wallets.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+
+import { Table } from "app/components/Table";
+import { Slider } from "app/components/Slider";
+import { WalletsControls } from "domains/dashboard/components/WalletsControls";
+import { WalletListItem } from "app/components/WalletListItem";
+import { WalletCard } from "app/components/WalletCard";
+import { Button } from "app/components/Button";
+
+type WalletsProps = {
+	wallets?: any;
+	title?: string;
+	filterProperties: any;
+	viewType?: "grid" | "list";
+	listColumns?: any;
+	onCreateWallet?: any;
+	onImportWallet?: any;
+	onWalletAction?: any;
+};
+
+export const Wallets = ({
+	viewType,
+	title,
+	wallets,
+	filterProperties,
+	listColumns,
+	onCreateWallet,
+	onImportWallet,
+	onWalletAction,
+}: WalletsProps) => {
+	const [walletsViewType, setWalletsViewType] = useState(viewType);
+
+	const walletSliderOptions = {
+		slideHeight: 210, // Wallet card height, including margin-bottom
+		slidesPerView: 4,
+		slidesPerColumn: 2,
+
+		// Responsive breakpoints
+		breakpoints: {
+			// 	// when window width is >= 320px
+			320: {
+				slidesPerView: 1,
+				slidesPerColumn: 2,
+			},
+			600: {
+				slidesPerView: 2,
+				slidesPerColumn: 2,
+			},
+			860: {
+				slidesPerView: 3,
+				slidesPerColumn: 2,
+			},
+			1140: {
+				slidesPerView: 4,
+				slidesPerColumn: 2,
+			},
+			1320: {
+				slidesPerView: 5,
+				slidesPerColumn: 2,
+			},
+		},
+	};
+
+	// Pad with empty cards to fill the row
+	const walletsGridData = (wallets: any, walletsPerPage: number) => {
+		if (wallets.length < walletsPerPage) {
+			const blankWalletsLength = walletsPerPage - wallets.length;
+			const blankWalletsCards = new Array(blankWalletsLength).fill({ isBlank: true });
+			return [...wallets, ...blankWalletsCards];
+		}
+		return wallets;
+	};
+
+	return (
+		<div className="p-10 bg-theme-background">
+			<div className="flex w-full pb-4">
+				<h2 className="w-2/4 mt-1">{title}</h2>
+				<div className="w-2/4 text-right">
+					<WalletsControls
+						onCreateWallet={onCreateWallet}
+						onImportWallet={onImportWallet}
+						onSelectGridView={() => setWalletsViewType("grid")}
+						onSelectListView={() => setWalletsViewType("list")}
+						filterProperties={filterProperties}
+						viewType={walletsViewType}
+					></WalletsControls>
+				</div>
+			</div>
+			<div className="mt-1">
+				{walletsViewType === "grid" && (
+					<Slider
+						data={walletsGridData(wallets, walletSliderOptions.slidesPerView)}
+						options={walletSliderOptions}
+					>
+						{(walletData: any) => <WalletCard {...walletData} onSelect={onWalletAction} />}
+					</Slider>
+				)}
+				{walletsViewType === "list" && (
+					<div>
+						<Table columns={listColumns} data={wallets}>
+							{(rowData: any) => <WalletListItem {...rowData}></WalletListItem>}
+						</Table>
+
+						<Button color="primary" variant="plain" className="w-full mt-10 mb-5">
+							See more
+						</Button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+Wallets.defaultProps = {
+	title: "Wallets",
+	networks: [],
+	wallets: [],
+	viewType: "grid",
+	listColumns: [
+		{
+			Header: "Asset Type",
+			accessor: "avatarId",
+		},
+		{
+			Header: "Wallet Address",
+			accessor: "address",
+		},
+		{
+			Header: "Wallet Type",
+		},
+		{
+			Header: "Balance",
+			accessor: "balance",
+			className: "float-right",
+		},
+		{
+			Header: "Fiat Value",
+			accessor: "fiat",
+			className: "float-right",
+		},
+	],
+};
