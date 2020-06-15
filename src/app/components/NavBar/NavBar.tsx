f2681db5420116674b27b97309ac3a61d0614d59
goga-m <goga.mamoulasvili@gmail.com>
2020-06-15 21:03:17 +0300

    feat: add `NavBar` UI elements

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,18 +1,145 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import tw, { styled } from "twin.macro";
+
 import { images } from "app/assets/images";
+import { defaultStyle } from "./style";
+
+import { Circle } from "app/components/Circle";
+import { Icon } from "app/components/Icon";
+import { Badge } from "app/components/Badge";
+import { Dropdown } from "app/components/Dropdown";
 
 const commonAssets = images.common;
 
-export const NavBar = () => (
-	<nav className="relative bg-white shadow-header-smooth">
+type NavbarProps = {
+	menu?: any;
+	userActions?: any;
+	currencyIcon?: any;
+	onUserAction?: any;
+};
+
+const NavWrapper = styled.nav`
+	${defaultStyle}
+	${tw`bg-white shadow-md sticky inset-x-0 top-0`}
+`;
+
+export const NavBar = ({ menu, userActions, onUserAction, currencyIcon }: NavbarProps) => (
+	<NavWrapper aria-labelledby="main menu">
 		<div className="px-4 sm:px-6 lg:px-8">
 			<div className="relative flex justify-between h-20 md:h-24">
 				<div className="flex items-center flex-shrink-0">
-					<div className="flex p-2 rounded-lg bg-logo">
+					<div className="flex p-2 rounded-lg bg-logo mr-4">
 						<img src={commonAssets.ARKLogo} className="h-6 md:h-8 lg:h-10" alt="ARK Logo" />
+					</div>
+					<ul className="flex h-20 md:h-24">
+						{menu.map((menuItem: any, index: number) => (
+							<li key={index} className="flex">
+								<NavLink
+									to={menuItem.path}
+									title={menuItem.title}
+									className="flex items-center mx-4 text-md font-bold text-theme-neutral-500"
+								>
+									{menuItem.title}
+								</NavLink>
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<div className="flex items-center flex-shrink-0">
+					<div className="flex h-full items-center text-theme-primary-300 px-6 cursor-pointer">
+						<Icon name="Notification" width={22} height={22} />
+					</div>
+
+					<div className="border-r h-8 border-theme-neutral-200"></div>
+					<div className="flex h-full items-center text-theme-primary-300 -mt-1 px-3 cursor-pointer">
+						<Icon name="Sent" width={42} height={42} />
+					</div>
+
+					<div className="border-r h-8 border-theme-neutral-200"></div>
+					<div className="flex h-full items-center text-theme-primary-300 px-6 cursor-pointer">
+						<Icon name="Receive" width={22} height={22} />
+					</div>
+
+					<div className="border-r h-8 border-theme-neutral-200"></div>
+
+					<div className="p-2 text-right ml-4">
+						<div className="text-xs text-theme-neutral-500">Your balance</div>
+						<div className="text-sm text-theme-neutral-700 font-bold">34,253.75</div>
+					</div>
+
+					<div className="flex p-1 cusror-pointer">
+						<Dropdown
+							onSelect={onUserAction}
+							options={userActions}
+							toggleContent={(isOpen: boolean) => (
+								<div className="cursor-pointer" data-testid="navbar__useractions">
+									<Circle className="border-theme-neutral-300 -mr-1" size="large">
+										<span className="text-theme-neutral-600">
+											<Icon name={currencyIcon}></Icon>
+										</span>
+									</Circle>
+									<Circle
+										className="rotate-90 bg-theme-primary-600 border-theme-primary-600 relative"
+										size="large"
+									>
+										<span className="text-theme-background text-sm">IO</span>
+										<Badge
+											className={`transform ${
+												isOpen ? "rotate-180" : ""
+											} bg-theme-primary-100 border-theme-primary-100 bottom-2 -right-4 text-theme-primary-500`}
+											icon="ChevronDown"
+											iconWidth={16}
+											iconHeight={16}
+										/>
+									</Circle>
+								</div>
+							)}
+						></Dropdown>
 					</div>
 				</div>
 			</div>
 		</div>
-	</nav>
+	</NavWrapper>
 );
+
+NavBar.defaultProps = {
+	currencyIcon: "Btc",
+	menu: [
+		{
+			title: "Portfolio",
+			path: "/portfolio",
+		},
+		{
+			title: "Plugins",
+			path: "/plugins",
+		},
+		{
+			title: "Exchange",
+			path: "/exchange",
+		},
+		{
+			title: "News",
+			path: "/news",
+		},
+	],
+	userActions: [
+		{
+			label: "Contacts",
+			value: "contacts",
+		},
+		{
+			label: "Settings",
+			value: "settings",
+		},
+		{
+			label: "Support",
+			value: "support",
+		},
+		{
+			label: "Exit",
+			value: "exit",
+		},
+	],
+};
--- src/app/components/NavBar/__tests__/NavBar.test.tsx
+++ src/app/components/NavBar/__tests__/NavBar.test.tsx
@@ -1,13 +1,58 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { NavBar } from "../";
+import { act } from "react-dom/test-utils";
 
 describe("NavBar", () => {
 	it("should render", () => {
-		const { container, asFragment } = render(<NavBar />);
+		const { container, asFragment } = render(
+			<Router>
+				<NavBar />
+			</Router>,
+		);
 
 		expect(container).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render with custom menu", () => {
+		const menu = [
+			{
+				title: "Portfolio",
+				path: "/portfolio",
+			},
+			{
+				title: "test",
+				path: "/test",
+			},
+		];
+		const { container, asFragment } = render(
+			<Router>
+				<NavBar menu={menu} />
+			</Router>,
+		);
+
+		expect(container).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should open user actions dropdown on click", () => {
+		const options = [
+			{ label: "Option 1", value: "1" },
+			{ label: "Option 2", value: -"2" },
+		];
+		const { getByTestId, getByText } = render(
+			<Router>
+				<NavBar userActions={options} />
+			</Router>,
+		);
+		const toggle = getByTestId("navbar__useractions");
+
+		act(() => {
+			fireEvent.click(toggle);
+		});
+		expect(getByText("Option 1")).toBeTruthy();
 	});
 });
