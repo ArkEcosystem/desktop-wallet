e234c6f08430dfbc7cd2f8ed4e631b63244be225
brenopolanski <breno.polanski@gmail.com>
2020-06-15 17:30:38 -0300

    feat: add download step

--- src/domains/plugins/components/InstallPlugin/InstallPlugin.tsx
+++ src/domains/plugins/components/InstallPlugin/InstallPlugin.tsx
@@ -1,38 +1,69 @@
 import React from "react";
 import { Button } from "app/components/Button";
+import { CircularProgressBar } from "app/components/CircularProgressBar";
 import { Modal } from "app/components/Modal";
 
-type InstallPluginProps = {
+type Props = {
+	step: number;
 	isOpen: boolean;
 	onClose?: any;
 	onCancel?: any;
 };
 
-export const InstallPlugin = ({ isOpen, onClose, onCancel }: InstallPluginProps) => {
+export const InstallPlugin = ({ step, isOpen, onClose, onCancel }: Props) => {
 	return (
 		<Modal title="Attention" isOpen={isOpen} onClose={onClose}>
 			<div className="container">
-				<p className="mt-4 text-base font-semibold text-theme-neutral-dark md:text-lg">
-					This plugin needs the following permissions:
-				</p>
-				<ul className="mt-2 text-sm leading-8 list-inside list-circle text-theme-neutral-dark md:text-base">
-					<li>Allows access to the Desktop Wallet alerts</li>
-					<li>Allows access to play audio from within the Desktop Wallet</li>
-					<li>Allows access to the Desktop Wallet events</li>
-				</ul>
-				<div className="flex justify-end mt-8">
-					<Button color="primary" variant="plain" onClick={onCancel} className="mr-2">
-						Cancel
-					</Button>
-					<Button type="submit" color="primary" variant="solid">
-						Download
-					</Button>
-				</div>
+				{step === 1 && (
+					<>
+						<p className="mt-4 text-base font-semibold text-theme-neutral-dark md:text-lg">
+							This plugin needs the following permissions:
+						</p>
+						<ul className="mt-2 text-sm leading-8 list-inside list-circle text-theme-neutral-dark md:text-base">
+							<li>Allows access to the Desktop Wallet alerts</li>
+							<li>Allows access to play audio from within the Desktop Wallet</li>
+							<li>Allows access to the Desktop Wallet events</li>
+						</ul>
+						<div className="flex justify-end mt-8">
+							<Button color="primary" variant="plain" onClick={onCancel} className="mr-2">
+								Cancel
+							</Button>
+							<Button type="submit" color="primary" variant="solid">
+								Download
+							</Button>
+						</div>
+					</>
+				)}
+
+				{step === 2 && (
+					<div className="grid grid-cols-3 gap-1 mt-4">
+						<div>
+							<img
+								className="rounded-xl"
+								src="https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/explorer/master/images/logo.png"
+							/>
+						</div>
+						<div className="flex flex-col justify-around">
+							<span>
+								<p className="text-sm font-semibold text-theme-neutral-light">Plugin</p>
+								<p className="font-semibold text-theme-black">ARK Explorer</p>
+							</span>
+							<span>
+								<p className="text-sm font-semibold text-theme-neutral-light">Downloaded</p>
+								<p className="text-sm font-bold text-theme-neutral-dark">154 KB / 154 KB</p>
+							</span>
+						</div>
+						<div className="flex flex-col justify-end h-full pb-2">
+							<CircularProgressBar value={78} size={50} strokeWidth={4} fontSize={0.8} />
+						</div>
+					</div>
+				)}
 			</div>
 		</Modal>
 	);
 };
 
 InstallPlugin.defaultProps = {
+	step: 2,
 	isOpen: false,
 };
