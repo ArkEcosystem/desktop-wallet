cff10d286ef361d8e8a75c7724986765712b0bf2
Breno Polanski <breno.polanski@gmail.com>
2020-06-15 08:43:32 -0300

    refactor: spinner component (#2138)

--- src/app/components/Checkbox/__tests__/Checkbox.test.tsx
+++ src/app/components/Checkbox/__tests__/Checkbox.test.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Checkbox } from "../Checkbox";
 
 describe("Checkbox", () => {
 	beforeAll(() => {
 		jest.spyOn(console, "error").mockImplementation(() => null);
+	});
+
+	afterAll(() => {
+		console.error.mockRestore();
 	});
 
 	it("should render", () => {
@@ -15,24 +19,41 @@ describe("Checkbox", () => {
 	});
 
 	it("should render if something went wrong", () => {
-		const ErrorFallback = ({ error }: any) => {
+		const ErrorFallback = ({ error, resetErrorBoundary }: any) => {
 			return (
 				<div role="alert">
 					<p>Something went wrong: </p>
 					<pre>{error.message}</pre>
+					<button onClick={resetErrorBoundary}>Try again?</button>
 				</div>
 			);
 		};
 
-		const { getByRole } = render(
+		const { rerender, getByText, queryByText, getByRole, queryByRole } = render(
 			<ErrorBoundary FallbackComponent={ErrorFallback}>
 				<Checkbox color="foo" />
 			</ErrorBoundary>,
 		);
 
+		expect(console.error).toHaveBeenCalledTimes(2);
+
 		expect(getByRole("alert").textContent).toMatchInlineSnapshot(
-			`"Something went wrong: Failed to find a color for \\"foo\\""`,
+			`"Something went wrong: Failed to find a color for \\"foo\\"Try again?"`,
 		);
+
+		console.error.mockClear();
+
+		rerender(
+			<ErrorBoundary FallbackComponent={ErrorFallback}>
+				<Checkbox color="primary" />
+			</ErrorBoundary>,
+		);
+
+		fireEvent.click(getByText(/try again/i));
+
+		expect(console.error).not.toHaveBeenCalled();
+		expect(queryByRole("alert")).not.toBeInTheDocument();
+		expect(queryByText(/try again/i)).not.toBeInTheDocument();
 	});
 
 	it("should render a primary color", () => {
