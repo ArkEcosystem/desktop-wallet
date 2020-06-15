ce42e5a3fe9f5d1ce68a42bcfd5d4163576cd5b4
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-15 12:10:16 -0300

    chore: remove log from context

--- src/app/contexts/Environment.tsx
+++ src/app/contexts/Environment.tsx
@@ -10,14 +10,12 @@ type Props = {
 	children: React.ReactNode;
 };
 
-console.log({ httpClient });
 const EnvironmentContext = createContext({});
 
 const EnvironmentProvider = ({ children }: Props) => {
 	const env: Environment = new Environment({ coins: { ARK }, httpClient, storage: "indexeddb" });
-	console.log({ env });
 
-	return <EnvironmentContext.Provider value={{ injected: true }}>{children}</EnvironmentContext.Provider>;
+	return <EnvironmentContext.Provider value={{ env }}>{children}</EnvironmentContext.Provider>;
 };
 
 export { EnvironmentContext, EnvironmentProvider };
