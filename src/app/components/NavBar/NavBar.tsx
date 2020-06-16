9ffb06202a07944a576e5686735aa6d133040d40
goga-m <goga.mamoulasvili@gmail.com>
2020-06-16 17:26:35 +0300

    feat: integrate `Notifications` in `NavBar`

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -3,6 +3,8 @@ import { Badge } from "app/components/Badge";
 import { Circle } from "app/components/Circle";
 import { Dropdown } from "app/components/Dropdown";
 import { Icon } from "app/components/Icon";
+import { Notifications } from "app/components/Notifications";
+import { Action, NotificationsProps } from "app/components/Notifications/models";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import tw, { styled } from "twin.macro";
@@ -11,11 +13,20 @@ import { defaultStyle } from "./style";
 
 const commonAssets = images.common;
 
+type MenuItem = {
+	title: string;
+	path: string;
+};
+
 type NavbarProps = {
-	menu?: any;
-	userActions?: any;
-	currencyIcon?: any;
+	menu?: MenuItem[];
+	userActions?: Action[];
+	userInitials?: string;
+	currencyIcon: string;
+	balance?: string;
+	notifications?: any;
 	onUserAction?: any;
+	onNotificationAction?: any;
 };
 
 const NavWrapper = styled.nav`
@@ -23,7 +34,73 @@ const NavWrapper = styled.nav`
 	${tw`bg-white shadow-md sticky inset-x-0 top-0`}
 `;
 
-export const NavBar = ({ menu, userActions, onUserAction, currencyIcon }: NavbarProps) => (
+const NotificationsDropdown = ({
+	pluginsHeader,
+	plugins,
+	transactionsHeader,
+	transactions,
+	onAction,
+}: NotificationsProps) => {
+	return (
+		<Dropdown
+			toggleContent={
+				<div className="flex items-center h-full px-6 cursor-pointer text-theme-primary-300">
+					<Icon name="Notification" width={22} height={22} />
+				</div>
+			}
+		>
+			<div className="w-128 p-8 py-3 mt-2">
+				<Notifications
+					pluginsHeader={pluginsHeader}
+					plugins={plugins}
+					transactionsHeader={transactionsHeader}
+					transactions={transactions}
+					onAction={onAction}
+				></Notifications>
+			</div>
+		</Dropdown>
+	);
+};
+
+const UserInfo = ({ onUserAction, currencyIcon, userActions, userInitials }: NavbarProps) => {
+	return (
+		<Dropdown
+			onSelect={onUserAction}
+			options={userActions}
+			toggleContent={(isOpen: boolean) => (
+				<div className="cursor-pointer" data-testid="navbar__useractions">
+					<Circle className="-mr-1 border-theme-neutral-300" size="large">
+						<span className="text-theme-neutral-600">
+							<Icon name={currencyIcon}></Icon>
+						</span>
+					</Circle>
+					<Circle className="relative rotate-90 bg-theme-primary-600 border-theme-primary-600" size="large">
+						<span className="text-sm text-theme-background">{userInitials}</span>
+						<Badge
+							className={`transform ${
+								isOpen ? "rotate-180" : ""
+							} bg-theme-primary-100 border-theme-primary-100 bottom-2 -right-4 text-theme-primary-500`}
+							icon="ChevronDown"
+							iconWidth={16}
+							iconHeight={16}
+						/>
+					</Circle>
+				</div>
+			)}
+		></Dropdown>
+	);
+};
+
+export const NavBar = ({
+	menu,
+	userActions,
+	userInitials,
+	balance,
+	onUserAction,
+	currencyIcon,
+	notifications,
+	onNotificationAction,
+}: NavbarProps) => (
 	<NavWrapper aria-labelledby="main menu">
 		<div className="px-4 sm:px-6 lg:px-8">
 			<div className="relative flex justify-between h-20 md:h-24">
@@ -32,26 +109,25 @@ export const NavBar = ({ menu, userActions, onUserAction, currencyIcon }: Navbar
 						<img src={commonAssets.ARKLogo} className="h-6 md:h-8 lg:h-10" alt="ARK Logo" />
 					</div>
 					<ul className="flex h-20 md:h-24">
-						{menu.map((menuItem: any, index: number) => (
-							<li key={index} className="flex">
-								<NavLink
-									to={menuItem.path}
-									title={menuItem.title}
-									className="flex items-center mx-4 font-bold text-md text-theme-neutral-500"
-								>
-									{menuItem.title}
-								</NavLink>
-							</li>
-						))}
+						{menu &&
+							menu.map((menuItem: any, index: number) => (
+								<li key={index} className="flex">
+									<NavLink
+										to={menuItem.path}
+										title={menuItem.title}
+										className="flex items-center mx-4 font-bold text-md text-theme-neutral-500"
+									>
+										{menuItem.title}
+									</NavLink>
+								</li>
+							))}
 					</ul>
 				</div>
 
-				<div className="flex items-center flex-shrink-0">
-					<div className="flex items-center h-full px-6 cursor-pointer text-theme-primary-300">
-						<Icon name="Notification" width={22} height={22} />
-					</div>
-
+				<div className="flex items-center">
+					<NotificationsDropdown {...notifications} onAction={onNotificationAction}></NotificationsDropdown>
 					<div className="h-8 border-r border-theme-neutral-200"></div>
+
 					<div className="flex items-center h-full px-3 -mt-1 cursor-pointer text-theme-primary-300">
 						<Icon name="Sent" width={42} height={42} />
 					</div>
@@ -65,37 +141,16 @@ export const NavBar = ({ menu, userActions, onUserAction, currencyIcon }: Navbar
 
 					<div className="p-2 ml-4 text-right">
 						<div className="text-xs text-theme-neutral-500">Your balance</div>
-						<div className="text-sm font-bold text-theme-neutral-700">34,253.75</div>
+						<div className="text-sm font-bold text-theme-neutral-700">{balance}</div>
 					</div>
 
 					<div className="flex p-1 cusror-pointer">
-						<Dropdown
-							onSelect={onUserAction}
-							options={userActions}
-							toggleContent={(isOpen: boolean) => (
-								<div className="cursor-pointer" data-testid="navbar__useractions">
-									<Circle className="-mr-1 border-theme-neutral-300" size="large">
-										<span className="text-theme-neutral-600">
-											<Icon name={currencyIcon}></Icon>
-										</span>
-									</Circle>
-									<Circle
-										className="relative rotate-90 bg-theme-primary-600 border-theme-primary-600"
-										size="large"
-									>
-										<span className="text-sm text-theme-background">IO</span>
-										<Badge
-											className={`transform ${
-												isOpen ? "rotate-180" : ""
-											} bg-theme-primary-100 border-theme-primary-100 bottom-2 -right-4 text-theme-primary-500`}
-											icon="ChevronDown"
-											iconWidth={16}
-											iconHeight={16}
-										/>
-									</Circle>
-								</div>
-							)}
-						></Dropdown>
+						<UserInfo
+							userInitials={userInitials}
+							currencyIcon={currencyIcon}
+							userActions={userActions}
+							onUserAction={onUserAction}
+						></UserInfo>
 					</div>
 				</div>
 			</div>
@@ -105,6 +160,12 @@ export const NavBar = ({ menu, userActions, onUserAction, currencyIcon }: Navbar
 
 NavBar.defaultProps = {
 	currencyIcon: "Btc",
+	notifications: {
+		transactionsHeader: "Transactions",
+		transactions: [],
+		pluginsHeader: "Plugins",
+		plugins: [],
+	},
 	menu: [
 		{
 			title: "Portfolio",
