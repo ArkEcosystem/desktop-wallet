aaba75c306bd7f5ff86867d6167a82fad0dc5b92
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-16 14:41:38 -0300

    chore: update welcome story to display profiles

--- src/domains/profile/pages/Welcome/Welcome.stories.tsx
+++ src/domains/profile/pages/Welcome/Welcome.stories.tsx
@@ -1,3 +1,4 @@
+import { EnvironmentContext } from "app/contexts";
 import React from "react";
 
 import { Welcome } from "./Welcome";
@@ -12,8 +13,28 @@ export const Default = () => (
 	</div>
 );
 
-export const WithProfiles = () => (
-	<div className="w-full h-full">
-		<Welcome />
-	</div>
-);
+export const WithProfiles = () => {
+	const env = {
+		profiles: () => ({
+			all: async () =>
+				new Promise((resolve) => {
+					resolve([
+						{
+							id: 1,
+							name: "Oleg Gelo",
+							balance: "234,500.46 USD",
+							avatar: "https://www.w3schools.com/howto/img_avatar.png",
+						},
+					]);
+				}),
+		}),
+	};
+
+	return (
+		<div className="w-full h-full">
+			<EnvironmentContext.Provider value={{ env }}>
+				<Welcome />
+			</EnvironmentContext.Provider>
+		</div>
+	);
+};
