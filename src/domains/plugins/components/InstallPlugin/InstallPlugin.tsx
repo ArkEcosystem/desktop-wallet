92d9bfb5dd41a12d21866dcbee06de09ec3af741
brenopolanski <breno.polanski@gmail.com>
2020-06-16 17:04:03 -0300

    feat: add step 3 - for install plugin

--- src/domains/plugins/components/InstallPlugin/InstallPlugin.tsx
+++ src/domains/plugins/components/InstallPlugin/InstallPlugin.tsx
@@ -62,12 +62,50 @@ export const InstallPlugin = ({ step, isOpen, onClose, onCancel }: InstallPlugin
 						</div>
 					</div>
 				)}
+
+				{step === 3 && (
+					<>
+						<div className="flex mt-4">
+							<div className="flex-shrink-0 mr-6">
+								<img
+									className="rounded-xl"
+									src="https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/explorer/master/images/logo.png"
+								/>
+							</div>
+							<div className="flex-1">
+								<div className="flex flex-col justify-around h-full">
+									<div>
+										<p className="text-sm font-semibold text-theme-neutral-light">Plugin</p>
+										<p className="font-semibold text-theme-black">ARK Explorer</p>
+									</div>
+									<div className="flex justify-between">
+										<span>
+											<p className="text-sm font-semibold text-theme-neutral-light">Downloaded</p>
+											<p className="text-sm font-bold text-theme-neutral-dark">Completed</p>
+										</span>
+										<div className="mr-2">
+											<CircularProgressBar value={78} size={50} strokeWidth={4} fontSize={0.8} />
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="flex justify-end mt-8">
+							<Button color="primary" variant="plain" onClick={onCancel} className="mr-2">
+								Cancel
+							</Button>
+							<Button type="submit" color="primary" variant="solid">
+								Install
+							</Button>
+						</div>
+					</>
+				)}
 			</div>
 		</Modal>
 	);
 };
 
 InstallPlugin.defaultProps = {
-	step: 2,
+	step: 3,
 	isOpen: false,
 };
