7a90134e73a6f568cd41010cd7cc9cf7e70e4409
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-17 07:35:24 +0100

    test: remove unusued prop

--- src/domains/search/components/SearchModal/SearchModal.test.tsx
+++ src/domains/search/components/SearchModal/SearchModal.test.tsx
@@ -11,7 +11,7 @@ describe("SearchModal", () => {
 	it("should not render if not open", () => {
 		const { asFragment, getByTestId } = render(
 			<I18nextProvider i18n={i18n}>
-				<SearchModal isOpen={false} onDelete={() => void 0} />
+				<SearchModal isOpen={false} />
 			</I18nextProvider>,
 		);
 
@@ -22,7 +22,7 @@ describe("SearchModal", () => {
 	it("should render a modal", () => {
 		const { asFragment, getByTestId } = render(
 			<I18nextProvider i18n={i18n}>
-				<SearchModal isOpen={true} onDelete={() => void 0} />
+				<SearchModal isOpen={true} />
 			</I18nextProvider>,
 		);
 
