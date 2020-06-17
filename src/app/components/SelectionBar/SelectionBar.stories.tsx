28f8b3575a5ee214c66fdabbd80ba52efbc51496
Brian Faust <hello@basecode.sh>
2020-06-17 19:59:53 +0300

    wip

--- src/app/components/SelectionBar/SelectionBar.stories.tsx
+++ src/app/components/SelectionBar/SelectionBar.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { SelectionBar,SelectionBarOption } from "./SelectionBar";
+import { SelectionBar, SelectionBarOption } from "./SelectionBar";
 import { useRadioState } from "./useRadioState";
 
 export default { title: "Basic / Selection Bar" };
