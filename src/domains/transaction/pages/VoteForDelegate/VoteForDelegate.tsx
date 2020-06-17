670ccdfee2e892863ceee32f4bf2a2ce1b6fe7b2
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-17 13:43:37 -0300

    feat(vote-delegate): add vote for delegate page

--- src/domains/transaction/pages/VoteForDelegate/VoteForDelegate.tsx
+++ src/domains/transaction/pages/VoteForDelegate/VoteForDelegate.tsx
@@ -1,27 +1,27 @@
 import { Address } from "app/components/Address";
 import { Button } from "app/components/Button";
 import { Circle } from "app/components/Circle";
-import { Form, FormField, FormLabel } from "app/components/Form";
+import { Form } from "app/components/Form";
 import { Icon } from "app/components/Icon";
-import { Input, InputPassword,InputRange } from "app/components/Input";
+import { Input, InputPassword, InputRange } from "app/components/Input";
 import { RadioButton, RadioButtonGroup, useRadioState } from "app/components/RadioButton";
 import { StepIndicator } from "app/components/StepIndicator";
 import { TabPanel, Tabs } from "app/components/Tabs";
 import { TransactionDetail } from "app/components/TransactionDetail";
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm, useFormContext } from "react-hook-form";
 
 export const FirstStep = () => {
-	const { register, setValue } = useFormContext();
+	const { register } = useFormContext();
 	const radioState = useRadioState(1);
-	// const [activeNetwork, setActiveNetwork] = React.useState<Network | undefined>(undefined);
 
-	// React.useEffect(() => {
-	// 	register("network", { required: true });
-	// }, [register]);
+	useEffect(() => {
+		register("amount", { required: true });
+		register("fee", { required: true });
+	}, [register]);
 
 	return (
-		<section data-testid="VoteForDelegate__first-step" className="space-y-8">
+		<section data-testid="VoteForDelegate__step--first" className="space-y-8">
 			<div>
 				<h1 className="mb-0">Vote for delegate</h1>
 				<p className="text-theme-neutral-dark">Enter details to send your money</p>
@@ -36,7 +36,12 @@ export const FirstStep = () => {
 						</div>
 					}
 				>
-					<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"ROBank"} size="large" />
+					<Address
+						name="account"
+						address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK"
+						walletName={"ROBank"}
+						size="large"
+					/>
 				</TransactionDetail>
 				<TransactionDetail
 					label="Delegate"
@@ -46,15 +51,15 @@ export const FirstStep = () => {
 						</div>
 					}
 				>
-					<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"ROBank"} size="large" />
+					<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"Delegate 3"} size="large" />
 				</TransactionDetail>
 				<TransactionDetail label="Amount ARK">
-					<Input />
+					<Input name="amount" />
 				</TransactionDetail>
 				<TransactionDetail border={false} label="Fee ARK">
 					<div className="flex">
 						<div className="w-3/5 mr-2">
-							<InputRange defaultValue={25} min={1} max={100} step={1} />
+							<InputRange name="fee" defaultValue={25} min={1} max={100} step={1} />
 						</div>
 						<div className="ml-2">
 							<RadioButtonGroup>
@@ -75,21 +80,109 @@ export const FirstStep = () => {
 		</section>
 	);
 };
-export const SecondStep = () => {
-	const { register, setValue } = useFormContext();
-	// const [activeNetwork, setActiveNetwork] = React.useState<Network | undefined>(undefined);
+export const SecondStep = () => (
+	<section data-testid="VoteForDelegate__step--second" className="space-y-8">
+		<div>
+			<h1 className="mb-0">Transaction Review</h1>
+			<p className="text-theme-neutral-dark">Check the informationa again before voting</p>
+		</div>
+		<div className="grid grid-flow-row gap-2">
+			<TransactionDetail
+				border={false}
+				label="Network"
+				extra={
+					<div className="ml-1 text-theme-danger-500">
+						<Circle className="bg-theme-background border-theme-danger-200" size="large">
+							<Icon name="Ark" width={20} height={20} />
+						</Circle>
+					</div>
+				}
+			>
+				<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"ROBank"} size="large" />
+			</TransactionDetail>
+			<TransactionDetail
+				label="Account"
+				extra={
+					<div>
+						<Circle avatarId="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK"></Circle>
+					</div>
+				}
+			>
+				<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"ROBank"} size="large" />
+			</TransactionDetail>
+			<TransactionDetail
+				label="Delegate"
+				extra={
+					<div>
+						<Circle avatarId="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK"></Circle>
+					</div>
+				}
+			>
+				<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"ROBank"} size="large" />
+			</TransactionDetail>
+			<TransactionDetail
+				label="Amount"
+				extra={
+					<div className="ml-1 text-theme-danger">
+						<Circle className="bg-theme-background border-theme-danger-200" size="large">
+							<Icon name="Sent" width={50} height={50} />
+						</Circle>
+					</div>
+				}
+			>
+				1.00 ARK <span className="text-theme-neutral-400">25.00 USD</span>
+			</TransactionDetail>
+			<TransactionDetail label="Transaction fee">0.09660435 ARK</TransactionDetail>
+		</div>
+	</section>
+);
 
-	// React.useEffect(() => {
-	// 	register("network", { required: true });
-	// }, [register]);
+export const ThirdStep = () => {
+	const { register } = useFormContext();
+
+	useEffect(() => {
+		register("passphrase", { required: true });
+	}, [register]);
 
 	return (
-		<section data-testid="VoteForDelegate__second-step" className="space-y-8">
+		<section data-testid="VoteForDelegate__step--third" className="space-y-8">
 			<div>
-				<h1 className="mb-0">Transaction Review</h1>
-				<p className="text-theme-neutral-dark">Check the informationa again before voting</p>
+				<h1 className="mb-0">Passphrase</h1>
+				<p className="text-theme-neutral-dark">Confirm your password to continue</p>
+				<div className="grid grid-flow-row gap-2">
+					<TransactionDetail border={false} label="Your password">
+						<InputPassword name="passphras" />
+					</TransactionDetail>
+				</div>
 			</div>
+		</section>
+	);
+};
+
+export const FourthStep = () => (
+	<section data-testid="VoteForDelegate__step--fourth" className="space-y-8">
+		<div>
+			<h1 className="mb-0">Transaction Successfull</h1>
 			<div className="grid grid-flow-row gap-2">
+				<div className="w-full mt-10">
+					<Icon name="TransactionSuccessful" width="100%" height={200} />
+				</div>
+				<p className="text-theme-neutral-dark">
+					Your transaction was successfully sent. Please monitor the blockchain to ensure your transactions is
+					confirmed and processed, The following is relevant information for your transaction.
+				</p>
+				<TransactionDetail label="ID">
+					<Address
+						address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWKAUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK"
+						maxChars={32}
+					/>
+				</TransactionDetail>
+				<TransactionDetail label="Block ID">
+					<Address
+						address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWKAUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK"
+						maxChars={32}
+					/>
+				</TransactionDetail>
 				<TransactionDetail
 					border={false}
 					label="Network"
@@ -121,7 +214,7 @@ export const SecondStep = () => {
 						</div>
 					}
 				>
-					<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"ROBank"} size="large" />
+					<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"Delegate 3"} size="large" />
 				</TransactionDetail>
 				<TransactionDetail
 					label="Amount"
@@ -133,60 +226,35 @@ export const SecondStep = () => {
 						</div>
 					}
 				>
-					1.00 ARK <span className="text-theme-neutral-400">25.00 USD</span>
+					1.00 ARK
 				</TransactionDetail>
-				<TransactionDetail label="Transaction fee">0.09660435 ARK</TransactionDetail>
+				<TransactionDetail
+					label="Transaction type"
+					extra={
+						<div className="ml-1 text-theme-danger">
+							<Circle className="bg-theme-background border-black" size="large">
+								<Icon name="Voted" />
+							</Circle>
+						</div>
+					}
+				>
+					Vote
+				</TransactionDetail>
 			</div>
-		</section>
-	);
+		</div>
+	</section>
+);
+
+type Props = {
+	onCopy: () => void;
 };
 
-export const ThirdStep = () => {
-	const { register, setValue } = useFormContext();
-	// const [activeNetwork, setActiveNetwork] = React.useState<Network | undefined>(undefined);
-
-	// React.useEffect(() => {
-	// 	register("network", { required: true });
-	// }, [register]);
-
-	return (
-		<section data-testid="VoteForDelegate__third-step" className="space-y-8">
-			<div>
-				<h1 className="mb-0">Passphrase</h1>
-				<p className="text-theme-neutral-dark">Confirm your password to continue</p>
-				<div className="grid grid-flow-row gap-2">
-					<TransactionDetail border={false} label="Your password">
-						<InputPassword />
-					</TransactionDetail>
-				</div>
-			</div>
-		</section>
-	);
-};
-
-export const FourthStep = () => {
-	const { register, setValue } = useFormContext();
-	// const [activeNetwork, setActiveNetwork] = React.useState<Network | undefined>(undefined);
-
-	// React.useEffect(() => {
-	// 	register("network", { required: true });
-	// }, [register]);
-
-	return (
-		<section data-testid="VoteForDelegate__fourth-step" className="space-y-8">
-			<div>
-				<h1 className="mb-0">Transaction Successfull</h1>
-			</div>
-		</section>
-	);
-};
-
-export const VoteForDelegate = () => {
+export const VoteForDelegate = ({ onCopy }: Props) => {
 	const [activeTab, setActiveTab] = React.useState(1);
 
 	const form = useForm({ mode: "onChange" });
-	const { formState } = form;
-	const { isValid } = formState;
+	// const { formState } = form;
+	// const { isValid } = formState;
 
 	const handleBack = () => {
 		setActiveTab(activeTab - 1);
@@ -198,11 +266,11 @@ export const VoteForDelegate = () => {
 
 	return (
 		<div className="max-w-xl mx-auto">
-			<Form context={form} onSubmit={(data: any) => onSubmit(data)}>
+			<Form context={form}>
 				<Tabs activeId={activeTab}>
 					<StepIndicator size={4} activeIndex={activeTab} />
 
-					<div className="mt-4">
+					<div className="mt-10">
 						<TabPanel tabId={1}>
 							<FirstStep />
 						</TabPanel>
@@ -217,38 +285,51 @@ export const VoteForDelegate = () => {
 						</TabPanel>
 
 						<div className="flex justify-start mt-6 space-x-2">
-							<Button
-								disabled={activeTab === 1}
-								data-testid="CreateWallet__back-button"
-								variant="plain"
-								size="large"
-								onClick={handleBack}
-							>
-								Back
-							</Button>
-
 							{activeTab < 4 && (
-								<Button
-									data-testid="CreateWallet__continue-button"
-									variant="solid"
-									size="large"
-									disabled={!isValid}
-									onClick={handleNext}
-								>
-									Continue
-								</Button>
+								<>
+									<Button
+										disabled={activeTab === 1}
+										data-testid="VoteForDelegate__button--back"
+										variant="plain"
+										size="large"
+										onClick={handleBack}
+									>
+										Back
+									</Button>
+									<Button
+										data-testid="VoteForDelegate__button--continue"
+										variant="solid"
+										size="large"
+										// disabled={!isValid}
+										onClick={handleNext}
+									>
+										Continue
+									</Button>
+								</>
 							)}
 
 							{activeTab === 4 && (
-								<Button
-									data-testid="CreateWallet__save-button"
-									type="submit"
-									variant="solid"
-									className={activeTab === 4 ? "block" : "hidden"}
-									size="large"
-								>
-									Save & Finish
-								</Button>
+								<>
+									<Button
+										data-testid="VoteForDelegate__button--back-to-wallet"
+										variant="plain"
+										className={"block"}
+										size="large"
+									>
+										Back to wallet
+									</Button>
+									<Button
+										onClick={onCopy}
+										data-testid="VoteForDelegate__button--copy"
+										variant="plain"
+										size="large"
+									>
+										<div className="flex items-center justify-between px-1">
+											<Icon name="Copy" />
+											<span className="ml-2">Copy</span>
+										</div>
+									</Button>
+								</>
 							)}
 						</div>
 					</div>
