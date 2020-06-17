c7309e6ee74d57ff817258d2fe3355a35a17e3bf
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-17 07:03:09 +0100

    feat: search modal component

---
+++ src/domains/search/components/SearchModal/SearchModal.test.tsx
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import { i18n } from "app/i18n";
+import React from "react";
+import { I18nextProvider } from "react-i18next";
+
+// i18n
+import { translations } from "../../i18n";
+import { SearchModal } from "./";
+
+describe("SearchModal", () => {
+	it("should not render if not open", () => {
+		const { asFragment, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<SearchModal isOpen={false} onDelete={() => void 0} />
+			</I18nextProvider>,
+		);
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal", () => {
+		const { asFragment, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<SearchModal isOpen={true} onDelete={() => void 0} />
+			</I18nextProvider>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_SEARCH.TITLE);
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_SEARCH.DESCRIPTION);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
