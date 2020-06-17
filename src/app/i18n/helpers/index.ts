1965c64fa55fbd7ed4165b1534c547b92b57e824
Breno Polanski <breno.polanski@gmail.com>
2020-06-16 23:30:07 -0300

    feat: add install plugin modal (#2154)

--- src/app/i18n/helpers/index.ts
+++ src/app/i18n/helpers/index.ts
@@ -1,5 +1,6 @@
 // Translations
 import { translations as HELP } from "../../../domains/help/i18n";
+import { translations as PLUGINS } from "../../../domains/plugins/i18n";
 import { translations as PROFILE } from "../../../domains/profile/i18n";
 import { translations as SEARCH } from "../../../domains/search/i18n";
 import { translations as TRANSACTION } from "../../../domains/transaction/i18n";
@@ -9,6 +10,7 @@ import { translations as COMMON } from "../common/i18n";
 export const buildTranslations = (): any => ({
 	COMMON,
 	HELP,
+	PLUGINS,
 	PROFILE,
 	SEARCH,
 	TRANSACTION,
---
+++ src/domains/plugins/components/InstallPlugin/InstallPlugin.test.tsx
@@ -0,0 +1,58 @@
+import { act, fireEvent, render } from "@testing-library/react";
+import { i18n } from "app/i18n";
+import { translations as COMMON } from "app/i18n/common/i18n";
+import React from "react";
+import { I18nextProvider } from "react-i18next";
+
+import { translations as PLUGINS } from "../../i18n";
+import { InstallPlugin } from "./";
+
+describe("InstallPlugin", () => {
+	it("should not render if not open", () => {
+		const { asFragment, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<InstallPlugin isOpen={false} />
+			</I18nextProvider>,
+		);
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal", () => {
+		const { asFragment, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<InstallPlugin isOpen={true} />
+			</I18nextProvider>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(PLUGINS.MODAL_INSTALL_PLUGIN.DESCRIPTION);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should handle download action", () => {
+		const handleDownload = jest.fn();
+
+		const { asFragment, getByTestId, rerender } = render(
+			<InstallPlugin step={1} isOpen={true} onDownload={handleDownload} />,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("install-plugin__download-button"));
+		});
+
+		expect(handleDownload).toHaveBeenCalled();
+		expect(getByTestId("modal__inner")).toHaveTextContent(COMMON.ATTENTION);
+		expect(asFragment()).toMatchSnapshot();
+
+		rerender(<InstallPlugin step={2} isOpen={true} />);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(COMMON.DOWNLOADED);
+		expect(asFragment()).toMatchSnapshot();
+
+		rerender(<InstallPlugin step={3} isOpen={true} />);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(COMMON.DOWNLOADED);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
