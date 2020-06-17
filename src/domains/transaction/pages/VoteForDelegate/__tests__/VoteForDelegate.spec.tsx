5ab49a5d5683166584726254725b0bfb9ac6ee19
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-17 13:44:41 -0300

    tests: add vote for delegate specs

---
+++ src/domains/transaction/pages/VoteForDelegate/__tests__/VoteForDelegate.spec.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { fireEvent, render, RenderResult, waitFor } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react-hooks";
+import React from "react";
+import { FormContext, useForm } from "react-hook-form";
+
+import { FirstStep, FourthStep, SecondStep, ThirdStep, VoteForDelegate } from "../VoteForDelegate";
+
+describe("Vote For Delegate", () => {
+	const onCopy = jest.fn();
+
+	it("should render 1st step", async () => {
+		const { result: form } = renderHook(() => useForm());
+		const { getByTestId, asFragment } = render(
+			<FormContext {...form.current}>
+				<FirstStep />
+			</FormContext>,
+		);
+
+		expect(getByTestId("VoteForDelegate__step--first")).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render 2nd step", async () => {
+		const { getByTestId, asFragment } = render(<SecondStep />);
+
+		expect(getByTestId("VoteForDelegate__step--second")).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render 3rd step", async () => {
+		const { result: form } = renderHook(() => useForm());
+		const { getByTestId, asFragment } = render(
+			<FormContext {...form.current}>
+				<ThirdStep />
+			</FormContext>,
+		);
+
+		expect(getByTestId("VoteForDelegate__step--third")).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render 4th step", async () => {
+		const { result: form } = renderHook(() => useForm());
+
+		const { getByTestId, asFragment } = render(
+			<FormContext {...form.current}>
+				<FourthStep />
+			</FormContext>,
+		);
+
+		expect(getByTestId("VoteForDelegate__step--fourth")).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render", async () => {
+		let rendered: RenderResult;
+
+		await act(async () => {
+			rendered = render(<VoteForDelegate onCopy={onCopy} />);
+			await waitFor(() => expect(rendered.getByTestId(`VoteForDelegate__step--first`)).toBeTruthy());
+		});
+
+		const { getByTestId, asFragment } = rendered!;
+
+		expect(asFragment()).toMatchSnapshot();
+
+		await act(async () => {
+			const continueButton = getByTestId(`VoteForDelegate__button--continue`);
+
+			// Navigation between steps
+			await waitFor(() => expect(continueButton).not.toHaveAttribute("disabled"));
+
+			fireEvent.click(continueButton);
+			expect(getByTestId(`VoteForDelegate__step--second`)).toBeTruthy();
+
+			fireEvent.click(continueButton);
+			expect(getByTestId(`VoteForDelegate__step--third`)).toBeTruthy();
+
+			// Back
+			fireEvent.click(getByTestId(`VoteForDelegate__button--back`));
+			expect(getByTestId(`VoteForDelegate__step--second`)).toBeTruthy();
+
+			fireEvent.click(continueButton);
+			fireEvent.click(continueButton);
+			await waitFor(() => expect(getByTestId(`VoteForDelegate__button--back-to-wallet`)).toBeTruthy());
+		});
+	});
+});
