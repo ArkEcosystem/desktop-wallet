745ccf29e72a25a74f45cb7613071d132bcebaec
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-17 08:24:25 +0100

    test: fix coverage for selection change

--- src/domains/search/components/SearchModal/SearchModal.test.tsx
+++ src/domains/search/components/SearchModal/SearchModal.test.tsx
@@ -1,4 +1,6 @@
 import { render } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+import { act } from "@testing-library/react-hooks";
 import { i18n } from "app/i18n";
 import React from "react";
 import { I18nextProvider } from "react-i18next";
@@ -28,6 +30,22 @@ describe("SearchModal", () => {
 
 		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_SEARCH.TITLE);
 		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_SEARCH.DESCRIPTION);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal", () => {
+		const { asFragment, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<SearchModal isOpen={true} />
+			</I18nextProvider>,
+		);
+
+		expect(getByTestId("modal__inner")).not.toHaveTextContent("Delegate");
+		act(() => {
+			fireEvent.click(getByTestId("dropdown__toggle"));
+			fireEvent.click(getByTestId("dropdown__option--1"));
+		});
+		expect(getByTestId("modal__inner")).toHaveTextContent("Delegate");
 		expect(asFragment()).toMatchSnapshot();
 	});
 });
