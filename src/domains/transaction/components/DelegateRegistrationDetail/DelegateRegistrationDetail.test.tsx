503cdb1eae6e2797430e435be67b4d4da4787e65
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-18 13:25:04 +0100

    refactor: remove redundant property on modals

--- src/domains/transaction/components/DelegateRegistrationDetail/DelegateRegistrationDetail.test.tsx
+++ src/domains/transaction/components/DelegateRegistrationDetail/DelegateRegistrationDetail.test.tsx
@@ -11,7 +11,7 @@ describe("DelegateRegistrationDetail", () => {
 	it("should not render if not open", () => {
 		const { asFragment, getByTestId } = render(
 			<I18nextProvider i18n={i18n}>
-				<DelegateRegistrationDetail isOpen={false} onDelete={() => void 0} />
+				<DelegateRegistrationDetail isOpen={false} />
 			</I18nextProvider>,
 		);
 
@@ -22,7 +22,7 @@ describe("DelegateRegistrationDetail", () => {
 	it("should render a modal", () => {
 		const { asFragment, getByTestId } = render(
 			<I18nextProvider i18n={i18n}>
-				<DelegateRegistrationDetail isOpen={true} onDelete={() => void 0} />
+				<DelegateRegistrationDetail isOpen={true} />
 			</I18nextProvider>,
 		);
 
