62e7658a5eb0f0f13c854275c6b1380334b99e31
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-06-18 14:24:31 -0300

    refactor: implement `InputCurrency` into `InputRange`

--- src/app/components/Input/InputCurrency.tsx
+++ src/app/components/Input/InputCurrency.tsx
@@ -3,15 +3,23 @@ import React from "react";
 
 import { Input } from "./Input";
 
-type Props = { onChange?: (value: string) => void; magnitude?: number } & React.InputHTMLAttributes<any>;
+type Props = { onChange?: (value: string) => void; magnitude?: number } & Omit<
+	React.InputHTMLAttributes<any>,
+	"onChange" | "defaultValue"
+>;
 
 export const InputCurrency = React.forwardRef<HTMLInputElement, Props>(
 	({ onChange, value, magnitude, ...props }: Props, ref) => {
-		const initialValue = value?.toString() || "";
-		const [amount, setAmount] = React.useState(Currency.fromString(initialValue, magnitude));
+		const convertValue = React.useCallback((value: string) => Currency.fromString(value, magnitude), [magnitude]);
+		const defaultValue = value?.toString() || "";
+		const [amount, setAmount] = React.useState(convertValue(defaultValue));
+
+		React.useLayoutEffect(() => {
+			setAmount(convertValue(defaultValue));
+		}, [defaultValue]);
 
 		const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-			const currency = Currency.fromString(event.target.value, magnitude);
+			const currency = convertValue(event.target.value);
 			setAmount(currency);
 			onChange?.(currency.value);
 		};
