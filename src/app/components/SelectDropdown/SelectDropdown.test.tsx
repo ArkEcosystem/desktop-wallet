96726c3d18f34a765dc3932807d8ea26fc75f9ee
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-06-18 19:47:21 +0300

    feat: add `SelectDropdown` component (#2184)

---
+++ src/app/components/SelectDropdown/SelectDropdown.test.tsx
@@ -0,0 +1,165 @@
+import { fireEvent, render } from "@testing-library/react";
+import { act } from "@testing-library/react-hooks";
+import React from "react";
+
+import { SelectDropdown } from "./SelectDropdown";
+
+describe("SelectDropdown", () => {
+	it("should render", () => {
+		const options = [
+			{
+				label: "Option 1",
+				value: "1",
+			},
+			{
+				label: "Option 2",
+				value: "2",
+			},
+			{
+				label: "Option 3",
+				value: "3",
+			},
+		];
+		const { container } = render(<SelectDropdown options={options}></SelectDropdown>);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("should render ignoring option.value property", () => {
+		const options = [
+			{
+				label: "Option 1",
+			},
+			{
+				label: "Option 2",
+			},
+			{
+				label: "Option 3",
+			},
+		];
+		const { container } = render(<SelectDropdown options={options}></SelectDropdown>);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("should render with custom toggle and option templates", () => {
+		const options = [
+			{
+				label: "Option 1",
+				value: "1",
+			},
+			{
+				label: "Option 2",
+				value: "2",
+			},
+			{
+				label: "Option 3",
+				value: "3",
+			},
+		];
+		const { container } = render(
+			<SelectDropdown
+				option={(option: any) => <div>{option.label}</div>}
+				toggle={() => {
+					return (
+						<div className="flex items-center flex-inline">
+							<div className="font-semibold text-theme-neutral-800">Select Option</div>
+						</div>
+					);
+				}}
+				options={options}
+			></SelectDropdown>,
+		);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("should toggle dropdown content", () => {
+		const options = [{ label: "Option 1", value: "1" }];
+		const { getByTestId } = render(
+			<SelectDropdown
+				option={(option: any) => <div>{option.label}</div>}
+				toggle={() => {
+					return (
+						<div className="flex items-center flex-inline">
+							<div className="font-semibold text-theme-neutral-800">Select Option</div>
+						</div>
+					);
+				}}
+				options={options}
+			></SelectDropdown>,
+		);
+		const toggle = getByTestId("select-dropdown__toggle");
+
+		act(() => {
+			fireEvent.click(toggle);
+		});
+
+		expect(getByTestId("select-dropdown__content")).toBeTruthy();
+
+		const firstOption = getByTestId("select-dropdown__option-0");
+		expect(firstOption).toBeTruthy();
+
+		act(() => {
+			fireEvent.click(firstOption);
+		});
+	});
+
+	it("should render without option content", () => {
+		const options = [
+			{
+				label: "Option 1",
+				value: "1",
+			},
+			{
+				label: "Option 2",
+				value: "2",
+			},
+			{
+				label: "Option 3",
+				value: "3",
+			},
+		];
+		const { container } = render(
+			<SelectDropdown
+				option={(option: any) => <div>{option.label}</div>}
+				toggle={() => {
+					return (
+						<div className="flex items-center flex-inline">
+							<div className="font-semibold text-theme-neutral-800">Select Option</div>
+						</div>
+					);
+				}}
+				options={options}
+			></SelectDropdown>,
+		);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("should toggle dropdown content without option content", () => {
+		const options = [{ label: "Option 1", value: "1" }];
+		const { getByTestId } = render(
+			<SelectDropdown
+				toggle={() => {
+					return (
+						<div className="flex items-center flex-inline">
+							<div className="font-semibold text-theme-neutral-800">Select Option</div>
+						</div>
+					);
+				}}
+				options={options}
+			></SelectDropdown>,
+		);
+		const toggle = getByTestId("select-dropdown__toggle");
+
+		act(() => {
+			fireEvent.click(toggle);
+		});
+
+		expect(getByTestId("select-dropdown__content")).toBeTruthy();
+
+		const firstOption = getByTestId("select-dropdown__option-0");
+		expect(firstOption).toBeTruthy();
+
+		act(() => {
+			fireEvent.click(firstOption);
+		});
+	});
+});
