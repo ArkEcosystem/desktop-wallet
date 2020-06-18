b50913d24609c5eca747398c847ed6a994f0efd7
Edgar Goetzendorff <hello@dated.fun>
2020-06-18 18:03:45 +0200

    feat: add generic DeleteResource component (#2186)

---
+++ src/domains/contacts/i18n.ts
@@ -0,0 +1,6 @@
+export const translations: { [key: string]: any } = {
+	MODAL_DELETE_CONTACT: {
+		TITLE: "Delete Contact",
+		DESCRIPTION: "Do you really want to delete this contact? Once deleted, you will not be able to restore it.",
+	},
+};
