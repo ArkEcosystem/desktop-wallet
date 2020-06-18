200aa579751be7ea1a42e7b17df40dacd7203925
Brian Faust <faustbrian@users.noreply.github.com>
2020-06-18 08:34:57 +0300

    chore: add missing `@types/*` dependencies (#2176)

--- src/app/services/HttpClient.ts
+++ src/app/services/HttpClient.ts
@@ -2,11 +2,11 @@ import { Contracts } from "@arkecosystem/platform-sdk";
 import fetch from "node-fetch";
 
 export class HttpClient implements Contracts.HttpClient {
-	public async get(path: string, query: object = {}) {
-		return (await fetch(path, { params: { ...query } })).json();
+	public async get(path: string, searchParams?: Record<string, any>) {
+		return (await fetch(searchParams ? `${path}?${new URLSearchParams(searchParams)}` : path)).json();
 	}
 
-	public async post(path: string, body: object, headers: object = {}) {
+	public async post(path: string, body: object, headers = {}) {
 		return (await fetch(path, { method: "POST", body: JSON.stringify(body), headers })).json();
 	}
 }
