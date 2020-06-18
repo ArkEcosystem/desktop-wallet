8f96d611050d6f1f4c87185d9f4ec54129a01298
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-18 22:27:54 +0100

    test: delegate selection

--- src/domains/transaction/components/SelectDelegateModal/SelectDelegateModal.test.tsx
+++ src/domains/transaction/components/SelectDelegateModal/SelectDelegateModal.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { i18n } from "app/i18n";
 import React from "react";
 import { I18nextProvider } from "react-i18next";
@@ -28,6 +28,41 @@ describe("SelectDelegateModal", () => {
 
 		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_SELECT_DELEGATE.TITLE);
 		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_SELECT_DELEGATE.DESCRIPTION);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should only allow one selection", () => {
+		const { asFragment, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<SelectDelegateModal isOpen={true} />
+			</I18nextProvider>,
+		);
+
+		fireEvent.click(getByTestId("SelectedDelegateModal__select-delegate-Delegate2"));
+		expect(getByTestId("SelectedDelegateModal__footer")).toHaveTextContent("Delegate2");
+
+		fireEvent.click(getByTestId("SelectedDelegateModal__select-delegate-Delegate3"));
+		expect(getByTestId("SelectedDelegateModal__footer")).not.toHaveTextContent("Delegate2");
+		expect(getByTestId("SelectedDelegateModal__footer")).toHaveTextContent("Delegate3");
+
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should allow multiple selection", () => {
+		const { asFragment, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<SelectDelegateModal isOpen={true} allowMultiple={true} />
+			</I18nextProvider>,
+		);
+
+		fireEvent.click(getByTestId("SelectedDelegateModal__select-delegate-Delegate2"));
+		fireEvent.click(getByTestId("SelectedDelegateModal__select-delegate-Delegate3"));
+		fireEvent.click(getByTestId("SelectedDelegateModal__select-delegate-Delegate4"));
+		fireEvent.click(getByTestId("SelectedDelegateModal__select-delegate-Delegate3"));
+		fireEvent.click(getByTestId("SelectedDelegateModal__toggle-show-selected"));
+		expect(getByTestId("SelectedDelegateModal__footer")).toHaveTextContent("Delegate2");
+		expect(getByTestId("SelectedDelegateModal__footer")).toHaveTextContent("Delegate4");
+
 		expect(asFragment()).toMatchSnapshot();
 	});
 });
