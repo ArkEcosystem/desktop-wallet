f0348ca130370a60015a5c2f2f83b4ab04d8a18b
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-19 11:37:16 -0300

    feat(total-amount): wip improve UI

--- src/domains/transaction/components/TotalAmount/TotalAmount.tsx
+++ src/domains/transaction/components/TotalAmount/TotalAmount.tsx
@@ -16,22 +16,24 @@ export const TotalAmount = ({ transactionAmount, transactionFee }: Props) => {
 	};
 
 	return (
-		<div className="w-full border rounded-lg">
-			<div className="py-5 flex justify-between items-center px-10">
+		<div className="w-full border border-theme-neutral-300 rounded-lg">
+			<div className="mt-1 py-5 flex justify-between items-center px-10">
 				<div className="flex flex-col">
-					<span>Transaction(s) Amount</span>
-					<span>1.00 ARK</span>
+					<span className="text-sm text-theme-neutral-500">Transaction(s) Amount</span>
+					<span className="mt-3">1.00 ARK</span>
 				</div>
-				<div className="border-r">
+
+				<div className="mr-3">
 					<Icon name="Plus" />
 				</div>
-				<div className="flex flex-col">
-					<span>Transaction fee</span>
-					<span>0.09660435 ARK</span>
+
+				<div className="flex flex-col text-right">
+					<span className="text-sm text-theme-neutral-500">Transaction fee</span>
+					<span className="mt-3 mr-3">0.09660435 ARK</span>
 				</div>
 			</div>
-			<div className="border-t flex flex-col justfiy-center items-center py-5">
-				<span>Total Amount</span>
+			<div className="border-t border-theme-neutral-300 rounded-b-lg flex flex-col justfiy-center items-center py-7 bg-theme-neutral-100">
+				<span className="text-sm text-theme-neutral-500">Total Amount</span>
 				<span className="font-bold text-2xl">1.09660435 ARK</span>
 			</div>
 		</div>
