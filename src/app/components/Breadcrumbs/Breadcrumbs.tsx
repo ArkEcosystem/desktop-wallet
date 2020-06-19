326a2b2f9b84e3442535df14dabca96e66cd147a
Edgar Goetzendorff <hello@dated.fun>
2020-06-19 20:54:18 +0200

    feat: add Breadcrumbs component

---
+++ src/app/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+type Crumb = {
+	route: string;
+	label: string;
+};
+
+type BreadcrumbsProps = {
+	crumbs: Crumbs[];
+	className?: string;
+};
+
+export const Breadcrumbs = ({ crumbs, className }: BreadcrumbsProps) => {
+	const isLast = (index) => {
+		return index === crumbs.length - 1;
+	};
+
+	return crumbs.length ? (
+		<div
+			data-testid="breadcrumbs__wrapper"
+			className={`flex items-center space-x-3 ${className} ${
+				crumbs.length === 1 ? "text-theme-neutral-700" : "text-theme-neutral-500"
+			}`}
+		>
+			{crumbs.length && <span> &lt;- </span>}
+
+			{crumbs.map((crumb: Crumb, index: number) => {
+				return (
+					<div key={index} className="space-x-3">
+						<NavLink to={crumb.route} className={`${isLast(index) ? "text-theme-neutral-700" : ""}`}>
+							<span>{crumb.label}</span>
+						</NavLink>
+
+						{!isLast(index) && <span> | </span>}
+					</div>
+				);
+			})}
+		</div>
+	) : null;
+};
+
+Breadcrumbs.defaultProps = {
+	className: "w-full font-semibold",
+};
