612529cc63ff5e6e2b23b8a3a0252168eb19f2a8
brenopolanski <breno.polanski@gmail.com>
2020-06-19 16:12:52 -0300

    feat: add some other fields

--- src/domains/settings/pages/Settings/available-settings/Plugins.tsx
+++ src/domains/settings/pages/Settings/available-settings/Plugins.tsx
@@ -1,7 +1,8 @@
 import { Button } from "app/components/Button";
-import { Form } from "app/components/Form";
+import { Form, FormField, FormLabel } from "app/components/Form";
 import { Header } from "app/components/Header";
 import { ListDivided } from "app/components/ListDivided";
+import { Select } from "app/components/Select";
 import { Toggle } from "app/components/Toggle";
 import React from "react";
 
@@ -17,15 +18,25 @@ export const Plugins = ({ formConfig, onSubmit }: PluginsProps) => {
 			label: "Apply Blacklist",
 			labelClass: "text-xl font-bold text-theme-neutral-dark",
 			content: (
-				<div className="flex flex-row justify-between">
-					<span className="w-3/4 text-sm text-theme-neutral-dark">
-						This list is selected safely by ARK Ecosystem. You can view it and add to the list of plugins
-						that you find suspicious.
-					</span>
-					<div className="-mt-2">
-						<Toggle />
+				<>
+					<div className="flex flex-row justify-between">
+						<span className="w-3/4 text-sm text-theme-neutral-dark">
+							This list is selected safely by ARK Ecosystem. You can view it and add to the list of
+							plugins that you find suspicious.
+						</span>
+						<div className="-mt-2">
+							<Toggle />
+						</div>
 					</div>
-				</div>
+					<div className="flex flex-col py-6 space-x-3 md:flex-row">
+						<Button color="primary" variant="plain" className="w-full">
+							Open List
+						</Button>
+						<Button color="primary" variant="plain" className="w-full mt-2 md:mt-0">
+							Add Plugin
+						</Button>
+					</div>
+				</>
 			),
 		},
 		{
@@ -33,14 +44,23 @@ export const Plugins = ({ formConfig, onSubmit }: PluginsProps) => {
 			label: "Plugin Source",
 			labelClass: "text-xl font-bold text-theme-neutral-dark",
 			content: (
-				<div className="flex flex-row justify-between">
-					<span className="w-3/4 text-sm text-theme-neutral-dark">
-						Turn this feature on, you can upload plugins to your wallet from third-party sources.
-					</span>
-					<div className="-mt-2">
-						<Toggle />
+				<>
+					<div className="flex flex-row justify-between">
+						<span className="w-3/4 text-sm text-theme-neutral-dark">
+							Turn this feature on, you can upload plugins to your wallet from third-party sources.
+						</span>
+						<div className="-mt-2">
+							<Toggle />
+						</div>
 					</div>
-				</div>
+					<FormField name="load-plugins" className="mt-5">
+						<FormLabel>Load plugins from</FormLabel>
+						<Select>
+							<option value="">Select</option>
+							<option value="github">GitHub</option>
+						</Select>
+					</FormField>
+				</>
 			),
 		},
 	];
--- src/domains/settings/pages/Settings/available-settings/index.ts
+++ src/domains/settings/pages/Settings/available-settings/index.ts
@@ -1,2 +1,3 @@
 export * from "./General";
 export * from "./Peer";
+export * from "./Plugins";
