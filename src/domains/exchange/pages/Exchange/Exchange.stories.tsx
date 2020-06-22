9f5e1d5978b44382c44d7170273fe4fe7acbaef1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-23 00:54:20 +0100

    feat: add exchange page

---
+++ src/domains/exchange/pages/Exchange/Exchange.stories.tsx
@@ -0,0 +1,43 @@
+import { NavigationBar } from "app/components/NavigationBar";
+import React from "react";
+
+import { Exchange } from "./Exchange";
+
+export default { title: "Exchange / Pages / Exchange" };
+
+export const Default = () => (
+	<div>
+		<NavigationBar currencyIcon="Ark" balance="34,253.75" userInitials="IO" />
+
+		<Exchange />
+	</div>
+);
+
+export const WithExchanges = () => {
+	const exchanges = [
+		{
+			id: "changenow-plugin",
+			name: "ChangeNOW Plugin",
+		},
+		{
+			id: "binance",
+			name: "Binance",
+		},
+		{
+			id: "atomars",
+			name: "Atomars",
+		},
+		{
+			id: "okex",
+			name: "OKEx",
+		},
+	];
+
+	return (
+		<div>
+			<NavigationBar currencyIcon="Ark" balance="34,253.75" userInitials="IO" />
+
+			<Exchange exchanges={exchanges} />
+		</div>
+	);
+};
