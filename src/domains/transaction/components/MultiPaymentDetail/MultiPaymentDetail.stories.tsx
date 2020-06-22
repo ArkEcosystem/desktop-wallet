437cc1d60957d88ca55799c740fa38e832c70905
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-06-22 12:44:38 -0300

    fix(storybook): merge transaction/transactions section (#2222)

--- src/domains/transaction/components/MultiPaymentDetail/MultiPaymentDetail.stories.tsx
+++ src/domains/transaction/components/MultiPaymentDetail/MultiPaymentDetail.stories.tsx
@@ -3,6 +3,6 @@ import React from "react";
 
 import { MultiPaymentDetail } from "./MultiPaymentDetail";
 
-export default { title: "Transaction / Components / MultiPayment Detail" };
+export default { title: "Transactions / Components / MultiPayment Detail" };
 
 export const Default = () => <MultiPaymentDetail isOpen={boolean("Is Open", true)} onClose={() => alert("closed")} />;
