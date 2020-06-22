bf0b168b0e194fb2583ee51798513c335916b074
Edgar Goetzendorff <hello@dated.fun>
2020-06-22 15:37:42 +0200

    feat: add icon overview (#2219)

--- src/app/components/Icon/Icon.stories.tsx
+++ src/app/components/Icon/Icon.stories.tsx
@@ -1,13 +1,34 @@
+import { number, text, withKnobs } from "@storybook/addon-knobs";
+import { SvgCollection } from "app/assets/svg";
+import { Clipboard } from "app/components/Clipboard";
 import React from "react";
 
 import { Icon } from "./Icon";
 
 export default {
 	title: "Basic / Icon",
+	decorators: [withKnobs],
 };
 
-export const Default = () => (
-	<div className="inline-flex space-x-4">
-		<Icon name="Upload" />
-	</div>
-);
+export const Default = () => {
+	const options = { range: true, min: 10, max: 100, step: 1 };
+
+	const iconColor = text("Color", "text-theme-primary");
+	const width = number("Width", 20, options);
+	const height = number("Height", 20, options);
+
+	return (
+		<div className="flex flex-wrap justify-center">
+			{Object.keys(SvgCollection).map((icon) => (
+				<div className="m-4" key={icon}>
+					<Clipboard data={icon}>
+						<div className="flex flex-col items-center justify-center w-24 h-24 space-y-6">
+							<span className="text-sm">{icon}</span>
+							<Icon className={iconColor} name={icon} width={width} height={height} />
+						</div>
+					</Clipboard>
+				</div>
+			))}
+		</div>
+	);
+};
