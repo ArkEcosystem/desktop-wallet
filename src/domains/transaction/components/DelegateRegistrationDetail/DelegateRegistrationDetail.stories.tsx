b8b90df1a0a32c1e267ecd7ad0e3b2751d25b40c
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-22 12:06:57 -0300

    fix(storybook): merge transaction/transactions section

--- src/domains/transaction/components/DelegateRegistrationDetail/DelegateRegistrationDetail.stories.tsx
+++ src/domains/transaction/components/DelegateRegistrationDetail/DelegateRegistrationDetail.stories.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import { DelegateRegistrationDetail } from "./DelegateRegistrationDetail";
 
-export default { title: "Transaction / Components / Delegate Registration Detail" };
+export default { title: "Transactions / Components / Delegate Registration Detail" };
 
 export const Default = () => (
 	<DelegateRegistrationDetail isOpen={boolean("Is Open", true)} onClose={() => alert("closed")} />
