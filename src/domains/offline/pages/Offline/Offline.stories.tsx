66ae558dd9429590c0b8a5893b877509be605ead
brenopolanski <breno.polanski@gmail.com>
2020-06-23 19:02:24 -0300

    feat: add offline page

---
+++ src/domains/offline/pages/Offline/Offline.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Offline } from "./Offline";
+
+export default {
+	title: "Splashscreen / Pages / Offline",
+};
+
+export const Default = () => <Offline />;
---
+++ src/domains/offline/pages/Offline/Offline.tsx
@@ -0,0 +1,32 @@
+import { images } from "app/assets/images";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const commonAssets = images.common;
+const { CannotConnectBanner } = images.offline.pages.Offline;
+
+export const Offline = () => {
+	const { t } = useTranslation();
+
+	return (
+		<div className="w-full h-full">
+			<div className="px-4 sm:px-6 lg:px-8">
+				<div className="flex items-center flex-shrink-0 h-20 md:h-24">
+					<div className="flex p-2 rounded-lg bg-logo">
+						<img src={commonAssets.ARKLogo} className="h-6 md:h-8 lg:h-10" alt="ARK Logo" />
+					</div>
+				</div>
+			</div>
+
+			<div className="container mx-auto mt-4 text-center">
+				<div className="w-2/6 mx-auto">
+					<CannotConnectBanner />
+				</div>
+				<div className="mt-8">
+					<h3 className="text-xl font-bold">{t("OFFLINE.TITLE")}</h3>
+					<p className="text-theme-neutral-dark">{t("OFFLINE.DESCRIPTION")}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
