a0ade221efbf5edf89eeaa720b94094010339523
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-06-23 11:34:51 -0300

    feat: add plugin header component (#2235)

---
+++ src/domains/plugins/components/PluginHeader/PluginHeader.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { PluginHeader } from "./PluginHeader";
+
+export default { title: "Plugins / Components / Plugin Header" };
+
+export const Default = () => (
+	<PluginHeader author="ARK Ecosystem" category="Utility" url="github.com" rating="4.6" version="1.3.8" size="4.2" />
+);
