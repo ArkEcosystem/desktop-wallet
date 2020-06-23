cbcd67f33b61cfb85bea991acda2fa8e9f40ac3c
goga-m <goga.mamoulasvili@gmail.com>
2020-06-23 19:11:40 +0300

    style: remove comments

--- src/app/components/PercentageBar/PercentageBar.test.tsx
+++ src/app/components/PercentageBar/PercentageBar.test.tsx
@@ -1,7 +1,6 @@
 import { render } from "@testing-library/react";
 import React from "react";
 
-// import { act } from "@testing-library/react-hooks";
 import { PercentageBar } from "./PercentageBar";
 
 describe("PercentageBar", () => {
