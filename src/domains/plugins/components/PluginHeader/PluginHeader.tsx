7b7f73d4cd9d8e7165b3d45f59c045721f3f644a
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-23 09:00:58 -0300

    feat(plugin-details): add plugin header component

---
+++ src/domains/plugins/components/PluginHeader/PluginHeader.tsx
@@ -0,0 +1,47 @@
+import { Button } from "app/components/Button";
+import { Icon } from "app/components/Icon";
+import Placeholder from "domains/plugins/images/placeholder.png";
+import React from "react";
+
+import { PluginSpecs } from "./components/PluginSpecs";
+
+type Props = {
+	author: string;
+	category: string;
+	url: string;
+	rating: string;
+	version: string;
+	size: string;
+};
+
+export const PluginHeader = ({ author, category, url, rating, version, size }: Props) => (
+	<div className="py-5 px-10 w-full bg-theme-background">
+		<div className="flex w-full">
+			<img className="rounded-lg w-40 h-40" alt="plugin-image" src={Placeholder} />
+			<div className="flex flex-col justify-center w-full px-5">
+				<div className="flex justify-between items-center">
+					<div className="flex flex-col">
+						<span className="font-bold text-2xl">ARK Explorer</span>
+						<span className="text-medium text-theme-neutral-500">
+							Use the ARK Mainnet explorer directly within the wallet
+						</span>
+					</div>
+					<div className="flex">
+						<Button data-testid="PluginHeader__button--install">Install</Button>
+						<Button className="ml-3" data-testid="PluginHeader__button--warning" variant="plain">
+							<Icon name="AlertDanger" height={25} width={25} />
+						</Button>
+					</div>
+				</div>
+				<PluginSpecs
+					author={author}
+					category={category}
+					url={url}
+					rating={rating}
+					version={version}
+					size={size}
+				/>
+			</div>
+		</div>
+	</div>
+);
---
+++ src/domains/plugins/components/PluginHeader/components/PluginSpecs/PluginSpecs.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+type Props = {
+	author: string;
+	category: string;
+	url: string;
+	rating: string;
+	version: string;
+	size: string;
+};
+
+type GridColProps = {
+	children: React.ReactNode;
+	colSpan?: string;
+	justify?: string;
+	padding?: string;
+};
+
+type GridItemProps = {
+	label: string;
+	value: string;
+	textDirection?: string;
+};
+
+const GridItem = ({ label, value, textDirection }: GridItemProps) => (
+	<div className={`flex flex-col ${textDirection && `text-${textDirection}`}`}>
+		<span className="font-bold text-theme-neutral-400">{label}</span>
+		<span className="font-bold text-theme-neutral-600">{value}</span>
+	</div>
+);
+
+const GridCol = ({ children, colSpan, justify, padding }: GridColProps) => {
+	const mountClassName = () => {
+		let styles = "flex";
+
+		if (colSpan) styles = `${styles} col-span-${colSpan}`;
+		if (justify) styles = `${styles} justify-${justify}`;
+		if (padding) styles = `${styles} ${padding}`;
+
+		return styles;
+	};
+
+	return <div className={mountClassName()}>{children}</div>;
+};
+
+export const PluginSpecs = ({ author, category, url, rating, version, size }: Props) => (
+	<div className="grid grid-cols-5 divide-x divide-gray-400 text-sm mt-5 grid-flow-col border-t border-dashed border-theme-neutral pt-5">
+		<GridCol>
+			<GridItem label="Author" value={author} />
+		</GridCol>
+		<GridCol padding="px-8">
+			<GridItem label="Category" value={category} />
+		</GridCol>
+		<GridCol padding="px-8">
+			<GridItem label="URL" value={url} />
+		</GridCol>
+		<GridCol colSpan={2} justify="between" padding="px-8">
+			<GridItem label="Rating" value={rating} />
+			<GridItem label="Version" value={`v.${version}`} textDirection="right" />
+		</GridCol>
+		<GridCol padding="pl-8">
+			<GridItem label="Size" value={`${size} Mb`} textDirection="right" />
+		</GridCol>
+	</div>
+);
