5b16d953fb38ceb1f4a9a90f42fde2bb198d06cf
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-23 09:05:56 +0100

    feat: exchanges page (#2229)

--- src/domains/exchange/i18n.ts
+++ src/domains/exchange/i18n.ts
@@ -1,4 +1,12 @@
 export const translations: { [key: string]: any } = {
+	ADD_EXCHANGE: "Add Exchange",
+	DESCRIPTION: "Trade your money safely from the comfort of your wallet.",
+	EXCHANGE_NAME: "Exchange Name",
+	NO_EXCHANGES_MESSAGE: "You don't have an Exchange yet",
+	SELECT_EXCHANGE_MESSAGE: "Select one of the exchanges you have installed",
+	TITLE: "Exchange",
+	YOUR_EXCHANGE_LIST: "Your Exchange List",
+
 	MODAL_ADD_EXCHANGE: {
 		TITLE: "Exchange",
 		DESCRIPTION:
