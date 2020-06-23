8a5fc71a4e1c24ff928db98b5d24f398573e7d3e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-23 01:01:06 +0100

    refactor: update exchange card story with on click

--- src/domains/exchange/components/ExchangeCard/ExchangeCard.stories.tsx
+++ src/domains/exchange/components/ExchangeCard/ExchangeCard.stories.tsx
@@ -4,7 +4,9 @@ import { AddExchangeCard, BlankCard, ExchangeCard } from "./ExchangeCard";
 
 export default { title: "Exchange / Components / Exchange Cards" };
 
-export const Exchange = () => <ExchangeCard exchange={{ id: "test-exchange", name: "Test Exchange" }} />;
+export const Exchange = () => (
+	<ExchangeCard exchange={{ id: "test-exchange", name: "Test Exchange" }} onClick={() => alert("clicked exchange")} />
+);
 
 export const AddExchange = () => <AddExchangeCard />;
 
