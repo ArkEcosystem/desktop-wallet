ab68fe8b79b79e05559a036d0ed912f4b0f4336b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-23 01:02:51 +0100

    refactor: update add exchange card story with callback

--- src/domains/exchange/components/ExchangeCard/ExchangeCard.stories.tsx
+++ src/domains/exchange/components/ExchangeCard/ExchangeCard.stories.tsx
@@ -8,6 +8,6 @@ export const Exchange = () => (
 	<ExchangeCard exchange={{ id: "test-exchange", name: "Test Exchange" }} onClick={() => alert("clicked exchange")} />
 );
 
-export const AddExchange = () => <AddExchangeCard />;
+export const AddExchange = () => <AddExchangeCard onAddExchange={() => alert("clicked add exchange")} />;
 
 export const BlankExchange = () => <BlankCard />;
