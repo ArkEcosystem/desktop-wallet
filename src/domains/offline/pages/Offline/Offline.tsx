d974a54023e26b9bb4dd369e2e3a086b2a54aeee
brenopolanski <breno.polanski@gmail.com>
2020-06-23 19:27:01 -0300

    fix: pin the container in the center

--- src/domains/offline/pages/Offline/Offline.tsx
+++ src/domains/offline/pages/Offline/Offline.tsx
@@ -18,12 +18,12 @@ export const Offline = () => {
 				</div>
 			</div>
 
-			<div className="container mx-auto mt-4 text-center" data-testid="Offline">
+			<div className="flex flex-col items-center justify-center my-4 text-center" data-testid="Offline">
 				<div className="w-2/6 mx-auto">
 					<CannotConnectBanner />
 				</div>
 				<div className="mt-8">
-					<h3 className="text-xl font-bold">{t("OFFLINE.TITLE")}</h3>
+					<h3 className="text-xl font-bold xl:text-2xl">{t("OFFLINE.TITLE")}</h3>
 					<p className="text-theme-neutral-dark">{t("OFFLINE.DESCRIPTION")}</p>
 				</div>
 			</div>
