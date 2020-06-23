6b717b475267870a7fa4477051be45acbe7d68b6
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-23 17:29:14 -0300

    feat(comments): add reply component

---
+++ src/domains/plugins/components/ReviewBox/components/Comments/components/Reply/Reply.stories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Reply } from "./Reply";
+
+export default { title: "Plugins / Components / Review Box / Components / Comments / Components / Reply" };
+
+export const Default = () => <Reply />;
