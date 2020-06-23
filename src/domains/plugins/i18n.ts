46f30da3580f6f1f7038a14cc9b17c27286bc347
Breno Polanski <breno.polanski@gmail.com>
2020-06-22 23:11:25 -0300

    feat: add best plugins modal (#2226)

--- src/domains/plugins/i18n.ts
+++ src/domains/plugins/i18n.ts
@@ -1,15 +1,23 @@
 export const translations: { [key: string]: any } = {
+	MODAL_BEST_PLUGINS: {
+		TITLE: "Best applications of the month",
+		DESCRIPTION:
+			"ARK is an open-source blockchain ecosystem that provides users with innovate and easy-to-use blockchain thechnologies. Our supportive community, extensive range of products and easy-to-implement technology, underpinned by the ARK Public Network, empowers individuals to adopt and apply blockhain tecnology in their everyday lives.",
+	},
+
+	MODAL_FEATURED_PLUGINS: {
+		TITLE: "Featured Plugins",
+		DESCRIPTION:
+			"ARK is an open-source blockchain ecosystem that provides users with innovate and easy-to-use blockchain thechnologies. Our supportive community, extensive range of products and easy-to-implement technology, underpinned by the ARK Public Network, empowers individuals to adopt and apply blockhain tecnology in their everyday lives.",
+	},
+
 	MODAL_INSTALL_PLUGIN: {
 		DESCRIPTION: "This plugin needs the following permissions:",
 		ITEM_1: "Allows access to the Desktop Wallet alerts",
 		ITEM_2: "Allows access to play audio from within the Desktop Wallet",
 		ITEM_3: "Allows access to the Desktop Wallet events",
 	},
-	MODAL_FEATURED_PLUGINS: {
-		TITLE: "Featured Plugins",
-		DESCRIPTION:
-			"ARK is an open-source blockchain ecosystem that provides users with innovate and easy-to-use blockchain thechnologies. Our supportive community, extensive range of products and easy-to-implement technology, underpinned by the ARK Public Network, empowers individuals to adopt and apply blockhain tecnology in their everyday lives.",
-	},
+
 	MODAL_ADD_BLACKLIST_PLUGIN: {
 		TITLE: "Add Blacklist",
 		DESCRIPTION: "Find and select a plugin to add or remove it from the blacklist.",
