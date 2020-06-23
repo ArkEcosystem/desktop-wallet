2a5354e3657b0cc4ca44ce507052c1be884913ed
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-23 17:31:16 -0300

    feat: wip add comments section

--- src/domains/plugins/components/ReviewBox/components/Comments/Comments.stories.tsx
+++ src/domains/plugins/components/ReviewBox/components/Comments/Comments.stories.tsx
@@ -8,7 +8,7 @@ const comments = [
 	{
 		author: "Rok Cernec",
 		score: "4.6",
-		date: "06-19-2020",
+		date: "2020-06-19T14:48:00.000Z",
 		comment:
 			"As ARK Core is written exclusively in Node.js, the server-side framework for JavaScript and Typescript, installing Node.js is a necessity for core development. The code below installs Node.js from the source.",
 	},
