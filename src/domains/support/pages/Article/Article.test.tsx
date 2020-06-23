d72b8402b8457bc6e9369da8def02a574eaff4c7
goga-m <goga.mamoulasvili@gmail.com>
2020-06-23 09:12:42 +0300

    chore: update snapshots

--- src/domains/support/pages/Article/Article.test.tsx
+++ src/domains/support/pages/Article/Article.test.tsx
@@ -8,7 +8,7 @@ import { article } from "../../data";
 import { Article } from "./Article";
 
 describe("SupportPage", () => {
-	it("should render faq portfolio category page", () => {
+	it("should render article support page", () => {
 		const { container } = render(
 			<I18nextProvider i18n={i18n}>
 				<Article
@@ -17,6 +17,22 @@ describe("SupportPage", () => {
 					categoryIcon={article.categoryIcon}
 					views={article.views}
 					sections={article.sections}
+				/>
+			</I18nextProvider>,
+		);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("should render article support page with main image", () => {
+		const { container } = render(
+			<I18nextProvider i18n={i18n}>
+				<Article
+					title={article.title}
+					category={article.category}
+					categoryIcon={article.categoryIcon}
+					views={article.views}
+					sections={article.sections}
+					image={article.image}
 				/>
 			</I18nextProvider>,
 		);
