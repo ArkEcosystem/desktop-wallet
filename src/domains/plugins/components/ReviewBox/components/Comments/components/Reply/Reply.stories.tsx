c29a1b357edfae4c571204c33a85686e5d47e686
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-24 08:25:24 -0300

    feat(comments): add reply with temp dangerous innerHTML

--- src/domains/plugins/components/ReviewBox/components/Comments/components/Reply/Reply.stories.tsx
+++ src/domains/plugins/components/ReviewBox/components/Comments/components/Reply/Reply.stories.tsx
@@ -4,4 +4,7 @@ import { Reply } from "./Reply";
 
 export default { title: "Plugins / Components / Review Box / Components / Comments / Components / Reply" };
 
-export const Default = () => <Reply />;
+const date = "2020-06-19T14:48:00.000Z";
+const content = "<a href='#'>@Gerard Blezer</a> Our GitHub bount reward program utlilizes a tiered structure.";
+
+export const Default = () => <Reply date={date} content={content} />;
--- src/domains/plugins/components/ReviewBox/components/Comments/components/Reply/Reply.tsx
+++ src/domains/plugins/components/ReviewBox/components/Comments/components/Reply/Reply.tsx
@@ -1,20 +1,29 @@
 import { Icon } from "app/components/Icon";
 import React from "react";
+import styled from "styled-components";
 
 import { getDateDifferenceFromNow } from "../../utils";
 
-export const Reply = ({
-	replyDate = "2020-06-19T14:48:00.000Z",
-	reply = "HSIUAHDIUSHDIUASHDIUASHDIHSAIDUHSAIUDHSA",
-}: ReplyProps) => (
-	<div className="flex flex-col">
+const ReplyContent = styled.div`
+	a {
+		color: #067cff;
+	}
+`;
+
+type ReplyProps = {
+	date: string;
+	content: string;
+};
+
+export const Reply = ({ date, content }: ReplyProps) => (
+	<div className="mt-3 ml-3 flex flex-col">
 		<div className="flex items-center text-theme-neutral-500">
 			<Icon name="ReplyArrow" />
 			<div className="grid grid-cols-2 divide-x">
 				<span className="font-bold text-theme-neutral-900 px-3">Developer response</span>
-				<span className="px-3">{getDateDifferenceFromNow(replyDate)}</span>
+				<span className="px-3">{getDateDifferenceFromNow(date)}</span>
 			</div>
 		</div>
-		<span className="ml-7 mt-2 text-theme-neutral-600">{reply}</span>
+		<ReplyContent className="ml-7 mt-2 text-theme-neutral-600" dangerouslySetInnerHTML={{ __html: content }} />
 	</div>
 );
