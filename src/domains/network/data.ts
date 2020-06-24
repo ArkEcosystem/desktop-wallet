174ff43212ef2e48fdfc7cd89c8c1931b2ad5ede
brenopolanski <breno.polanski@gmail.com>
2020-06-24 15:34:12 -0300

    chore: some tweaks

--- src/domains/network/data.ts
+++ src/domains/network/data.ts
@@ -1,6 +1,6 @@
 export const networks = [
-	{ icon: "Ark", name: "ARK Ecosystem", network: "mainnet" },
-	{ icon: "Cosmos", name: "Cosmos", network: "mainnet" },
-	{ icon: "Eos", name: "EOS", network: "mainnet" },
-	{ icon: "Komodo", name: "Komodo", network: "mainnet" },
+	{ icon: "Ark", name: "ARK Ecosystem", network: "Mainnet" },
+	{ icon: "Cosmos", name: "Cosmos", network: "Mainnet" },
+	{ icon: "Eos", name: "EOS", network: "Mainnet" },
+	{ icon: "Komodo", name: "Komodo", network: "Mainnet" },
 ];
