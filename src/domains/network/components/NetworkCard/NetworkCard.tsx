37669dc66f4f913a1b76df2075439dfe3e5095d6
Breno Polanski <breno.polanski@gmail.com>
2020-06-24 22:48:53 -0300

    feat: add network card (#2251)

---
+++ src/domains/network/components/NetworkCard/NetworkCard.tsx
@@ -0,0 +1,33 @@
+import { CardControl } from "app/components/Card";
+import { Circle } from "app/components/Circle";
+import { Icon } from "app/components/Icon";
+import React from "react";
+
+type NetworkCardProps = {
+	icon?: string;
+	name?: string;
+	network?: string;
+	onChange?: any;
+};
+
+export const NetworkCard = ({ icon, name, network, onChange }: NetworkCardProps) => (
+	<CardControl type="radio" value={name} name="network" onChange={() => onChange(name)}>
+		<div className="flex items-center py-2">
+			<Circle className="transition-colors duration-100 border-theme-neutral-300" size="large" noShadow>
+				<Icon name={icon!} />
+			</Circle>
+			<div className="flex flex-col ml-4">
+				<span className="text-sm font-semibold text-theme-neutral-500" data-testid="network-card--network">
+					{network}
+				</span>
+				<span className="font-semibold text-theme-text" data-testid="network-card--name">
+					{name}
+				</span>
+			</div>
+		</div>
+	</CardControl>
+);
+
+NetworkCard.defaultProps = {
+	network: "Mainnet",
+};
