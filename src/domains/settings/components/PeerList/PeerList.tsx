673fdc415bbc22d2887c861ad9d87f31945e3b7f
brenopolanski <breno.polanski@gmail.com>
2020-06-25 10:26:36 -0300

    wip

--- src/domains/settings/components/PeerList/PeerList.tsx
+++ src/domains/settings/components/PeerList/PeerList.tsx
@@ -21,7 +21,7 @@ export const PeerList = ({ listColumns, peers }: PeerListProps) => {
 
 			<Button
 				variant="plain"
-				className="w-full mt-5"
+				className="w-full mt-3"
 				onClick={() => setModalIsOpen(true)}
 				data-testid="peer-list__add-button"
 			>
