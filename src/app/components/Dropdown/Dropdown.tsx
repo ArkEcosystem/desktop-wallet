cf5ac1a6639e27980f7039facd4900bf5584dd5b
goga-m <goga.mamoulasvili@gmail.com>
2020-06-25 21:35:40 +0300

    refactor: revert dropdown and slider changes

--- src/app/components/Dropdown/Dropdown.tsx
+++ src/app/components/Dropdown/Dropdown.tsx
@@ -62,7 +62,7 @@ const renderToggle = (
 
 	if (!children) {
 		return (
-			<div className="float-right outline-none focus:outline-none cursor-pointer">
+			<div className="float-right outline-none focus:outline-none">
 				<Icon name={toggleIcon} width={size[toggleSize]} height={size[toggleSize]} />
 			</div>
 		);
