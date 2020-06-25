eb202875b82e3c70884de908c674b61f13d89f24
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-25 14:44:33 +0100

    test: addition plugin manager tests

--- src/domains/plugins/pages/PluginManager/PluginManager.test.tsx
+++ src/domains/plugins/pages/PluginManager/PluginManager.test.tsx
@@ -8,6 +8,8 @@ import { I18nextProvider } from "react-i18next";
 import { translations } from "../../i18n";
 import { PluginManager } from "./PluginManager";
 
+jest.useFakeTimers();
+
 describe("PluginManager", () => {
 	it("should render", () => {
 		const { asFragment, getByTestId } = render(
@@ -69,11 +71,10 @@ describe("PluginManager", () => {
 		});
 
 		expect(within(getByTestId("PluginManager__container--game")).getByTestId("PluginGrid")).toBeTruthy();
-
 		expect(asFragment()).toMatchSnapshot();
 	});
 
-	it("should open featured modal", () => {
+	it("should open & close featured modal", () => {
 		const { asFragment, getByTestId, rerender } = render(
 			<I18nextProvider i18n={i18n}>
 				<PluginManager />
@@ -85,9 +86,16 @@ describe("PluginManager", () => {
 		});
 
 		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_FEATURED_PLUGINS.TITLE);
+
+		act(() => {
+			fireEvent.click(getByTestId("modal__close-btn"));
+		});
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
 	});
 
-	it("should open top rated modal", () => {
+	it("should open & close top rated modal", () => {
 		const { asFragment, getByTestId, rerender } = render(
 			<I18nextProvider i18n={i18n}>
 				<PluginManager />
@@ -99,5 +107,261 @@ describe("PluginManager", () => {
 		});
 
 		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_BEST_PLUGINS.TITLE);
+
+		act(() => {
+			fireEvent.click(getByTestId("modal__close-btn"));
+		});
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should download & install plugin on home", () => {
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("PluginManagerControls__list--icon"));
+			fireEvent.click(getAllByTestId("PluginListItem__install")[0]);
+		});
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_INSTALL_PLUGIN.DESCRIPTION);
+
+		act(() => {
+			fireEvent.click(getByTestId("install-plugin__download-button"));
+		});
+
+		jest.runAllTimers();
+
+		expect(setTimeout).toHaveBeenCalled();
+		expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000);
+
+		act(() => {
+			fireEvent.click(getByTestId("install-plugin__install-button"));
+		});
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should download & install plugin on game", () => {
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("PluginManagerNavigationBar__game"));
+			fireEvent.click(getByTestId("PluginManagerControls__list--icon"));
+			fireEvent.click(getAllByTestId("PluginListItem__install")[0]);
+		});
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_INSTALL_PLUGIN.DESCRIPTION);
+
+		act(() => {
+			fireEvent.click(getByTestId("install-plugin__download-button"));
+		});
+
+		jest.runAllTimers();
+
+		expect(setTimeout).toHaveBeenCalled();
+		expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000);
+
+		act(() => {
+			fireEvent.click(getByTestId("install-plugin__install-button"));
+		});
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should close install plugin modal", () => {
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("PluginManagerControls__list--icon"));
+			fireEvent.click(getAllByTestId("PluginListItem__install")[0]);
+		});
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_INSTALL_PLUGIN.DESCRIPTION);
+
+		act(() => {
+			fireEvent.click(getByTestId("modal__close-btn"));
+		});
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should cancel install plugin", () => {
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("PluginManagerControls__list--icon"));
+			fireEvent.click(getAllByTestId("PluginListItem__install")[0]);
+		});
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_INSTALL_PLUGIN.DESCRIPTION);
+
+		act(() => {
+			fireEvent.click(getByTestId("install-plugin__cancel-button"));
+		});
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should search for plugin", (done) => {
+		const consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
+
+		const { asFragment, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("header-search-bar__button"));
+			fireEvent.change(within(getByTestId("header-search-bar__input")).getByTestId("Input"), {
+				target: { value: "test" },
+			});
+		});
+
+		setTimeout(() => {
+			// expect(onSearch).toHaveBeenCalled();
+			expect(consoleSpy).toHaveBeenCalledTimes(1);
+			expect(consoleSpy).toHaveBeenCalledWith("search");
+			expect(asFragment()).toMatchSnapshot();
+			consoleSpy.mockRestore();
+			done();
+		}, 550);
+
+		jest.runAllTimers();
+	});
+
+	it("should select plugin on home grids", () => {
+		const consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
+
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(
+				within(getByTestId("PluginManager__home__featured")).getByTestId("PluginCard--ark-explorer-1"),
+			);
+		});
+
+		expect(consoleSpy).toHaveBeenLastCalledWith("selected");
+
+		act(() => {
+			fireEvent.click(
+				within(getByTestId("PluginManager__home__top-rated")).getByTestId("PluginCard--ark-explorer-1"),
+			);
+		});
+
+		expect(consoleSpy).toHaveBeenLastCalledWith("selected");
+
+		act(() => {
+			fireEvent.click(
+				within(getByTestId("PluginManager__home__top-utilities")).getByTestId("PluginCard--ark-explorer-1"),
+			);
+		});
+
+		expect(consoleSpy).toHaveBeenLastCalledWith("selected");
+		expect(consoleSpy).toHaveBeenCalledTimes(3);
+		expect(asFragment()).toMatchSnapshot();
+
+		consoleSpy.mockRestore();
+	});
+
+	it("should select plugin on game grid", () => {
+		const consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
+
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("PluginManagerNavigationBar__game"));
+			fireEvent.click(getAllByTestId("PluginCard--ark-explorer-1")[0]);
+		});
+
+		expect(consoleSpy).toHaveBeenCalledTimes(1);
+		expect(consoleSpy).toHaveBeenCalledWith("selected");
+		expect(asFragment()).toMatchSnapshot();
+
+		consoleSpy.mockRestore();
+	});
+
+	it("should delete plugin on home", () => {
+		const consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
+
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(within(getByTestId("PluginManager__home__featured")).getAllByTestId("dropdown__toggle")[0]);
+			fireEvent.click(within(getByTestId("PluginManager__home__featured")).getByTestId("dropdown__option--1"));
+		});
+
+		expect(consoleSpy).toHaveBeenLastCalledWith("delete");
+		expect(consoleSpy).toHaveBeenCalledTimes(1);
+		expect(asFragment()).toMatchSnapshot();
+
+		consoleSpy.mockRestore();
+	});
+
+	it("should delete plugin on game", () => {
+		const consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
+
+		const { asFragment, getAllByTestId, getByTestId, rerender } = render(
+			<I18nextProvider i18n={i18n}>
+				<PluginManager />
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.click(getByTestId("PluginManagerNavigationBar__game"));
+			fireEvent.click(
+				within(getByTestId("PluginManager__container--game")).getAllByTestId("dropdown__toggle")[1],
+			);
+			fireEvent.click(within(getByTestId("PluginManager__container--game")).getByTestId("dropdown__option--1"));
+		});
+
+		expect(consoleSpy).toHaveBeenLastCalledWith("delete");
+
+		act(() => {
+			fireEvent.click(getByTestId("PluginManagerControls__list--icon"));
+			fireEvent.click(
+				within(getByTestId("PluginManager__container--game")).getAllByTestId("dropdown__toggle")[1],
+			);
+			fireEvent.click(within(getByTestId("PluginManager__container--game")).getByTestId("dropdown__option--1"));
+		});
+
+		expect(consoleSpy).toHaveBeenLastCalledWith("delete");
+		expect(consoleSpy).toHaveBeenCalledTimes(2);
+		expect(asFragment()).toMatchSnapshot();
+
+		consoleSpy.mockRestore();
 	});
 });
