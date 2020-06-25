2ad57582e2ac1237d1d4f7fb046dc4c0388f7ccb
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-06-25 12:26:10 -0300

    refactor: rename domains to singular (#2259)

---
+++ src/domains/plugin/components/PluginGrid/PluginGrid.tsx
@@ -0,0 +1,25 @@
+import { PluginCard } from "domains/plugin/components/PluginCard";
+import React from "react";
+
+type PluginGridProps = {
+	onDelete: any;
+	onSelect: any;
+	plugins: any[];
+};
+
+export const PluginGrid = ({ onDelete, onSelect, plugins }: PluginGridProps) => {
+	const entries = [];
+
+	for (const plugin of plugins) {
+		entries.push(
+			<PluginCard
+				key={plugin.id}
+				plugin={plugin}
+				onClick={() => onSelect(plugin)}
+				onDelete={() => onDelete(plugin)}
+			/>,
+		);
+	}
+
+	return <div className="grid grid-cols-4 gap-5">{entries}</div>;
+};
