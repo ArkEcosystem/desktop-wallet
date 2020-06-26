2b4e2c1f853b42ec9c9d61aee590a88f23fc3e80
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-26 15:42:32 -0300

    feat: add business table

---
+++ src/domains/profile/pages/MyRegistrations/components/BusinessTable/BusinessTable.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { BusinessTable } from "./BusinessTable";
+
+export default {
+	title: "Profile / Pages / My Registrations / Components / Business Registrations Table",
+};
+
+export const Default = () => {
+	const registrations = [
+		{
+			agent: "OLEBank",
+			businessName: "ARK Ecosystem",
+			history: [],
+			website: "",
+			msq: true,
+			repository: [],
+		},
+		{
+			agent: "OLEBank",
+			businessName: "ARK Ecosystem",
+			history: [],
+			website: "",
+			msq: true,
+			repository: [],
+		},
+	];
+
+	return <BusinessTable data={registrations} />;
+};
