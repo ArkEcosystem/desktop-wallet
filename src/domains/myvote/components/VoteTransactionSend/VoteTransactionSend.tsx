6efdccdc1ffd9563c82d1d6ad75b2c3f2e3ee0a4
brenopolanski <breno.polanski@gmail.com>
2020-06-29 08:43:47 -0300

    wip

--- src/domains/myvote/components/VoteTransactionSend/VoteTransactionSend.tsx
+++ src/domains/myvote/components/VoteTransactionSend/VoteTransactionSend.tsx
@@ -1,5 +1,11 @@
+import { Address } from "app/components/Address";
+import { Circle } from "app/components/Circle";
+import { Icon } from "app/components/Icon";
+import { Label } from "app/components/Label";
 import { StepIndicator } from "app/components/StepIndicator";
 import { TabPanel, Tabs } from "app/components/Tabs";
+import { TransactionDetail } from "app/components/TransactionDetail";
+import { Avatar } from "domains/wallet/components/Avatar";
 import React, { useState } from "react";
 
 export const FirstStep = () => (
@@ -7,6 +13,37 @@ export const FirstStep = () => (
 		<div>
 			<h1 className="mb-0">Vote Transaction</h1>
 			<p className="text-theme-neutral-dark">Select a fee to continue</p>
+		</div>
+		<div className="grid grid-flow-row gap-2">
+			<TransactionDetail
+				border={false}
+				label="Cryptoasset"
+				extra={
+					<div className="ml-1">
+						<Circle className="bg-theme-background border-theme-danger-200" size="large">
+							<Icon name="Ark" width={20} height={20} className="text-theme-danger-500" />
+						</Circle>
+					</div>
+				}
+			>
+				<div className="flex-auto text-xl font-semibold truncate text-theme-neutral-800 max-w-24">
+					ARK Ecosystem
+				</div>
+			</TransactionDetail>
+			<TransactionDetail
+				label=" "
+				extra={
+					<div>
+						<Avatar address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" />
+					</div>
+				}
+			>
+				<div className="mb-2 text-sm font-semibold text-theme-neutral-500">
+					<span className="mr-1">Sender</span>
+					<Label color="warning">Your address</Label>
+				</div>
+				<Address address="AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" walletName={"ROBank"} size="large" />
+			</TransactionDetail>
 		</div>
 	</section>
 );
