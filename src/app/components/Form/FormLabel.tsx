aa6e4887c6584a090716a5e0b1654fb24a8e239c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-06-29 10:41:22 +0100

    feat: add tippy & required prop for form label

--- src/app/components/Form/FormLabel.tsx
+++ src/app/components/Form/FormLabel.tsx
@@ -1,8 +1,14 @@
+import "tippy.js/dist/tippy.css";
+
+import Tippy from "@tippyjs/react";
 import React from "react";
 
 import { useFormField } from "./useFormField";
 
-type FormLabelProps = { label?: string } & React.LabelHTMLAttributes<any>;
+type FormLabelProps = {
+	label?: string;
+	required?: boolean;
+} & React.LabelHTMLAttributes<any>;
 
 export function FormLabel(props: FormLabelProps) {
 	const fieldContext = useFormField();
@@ -10,11 +16,21 @@ export function FormLabel(props: FormLabelProps) {
 	return (
 		<label
 			data-testid="FormLabel"
-			className={`FormLabel transition-colors duration-100 inline-block text-sm font-semibold text-theme-neutral-dark`}
+			className="FormLabel flex transition-colors duration-100 inline-block text-sm text-theme-neutral-dark"
 			htmlFor={fieldContext?.name}
 			{...props}
 		>
 			{props.label || props.children}
+
+			{props.required && (
+				<Tippy content="These fields are required to be filled in">
+					<div className="mt-1 ml-1 w-1 h-1 rounded-full bg-theme-danger-400" />
+				</Tippy>
+			)}
 		</label>
 	);
 }
+
+FormLabel.defaultProps = {
+	required: false,
+};
