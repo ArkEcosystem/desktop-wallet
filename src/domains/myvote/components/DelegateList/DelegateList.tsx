a724d039a735590e9c15f10a08348c4e036a2694
brenopolanski <breno.polanski@gmail.com>
2020-06-29 16:29:23 -0300

    feat: add action to select delegates

--- src/domains/myvote/components/DelegateList/DelegateList.tsx
+++ src/domains/myvote/components/DelegateList/DelegateList.tsx
@@ -1,5 +1,8 @@
+import { Button } from "app/components/Button";
 import { Table } from "app/components/Table";
-import React from "react";
+import { Avatar } from "domains/wallet/components/Avatar";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 import { DelegateListItem } from "../DelegateListItem";
 
@@ -7,7 +10,37 @@ type DelegateListProps = {
 	data?: any;
 };
 
+type SelectedDelegateListProps = {
+	delegates: any[];
+	className?: string;
+};
+
+const SelectedDelegateList = (props: SelectedDelegateListProps) => {
+	const output = [];
+
+	for (const delegate of props.delegates) {
+		output.push(
+			<div
+				key={delegate.username}
+				className="flex items-center py-4 font-semibold border-b border-dashed border-theme-neutral-200 last:border-0"
+			>
+				<div className="flex flex-1">
+					<Avatar address={delegate.address} className="mr-8" />
+					<div className="mr-2">{delegate.username}</div>
+					<div className="text-theme-neutral-500">{delegate.address}</div>
+				</div>
+				#{delegate.rank}
+			</div>,
+		);
+	}
+
+	return <div className={props.className}>{output}</div>;
+};
+
 export const DelegateList = (props: DelegateListProps) => {
+	const { t } = useTranslation();
+	const [selected, setSelected] = useState([] as any);
+	const [showSelectedList, setShowSelectedList] = useState(false);
 	const columns = [
 		{
 			Header: "",
@@ -55,10 +88,58 @@ export const DelegateList = (props: DelegateListProps) => {
 		},
 	];
 
+	const toggleSelected = (delegate: any) => {
+		if (selected.find((selectedDelegate: any) => selectedDelegate.username === delegate.username)) {
+			setSelected(selected.filter((selectedDelegate: any) => selectedDelegate.username !== delegate.username));
+
+			return;
+		}
+
+		setSelected([...selected, delegate]);
+	};
+
 	return (
-		<Table columns={columns} data={props.data}>
-			{(rowData: any) => <DelegateListItem {...rowData} />}
-		</Table>
+		<div>
+			<Table columns={columns} data={props.data}>
+				{(rowData: any) => <DelegateListItem {...rowData} selected={selected} onSelect={toggleSelected} />}
+			</Table>
+
+			{selected.length ? (
+				<div
+					className="absolute bottom-0 left-0 right-0 pt-8 pb-10 pl-4 pr-12 bg-white shadow-2xl"
+					data-testid="SelectedDelegateModal__footer"
+				>
+					<div className="flex">
+						<div className="px-8 mr-8 font-semibold border-r border-theme-neutral-300">
+							<div className="text-sm text-theme-neutral-500">{t("COMMON.QUANTITY")}</div>
+							<div className="text-theme-neutral-700">{selected.length}/50</div>
+						</div>
+
+						<div className="flex-1">
+							<div className="flex justify-between">
+								<div className="font-semibold">
+									<div className="text-sm text-theme-neutral-500">{t("COMMON.DELEGATES")}</div>
+
+									<div
+										data-testid="SelectedDelegateModal__toggle-show-selected"
+										className="cursor-pointer text-theme-primary-700 hover:text-theme-primary-500"
+										onClick={() => setShowSelectedList(!showSelectedList)}
+									>
+										{showSelectedList ? "Hide" : "Show"} List
+									</div>
+								</div>
+
+								<Button>Continue</Button>
+							</div>
+
+							{showSelectedList && <SelectedDelegateList delegates={selected} className="mt-2" />}
+						</div>
+					</div>
+				</div>
+			) : (
+				""
+			)}
+		</div>
 	);
 };
 
--- src/domains/myvote/components/DelegateListItem/DelegateListItem.tsx
+++ src/domains/myvote/components/DelegateListItem/DelegateListItem.tsx
@@ -14,7 +14,8 @@ type DelegateListItemProps = {
 	commissionDaily?: number;
 	payout?: string;
 	min?: number;
-	onSelect?: (delegateAddress: string) => void;
+	selected?: any[];
+	onSelect?: ({ address, username, rank }: { address: string; username: string; rank: number }) => void;
 };
 
 export const DelegateListItem = ({
@@ -27,9 +28,11 @@ export const DelegateListItem = ({
 	commissionDaily,
 	payout,
 	min,
+	selected,
 	onSelect,
 }: DelegateListItemProps) => {
 	const { t } = useTranslation();
+	const isSelected = selected?.find((selectedDelegate: any) => selectedDelegate.username === delegateName) || false;
 
 	return (
 		<tr className="border-b border-theme-neutral-200">
@@ -69,13 +72,18 @@ export const DelegateListItem = ({
 				<div className="text-right">
 					<Button
 						variant="plain"
-						onClick={() => onSelect?.(delegateAddress!)}
+						color={isSelected ? "danger" : "primary"}
+						onClick={() => onSelect?.({ address: delegateAddress!, username: delegateName!, rank: rank! })}
 						data-testid="DelegateListItem__button--select"
 					>
-						{t("COMMON.SELECT")}
+						{isSelected ? t("COMMON.UNSELECT") : t("COMMON.SELECT")}
 					</Button>
 				</div>
 			</td>
 		</tr>
 	);
+};
+
+DelegateListItem.defaultProps = {
+	selected: [],
 };
