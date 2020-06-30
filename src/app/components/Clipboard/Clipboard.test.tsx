726b7cdcea3a3a13a11d52160387eebcd2619dd9
Edgar Goetzendorff <hello@dated.fun>
2020-06-30 12:59:17 +0200

    refactor: add dynamic tooltip to Clipboard component (#2297)

--- src/app/components/Clipboard/Clipboard.test.tsx
+++ src/app/components/Clipboard/Clipboard.test.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { act, fireEvent, render } from "@testing-library/react";
+import { i18n } from "app/i18n";
 import React from "react";
+import { I18nextProvider } from "react-i18next";
 
 import { Clipboard } from "./Clipboard";
 
@@ -31,6 +33,30 @@ describe("Clipboard", () => {
 
 		expect(getByTestId("clipboard__wrapper")).toHaveTextContent("Hello!");
 		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should change the tooltip content when clicked", async () => {
+		const { baseElement, getByTestId } = render(
+			<I18nextProvider i18n={i18n}>
+				<Clipboard>
+					<span>Hello!</span>
+				</Clipboard>
+			</I18nextProvider>,
+		);
+
+		act(() => {
+			fireEvent.mouseEnter(getByTestId("clipboard__wrapper"));
+		});
+
+		expect(baseElement).toHaveTextContent("Copy to clipboard");
+		expect(baseElement).not.toHaveTextContent("Copied!");
+
+		await act(async () => {
+			fireEvent.click(getByTestId("clipboard__wrapper"));
+		});
+
+		expect(baseElement).not.toHaveTextContent("Copy to clipboard");
+		expect(baseElement).toHaveTextContent("Copied!");
 	});
 
 	it.each([
