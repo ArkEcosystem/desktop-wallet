1c5c612c68b554b64ee830f33e019127b55c6068
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-01 10:47:14 -0300

    chore(storybook): add state to storybook

--- src/app/components/Pagination/Pagination.stories.tsx
+++ src/app/components/Pagination/Pagination.stories.tsx
@@ -1,16 +1,20 @@
 import { select, withKnobs } from "@storybook/addon-knobs";
-import React from "react";
+import React, { useState } from "react";
 
 import { Pagination } from "./Pagination";
 
 export default { title: "App / Components / Pagination", decorators: [withKnobs] };
 
-export const Default = () => (
-	<Pagination
-		totalCount={12}
-		itemsPerPage={4}
-		onSelectPage={console.log}
-		currentPage={1}
-		size={select("Size", ["sm", "md", "lg"], "md")}
-	/>
-);
+export const Default = () => {
+	const [currentPage, selectPage] = useState(1);
+
+	return (
+		<Pagination
+			totalCount={12}
+			itemsPerPage={4}
+			onSelectPage={selectPage}
+			currentPage={currentPage}
+			size={select("Size", ["sm", "md", "lg"], "md")}
+		/>
+	);
+};
