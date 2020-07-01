984292c6d449181f365f68ca37817fc7e2ebc869
Edgar Goetzendorff <hello@dated.fun>
2020-07-01 13:02:14 +0200

    feat: add TimeAgo component (#2311)

--- src/domains/plugin/components/Comments/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { getDateDifferenceFromNow } from "./utils";
-
-describe("getDateDifferenceFromNow", () => {
-	it("should return string with days ago from now", () => {
-		const targetDate = "2020-07-01T00:00:00.000Z";
-
-		expect(getDateDifferenceFromNow(targetDate)).toEqual("0 day ago");
-	});
-});
