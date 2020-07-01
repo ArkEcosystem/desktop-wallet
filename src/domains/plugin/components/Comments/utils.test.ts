12583cd2094f7a0db845abe621ee76bc59aa7349
Edgar Goetzendorff <hello@dated.fun>
2020-07-01 02:59:58 +0200

    fix: increase coverage

--- src/domains/plugin/components/Comments/utils.test.ts
+++ src/domains/plugin/components/Comments/utils.test.ts
@@ -2,8 +2,8 @@ import { getDateDifferenceFromNow } from "./utils";
 
 describe("getDateDifferenceFromNow", () => {
 	it("should return string with days ago from now", () => {
-		const targetDate = "2020-06-22T14:48:00.000Z";
+		const targetDate = "2020-07-01T00:00:00.000Z";
 
-		expect(getDateDifferenceFromNow(targetDate)).toEqual("8 days ago");
+		expect(getDateDifferenceFromNow(targetDate)).toEqual("0 day ago");
 	});
 });
