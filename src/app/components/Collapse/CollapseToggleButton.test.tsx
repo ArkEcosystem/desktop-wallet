65066a6b495615903c459aba1eb524cd484ec68c
Edgar Goetzendorff <hello@dated.fun>
2020-07-01 13:39:16 +0200

    chore: reference i18n keys in tests instead of hardcoded values (#2304)

--- src/app/components/Collapse/CollapseToggleButton.test.tsx
+++ src/app/components/Collapse/CollapseToggleButton.test.tsx
@@ -1,3 +1,4 @@
+import { translations } from "app/i18n/common/i18n";
 import React from "react";
 import { fireEvent, render } from "test-utils";
 
@@ -6,16 +7,22 @@ import { CollapseToggleButton } from "./CollapseToggleButton";
 describe("CollapseToggleButton", () => {
 	it("should render", () => {
 		const onClick = jest.fn();
+
 		const { getByTestId } = render(<CollapseToggleButton isOpen={false} onClick={onClick} />);
+
 		const button = getByTestId("CollapseToggleButton");
-		expect(button).toHaveTextContent("Show");
+		expect(button).toHaveTextContent(translations.SHOW);
+
 		fireEvent.click(button);
+
 		expect(onClick).toHaveBeenCalled();
 	});
 
 	it("should render open", () => {
 		const { getByTestId } = render(<CollapseToggleButton isOpen={true} />);
+
 		const button = getByTestId("CollapseToggleButton");
-		expect(button).toHaveTextContent("Hide");
+
+		expect(button).toHaveTextContent(translations.HIDE);
 	});
 });
