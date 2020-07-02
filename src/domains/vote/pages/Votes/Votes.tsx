4521172a795efb3168d2aff29185fd5a5070e117
brenopolanski <breno.polanski@gmail.com>
2020-07-02 09:58:17 -0300

    refactor: votes page ui adjustments

--- src/domains/vote/pages/Votes/Votes.tsx
+++ src/domains/vote/pages/Votes/Votes.tsx
@@ -7,6 +7,7 @@ import { Header } from "app/components/Header";
 import { HeaderSearchBar } from "app/components/Header/HeaderSearchBar";
 import { Icon } from "app/components/Icon";
 import { Input } from "app/components/Input";
+import { NavigationBar } from "app/components/NavigationBar";
 import { SelectAsset } from "app/components/SelectAsset";
 import { TransactionDetail } from "app/components/TransactionDetail";
 import React, { useState } from "react";
@@ -52,7 +53,9 @@ export const Votes = ({ assets, addressList, delegateList }: VotesProps) => {
 	};
 
 	return (
-		<div data-testid="MyVotes" className="flex flex-col min-h-screen -m-5 bg-theme-neutral-200">
+		<div data-testid="MyVotes" className="flex flex-col min-h-screen -m-5 bg-theme-neutral-100">
+			<NavigationBar currencyIcon="Ark" balance="34,253.75" userInitials="IO" />
+
 			<Breadcrumbs crumbs={crumbs} className="p-5 font-semibold" />
 
 			<div className="flex flex-col flex-1 space-y-5">
@@ -62,7 +65,7 @@ export const Votes = ({ assets, addressList, delegateList }: VotesProps) => {
 						subtitle={t("VOTE.VOTES_PAGE.SUBTITLE")}
 						extra={
 							<div className="flex items-center justify-end">
-								<HeaderSearchBar />
+								<HeaderSearchBar placeholder="Enter the delegate’s name or address for a quick search" />
 							</div>
 						}
 					/>
