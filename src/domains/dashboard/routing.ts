51bf498ac7bbba2001a6ca4f662bb0fb59124df7
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-02 16:44:34 -0300

    feat: add entry portfolio route (all wallets)

--- src/domains/dashboard/routing.ts
+++ src/domains/dashboard/routing.ts
@@ -2,6 +2,11 @@ import { Dashboard } from "./pages";
 
 export const DashboardRoutes = [
 	{
+		path: "/portfolio",
+		exact: true,
+		component: Dashboard,
+	},
+	{
 		path: "/portfolio/:walletId",
 		exact: true,
 		component: Dashboard,
