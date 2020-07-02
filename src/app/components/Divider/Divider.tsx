15106696fdfaf9b98e24e5494db458979fcaea14
Breno Polanski <breno.polanski@gmail.com>
2020-07-01 23:08:21 -0300

    refactor: breadcrumbs component ui adjustments (#2330)

--- src/app/components/Divider/Divider.tsx
+++ src/app/components/Divider/Divider.tsx
@@ -5,10 +5,12 @@ import { getStyles } from "./Divider.styles";
 type DividerProps = {
 	type?: "horizontal" | "vertical";
 	dashed?: boolean;
+	className?: string;
 };
 
 export const Divider = styled.div<DividerProps>(getStyles);
 
 Divider.defaultProps = {
 	type: "horizontal",
+	className: "border-theme-neutral-300",
 };
