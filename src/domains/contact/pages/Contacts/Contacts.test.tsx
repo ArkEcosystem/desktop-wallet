598807c4dfb75384323163994ba55be3456a4b93
goga-m <goga.mamoulasvili@gmail.com>
2020-07-02 20:57:47 +0300

    test: add `Contacts` page coverage

--- src/domains/contact/pages/Contacts/Contacts.test.tsx
+++ src/domains/contact/pages/Contacts/Contacts.test.tsx
@@ -6,6 +6,24 @@ import { contacts } from "../../data";
 import { translations } from "../../i18n";
 import { Contacts } from "./Contacts";
 
+const assets = [
+	{
+		icon: "Ark",
+		name: "Ark Ecosystem",
+		className: "text-theme-danger-400 border-theme-danger-200",
+	},
+	{
+		icon: "Bitcoin",
+		name: "Bitcoin",
+		className: "text-theme-warning-400 border-theme-warning-200",
+	},
+	{
+		icon: "Ethereum",
+		name: "Ethereum",
+		className: "text-theme-neutral-800 border-theme-neutral-600",
+	},
+];
+
 describe("Contacts", () => {
 	it("should render", () => {
 		const { asFragment, getByTestId } = render(<Contacts contacts={[]} />);
@@ -34,7 +52,7 @@ describe("Contacts", () => {
 		["cancel", "contact-form__cancel-btn"],
 		["save", "contact-form__save-btn"],
 	])("should open & close add contact modal (%s)", async (buttonName, buttonId) => {
-		const { asFragment, getAllByTestId, getByTestId, queryByTestId } = render(<Contacts contacts={[]} />);
+		const { getAllByTestId, getByTestId, queryByTestId } = render(<Contacts contacts={[]} assets={assets} />);
 
 		fireEvent.click(getByTestId("contacts__add-contact-btn"));
 
@@ -42,29 +60,33 @@ describe("Contacts", () => {
 			expect(getByTestId("contact-form__save-btn")).toBeDisabled();
 			expect(getByTestId("contact-form__add-address-btn")).toBeDisabled();
 
+			const assetInput = getByTestId("select-asset__input");
+
 			expect(() => getAllByTestId("contact-form__address-list-item")).toThrow(/Unable to find an element by/);
 
-			act(() => {
+			await act(async () => {
+				await fireEvent.change(getByTestId("contact-form__address-input"), {
+					target: { value: "address" },
+				});
+
 				fireEvent.change(getByTestId("contact-form__name-input"), {
 					target: { value: "name" },
 				});
 
-				fireEvent.change(getByTestId("contact-form__network-select"), {
-					target: { value: "ark" },
+				fireEvent.change(assetInput, { target: { value: "Bitco" } });
+
+				fireEvent.keyDown(assetInput, { key: "Enter", code: 13 });
+
+				await waitFor(() => {
+					expect(queryByTestId("contact-form__add-address-btn")).not.toBeDisabled();
 				});
 
-				fireEvent.change(getByTestId("contact-form__address-input"), {
-					target: { value: "address" },
+				fireEvent.click(getByTestId("contact-form__add-address-btn"));
+
+				await waitFor(() => {
+					expect(getAllByTestId("contact-form__address-list-item")).toHaveLength(1);
 				});
 			});
-
-			await waitFor(() => {
-				expect(queryByTestId("contact-form__add-address-btn")).not.toBeDisabled();
-			});
-
-			fireEvent.click(getByTestId("contact-form__add-address-btn"));
-
-			expect(getAllByTestId("contact-form__address-list-item")).toHaveLength(1);
 		}
 
 		await waitFor(() => {
