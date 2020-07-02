ecccdf3999131f2507c07a1212dafaec69913adc
Edgar Goetzendorff <hello@dated.fun>
2020-07-02 09:41:40 +0200

    wip

--- src/domains/transaction/pages/UpdateRegistration/UpdateRegistration.test.tsx
+++ src/domains/transaction/pages/UpdateRegistration/UpdateRegistration.test.tsx
@@ -37,7 +37,13 @@ describe("UpdateRegistration", () => {
 	});
 
 	it("should render 2nd step", async () => {
-		const { asFragment, getByTestId } = render(<UpdateRegistration {...defaultFormValues} />);
+		let context;
+
+		await act(async () => {
+			context = render(<UpdateRegistration {...defaultFormValues} />);
+		});
+
+		const { asFragment, getByTestId } = context;
 
 		await act(async () => {
 			fireEvent.click(getByTestId("UpdateRegistration__continue-button"));
