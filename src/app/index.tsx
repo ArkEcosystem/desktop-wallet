fa38c4caf14a20636e19d8c9ad56334416bf7836
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-07-02 22:44:24 -0300

    feat: add demo environment (#2345)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,6 @@
+// Platform SDK
+import { ARK } from "@arkecosystem/platform-sdk-ark";
+import { Environment } from "@arkecosystem/platform-sdk-profiles";
 // React
 import React from "react";
 import { I18nextProvider } from "react-i18next";
@@ -6,22 +9,42 @@ import { withRouter } from "react-router-dom";
 
 // Routes
 import { routes } from "../router";
+// Fixtures
+import { identity } from "../tests/fixtures/identity";
+// UI Elements
 import { NavigationBar } from "./components/NavigationBar";
 // Context
-import { EnvironmentProvider } from "./contexts";
+import { EnvironmentConsumer, EnvironmentProvider } from "./contexts";
 // i18n
 import { i18n } from "./i18n";
 
-const routesWithoutNavBar = ["/", "/profile/create"];
+const routesWithoutNavBar = ["/", "s/create"];
+
+const buildMockEnvironment = async (env: Environment) => {
+	const profile = env.profiles().create("Anne Doe");
+	await profile.wallets().import(identity.mnemonic, ARK, "devnet");
+
+	env.persist();
+};
 
 export const App = withRouter(({ location }: any) => (
 	<I18nextProvider i18n={i18n}>
 		<main className={process.env.NODE_ENV === "development" ? "debug-screens" : ""}>
 			<EnvironmentProvider>
-				{!routesWithoutNavBar.includes(location.pathname) && (
-					<NavigationBar currencyIcon="Ark" balance="34,253.75" userInitials="IO" />
-				)}
-				{renderRoutes(routes)}
+				<EnvironmentConsumer>
+					{({ env }: any) => {
+						if (process.env.REACT_APP_BUILD_MODE === "demo") buildMockEnvironment(env);
+
+						return (
+							<>
+								{!routesWithoutNavBar.includes(location.pathname) && (
+									<NavigationBar currencyIcon="Ark" balance="34,253.75" userInitials="IO" />
+								)}
+								{renderRoutes(routes)}
+							</>
+						);
+					}}
+				</EnvironmentConsumer>
 			</EnvironmentProvider>
 		</main>
 	</I18nextProvider>
