38013b9b19e5bd647c1248544f0701921afe16f3
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-03 13:00:46 +0100

    refactor: change header component spacing

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -9,11 +9,11 @@ type Props = {
 export const Header = ({ title, subtitle, extra }: Props) => (
 	<div className="flex items-center justify-between bg-theme-background">
 		<div className="flex flex-col items-start">
-			<h1 className="text-3xl font-bold md:text-4xl" data-testid="header__title">
+			<h1 className="text-3xl font-bold md:text-4xl mb-0" data-testid="header__title">
 				{title}
 			</h1>
 			{subtitle && (
-				<div className="mt-2 text-theme-neutral-dark" data-testid="header__subtitle">
+				<div className="text-theme-neutral-dark" data-testid="header__subtitle">
 					{subtitle}
 				</div>
 			)}
