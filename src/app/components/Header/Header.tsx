ab49de49c09595349b8c34a9600ba579761c713d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-03 13:00:19 +0100

    fix: remove forced header width

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Header = ({ title, subtitle, extra }: Props) => (
 	<div className="flex items-center justify-between bg-theme-background">
-		<div className="flex flex-col items-start w-2/5">
+		<div className="flex flex-col items-start">
 			<h1 className="text-3xl font-bold md:text-4xl" data-testid="header__title">
 				{title}
 			</h1>
