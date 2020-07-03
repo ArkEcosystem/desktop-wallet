35eb54d977b1dcf6afbc521732b4ec1fc713c83b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-03 17:46:10 +0100

    refactor: total amount box styling

--- src/domains/transaction/components/TotalAmountBox/TotalAmountBox.tsx
+++ src/domains/transaction/components/TotalAmountBox/TotalAmountBox.tsx
@@ -17,14 +17,14 @@ export const TotalAmountBox = ({ transactionAmount, transactionFee, magnitude }:
 				<div className="grid grid-cols-2 divide-x divide-gray-400">
 					<div className="flex flex-col justify-center px-6 py-5">
 						<span className="text-sm text-theme-neutral-500">Transaction(s) Amount</span>
-						<span className="mt-3" data-testid="total-amount-box__transaction-amount">
+						<span className="mt-2 font-semibold" data-testid="total-amount-box__transaction-amount">
 							{transactionAmount} ARK
 						</span>
 					</div>
 
 					<div className="flex flex-col justify-center px-6 py-5 text-right">
 						<span className="text-sm text-theme-neutral-500">Transaction fee</span>
-						<span className="mt-3" data-testid="total-amount-box__transaction-fee">
+						<span className="mt-2 font-semibold text-lg" data-testid="total-amount-box__transaction-fee">
 							{transactionFee} ARK
 						</span>
 					</div>
