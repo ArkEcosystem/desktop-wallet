9e88f82cf9fbf7970b678c4c8cc8ceaaa35f2128
brenopolanski <breno.polanski@gmail.com>
2020-07-03 08:51:37 -0300

    refactor: InstallPlugin component ui adjustments

--- src/domains/plugin/components/InstallPlugin/InstallPlugin.stories.tsx
+++ src/domains/plugin/components/InstallPlugin/InstallPlugin.stories.tsx
@@ -1,26 +1,14 @@
 import { boolean } from "@storybook/addon-knobs";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { InstallPlugin } from "./InstallPlugin";
 
 export default { title: "Domains / Plugin / Components / InstallPlugin" };
 
-export const Default = () => {
-	const [step, setStep] = useState(1);
-
-	useEffect(() => {
-		if (step === 2) {
-			setTimeout(() => setStep(3), 3000);
-		}
-	}, [step]);
-
-	return (
-		<InstallPlugin
-			step={step}
-			isOpen={boolean("Is Open", true)}
-			onClose={() => alert("closed")}
-			onCancel={() => alert("cancelled")}
-			onDownload={() => setStep(2)}
-		/>
-	);
-};
+export const Default = () => (
+	<InstallPlugin
+		isOpen={boolean("Is Open", true)}
+		onClose={() => alert("closed")}
+		onCancel={() => alert("cancelled")}
+	/>
+);
--- src/domains/plugin/components/InstallPlugin/InstallPlugin.tsx
+++ src/domains/plugin/components/InstallPlugin/InstallPlugin.tsx
@@ -1,149 +1,154 @@
-import { Button } from "app/components/Button";
+
 import { Circle } from "app/components/Circle";
 import { CircularProgressBar } from "app/components/CircularProgressBar";
 import { Icon } from "app/components/Icon";
 import { Modal } from "app/components/Modal";
+import { TabPanel, Tabs } from "app/components/Tabs";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
 type InstallPluginProps = {
-	// TODO: This props `step` is used only as a mock to navigate between steps
-	step?: number;
 	isOpen: boolean;
 	onClose?: any;
 	onCancel?: any;
-	onDownload?: any;
-	onInstall?: any;
 };
 
-export const InstallPlugin = ({ step, isOpen, onClose, onCancel, onDownload, onInstall }: InstallPluginProps) => {
+export const FirstStep = () => {
 	const { t } = useTranslation();
 
 	return (
+		<section data-testid="InstallPlugin__first-step">
+			<p className="mt-4 text-base font-semibold text-theme-neutral-dark">
+				{t("PLUGINS.MODAL_INSTALL_PLUGIN.DESCRIPTION")}
+			</p>
+			<ul className="mt-2 text-sm leading-8 list-inside list-circle text-theme-neutral-dark">
+				<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_1")}</li>
+				<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_2")}</li>
+				<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_3")}</li>
+			</ul>
+		</section>
+	);
+};
+
+export const SecondStep = () => {
+	const { t } = useTranslation();
+
+	return (
+		<section data-testid="InstallPlugin__second-step">
+			<div className="flex mt-4">
+				<div className="flex-shrink-0 mr-6">
+					<img
+						className="rounded-xl"
+						src="https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/explorer/master/images/logo.png"
+					/>
+				</div>
+				<div className="flex-1">
+					<div className="flex flex-col justify-around h-full">
+						<div>
+							<p className="text-sm font-semibold text-theme-neutral-light">{t("COMMON.PLUGIN")}</p>
+							<p className="font-semibold text-theme-black">ARK Explorer</p>
+						</div>
+						<div className="flex justify-between">
+							<span>
+								<p className="text-sm font-semibold text-theme-neutral-light">
+									{t("COMMON.DOWNLOADED")}
+								</p>
+								<p className="text-sm font-bold text-theme-neutral-dark">154 KB / 154 KB</p>
+							</span>
+							<div className="mr-2">
+								<CircularProgressBar value={78} size={50} strokeWidth={4} fontSize={0.8} />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export const ThirdStep = () => {
+	const { t } = useTranslation();
+
+	return (
+		<section data-testid="InstallPlugin__third-step">
+			<div className="flex mt-4">
+				<div className="flex-shrink-0 mr-6">
+					<img
+						className="rounded-xl"
+						src="https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/explorer/master/images/logo.png"
+					/>
+				</div>
+				<div className="flex-1">
+					<div className="flex flex-col justify-around h-full">
+						<div>
+							<p className="text-sm font-semibold text-theme-neutral-light">{t("COMMON.PLUGIN")}</p>
+							<p className="font-semibold text-theme-black">ARK Explorer</p>
+						</div>
+						<div className="flex justify-between">
+							<span>
+								<p className="text-sm font-semibold text-theme-neutral-light">
+									{t("COMMON.DOWNLOADED")}
+								</p>
+								<p className="text-sm font-bold text-theme-neutral-dark">{t("COMMON.COMPLETED")}</p>
+							</span>
+							<div className="">
+								<Circle
+									size="lg"
+									className="relative z-10 bg-theme-background border-theme-neutral-300"
+								>
+									<span className="text-theme-success-600">
+										<Icon name="Checkmark" width={28} height={28} />
+									</span>
+								</Circle>
+								<Circle
+									size="lg"
+									className="relative z-0 -ml-1 bg-theme-background border-theme-success-600"
+								>
+									<span className="text-xs font-semibold text-theme-success-600">100%</span>
+								</Circle>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export const InstallPlugin = ({ isOpen, onClose, onCancel }: InstallPluginProps) => {
+	const { t } = useTranslation();
+	const [activeStep, setActiveStep] = React.useState(1);
+
+	const handleBack = () => {
+		setActiveStep(activeStep - 1);
+	};
+
+	const handleNext = () => {
+		setActiveStep(activeStep + 1);
+	};
+
+	return (
 		<Modal
-			title={t(step === 1 ? "COMMON.ATTENTION" : "COMMON.DOWNLOADED")}
+			title={t(activeStep === 1 ? "COMMON.ATTENTION" : "COMMON.DOWNLOADED")}
 			size="lg"
 			isOpen={isOpen}
 			onClose={onClose}
 		>
-			<div className="container">
-				{step === 1 && (
-					<>
-						<p className="mt-4 text-base font-semibold text-theme-neutral-dark">
-							{t("PLUGINS.MODAL_INSTALL_PLUGIN.DESCRIPTION")}
-						</p>
-						<ul className="mt-2 text-sm list-inside leading-8 list-circle text-theme-neutral-dark">
-							<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_1")}</li>
-							<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_2")}</li>
-							<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_3")}</li>
-						</ul>
-						<div className="flex justify-end mt-8 space-x-3">
-							<Button variant="plain" onClick={onCancel} data-testid="install-plugin__cancel-button">
-								{t("COMMON.CANCEL")}
-							</Button>
-							<Button onClick={onDownload} data-testid="install-plugin__download-button">
-								{t("COMMON.DOWNLOAD")}
-							</Button>
-						</div>
-					</>
-				)}
-
-				{step === 2 && (
-					<div className="flex mt-4">
-						<div className="flex-shrink-0 mr-6">
-							<img
-								className="rounded-xl"
-								src="https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/explorer/master/images/logo.png"
-							/>
-						</div>
-						<div className="flex-1">
-							<div className="flex flex-col justify-around h-full">
-								<div>
-									<p className="text-sm font-semibold text-theme-neutral-light">
-										{t("COMMON.PLUGIN")}
-									</p>
-									<p className="font-semibold text-theme-black">ARK Explorer</p>
-								</div>
-								<div className="flex justify-between">
-									<span>
-										<p className="text-sm font-semibold text-theme-neutral-light">
-											{t("COMMON.DOWNLOADED")}
-										</p>
-										<p className="text-sm font-bold text-theme-neutral-dark">154 KB / 154 KB</p>
-									</span>
-									<div className="mr-2">
-										<CircularProgressBar value={78} size={50} strokeWidth={4} fontSize={0.8} />
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				)}
-
-				{step === 3 && (
-					<>
-						<div className="flex mt-4">
-							<div className="flex-shrink-0 mr-6">
-								<img
-									className="rounded-xl"
-									src="https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/explorer/master/images/logo.png"
-								/>
-							</div>
-							<div className="flex-1">
-								<div className="flex flex-col justify-around h-full">
-									<div>
-										<p className="text-sm font-semibold text-theme-neutral-light">
-											{t("COMMON.PLUGIN")}
-										</p>
-										<p className="font-semibold text-theme-black">ARK Explorer</p>
-									</div>
-									<div className="flex justify-between">
-										<span>
-											<p className="text-sm font-semibold text-theme-neutral-light">
-												{t("COMMON.DOWNLOADED")}
-											</p>
-											<p className="text-sm font-bold text-theme-neutral-dark">
-												{t("COMMON.COMPLETED")}
-											</p>
-										</span>
-										<div className="">
-											<Circle
-												size="lg"
-												className="relative z-10 bg-theme-background border-theme-neutral-300"
-											>
-												<span className="text-theme-success-600">
-													<Icon name="Checkmark" width={28} height={28} />
-												</span>
-											</Circle>
-											<Circle
-												size="lg"
-												className="relative z-0 -ml-1 bg-theme-background border-theme-success-600"
-											>
-												<span className="text-xs font-semibold text-theme-success-600">
-													100%
-												</span>
-											</Circle>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="flex justify-end mt-8 space-x-3">
-							<Button variant="plain" onClick={onCancel} data-testid="install-plugin__cancel-button">
-								{t("COMMON.CANCEL")}
-							</Button>
-							<Button data-testid="install-plugin__install-button" onClick={onInstall}>
-								{t("COMMON.INSTALL")}
-							</Button>
-						</div>
-					</>
-				)}
-			</div>
+			<Tabs activeId={activeStep}>
+				<TabPanel tabId={1}>
+					<FirstStep />
+				</TabPanel>
+				<TabPanel tabId={2}>
+					<SecondStep />
+				</TabPanel>
+				<TabPanel tabId={3}>
+					<ThirdStep />
+				</TabPanel>
+			</Tabs>
 		</Modal>
 	);
 };
 
 InstallPlugin.defaultProps = {
-	step: 1,
 	isOpen: false,
 };
