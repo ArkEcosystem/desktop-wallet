722d325eb287e83a78396654fa69be1ad3be4280
goga-m <goga.mamoulasvili@gmail.com>
2020-07-03 15:15:44 +0300

    refactor: adjust styleguide rules on  `Header` component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,18 +7,18 @@ type Props = {
 };
 
 export const Header = ({ title, subtitle, extra }: Props) => (
-	<div className="flex items-center justify-between bg-theme-background">
-		<div className="flex flex-col items-start w-2/5">
-			<h1 className="text-3xl font-bold md:text-4xl" data-testid="header__title">
+	<div className="flex items-end justify-between bg-theme-background">
+		<div className="-mt-2">
+			<h1 className="mb-0 text-3xl font-bold md:text-4xl" data-testid="header__title">
 				{title}
 			</h1>
 			{subtitle && (
-				<div className="mt-2 text-theme-neutral-dark" data-testid="header__subtitle">
+				<div className="text-theme-neutral-dark" data-testid="header__subtitle">
 					{subtitle}
 				</div>
 			)}
 		</div>
 
-		{extra && <div className="w-3/5">{extra}</div>}
+		{extra && <div>{extra}</div>}
 	</div>
 );
