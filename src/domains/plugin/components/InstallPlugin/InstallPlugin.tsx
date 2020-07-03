29d7c0a1ae9a2caec719cc9a582d9fc05dfe55fc
brenopolanski <breno.polanski@gmail.com>
2020-07-03 09:28:57 -0300

    refactor: align list items

--- src/domains/plugin/components/InstallPlugin/InstallPlugin.tsx
+++ src/domains/plugin/components/InstallPlugin/InstallPlugin.tsx
@@ -19,14 +19,16 @@ export const FirstStep = () => {
 
 	return (
 		<section data-testid="InstallPlugin__first-step">
-			<p className="mt-4 text-base font-semibold text-theme-neutral-dark">
+			<p className="mt-4 text-lg font-semibold text-theme-neutral-dark">
 				{t("PLUGINS.MODAL_INSTALL_PLUGIN.DESCRIPTION")}
 			</p>
-			<ul className="mt-2 text-sm leading-8 list-inside list-circle text-theme-neutral-dark">
-				<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_1")}</li>
-				<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_2")}</li>
-				<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_3")}</li>
-			</ul>
+			<div className="max-w-sm">
+				<ul className="mt-2 ml-5 leading-8 list-outside list-circle text-theme-neutral-dark">
+					<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_1")}</li>
+					<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_2")}</li>
+					<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_3")}</li>
+				</ul>
+			</div>
 		</section>
 	);
 };
