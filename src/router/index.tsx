ee8a4adb16f4d2d867b979992f758bd16bffb905
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-07-03 23:30:08 -0300

    feat: add dashboard routes (#2347)

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,3 +1,4 @@
+import { DashboardRoutes } from "domains/dashboard/routing";
 import { ProfileRoutes } from "domains/profile/routing";
 
-export const routes: Array<Object> = [...ProfileRoutes];
+export const routes: Array<Object> = [...DashboardRoutes, ...ProfileRoutes];
