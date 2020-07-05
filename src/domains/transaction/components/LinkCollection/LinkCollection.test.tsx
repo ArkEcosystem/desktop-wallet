bc209902c9c4eb130682dc423edf70efeb622b65
goga-m <goga-m@users.noreply.github.com>
2020-07-05 18:38:31 +0000

    style: resolve style guide violations

--- src/domains/transaction/components/LinkCollection/LinkCollection.test.tsx
+++ src/domains/transaction/components/LinkCollection/LinkCollection.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { act,fireEvent, render } from "testing-library";
+import { act, fireEvent, render } from "testing-library";
 
 import { LinkCollection } from "./LinkCollection";
 
