fd4c7db547011bd355c239b52befb0fd53234f9f
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-06 13:28:28 -0300

    tests: add location params mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,13 @@
 import MockDate from "mockdate";
 
+jest.mock("react-router-dom", () => ({
+	...jest.requireActual("react-router-dom"),
+	useParams: () => ({
+		profileId: "bli",
+		walletId: "bleh",
+	}),
+}));
+
 beforeEach(() => {
 	MockDate.set(new Date("2020-07-01T00:00:00.000Z"));
 });
