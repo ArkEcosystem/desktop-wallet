642a5b2826fe71d26aab4a070b4c23a77d38e6a0
goga-m <goga-m@users.noreply.github.com>
2020-07-06 12:44:09 +0000

    style: resolve style guide violations

--- src/domains/contact/pages/Contacts/Contacts.tsx
+++ src/domains/contact/pages/Contacts/Contacts.tsx
@@ -97,7 +97,7 @@ export const Contacts = ({ contacts, assets, onSearch }: ContactsProps) => {
 					/>
 				</div>
 
-				<div className="flex flex-1 items-start px-10 py-16 bg-theme-background">
+				<div className="flex items-start flex-1 px-10 py-16 bg-theme-background">
 					{contacts.length === 0 && (
 						<div
 							data-testid="contacts__banner"
