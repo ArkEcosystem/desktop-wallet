1f8a130a1017a3d0c398aa81f7c68896fa695242
goga-m <goga.mamoulasvili@gmail.com>
2020-07-06 19:16:31 +0300

    refactor: reduce `Toggle` switch size

--- src/app/components/Toggle/Toggle.tsx
+++ src/app/components/Toggle/Toggle.tsx
@@ -6,11 +6,15 @@ const Input = styled.input`
 `;
 
 const Handle = styled.div`
-	${tw`inline-flex rounded-full relative h-2 w-10 cursor-pointer bg-theme-neutral-light`}
+	height: 7px;
+	${tw`inline-flex rounded-full relative w-9 cursor-pointer bg-theme-neutral-light`}
 `;
 
 const HandleInner = styled.span`
-    ${tw`h-5 w-5 bg-theme-neutral-light rounded-full absolute transform -translate-y-1/2 mt-1 transition transition-colors transition-transform ease-in-out duration-200`}
+	margin-top: 3px;
+	width: 18px;
+	height: 18px;
+    ${tw`bg-theme-neutral-light rounded-full absolute transform -translate-y-1/2 transition transition-colors transition-transform ease-in-out duration-200`}
 
     ${Input}:checked ~ ${Handle} & {
         ${tw`translate-x-full bg-theme-primary text-2xl`}
