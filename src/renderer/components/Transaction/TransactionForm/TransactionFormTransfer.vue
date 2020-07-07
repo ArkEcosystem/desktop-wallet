2b5545da759ede6243ec7f05883c3047180cf55c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-07 13:14:08 +0100

    fix: uri loading transaction

--- src/renderer/components/Transaction/TransactionForm/TransactionFormTransfer.vue
+++ src/renderer/components/Transaction/TransactionForm/TransactionFormTransfer.vue
@@ -611,18 +611,12 @@ export default {
 
       try {
         if (WalletService.validateAddress(this.schema.address, this.session_network.version)) {
-          if (this.schema.address && this.schema.amount) {
-            this.$v.form.recipients.$model = []
-            this.$v.form.recipients.$model.push({
-              address: this.schema.address,
-              amount: this.currency_unitToSub(this.schema.amount),
-              sendAll: false
-            })
-          } else {
-            this.$set(this, 'recipientId', this.schema.address || '')
-          }
-
+          this.$set(this, 'recipientId', this.schema.address || '')
           this.$set(this.form, 'vendorField', this.schema.vendorField || '')
+
+          if (this.schema.amount) {
+            this.$set(this, 'amount', this.schema.amount || '')
+          }
         } else {
           throw new Error(this.$t('VALIDATION.RECIPIENT_DIFFERENT_NETWORK', [
             this.wallet_truncate(this.schema.address)
