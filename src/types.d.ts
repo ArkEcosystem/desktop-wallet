d95a9d65161e9bb89dc1374f586b4726302754a5
Edgar Goetzendorff <hello@dated.fun>
2020-07-07 18:03:47 +0200

    refactor: add `ButtonVariant` constraint

--- src/types.d.ts
+++ src/types.d.ts
@@ -8,3 +8,5 @@ declare module "twin.macro" {
 }
 
 export type Size = "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "icon";
+
+export type ButtonVariant = "solid" | "plain" | "outline";
