70b42fa744228d0f730d2a7cba41c60469545787
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-07-08 05:20:19 -0300

    feat: add `TransactionRowRecipientIcon` component

---
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowRecipientIcon.tsx
@@ -0,0 +1,66 @@
+import { Avatar } from "app/components/Avatar";
+import { Circle } from "app/components/Circle";
+import { Icon } from "app/components/Icon";
+import React from "react";
+
+type Props = {
+	type: string;
+	recipient?: string;
+	recipients?: { amount: string; address: string }[];
+	className?: string;
+};
+
+const Wrapper = ({ children, className }: { children: React.ReactNode; className?: string }) => {
+	return (
+		<Circle
+			data-testid="TransactionRowRecipientIcon"
+			className={className ? className : "bg-theme-background border-theme-neutral-900 text-theme-neutral-900"}
+		>
+			{children}
+		</Circle>
+	);
+};
+
+export const TransactionRowRecipientIcon = ({ type, recipient, recipients, className }: Props) => {
+	const transactionIcon: Record<string, string> = {
+		transfer: "Transfer",
+		secondSignature: "Key",
+		delegateRegistration: "Delegate",
+		vote: "Voted",
+		multiSignature: "Multisig",
+		ipfs: "Ipfs",
+		multiPayment: "Multisig",
+		delegateResignation: "Delegate",
+		htlcLock: "StatusClock",
+		htlcClaim: "StatusClock",
+		htlcRefund: "StatusClock",
+		businessRegistration: "Business",
+		businessResignation: "Business",
+		businessUpdate: "Business",
+		bridgechainRegistration: "Bridgechain",
+		bridgechainResignation: "Bridgechain",
+		bridgechainUpdate: "Bridgechain",
+	};
+
+	if (type === "transfer") {
+		return <Avatar address={recipient} />;
+	}
+
+	if (type === "multiPayment") {
+		return (
+			<Wrapper className={className}>
+				<span>{recipients!.length}</span>
+			</Wrapper>
+		);
+	}
+
+	return (
+		<Wrapper>
+			<Icon name={transactionIcon[type]} />
+		</Wrapper>
+	);
+};
+
+TransactionRowRecipientIcon.defaultProps = {
+	recipients: [],
+};
