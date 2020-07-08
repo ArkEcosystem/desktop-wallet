1e7d166be287ba3f3f1d14810155fde71c7f1687
brenopolanski <breno.polanski@gmail.com>
2020-07-08 12:12:04 -0300

    wip

--- src/domains/news/components/NewsSectionOptions/components/CategoryControl/CategoryControl.tsx
+++ src/domains/news/components/NewsSectionOptions/components/CategoryControl/CategoryControl.tsx
@@ -15,9 +15,29 @@ const Input = styled.input`
 	${tw`sr-only`}
 `;
 
+const CustomButton = styled.div`
+	${tw`p-6 transition-colors duration-200 border-2 rounded-lg border-theme-primary-contrast`}
+	${Input}:checked + & {
+		${tw`bg-theme-success-contrast border-theme-success text-theme-success-contrast`}
+	}
+`;
+
 export const CategoryControl = React.forwardRef<HTMLInputElement, Props>(
-	({ children, type, checked, defaultChecked, readonly, value, name, ...props }: Props, ref) => (
-		<label htmlFor={name} />
+	({ children, type, checked, defaultChecked, onChange, readonly, value, name, ...props }: Props, ref) => (
+		<label htmlFor={name}>
+			<Input
+				data-testid={`CategoryControl__${name}`}
+				ref={ref}
+				type={type}
+				checked={checked}
+				onChange={onChange}
+				defaultChecked={defaultChecked}
+				readOnly={readonly}
+				value={value}
+				name={name}
+			/>
+			<CustomButton>{children}</CustomButton>
+		</label>
 	),
 );
 
