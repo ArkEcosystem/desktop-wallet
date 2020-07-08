f20867751a13b0e33fd3685ef3b66f3574ac9759
brenopolanski <breno.polanski@gmail.com>
2020-07-07 21:43:38 -0300

    chore: add data

--- src/domains/news/components/NewsCard/NewsCard.stories.tsx
+++ src/domains/news/components/NewsCard/NewsCard.stories.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
+import { news } from "../../data";
 import { NewsCard } from "./NewsCard";
 
 export default { title: "Domains / News / Components / News Card" };
 
-export const Default = () => <NewsCard />;
+export const Default = () => <NewsCard {...news[0]} />;
+
+export const WithCoverImage = () => <NewsCard {...news[0]} />;
