253243663b5006b4c451dcac0e0fab7ed9f7c4a0
brenopolanski <breno.polanski@gmail.com>
2020-07-07 21:24:10 -0300

    feat: add props

--- src/domains/news/components/NewsCard/NewsCard.tsx
+++ src/domains/news/components/NewsCard/NewsCard.tsx
@@ -5,44 +5,63 @@ import { Icon } from "app/components/Icon";
 import { Label } from "app/components/Label";
 import React from "react";
 
-export const NewsCard = () => (
+type Asset = {
+	icon: string;
+	name: string;
+	className?: string;
+};
+
+type Author = {
+	name: string;
+	role: string;
+};
+
+type Props = {
+	asset: Asset;
+	author: Author;
+	dateCreated: string;
+	category: string;
+	content: string;
+	coverImage?: string;
+};
+
+export const NewsCard = ({ asset, author, dateCreated, category, content, coverImage }: Props) => (
 	<Card>
 		<div className="flex flex-col p-4 space-y-8">
 			<div className="flex justify-between w-full">
 				<div className="flex items-center space-x-4">
-					<Circle className="text-theme-danger-400 border-theme-danger-200" size="lg">
-						<Icon name="Ark" width={20} height={20} />
+					<Circle className={asset?.className} size="lg">
+						<Icon name={asset?.icon} width={20} height={20} />
 					</Circle>
 
 					<div>
-						<p className="text-lg font-semibold">ARK Ecosystem</p>
-
+						<p className="text-lg font-semibold">{asset?.name}</p>
 						<div className="flex items-center space-x-4">
-							<p className="text-sm font-semibold text-theme-neutral">Travis Walker, Co-Founder</p>
+							<p className="text-sm font-semibold text-theme-neutral">
+								{author?.name}, {author?.role}
+							</p>
 							<Divider type="vertical" />
-							<p className="text-sm font-semibold text-theme-neutral">2 days ago</p>
+							<p className="text-sm font-semibold text-theme-neutral">{dateCreated}</p>
 						</div>
 					</div>
 				</div>
 
 				<div className="flex flex-col justify-end">
 					<Label color="warning">
-						<span className="text-sm">#Technical</span>
+						<span className="text-sm">#{category}</span>
 					</Label>
 				</div>
 			</div>
 
 			<Divider />
 
-			<p className="text-theme-neutral-dark">
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, voluptate! Rem molestiae mollitia error
-				quidem nostrum saepe explicabo quas velit ipsum facere, excepturi iste. Minus dolore alias modi
-				recusandae ab.
-			</p>
+			<p className="text-theme-neutral-dark">{content}</p>
 
-			<div className="flex justify-center p-1 -mx-10 border-t-2 border-theme-primary-contrast">
-				<img src="https://miro.medium.com/max/1000/1*vMM0Z7qfGgA8a8NRhB_ZQA.png" alt="ARK Banner" />
-			</div>
+			{coverImage && (
+				<div className="flex justify-center p-1 -mx-10 border-t-2 border-theme-primary-contrast">
+					<img src={coverImage} alt="ARK Banner" />
+				</div>
+			)}
 		</div>
 	</Card>
 );
