83d8a1eaa68f45d071d8ee012bf6fd30aef966d7
brenopolanski <breno.polanski@gmail.com>
2020-07-07 21:44:21 -0300

    chore: update stories

--- src/domains/news/components/NewsCard/NewsCard.stories.tsx
+++ src/domains/news/components/NewsCard/NewsCard.stories.tsx
@@ -7,4 +7,4 @@ export default { title: "Domains / News / Components / News Card" };
 
 export const Default = () => <NewsCard {...news[0]} />;
 
-export const WithCoverImage = () => <NewsCard {...news[0]} />;
+export const WithCoverImage = () => <NewsCard {...news[1]} />;
