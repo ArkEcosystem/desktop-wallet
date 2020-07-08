e8830d1e1836c5ffd8900968972c3911d7be6016
Breno Polanski <breno.polanski@gmail.com>
2020-07-07 22:57:32 -0300

    feat: add `NewsCard` component (#2399)

---
+++ src/domains/news/components/NewsCard/NewsCard.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { news } from "../../data";
+import { NewsCard } from "./NewsCard";
+
+export default { title: "Domains / News / Components / News Card" };
+
+export const Default = () => <NewsCard {...news[0]} />;
+
+export const WithCoverImage = () => <NewsCard {...news[1]} />;
