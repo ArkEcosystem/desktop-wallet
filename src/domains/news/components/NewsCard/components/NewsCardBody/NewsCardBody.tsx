b9add8289026ef51c6a35a66dca234435e0ff0c5
brenopolanski <breno.polanski@gmail.com>
2020-07-07 21:07:21 -0300

    wip

--- src/domains/news/components/NewsCard/components/NewsCardBody/NewsCardBody.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-type Props = {
-	children: React.ReactNode;
-};
-
-export const NewsCardBody = ({ children }: Props) => <div data-testid="NewsCardBody">{children}</div>;
