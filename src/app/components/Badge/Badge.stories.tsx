5a22afb890bda473909181d77447162f264a17f1
Edgar Goetzendorff <hello@dated.fun>
2020-07-08 04:32:51 +0200

    refactor: add `position` prop to Badges component (#2397)

--- src/app/components/Badge/Badge.stories.tsx
+++ src/app/components/Badge/Badge.stories.tsx
@@ -13,27 +13,31 @@ export const Default = () => {
 		<div>
 			<div className="w-64 text--xs">Parent must have relative class</div>
 			<div className="w-64 text--xs">Colors, positions, size of badge controled by tailwind classes.</div>
-			<div className="my-10">
+
+			<div className="mt-5 mb-10 space-x-5">
 				<Circle className="relative border-theme-neutral-200">
-					<Badge className="border-theme-neutral-200 -bottom-2 -right-2" />
+					<Badge className="border-theme-neutral-200" position="top" />
 				</Circle>
-			</div>
-			<div className="text-sm color--neutral-600" />
-			<div className="mt-3 mb-10">
-				<Circle className="relative mr-3 border-theme-neutral-200">
-					<Badge className="border-theme-neutral-200 -bottom-2 -right-2" />
+				<Circle className="relative border-theme-neutral-200">
+					<Badge className="border-theme-neutral-200" position="top-right" />
 				</Circle>
-				<Circle className="relative mr-3 border-theme-neutral-200">
-					<Badge className="border-theme-neutral-200 -right-2 -top-2" />
+				<Circle className="relative border-theme-neutral-200">
+					<Badge className=" border-theme-neutral-200" position="right" />
 				</Circle>
-				<Circle className="relative mr-3 border-theme-neutral-200">
-					<Badge className=" border-theme-neutral-200 -bottom-2 -left-2" />
+				<Circle className="relative border-theme-neutral-200">
+					<Badge className="border-theme-neutral-200" position="bottom-right" />
 				</Circle>
-				<Circle className="relative mr-3 border-theme-neutral-200">
-					<Badge className="border-theme-neutral-200 -left-2 -top-2" />
+				<Circle className="relative border-theme-neutral-200">
+					<Badge className="border-theme-neutral-200" position="bottom" />
 				</Circle>
-				<Circle className="relative mr-3 border-theme-neutral-200">
-					<Badge className="border-theme-neutral-200 -right-4 top-2" />
+				<Circle className="relative border-theme-neutral-200">
+					<Badge className=" border-theme-neutral-200" position="bottom-left" />
+				</Circle>
+				<Circle className="relative border-theme-neutral-200">
+					<Badge className="border-theme-neutral-200" position="left" />
+				</Circle>
+				<Circle className="relative border-theme-neutral-200">
+					<Badge className="border-theme-neutral-200" position="top-left" />
 				</Circle>
 			</div>
 		</div>
@@ -42,27 +46,31 @@ export const Default = () => {
 
 export const Colored = () => {
 	return (
-		<div>
-			<div className="mb-10">
-				<Circle className="relative border-theme-neutral-200">
-					<Badge className="border-theme-neutral-200 -bottom-2 -right-2" />
-				</Circle>
-			</div>
-
-			<div className="mt-3 mb-10">
-				<Circle className="relative text-white border-theme-success-500">
-					<Badge className="bg-theme-success-500 -top-1 -right-4" />
-				</Circle>
-				<Circle className="relative ml-5 text-white border-theme-warning-500">
-					<Badge className="bg-theme-warning-500 -bottom-1 -right-4" />
-				</Circle>
-				<Circle className="relative text-white border-theme-primary-500 ml-7">
-					<Badge className="border-theme-primary-500 -top-1 -left-4" />
-				</Circle>
-				<Circle className="relative ml-5 text-white border-theme-danger-400">
-					<Badge className="border-theme-danger-400 top-2 -right-4" />
-				</Circle>
-			</div>
+		<div className="mt-3 mb-10 space-x-5">
+			<Circle className="relative text-white border-theme-success-500">
+				<Badge className="bg-theme-success-500" position="top" />
+			</Circle>
+			<Circle className="relative text-white border-theme-warning-500">
+				<Badge className="bg-theme-warning-500" position="top-right" />
+			</Circle>
+			<Circle className="relative text-white border-theme-primary-500 ml-7">
+				<Badge className="border-theme-primary-500" position="right" />
+			</Circle>
+			<Circle className="relative text-white border-theme-danger-400">
+				<Badge className="border-theme-danger-400" position="bottom-right" />
+			</Circle>
+			<Circle className="relative text-white border-theme-success-500">
+				<Badge className="bg-theme-success-500" position="bottom" />
+			</Circle>
+			<Circle className="relative text-white border-theme-warning-500">
+				<Badge className="bg-theme-warning-500" position="bottom-left" />
+			</Circle>
+			<Circle className="relative text-white border-theme-primary-500 ml-7">
+				<Badge className="border-theme-primary-500" position="left" />
+			</Circle>
+			<Circle className="relative text-white border-theme-danger-400">
+				<Badge className="border-theme-danger-400" position="top-left" />
+			</Circle>
 		</div>
 	);
 };
@@ -73,37 +81,28 @@ export const WithIcon = () => {
 			<div className="mt-3 mb-10">
 				<Circle className="relative border-theme-success-500 text-theme-success-500">
 					<Icon name="Ark" />
-					<Badge
-						className="-bottom-1 -right-4 bg-theme-success-500 text-theme-success-contrast"
-						icon="Checkmark"
-					/>
+					<Badge className="bg-theme-success-500 text-theme-success-contrast" icon="Checkmark" />
 				</Circle>
 				<Circle className="relative ml-5 border-theme-success-500 text-theme-success-500">
 					<Icon name="Eth" />
-					<Badge
-						className="bg-theme-success-500 -bottom-1 -right-4 text-theme-success-contrast"
-						icon="Checkmark"
-					/>
+					<Badge className="bg-theme-success-500 text-theme-success-contrast" icon="Checkmark" />
 				</Circle>
 				<Circle className="relative ml-5 border-theme-success-500 text-theme-success-500">
 					<Icon name="Btc" />
-					<Badge
-						className="bg-theme-success-500 -bottom-1 -right-4 text-theme-success-contrast"
-						icon="Checkmark"
-					/>
+					<Badge className="bg-theme-success-500 text-theme-success-contrast" icon="Checkmark" />
 				</Circle>
 				<Circle className="relative ml-5 border-theme-neutral-200">
 					<div className="italic text-md text-theme-neutral-200">D</div>
-					<Badge className="border-theme-neutral-200 -bottom-1 -right-4" />
+					<Badge className="border-theme-neutral-200" />
 				</Circle>
 				<Circle className="relative ml-5 border-theme-neutral-200">
 					<div className="italic text-md text-theme-neutral-200">D</div>
-					<Badge className="border-theme-neutral-200 -bottom-1 -right-4" />
+					<Badge className="border-theme-neutral-200" />
 				</Circle>
 				<Circle className="relative ml-5 border-theme-primary-100">
 					<div className="text-xs text-theme-primary-500">All</div>
 					<Badge
-						className="border-theme-primary-100 -bottom-1 -right-4 text-theme-primary-500"
+						className="border-theme-primary-100 text-theme-primary-500"
 						icon="ChevronDown"
 						iconWidth={10}
 						iconHeight={10}
--- src/app/components/Badge/Badge.tsx
+++ src/app/components/Badge/Badge.tsx
@@ -2,7 +2,7 @@ import { Icon } from "app/components/Icon";
 import React from "react";
 import { styled } from "twin.macro";
 
-import { defaultClasses, defaultStyle } from "./Badge.styles";
+import { defaultClasses, getStyles } from "./Badge.styles";
 
 type BadgeProps = {
 	className?: string;
@@ -10,15 +10,14 @@ type BadgeProps = {
 	icon?: string;
 	iconWidth?: number;
 	iconHeight?: number;
+	position?: "top" | "top-right" | "right" | "bottom-right" | "bottom" | "bottom-left" | "left" | "top-left";
 };
 
-export const Wrapper = styled.span`
-	${defaultStyle}
-`;
+export const Wrapper = styled.span<BadgeProps>(getStyles);
 
-export const Badge = ({ className, children, icon, iconWidth, iconHeight }: BadgeProps) => {
+export const Badge = ({ className, children, icon, iconWidth, iconHeight, position }: BadgeProps) => {
 	return (
-		<Wrapper className={`${defaultClasses} ${className}`}>
+		<Wrapper position={position} className={`${defaultClasses} ${className}`}>
 			{!!icon && <Icon name={icon} width={iconWidth} height={iconHeight} />}
 			<span>{children}</span>
 		</Wrapper>
@@ -28,4 +27,5 @@ export const Badge = ({ className, children, icon, iconWidth, iconHeight }: Badg
 Badge.defaultProps = {
 	iconWidth: 12,
 	iconHeight: 12,
+	position: "bottom-right",
 };
