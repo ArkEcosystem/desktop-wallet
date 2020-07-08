13e9c084d93f63dc4834a6c6ca9ca544a5a43141
brenopolanski <breno.polanski@gmail.com>
2020-07-07 22:03:56 -0300

    test: add tests

--- src/domains/news/components/NewsCard/NewsCard.tsx
+++ src/domains/news/components/NewsCard/NewsCard.tsx
@@ -27,35 +27,46 @@ type Props = {
 
 export const NewsCard = ({ asset, author, dateCreated, category, content, coverImage }: Props) => (
 	<Card>
-		<div className="flex flex-col p-4 space-y-8">
+		<div className="flex flex-col p-4 space-y-8" data-testid="NewsCard">
 			<div className="flex justify-between w-full">
 				<div className="flex items-center space-x-4">
-					<Circle className={asset?.className} size="lg">
+					<Circle className={asset?.className} size="lg" data-testid={`NewsCard__asset-${asset?.icon}`}>
 						<Icon name={asset?.icon} width={20} height={20} />
 					</Circle>
 
 					<div>
-						<p className="text-lg font-semibold">{asset?.name}</p>
+						<p className="text-lg font-semibold" data-testid={`NewsCard__asset-${asset?.name}`}>
+							{asset?.name}
+						</p>
 						<div className="flex items-center space-x-4">
-							<p className="text-sm font-semibold text-theme-neutral">
+							<p className="text-sm font-semibold text-theme-neutral" data-testid="NewsCard__author">
 								{author?.name}, {author?.role}
 							</p>
 							<Divider type="vertical" />
-							<p className="text-sm font-semibold text-theme-neutral">{dateCreated}</p>
+							<p
+								className="text-sm font-semibold text-theme-neutral"
+								data-testid="NewsCard__date-created"
+							>
+								{dateCreated}
+							</p>
 						</div>
 					</div>
 				</div>
 
 				<div className="flex flex-col justify-end">
 					<Label color="warning">
-						<span className="text-sm">#{category}</span>
+						<span className="text-sm" data-testid="NewsCard__category">
+							#{category}
+						</span>
 					</Label>
 				</div>
 			</div>
 
 			<Divider />
 
-			<p className="text-theme-neutral-dark">{content}</p>
+			<p className="text-theme-neutral-dark" data-testid="NewsCard__content">
+				{content}
+			</p>
 
 			{coverImage && (
 				<div className="flex justify-center p-1 -mx-10 border-t-2 border-theme-primary-contrast">
