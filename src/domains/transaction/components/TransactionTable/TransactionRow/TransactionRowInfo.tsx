6f524c1c6851b0f802515917bbe01a23e100fafd
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-07-09 07:31:39 -0300

    feat: add `TransactionRowInfo` component

---
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowInfo.tsx
@@ -0,0 +1,38 @@
+import Tippy from "@tippyjs/react";
+import { Icon } from "app/components/Icon";
+import React from "react";
+
+const VendorField = ({ vendorField }: { vendorField: string }) => {
+	return (
+		<Tippy content={vendorField}>
+			<span className="p-1">
+				<Icon data-testid="TransactionRowInfo__vendorField" name="Smartbridge" />
+			</span>
+		</Tippy>
+	);
+};
+
+const MultiSignature = () => {
+	// TODO: i18n
+	return (
+		<Tippy content="MultiSignature">
+			<span className="p-1">
+				<Icon data-testid="TransactionRowInfo__multiSignature" name="Smartbridge" />
+			</span>
+		</Tippy>
+	);
+};
+
+type Props = {
+	vendorField?: string;
+	isMultiSignature?: boolean;
+};
+
+export const TransactionRowInfo = ({ vendorField, isMultiSignature }: Props) => {
+	return (
+		<div className="flex items-center space-x-2">
+			{isMultiSignature && <MultiSignature />}
+			{vendorField && <VendorField vendorField={vendorField} />}
+		</div>
+	);
+};
