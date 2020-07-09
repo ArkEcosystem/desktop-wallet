00177ab0fb8112bdd8f8149fe3a09363111a05f1
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-09 13:44:16 -0300

    feat: make wallet card redirect to details using to the wallet id

--- src/app/components/WalletCard/WalletCard.test.tsx
+++ src/app/components/WalletCard/WalletCard.test.tsx
@@ -1,44 +1,101 @@
+import { ARK } from "@arkecosystem/platform-sdk-ark";
+import { Environment } from "@arkecosystem/platform-sdk-profiles";
+import { EnvironmentContext } from "app/contexts";
+import { httpClient } from "app/services";
+import { createMemoryHistory } from "history";
 import React from "react";
-import { render } from "testing-library";
+import { Route } from "react-router-dom";
+import { renderWithRouter } from "testing-library";
+import { StubStorage } from "tests/mocks";
 
 import { WalletCard } from "./WalletCard";
 
 describe("Formatted Address", () => {
+	const history = createMemoryHistory();
+	const env = new Environment({ coins: { ARK }, httpClient, storage: new StubStorage() });
+	const profile = env.profiles().create("Jane Doe");
+
+	const dashboardURL = `/profiles/${profile.id()}/dashboard`;
+	history.push(dashboardURL);
+
 	it("should render", () => {
-		const { container } = render(<WalletCard />);
+		const { container } = renderWithRouter(
+			<EnvironmentContext.Provider value={env}>
+				<Route path="/profiles/:profileId/dashboard">
+					<WalletCard id="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT" />
+				</Route>
+			</EnvironmentContext.Provider>,
+			{
+				routes: [dashboardURL],
+				history,
+			},
+		);
+
 		expect(container).toMatchSnapshot();
 	});
 
 	it("should render blank", () => {
-		const { container } = render(<WalletCard isBlank={true} />);
+		const { container } = renderWithRouter(
+			<EnvironmentContext.Provider value={env}>
+				<Route path="/profiles/:profileId/dashboard">
+					<WalletCard isBlank id="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT" />
+				</Route>
+			</EnvironmentContext.Provider>,
+			{
+				routes: [dashboardURL],
+				history,
+			},
+		);
+
 		expect(container).toMatchSnapshot();
 	});
 
 	it("should render with wallet data", () => {
-		const { container } = render(
-			<WalletCard
-				coinIcon="Bitcoin"
-				coinClass="border-theme-warning-200"
-				avatarId="test"
-				walletName="My wallet"
-				address="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
-				balance="100,000 BTC"
-			/>,
+		const { container } = renderWithRouter(
+			<EnvironmentContext.Provider value={env}>
+				<Route path="/profiles/:profileId/dashboard">
+					<WalletCard
+						id="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
+						coinIcon="Bitcoin"
+						coinClass="border-theme-warning-200"
+						avatarId="test"
+						walletName="My wallet"
+						address="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
+						balance="100,000 BTC"
+					/>
+					,
+				</Route>
+			</EnvironmentContext.Provider>,
+			{
+				routes: [dashboardURL],
+				history,
+			},
 		);
+
 		expect(container).toMatchSnapshot();
 	});
 
 	it("should render with wallet data and optional icon", () => {
-		const { container } = render(
-			<WalletCard
-				coinIcon="Bitcoin"
-				coinClass="border-theme-warning-200"
-				avatarId="test"
-				walletName="My wallet"
-				address="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
-				balance="100,000 BTC"
-			/>,
+		const { container } = renderWithRouter(
+			<EnvironmentContext.Provider value={env}>
+				<Route path="/profiles/:profileId/dashboard">
+					<WalletCard
+						id="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
+						coinIcon="Bitcoin"
+						coinClass="border-theme-warning-200"
+						avatarId="test"
+						walletName="My wallet"
+						address="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT"
+						balance="100,000 BTC"
+					/>
+				</Route>
+			</EnvironmentContext.Provider>,
+			{
+				routes: [dashboardURL],
+				history,
+			},
 		);
+
 		expect(container).toMatchSnapshot();
 	});
 });
