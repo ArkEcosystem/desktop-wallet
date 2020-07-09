abf7bafbdfb740ac4a04afbb5c1d6b4d28d93bbd
goga-m <goga.mamoulasvili@gmail.com>
2020-07-09 14:09:58 +0300

    refactor: apply msq ui

--- src/app/components/SearchBar/SearchBarPluginFilters/SearchBarPluginFilters.tsx
+++ src/app/components/SearchBar/SearchBarPluginFilters/SearchBarPluginFilters.tsx
@@ -12,17 +12,19 @@ type FilterProps = {
 	categories: Category[];
 	ratingsLabel?: string;
 	ratingsSuffix?: string;
+	claimedLabel?: string;
+	resetFiltersLabel?: string;
 	ratings?: number[];
 };
 
 const Stars = ({ length = 5, value }: any) => {
 	const stars = Array.from({ length });
 	return (
-		<span className="flex text-theme-neutral-200 space-x-1">
+		<span className="flex text-theme-neutral-700">
 			{stars.map((_, index: number) => {
 				return (
-					<span key={index} className={`${index < value ? "text-theme-warning-300" : ""}`}>
-						<Icon name={`${index < value ? "Star" : "StarOutline"}`} width={20} height={20} stroke="none" />
+					<span key={index} className={`${index < value ? "text-theme-warning-300" : ""} mr-1`}>
+						<Icon name={`${index < value ? "Star" : "StarOutline"}`} width={20} height={20} />
 					</span>
 				);
 			})}
@@ -37,18 +39,18 @@ const RatingsCheckboxes = ({ ratings, suffixLabel }: any) => {
 				ratings.map((rating: number) => {
 					return (
 						<label
-							className="flex items-center block mt-2 cursor-pointer space-x-3 text-theme-neutral-700"
+							className="flex items-center block px-2 pb-1 cursor-pointer space-x-2 text-theme-neutral-700 hover:bg-theme-neutral-100 rounded-md"
 							key={rating}
 						>
 							<span>
 								<Checkbox
 									name="test"
 									type="radio"
-									className="rounded-lg"
+									className="rounded-lg mt-px"
 									data-testid={`SearchBarPluginFilters-rating-${rating}`}
 								/>
 							</span>
-							<span className="flex items-center mt-1 space-x-2">
+							<span className="flex items-center mt-1 space-x-1">
 								<Stars value={rating} />
 								<span>{suffixLabel}</span>
 							</span>
@@ -66,7 +68,7 @@ const CategoryCheckboxes = ({ categories }: any) => {
 				categories.map((category: Category, index: number) => {
 					return (
 						<label
-							className="flex items-center block mt-2 cursor-pointer space-x-3 text-theme-neutral-700"
+							className="flex items-center block px-2 pb-1 mb-1 cursor-pointer space-x-2 text-theme-neutral-700 hover:bg-theme-neutral-100 rounded-md"
 							key={index}
 						>
 							<span>
@@ -82,6 +84,7 @@ const CategoryCheckboxes = ({ categories }: any) => {
 		</fieldset>
 	);
 };
+
 export const SearchBarPluginFilters = ({
 	categories,
 	ratings,
@@ -99,14 +102,32 @@ export const SearchBarPluginFilters = ({
 					</div>
 				}
 			>
-				<div className="w-64 px-8 py-6">
-					<div className="mb-2 font-semibold text-theme-neutral-700">{categoriesLabel}</div>
+				<div className="w-64 px-6 py-4">
+					<label className="flex items-center block px-2 pb-1 cursor-pointer space-x-3 text-theme-neutral-700 hover:bg-theme-neutral-100 rounded-md">
+						<span>
+							<Checkbox
+								name="claim"
+								className="rounded-lg"
+								data-testid={`SearchBarPluginFilters-claim`}
+							/>
+						</span>
+						<span className="ml-1 mt-1">Claimed</span>
+					</label>
+
+					<div className="mt-3 mb-4 border-b border-dashed border-theme-neutral-200" />
+					<div className="mb-1 font-semibold text-theme-neutral-700">{categoriesLabel}</div>
 					<CategoryCheckboxes categories={categories} />
 
-					<div className="my-8 border-b border-theme-neutral-200" />
+					<div className="my-4 border-b border-dashed border-theme-neutral-200" />
 
-					<div className="mb-2 font-semibold text-theme-neutral-700">{ratingsLabel}</div>
+					<div className="font-semibold text-theme-neutral-700">{ratingsLabel}</div>
 					<RatingsCheckboxes ratings={ratings} suffixLabel={ratingsSuffix} />
+
+					<div className="my-4 border-b border-dashed border-theme-neutral-200" />
+					<div className="flex items-center text-theme-primary-500 hover:text-theme-primary-600 cursor-pointer pl-2 hover:underline">
+						<Icon name="Reset" />
+						<span className="pl-2">Reset Filters</span>
+					</div>
 				</div>
 			</Dropdown>
 		</div>
@@ -136,4 +157,6 @@ SearchBarPluginFilters.defaultProps = {
 	],
 	ratings: [4, 3, 2, 1],
 	ratingsSuffix: "& up",
+	claimedLabel: "Claimed",
+	resetFiltersLabel: "Reset Filters",
 };
