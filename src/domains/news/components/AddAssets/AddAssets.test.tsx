70b96aa4b02aa65faee490b094624d544699f754
brenopolanski <breno.polanski@gmail.com>
2020-07-09 22:57:56 -0300

    feat: add assets modal

---
+++ src/domains/news/components/AddAssets/AddAssets.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "testing-library";
+
+import { assets } from "../../data";
+import { AddAssets } from "./AddAssets";
+
+describe("AddAssets", () => {
+	const allAssets = [...Array(27)].map(() => "Ark");
+
+	it("should not render if not open", () => {
+		const { asFragment, getByTestId } = render(<AddAssets isOpen={false} onUpdate={() => void 0} />);
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal", () => {
+		const { asFragment } = render(
+			<AddAssets selectedAssets={assets} allAssets={allAssets} isOpen={true} onUpdate={() => void 0} />,
+		);
+
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
