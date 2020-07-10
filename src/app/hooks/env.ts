96310841817f6efe36841a45d5f116060c9b9ff5
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-10 09:15:03 -0300

    tests(coverage): add istanbul ignore

--- src/app/hooks/env.ts
+++ src/app/hooks/env.ts
@@ -6,6 +6,7 @@ export const useActiveProfile = () => {
 	const env = useEnvironment();
 	const { profileId } = useParams();
 
+	/* istanbul ignore next */
 	return React.useMemo(() => {
 		if (process.env.NODE_ENV === "test") return profileId ? { id: () => profileId } : undefined;
 
