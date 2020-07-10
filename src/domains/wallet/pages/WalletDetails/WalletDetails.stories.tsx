b6b44974f05a5eb8ccc62d1cb7d5e5daff33b4dc
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-10 09:51:19 -0300

    fix: import statement

--- src/domains/wallet/pages/WalletDetails/WalletDetails.stories.tsx
+++ src/domains/wallet/pages/WalletDetails/WalletDetails.stories.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 
+import { wallet, wallets } from "./data";
 import { WalletDetails } from "./WalletDetails";
 
 export default { title: "Domains / Wallet / Pages / WalletDetails" };
-
-import { wallet, wallets } from "./data";
 
 export const Default = () => <WalletDetails wallets={wallets} wallet={wallet} />;
