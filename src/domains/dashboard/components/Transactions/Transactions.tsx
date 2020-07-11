d4e5d9d37251ba09a2556807200e2c5102f2e96a
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-07-11 11:45:29 -0300

    refactor: update `Dashboard` to use `TransactionTable`

--- src/domains/dashboard/components/Transactions/Transactions.tsx
+++ src/domains/dashboard/components/Transactions/Transactions.tsx
@@ -1,6 +1,5 @@
 import { Button } from "app/components/Button";
-import { Table } from "app/components/Table";
-import { TransactionListItem } from "app/components/TransactionListItem";
+import { TransactionTable } from "domains/transaction/components/TransactionTable";
 import React from "react";
 
 type TransactionsProps = {
@@ -16,9 +15,7 @@ export const Transactions = ({ transactions, columns, title, emptyText }: Transa
 			<div className="text-4xl font-bold">{title}</div>
 			{transactions.length > 0 && (
 				<div className="pt-8">
-					<Table columns={columns} data={transactions}>
-						{(rowData: any) => <TransactionListItem {...rowData} />}
-					</Table>
+					<TransactionTable transactions={transactions} currencyRate="2" />
 
 					<Button variant="plain" className="w-full mt-10 mb-5">
 						See more
@@ -34,29 +31,5 @@ Transactions.defaultProps = {
 	title: "Transactions History",
 	emptyText:
 		"This will display the history of your transactions. But you don't have more than one transaction at the moment.",
-	columns: [
-		{
-			Header: "Date",
-		},
-		{
-			Header: "Type",
-			className: "invisible",
-		},
-		{
-			Header: "Wallet Address",
-		},
-		{
-			Header: "Info",
-			className: "justify-center",
-		},
-		{
-			Header: "Amount",
-			className: "float-right",
-		},
-		{
-			Header: "Fiat Value",
-			className: "float-right",
-		},
-	],
 	transactions: [],
 };
