b613aaaf7347e616b3d69f84cd8e6e5409d25d08
Edgar Goetzendorff <hello@dated.fun>
2020-07-11 23:10:15 +0200

    refactor: offline page

--- src/app/components/Layout/components/Page/Page.tsx
+++ src/app/components/Layout/components/Page/Page.tsx
@@ -1,21 +1,34 @@
+import { images } from "app/assets/images";
 import { Breadcrumbs } from "app/components/Breadcrumbs";
 import { NavigationBar } from "app/components/NavigationBar";
 import React from "react";
 
+const commonAssets = images.common;
+
 type PageProps = {
-	hideNavigation?: boolean;
+	navbarStyle?: string;
 	crumbs?: any;
 	children: React.ReactNode;
 };
 
-export const Page = ({ hideNavigation, crumbs, children }: PageProps) => {
+export const Page = ({ navbarStyle = "full", crumbs, children }: PageProps) => {
 	return (
 		<div className="relative flex flex-col min-h-screen bg-theme-neutral-contrast">
-			{!hideNavigation && <NavigationBar />}
+			{navbarStyle === "full" && <NavigationBar />}
+
+			{navbarStyle === "logo-only" && (
+				<div className="px-4 sm:px-6 lg:px-8 bg-theme-background">
+					<div className="flex items-center flex-shrink-0 h-20 md:h-24">
+						<div className="flex p-2 rounded-lg bg-logo">
+							<img src={commonAssets.ARKLogo} className="h-6 md:h-8 lg:h-10" alt="ARK Logo" />
+						</div>
+					</div>
+				</div>
+			)}
 
 			{crumbs?.length && <Breadcrumbs crumbs={crumbs} className="container py-5 mx-auto font-semibold px-14" />}
 
-			<div className={`flex flex-col flex-1 space-y-5 ${!hideNavigation && !crumbs?.length ? "mt-5" : ""}`}>
+			<div className={`flex flex-col flex-1 space-y-5 ${!navbarStyle && !crumbs?.length ? "mt-5" : ""}`}>
 				{children}
 			</div>
 		</div>
--- src/domains/offline/pages/Offline/Offline.tsx
+++ src/domains/offline/pages/Offline/Offline.tsx
@@ -1,38 +1,26 @@
 import { images } from "app/assets/images";
+import { Page, Section } from "app/components/Layout";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import tw, { styled } from "twin.macro";
 
 const commonAssets = images.common;
 const { CannotConnectBanner } = images.offline.pages.Offline;
-
-const Container = styled.div`
-	${tw`flex flex-col items-center justify-center text-center`}
-	height: calc(100vh - 140px);
-`;
 
 export const Offline = () => {
 	const { t } = useTranslation();
 
 	return (
-		<div className="w-full h-full">
-			<div className="px-4 sm:px-6 lg:px-8">
-				<div className="flex items-center flex-shrink-0 h-20 md:h-24">
-					<div className="flex p-2 rounded-lg bg-logo">
-						<img src={commonAssets.ARKLogo} className="h-6 md:h-8 lg:h-10" alt="ARK Logo" />
-					</div>
-				</div>
-			</div>
-
-			<Container data-testid="Offline">
+		<Page navbarStyle="logo-only">
+			<Section className="flex flex-col flex-1 justify-center">
 				<div className="sm:mx-auto sm:w-full sm:max-w-md">
 					<CannotConnectBanner />
 				</div>
-				<div className="mt-8">
+
+				<div data-testid="Offline__text" className="mt-8 text-center">
 					<h2 className="text-2xl font-bold">{t("OFFLINE.TITLE")}</h2>
 					<p className="text-theme-neutral-dark">{t("OFFLINE.DESCRIPTION")}</p>
 				</div>
-			</Container>
-		</div>
+			</Section>
+		</Page>
 	);
 };
