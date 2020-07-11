81085ccb20d2afc8cf3795a63c8d83ccc2981d68
Edgar Goetzendorff <hello@dated.fun>
2020-07-11 14:28:12 +0200

    news routing tests

--- src/domains/news/pages/News/News.test.tsx
+++ src/domains/news/pages/News/News.test.tsx
@@ -2,6 +2,7 @@ import { ARK } from "@arkecosystem/platform-sdk-ark";
 import { Environment } from "@arkecosystem/platform-sdk-profiles";
 import { EnvironmentContext } from "app/contexts";
 import { httpClient } from "app/services";
+import { createMemoryHistory } from "history";
 import React from "react";
 import { Route } from "react-router-dom";
 import { renderWithRouter } from "testing-library";
@@ -13,6 +14,11 @@ import { News } from "./News";
 describe("News", () => {
 	const env = new Environment({ coins: { ARK }, httpClient, storage: new StubStorage() });
 
+	const history = createMemoryHistory();
+	const newsURL = "/profiles/qwe123/news";
+
+	history.push(newsURL);
+
 	it("should render", () => {
 		const { container } = renderWithRouter(
 			<EnvironmentContext.Provider value={env}>
@@ -20,6 +26,10 @@ describe("News", () => {
 					<News news={news} />
 				</Route>
 			</EnvironmentContext.Provider>,
+			{
+				routes: [newsURL],
+				history,
+			},
 		);
 
 		expect(container).toMatchSnapshot();
