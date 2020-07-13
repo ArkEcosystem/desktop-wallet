7153dcac4983019d1ce57abaf3dd48f756ed7029
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-13 17:27:21 +0100

    refactor: test & stories string mnemonic

--- src/domains/wallet/components/MnemonicVerification/MnemonicVerification.test.tsx
+++ src/domains/wallet/components/MnemonicVerification/MnemonicVerification.test.tsx
@@ -4,11 +4,13 @@ import { fireEvent, render } from "testing-library";
 import { MnemonicVerification } from "./MnemonicVerification";
 
 describe("MnemonicVerification", () => {
+	const mnemonic = "ark btc usd bnb eth ltc";
+	const mnemonicWords = mnemonic.split(" ");
+	const limit = 6;
+	const handleComplete = jest.fn();
+
 	it("should verify mnemonic", () => {
-		const mnemonic = ["ark", "btc", "usd", "bnb", "eth", "ltc"];
-		const limit = 6;
 		const wordPositions = [1, 2, 3];
-		const handleComplete = jest.fn();
 
 		const { asFragment, getByText } = render(
 			<MnemonicVerification
@@ -20,18 +22,18 @@ describe("MnemonicVerification", () => {
 		);
 
 		const firstTab = asFragment();
-		const wrongButton = getByText(mnemonic[4]);
+		const wrongButton = getByText(mnemonicWords[4]);
 		fireEvent.click(wrongButton);
 		expect(firstTab).toEqual(asFragment());
 
-		const firstButton = getByText(mnemonic[wordPositions[0] - 1]);
+		const firstButton = getByText(mnemonicWords[wordPositions[0] - 1]);
 		fireEvent.click(firstButton);
 		expect(firstTab).not.toEqual(asFragment());
 
-		const secondButton = getByText(mnemonic[wordPositions[1] - 1]);
+		const secondButton = getByText(mnemonicWords[wordPositions[1] - 1]);
 		fireEvent.click(secondButton);
 
-		const thirdButton = getByText(mnemonic[wordPositions[2] - 1]);
+		const thirdButton = getByText(mnemonicWords[wordPositions[2] - 1]);
 		fireEvent.click(thirdButton);
 
 		expect(handleComplete).toHaveBeenCalled();
