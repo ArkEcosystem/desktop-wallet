d84c5585007ccadb6996d4b0131c3e6adca57ceb
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-14 12:08:05 -0300

    tests(e2e): add routing cases

--- cypress/integration/transactions-routing.spec.js
+++ cypress/integration/transactions-routing.spec.js
@@ -7,8 +7,31 @@ describe("Transactions routing", () => {
 		cy.get("p").contains("Enter details to send your money");
 	});
 
+	it("should handle the sign message action from dropdown", () => {
+		cy.get("span").contains("Go back to Portfolio").click();
+		cy.get("div").contains("2,000,000 ARK").click();
+		cy.get("[data-testid=WalletHeader__more-button]").click();
+		cy.get("li").contains("Sign Message").click();
+		cy.get("h2").contains("Sign Message").click();
+		cy.get("[data-testid=sign-message__sign-button]").click();
+		cy.get("h2").contains("Message Successfully Signed").click();
+		cy.get("[data-testid=modal__close-btn]").click();
+	});
+
 	it("should navigate back to porfolio", () => {
 		cy.get("span").contains("Go back to Portfolio").click();
-		cy.get("div").contains("Wallets");
+	});
+
+	it("should navigate to registrations", () => {
+		cy.get("div").contains("2,000,000 ARK").click();
+		cy.get("[data-testid=WalletRegistrations__show-all]").click();
+		cy.get("h1").contains("My Registrations");
+	});
+
+	it("should navigate to registration", () => {
+		cy.get("span").contains("Go back to Portfolio").click();
+		cy.get("div").contains("2,000,000 ARK").click();
+		cy.get("[data-testid=WalletRegistrations__register]").click();
+		cy.get("h1").contains("Registration");
 	});
 });
