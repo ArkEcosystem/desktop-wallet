b9963ccc3bdfb8c351a94621df8db35daf88cc7d
Brian Faust <hello@basecode.sh>
2020-07-16 06:59:42 +0300

    wip

--- src/domains/setting/pages/Settings/available-settings/index.ts
+++ src/domains/setting/pages/Settings/available-settings/index.ts
@@ -2,4 +2,4 @@ import { General } from "./General";
 import { Peer } from "./Peer";
 import { Plugins } from "./Plugins";
 
-export const availableSettings: Record<string, unknown> = { General, Peer, Plugins };
+export const availableSettings: Record<string, any> = { General, Peer, Plugins };
