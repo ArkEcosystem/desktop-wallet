b8f876c40c5e552a731662f77855169ca40f0c92
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-17 21:42:51 +0100

    test: initial e2e

---
+++ cypress/integration/wallet-create.spec.js
@@ -0,0 +1,58 @@
+describe("Create Wallet", () => {
+	it("should navigate to portfolio page", () => {
+		cy.visit("/");
+
+		cy.get("p").contains("Anne Doe").click();
+		cy.get("div").contains("Wallets");
+	});
+
+	it("should navigate to create wallet page", () => {
+		cy.get("button").contains("Create").click();
+		cy.get("h1").contains("Select a Cryptoasset");
+	});
+
+	it("should select a network", () => {
+		cy.get("input").should("have.attr", "placeholder", "Enter a network name").type("ARK{enter}");
+		// cy.wait(1000);
+		cy.get("button").contains("Continue").click();
+		cy.get("h1").contains("Your Passphrase");
+	});
+
+	it("it should confirm passphrase", () => {
+		const mnemonicWords = [];
+
+		cy.get("li[data-testid=MnemonicList__item]").each(($el) => {
+			mnemonicWords.push(
+				$el
+					.text()
+					.replace(/[0-9]+/, "")
+					.trim(),
+			);
+		});
+
+		cy.get("button").contains("Continue").click();
+		cy.get("h1").contains("Confirm your passphrase");
+		cy.get("button").contains("Continue").should("be.disabled");
+
+		// TODO: Fix form submit on third button press (see TODO in submit callback)
+		for (let i = 0; i < 3; i++) {
+			cy.get("p[data-testid=MnemonicVerificationOptions__title]")
+				.invoke("text")
+				.then((selectWordTitle) => {
+					const wordNumber = selectWordTitle.replace(/Select word #/, "");
+
+					cy.get("button[data-testid=MnemonicVerificationOptions__button]")
+						.contains(new RegExp(`^${mnemonicWords[wordNumber - 1]}$`))
+						.click();
+				});
+		}
+
+		cy.get("button").contains("Continue").click();
+		cy.get("h1").contains("Completed");
+	});
+
+	it("should navigate back to portfolio page", () => {
+		cy.get("button").contains("Save & Finish").click();
+		cy.get("div").contains("Wallets");
+	});
+});
