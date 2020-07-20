81e897dba47d5db3a9c763d28fb41ff25429359e
Brian Faust <hello@basecode.sh>
2020-07-20 08:53:39 +0300

    wip

--- src/app/services/HttpClient.ts
+++ src/app/services/HttpClient.ts
@@ -1,12 +1,1 @@
-import { Contracts } from "@arkecosystem/platform-sdk";
-import fetch from "node-fetch";
-
-export class HttpClient implements Contracts.HttpClient {
-	public async get(path: string, searchParams?: Record<string, any>) {
-		return (await fetch(searchParams ? `${path}?${new URLSearchParams(searchParams)}` : path)).json();
-	}
-
-	public async post(path: string, body: object, headers = {}) {
-		return (await fetch(path, { method: "POST", body: JSON.stringify(body), headers })).json();
-	}
-}
+export * from "@arkecosystem/platform-sdk-http-node-fetch";
