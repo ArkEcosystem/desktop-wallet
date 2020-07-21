6e5d5b1d060c4064a49fac9a4febb9557d67906c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-21 12:44:25 +0100

    refactor: boot test environment from fixture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,28 +2,22 @@ import { ARK } from "@arkecosystem/platform-sdk-ark";
 import { Environment } from "@arkecosystem/platform-sdk-profiles";
 import React from "react";
 import { I18nextProvider } from "react-i18next";
+import fixtureData from "tests/fixtures/env/storage.json";
 import { StubStorage } from "tests/mocks";
 
 import { RouterView, routes } from "../router";
-import { identity } from "../tests/fixtures/identity";
 import { EnvironmentProvider, useEnvironment } from "./contexts";
 import { i18n } from "./i18n";
 import { httpClient } from "./services";
 
 const __DEV__ = process.env.NODE_ENV !== "production";
 
-const buildMockEnvironment = async (env: Environment) => {
-	const profile = env.profiles().create("Anne Doe");
-
-	await profile.wallets().import(identity.mnemonic, "ARK", "devnet");
-};
-
 const Main = () => {
 	const env = useEnvironment();
 
 	React.useLayoutEffect(() => {
 		if (process.env.REACT_APP_BUILD_MODE === "demo") {
-			buildMockEnvironment(env!);
+			env?.bootFromObject(fixtureData);
 		}
 	}, [env]);
 
