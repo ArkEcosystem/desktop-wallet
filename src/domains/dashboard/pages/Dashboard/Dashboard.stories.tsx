daf10d5b7e9246a82e0d40a57308b997615f2e03
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-22 12:59:59 +0100

    wip: update dashboard stories

--- src/domains/dashboard/pages/Dashboard/Dashboard.stories.tsx
+++ src/domains/dashboard/pages/Dashboard/Dashboard.stories.tsx
@@ -1,31 +1,66 @@
+import { ARK } from "@arkecosystem/platform-sdk-ark";
+import { Environment } from "@arkecosystem/platform-sdk-profiles";
+import { EnvironmentProvider } from "app/contexts";
+import { httpClient } from "app/services";
 import React from "react";
+import { MemoryRouter, Route } from "react-router";
+import { StubStorage } from "tests/mocks";
 
-import { balances, networks, portfolioPercentages, transactions, wallets } from "../../data";
+import { balances, networks, portfolioPercentages, transactions } from "../../data";
 import { Dashboard } from "./Dashboard";
 
 export default { title: "Domains / Dashboard / Pages / Dashboard" };
 
+const env = new Environment({ coins: { ARK }, httpClient, storage: new StubStorage() });
+const profile = env.profiles().create("John Doe");
+
 export const Default = () => {
 	return (
-		<Dashboard
-			balances={balances}
-			networks={networks}
-			wallets={wallets}
-			transactions={transactions}
-			portfolioPercentages={portfolioPercentages}
-		/>
+		<EnvironmentProvider env={env}>
+			<MemoryRouter initialEntries={[`/profiles/${profile.id()}/dashboard`]}>
+				<Route
+					path="/profiles/:profileId/dashboard"
+					component={() => (
+						<Dashboard
+							balances={balances}
+							networks={networks}
+							transactions={transactions}
+							portfolioPercentages={portfolioPercentages}
+						/>
+					)}
+				/>
+			</MemoryRouter>
+		</EnvironmentProvider>
 	);
 };
 
 export const FewerWallets = () => {
+	profile
+		.wallets()
+		.generate("ARK", "mainnet")
+		.then(() => {
+			console.log("generate wallet", profile.wallets().all());
+		})
+		.catch((e) => {
+			console.log("generate wallet error", e);
+		});
+
 	return (
-		<Dashboard
-			balances={balances}
-			networks={networks}
-			wallets={wallets.concat().splice(0, 2)}
-			transactions={transactions}
-			portfolioPercentages={portfolioPercentages}
-		/>
+		<EnvironmentProvider env={env}>
+			<MemoryRouter initialEntries={[`/profiles/${profile.id()}/dashboard`]}>
+				<Route
+					path="/profiles/:profileId/dashboard"
+					component={() => (
+						<Dashboard
+							balances={balances}
+							networks={networks}
+							transactions={transactions}
+							portfolioPercentages={portfolioPercentages}
+						/>
+					)}
+				/>
+			</MemoryRouter>
+		</EnvironmentProvider>
 	);
 };
 
