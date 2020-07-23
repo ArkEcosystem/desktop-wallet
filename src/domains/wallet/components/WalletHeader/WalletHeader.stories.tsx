fd513a5b6e7c0eb5795527c1fa79faa948f9e2cf
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-07-23 12:48:23 +0300

    feat: implement wallet labeling (#2514)

--- src/domains/wallet/components/WalletHeader/WalletHeader.stories.tsx
+++ src/domains/wallet/components/WalletHeader/WalletHeader.stories.tsx
@@ -6,12 +6,13 @@ export default { title: "Domains / Wallet / Components / WalletHeader" };
 
 export const Default = () => (
 	<WalletHeader
-		name="ARK Wallet 1"
+		name="ARK Wallet 2"
 		publicKey="028fe98e42e159f2450a52371dfb23ae69a39fc5fee6545690b7f51bfcee933357"
 		address="AdzbhuDTyhnfAqepZzVcVsgd1Ym6FgETuW"
 		balance="432,509.0892 ARK"
 		currencyBalance="789,008.45 USD"
 		onSignMessage={console.log}
+		onUpdateWalletName={console.log}
 		onDeleteWallet={console.log}
 		coin="Ark"
 		isLedger
