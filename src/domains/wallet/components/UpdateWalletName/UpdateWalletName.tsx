6619447d53f65026b319381a0758ab50f6187869
goga-m <goga.mamoulasvili@gmail.com>
2020-07-23 12:10:44 +0300

    test: correct network

--- src/domains/wallet/components/UpdateWalletName/UpdateWalletName.tsx
+++ src/domains/wallet/components/UpdateWalletName/UpdateWalletName.tsx
@@ -30,7 +30,7 @@ export const UpdateWalletName = ({ isOpen, onClose, onCancel, onSave }: UpdateWa
 			onClose={onClose}
 		>
 			<Form context={methods} onSubmit={onSave} className="mt-8">
-				<FormField name="name">
+				<FormField name="UpdateWalletName__input">
 					<FormLabel>{t("WALLETS.MODAL_NAME_WALLET.FIELD_NAME")}</FormLabel>
 					<Input
 						data-testid="UpdateWalletName__input"
