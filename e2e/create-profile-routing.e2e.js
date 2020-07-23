6a1b03536eebf27dd092df26e9c969d03ce10f34
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-23 15:01:45 -0300

    chore: properly rename e2e test

--- e2e/create-profile-routing.e2e.js
+++ e2e/create-profile-routing.e2e.js
@@ -1,15 +1,14 @@
 import { ClientFunction, Selector } from "testcafe";
 
-fixture`Welcome -> Create Profile routing`.page`http://localhost:3000`;
+fixture`Welcome -> Create Profile routing`.page`http://localhost:3000/`;
 
 const getLocation = ClientFunction(() => document.location.href);
 
-test("should navigate to create profile and get back to welcome screen", async (t) => {
+test("should navigate to create profile and back to welcome screen", async (t) => {
 	await t.click(Selector("button").withExactText("Create Profile"));
 	await t.expect(getLocation()).contains("http://localhost:3000/profiles/create");
 	await t.click(Selector("h1").withExactText("Create Profile"));
-
-	// Back to welcome screen
+	// Navigate back
 	await t.click(Selector("button").withExactText("Back"));
 	await t.click(Selector("h1").withExactText("Welcome to ARK"));
 	await t.expect(getLocation()).contains("http://localhost:3000/");
