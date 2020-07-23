f63a7bcb08e8917268224fc28a2900d83773dfb9
goga-m <goga.mamoulasvili@gmail.com>
2020-07-23 11:06:28 +0300

    fix: correct network

--- src/domains/wallet/components/VerifyMessage/VerifyMessage.test.tsx
+++ src/domains/wallet/components/VerifyMessage/VerifyMessage.test.tsx
@@ -21,9 +21,11 @@ describe("VerifyMessage", () => {
 	beforeAll(() => {
 		nock.disableNetConnect();
 
-		nock(/.+/)
+		nock("https://dwallets.ark.io")
 			.get("/api/node/configuration")
-			.reply(200, require("../../../../tests/fixtures/coins/ark/configuration.json"))
+			.reply(200, require("../../../../tests/fixtures/coins/ark/configuration-devnet.json"))
+			.get("/api/peers")
+			.reply(200, require("../../../../tests/fixtures/coins/ark/peers.json"))
 			.get("/api/node/configuration/crypto")
 			.reply(200, require("../../../../tests/fixtures/coins/ark/cryptoConfiguration.json"))
 			.get("/api/node/syncing")
