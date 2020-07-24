e023406bf8e03d694144a0a7778e4a53660583ae
Brian Faust <hello@basecode.sh>
2020-07-24 17:18:25 +0300

    wip

--- src/domains/network/data.ts
+++ src/domains/network/data.ts
@@ -1,8 +1,8 @@
 import { ARK } from "@arkecosystem/platform-sdk-ark";
 import { BTC } from "@arkecosystem/platform-sdk-btc";
-import { LSK } from "@arkecosystem/platform-sdk-lsk";
+// import { LSK } from "@arkecosystem/platform-sdk-lsk";
 import { NetworkData } from "@arkecosystem/platform-sdk-profiles";
-import { XRP } from "@arkecosystem/platform-sdk-xrp";
+// import { XRP } from "@arkecosystem/platform-sdk-xrp";
 
 export interface CoinNetworkExtended {
 	displayName: string;
@@ -34,28 +34,28 @@ export const coinsNetworkExtendedData: Record<string, Record<string, CoinNetwork
 			iconName: "Bitcoin",
 		},
 	},
-	LSK: {
-		Mainnet: {
-			displayName: "Lisk",
-			borderClass: "border-theme-primary-400",
-			textClass: "text-theme-primary",
-			iconName: "Lisk",
-		},
-	},
-	XRP: {
-		Mainnet: {
-			displayName: "Ripple",
-			borderClass: "border-theme-primary-500",
-			textClass: "text-theme-primary-dark",
-			iconName: "Ripple",
-		},
-	},
+	// LSK: {
+	// 	Mainnet: {
+	// 		displayName: "Lisk",
+	// 		borderClass: "border-theme-primary-400",
+	// 		textClass: "text-theme-primary",
+	// 		iconName: "Lisk",
+	// 	},
+	// },
+	// XRP: {
+	// 	Mainnet: {
+	// 		displayName: "Ripple",
+	// 		borderClass: "border-theme-primary-500",
+	// 		textClass: "text-theme-primary-dark",
+	// 		iconName: "Ripple",
+	// 	},
+	// },
 };
 
 export const availableNetworksMock: NetworkData[] = [
 	new NetworkData(ARK.manifest.name, ARK.manifest.networks.mainnet),
 	new NetworkData(ARK.manifest.name, ARK.manifest.networks.devnet),
 	new NetworkData(BTC.manifest.name, BTC.manifest.networks.livenet),
-	new NetworkData(LSK.manifest.name, LSK.manifest.networks.mainnet),
-	new NetworkData(XRP.manifest.name, XRP.manifest.networks.mainnet),
+	// new NetworkData(LSK.manifest.name, LSK.manifest.networks.mainnet),
+	// new NetworkData(XRP.manifest.name, XRP.manifest.networks.mainnet),
 ];
