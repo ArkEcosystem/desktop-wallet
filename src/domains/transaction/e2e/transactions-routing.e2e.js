a50cb940c1bce9762306f9cc8354323455a816f7
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-24 16:57:38 -0300

    refactor: reorganize e2e files

---
+++ src/domains/transaction/e2e/transactions-routing.e2e.js
@@ -0,0 +1,38 @@
+import { Selector } from "testcafe";
+
+fixture`Transactions routing`.page`http://localhost:3000/`;
+
+test("should navigate to portfolio and sign message", async (t) => {
+	await t.click(Selector("p").withText("Anne Doe"));
+	await t.expect(Selector("div").withText("Wallets").exists).ok();
+	await t.click(Selector("[data-testid=navbar__buttons--send]"));
+	await t.expect(Selector("div").withText("Enter details to send your money").exists).ok();
+
+	// Handle sign message
+	await t.click(Selector("span").withText("Go back to Portfolio"));
+	await t.click(Selector("[data-testid=WalletCard__1ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT]"));
+	await t.click(Selector("[data-testid=WalletHeader__more-button]"));
+	await t.click(Selector("li").withText("Sign Message"));
+
+	await t.click(Selector("[data-testid=sign-message__sign-button]"));
+	await t.expect(Selector("h2").withText("Message Successfully Signed").exists).ok();
+	await t.click(Selector("[data-testid=modal__close-btn]"));
+});
+
+test("should navigate to portfolio and access registrations", async (t) => {
+	await t.click(Selector("p").withText("Anne Doe"));
+	await t.expect(Selector("div").withText("Wallets").exists).ok();
+	// Go to registrations
+	await t.click(Selector("[data-testid=WalletCard__1ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT]"));
+	await t.click(Selector("[data-testid=WalletRegistrations__show-all]"));
+	await t.expect(Selector("h1").withText("My Registrations").exists).ok();
+});
+
+test("should navigate to portfolio and registrate", async (t) => {
+	await t.click(Selector("p").withText("Anne Doe"));
+	await t.expect(Selector("div").withText("Wallets").exists).ok();
+	// Go to registrations
+	await t.click(Selector("[data-testid=WalletCard__1ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT]"));
+	await t.click(Selector("[data-testid=WalletRegistrations__register]"));
+	await t.expect(Selector("h1").withText("Registration").exists).ok();
+});
