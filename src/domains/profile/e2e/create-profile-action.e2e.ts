fbce423f3ff129a92c6caec4bba48511bad64f72
brenopolanski <breno.polanski@gmail.com>
2020-07-27 14:04:31 -0300

    chore: add i18n to profile routing

--- src/domains/profile/e2e/create-profile-action.e2e.ts
+++ src/domains/profile/e2e/create-profile-action.e2e.ts
@@ -1,13 +1,15 @@
 import { Selector } from "testcafe";
 
+import { buildTranslations as translations } from "../../../app/i18n/helpers";
+
 fixture`Create Profile action`.page`http://localhost:3000/profiles/create`;
 
 test("should return an error when submit without required fields", async (t) => {
-	await t.click(Selector("button").withExactText("Complete"));
+	await t.click(Selector("button").withExactText(translations().COMMON.COMPLETE));
 	await t.click(Selector("fieldset p").withText("Name is required"));
 	await t.click(Selector("fieldset p").withText("Market Provider is required"));
 	await t.click(Selector("fieldset p").withText("Currency is required"));
-	await t.click(Selector("h1").withExactText("Create Profile"));
+	await t.click(Selector("h1").withExactText(translations().PROFILE.PAGE_CREATE_PROFILE.TITLE));
 });
 
 test("should create a profile and navigate to welcome screen", async (t) => {
@@ -19,8 +21,9 @@ test("should create a profile and navigate to welcome screen", async (t) => {
 	await t.click(Selector("button").withText("Select Currency"));
 	await t.click(Selector("li.select-list-option").withText("Option 1"));
 	await t.click(Selector("input[name=isDarkMode]").parent());
-	await t.click(Selector("button").withExactText("Complete"));
+	await t.click(Selector("button").withExactText(translations().COMMON.COMPLETE));
+
 	// Check welcome with created profiles
-	await t.expect(Selector("p").withText("Anne Doe").exists).ok();
 	await t.expect(Selector("p").withText("John Doe").exists).ok();
+	await t.expect(Selector("p").withText("Anne Doe").exists).ok();
 });
