d911950f1af35456cf718ecfa5105d216c06a347
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-07-28 00:56:13 +0100

    refactor: improve storybook env setup

--- src/utils/storybook.tsx
+++ src/utils/storybook.tsx
@@ -1,6 +1,6 @@
 import { Coins, Contracts } from "@arkecosystem/platform-sdk";
 import { ARK } from "@arkecosystem/platform-sdk-ark";
-import { Environment, Profile, Wallet } from "@arkecosystem/platform-sdk-profiles";
+import { Environment, Profile, Wallet, WalletSetting } from "@arkecosystem/platform-sdk-profiles";
 import { httpClient } from "app/services";
 import React from "react";
 import { StubStorage } from "tests/mocks";
@@ -12,34 +12,67 @@ type WalletsDecoratorProps = {
 	withDelegates?: boolean;
 };
 
-export const setupEnvironment = () => {
-	const env = new Environment({ coins: { ARK }, httpClient, storage: new StubStorage() });
-	const profile = env.profiles().create("John Doe");
-
-	return { env, profile };
-};
-
 // TODO: Implement generate & count when mocking is in SDK
-export const generateWallets = async (profile: Profile, setWallets: any, count = 2) => {
-	const promises = [
-		profile.wallets().importByAddress("D6Z26L69gdk9qYmTv5uzk3uGepigtHY4ax", "ARK", "devnet"),
-		profile.wallets().importByAddress("D8vwEEvKgMPVvvK2Zwzyb5uHzRadurCcKq", "ARK", "devnet"),
-		profile.wallets().importByAddress("DRgF3PvzeGWndQjET7dZsSmnrc6uAy23ES", "ARK", "devnet"),
-	];
+export const generateWallets = async ({
+	env,
+	profile,
+	count,
+}: {
+	env: Environment;
+	profile: Profile;
+	count?: Number;
+}) => {
+	const promises = [];
 
-	return Promise.all(promises);
+	try {
+		promises.push(
+			profile.wallets().importByAddress("D6Z26L69gdk9qYmTv5uzk3uGepigtHY4ax", "ARK", "devnet"),
+			profile.wallets().importByAddress("D8vwEEvKgMPVvvK2Zwzyb5uHzRadurCcKq", "ARK", "devnet"),
+			profile.wallets().importByAddress("DRgF3PvzeGWndQjET7dZsSmnrc6uAy23ES", "ARK", "devnet"),
+		);
+
+		const wallets = await Promise.all(promises);
+
+		for (const walletIndex of Object.keys(wallets)) {
+			wallets[walletIndex as any].settings().set(WalletSetting.Alias, `ARK Wallet ${Number(walletIndex) + 1}`);
+		}
+
+		await env.persist();
+
+		return wallets;
+	} catch (e) {
+		console.error("Couldn't import addresses: ", e);
+	}
+
+	return profile.wallets().values();
 };
+
+const env = new Environment({ coins: { ARK }, httpClient, storage: new StubStorage() });
 
 export const WalletsDecorator = ({ count, children, withDelegate, withDelegates }: WalletsDecoratorProps) => {
-	const { profile } = setupEnvironment();
 	const [wallets, setWallets] = React.useState<Wallet[]>([]);
+	const [profile, setProfile] = React.useState<Profile>((null as unknown) as Profile);
 	const [delegate, setDelegate] = React.useState<Contracts.WalletData>((null as unknown) as Contracts.WalletData);
 	const [delegates, setDelegates] = React.useState<Coins.WalletDataCollection>(
 		(null as unknown) as Coins.WalletDataCollection,
 	);
 
 	const loadData = async () => {
-		const wallets = await generateWallets(profile, setWallets);
+		let profileObject;
+		if (!profile) {
+			const existingProfile = env
+				.profiles()
+				.all()
+				.find((profile) => profile.name() === "John Doe");
+			if (existingProfile) {
+				env.profiles().forget(existingProfile.id());
+			}
+
+			profileObject = env.profiles().create("John Doe");
+			setProfile(profileObject);
+		}
+
+		const wallets = await generateWallets({ env, profile: profile || profileObject, count });
 
 		if (withDelegate) {
 			setDelegate(await wallets[0].delegate("arkx"));
@@ -50,6 +83,8 @@ export const WalletsDecorator = ({ count, children, withDelegate, withDelegates 
 		}
 
 		setWallets(wallets);
+
+		await env.persist();
 	};
 
 	React.useEffect(() => {
@@ -60,5 +95,5 @@ export const WalletsDecorator = ({ count, children, withDelegate, withDelegates 
 		return <>Loading Wallets</>;
 	}
 
-	return <>{children({ delegate, delegates, wallets })}</>;
+	return <>{children({ delegate, delegates, env, profile, wallets })}</>;
 };
