446e7b828489bd3cc2d693a0176fce74ebdaa43d
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-07-28 13:55:35 -0300

    fix(storybook): fix webpack config conflict

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
-const configOverrides = require("./config-overrides");
+const path = require("path");
+const { override, addPostcssPlugins } = require("customize-cra");
 
 module.exports = {
 	stories: ["../src/**/**/*.stories.tsx"],
@@ -9,30 +10,43 @@ module.exports = {
 		"@storybook/preset-create-react-app",
 		"storybook-addon-themes",
 	],
-	webpackFinal: (storybookConfig) => {
-		const customConfig = configOverrides(storybookConfig);
-
-		storybookConfig.module.rules = [
-			...storybookConfig.module.rules,
-			{
-				test: /\.(ts|tsx)$/,
-				use: [
-					{
-						loader: require.resolve("babel-loader"),
-						options: {
-							presets: [require.resolve("babel-preset-react-app")],
+	webpackFinal: override(
+		addPostcssPlugins([
+			require("postcss-import"),
+			require("tailwindcss")("./src/tailwind.config.js"),
+			require("autoprefixer"),
+		]),
+		async (storybookConfig) => {
+			storybookConfig.module.rules = [
+				...storybookConfig.module.rules,
+				{
+					test: /\.(ts|tsx)$/,
+					use: [
+						{
+							loader: require.resolve("babel-loader"),
+							options: {
+								presets: [require.resolve("babel-preset-react-app")],
+							},
 						},
-					},
-					require.resolve("react-docgen-typescript-loader"),
-				],
-			},
-		];
+						require.resolve("react-docgen-typescript-loader"),
+					],
+				},
+			];
 
-		storybookConfig.resolve.extensions.push(".ts", ".tsx");
+			storybookConfig.resolve = {
+				extensions: [".ts", ".js", ".jsx", ".tsx", ".scss", ".json", ".node"],
+				alias: {
+					app: path.resolve(__dirname, "../src/app/"),
+					domains: path.resolve(__dirname, "../src/domains"),
+					resources: path.resolve(__dirname, "../src/resources"),
+					styles: path.resolve(__dirname, "../src/styles"),
+					utils: path.resolve(__dirname, "../src/utils"),
+					tests: path.resolve(__dirname, "../src/tests"),
+					fs: path.resolve(__dirname, "mocks/fsMock.js"),
+				},
+			};
 
-		return {
-			...storybookConfig,
-			module: { ...storybookConfig.module, rules: customConfig.module.rules },
-		};
-	},
+			return storybookConfig;
+		},
+	),
 };
