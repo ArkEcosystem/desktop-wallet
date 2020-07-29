7bfb2c6f640db56508a4e3a918bc7a8268de8846
Brian Faust <hello@basecode.sh>
2020-07-29 13:01:52 +0300

    wip

--- src/domains/setting/pages/Settings/available-settings/General.tsx
+++ src/domains/setting/pages/Settings/available-settings/General.tsx
@@ -10,6 +10,7 @@ import { Toggle } from "app/components/Toggle";
 import { PlatformSdkChoices } from "data";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { setScreenshotProtection } from "utils/electron-utils";
 
 type GeneralProps = {
 	env: Environment;
@@ -32,21 +33,21 @@ export const General = ({ env, formConfig, pageConfig, onSubmit }: GeneralProps)
 			labelDescriptionClass: "mt-1",
 			content: (
 				<div className="flex flex-row mt-2">
-					<div className="flex items-center justify-center w-24 h-24 mr-6 border border-dashed rounded border-theme-neutral-200">
+					<div className="border-theme-neutral-200 flex items-center justify-center w-24 h-24 mr-6 border border-dashed rounded">
 						<button
 							type="button"
-							className="flex items-center justify-center w-20 h-20 rounded-full bg-theme-primary-contrast"
+							className="bg-theme-primary-contrast flex items-center justify-center w-20 h-20 rounded-full"
 						>
 							<Icon name="Upload" />
 						</button>
 					</div>
-					<div className="relative w-24 h-24 rounded bg-theme-neutral-light">
+					<div className="bg-theme-neutral-light relative w-24 h-24 rounded">
 						<img
 							src="https://randomuser.me/api/portraits/men/3.jpg"
 							className="object-cover rounded"
 							alt="random avatar"
 						/>
-						<button className="absolute flex items-center justify-center w-6 h-6 p-1 rounded bg-theme-danger-contrast text-theme-danger -top-3 -right-3">
+						<button className="bg-theme-danger-contrast text-theme-danger -top-3 -right-3 absolute flex items-center justify-center w-6 h-6 p-1 rounded">
 							<Icon name="Close" height={12} width={12} />
 						</button>
 					</div>
@@ -63,7 +64,7 @@ export const General = ({ env, formConfig, pageConfig, onSubmit }: GeneralProps)
 			wrapperClass: "pb-6",
 			content: (
 				<div className="flex flex-row justify-between">
-					<span className="mt-1 text-sm text-theme-neutral">
+					<span className="text-theme-neutral mt-1 text-sm">
 						{t("SETTINGS.GENERAL.SECURITY.SCREENSHOT_PROTECTION.DESCRIPTION")}
 					</span>
 					<div className="-mt-7">
@@ -83,7 +84,7 @@ export const General = ({ env, formConfig, pageConfig, onSubmit }: GeneralProps)
 			wrapperClass: "py-6",
 			content: (
 				<div className="flex flex-row justify-between">
-					<span className="mt-1 text-sm text-theme-neutral">
+					<span className="text-theme-neutral mt-1 text-sm">
 						{t("SETTINGS.GENERAL.SECURITY.ADVANCED_MODE.DESCRIPTION")}
 					</span>
 					<div className="-mt-7">
@@ -125,7 +126,7 @@ export const General = ({ env, formConfig, pageConfig, onSubmit }: GeneralProps)
 			wrapperClass: "pb-6",
 			content: (
 				<div className="flex flex-row justify-between">
-					<span className="mt-1 text-sm text-theme-neutral">
+					<span className="text-theme-neutral mt-1 text-sm">
 						{t("SETTINGS.GENERAL.OTHER.DARK_THEME.DESCRIPTION")}
 					</span>
 					<div className="-mt-7">
@@ -141,7 +142,7 @@ export const General = ({ env, formConfig, pageConfig, onSubmit }: GeneralProps)
 			wrapperClass: "pt-6",
 			content: (
 				<div className="flex flex-row justify-between">
-					<span className="mt-1 text-sm text-theme-neutral">
+					<span className="text-theme-neutral mt-1 text-sm">
 						{t("SETTINGS.GENERAL.OTHER.UPDATE_LEDGER.DESCRIPTION")}
 					</span>
 					<div className="-mt-7">
@@ -178,6 +179,8 @@ export const General = ({ env, formConfig, pageConfig, onSubmit }: GeneralProps)
 		profile.settings().set(ProfileSetting.AdvancedMode, isAdvancedMode);
 		profile.settings().set(ProfileSetting.Theme, isDarkMode ? "dark" : "light");
 		profile.settings().set(ProfileSetting.LedgerUpdateMethod, isUpdateLedger);
+
+		setScreenshotProtection(isScreenshotProtection);
 
 		await env.persist();
 
