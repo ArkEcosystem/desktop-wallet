735bf96562da0af3e8d678d796e30952087f9172
goga-m <goga.mamoulasvili@gmail.com>
2020-07-30 14:29:24 +0300

    fix: correct fixtures in `Main`

--- src/domains/help/pages/Main/Main.test.tsx
+++ src/domains/help/pages/Main/Main.test.tsx
@@ -2,17 +2,22 @@
 import { createMemoryHistory } from "history";
 import React from "react";
 import { Route } from "react-router-dom";
-import { renderWithRouter } from "testing-library";
-import { identity } from "tests/fixtures/identity";
+import { env, renderWithRouter } from "testing-library";
+import fixtureData from "tests/fixtures/env/storage.json";
 
 import { categories, helpfulArticles, newestArticles, popularArticles } from "../../data";
 import { Main } from "./Main";
 
 const history = createMemoryHistory();
-const supportURL = `/profiles/${identity.profiles.bob.id}/support`;
+
+const supportURL = "/profiles/b999d134-7a24-481e-a95d-bc47c543bfc9/support";
 
 describe("SupportPage", () => {
 	beforeAll(() => {
+		beforeAll(async () => {
+			await env.bootFromObject(fixtureData);
+			await env.persist();
+		});
 		history.push(supportURL);
 	});
 
