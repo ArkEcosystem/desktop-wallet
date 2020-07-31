85cee09eba04ce0c7370efa2639e60c9f613fb35
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-07-31 07:12:23 -0300

    feat: add `DeleteProfile` component

---
+++ src/domains/profile/components/DeleteProfile/DeleteProfile.tsx
@@ -0,0 +1,39 @@
+import { DeleteResource } from "app/components/DeleteResource";
+import { useEnvironmentContext } from "app/contexts";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+type DeleteProfileProps = {
+	isOpen: boolean;
+	onClose?: any;
+	onCancel?: any;
+	onDelete?: any;
+	profileId: string;
+};
+
+export const DeleteProfile = ({ isOpen, onClose, onCancel, onDelete, profileId }: DeleteProfileProps) => {
+	const { t } = useTranslation();
+	const { env, persist } = useEnvironmentContext();
+
+	const handleDelete = async () => {
+		env.profiles().forget(profileId);
+		await persist();
+
+		onDelete?.(profileId);
+	};
+
+	return (
+		<DeleteResource
+			title={t("CONTACTS.MODAL_DELETE_PROFILE.TITLE")}
+			description={t("CONTACTS.MODAL_DELETE_PROFILE.DESCRIPTION")}
+			isOpen={isOpen}
+			onClose={onClose}
+			onCancel={onCancel}
+			onDelete={handleDelete}
+		/>
+	);
+};
+
+DeleteProfile.defaultProps = {
+	isOpen: false,
+};
