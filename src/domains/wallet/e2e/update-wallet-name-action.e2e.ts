3ae38d4fba23c5af5ab959e9bf265510bb6c8676
goga-m <goga.mamoulasvili@gmail.com>
2020-08-04 13:39:12 +0300

    test: update tests

--- src/domains/wallet/e2e/update-wallet-name-action.e2e.ts
+++ src/domains/wallet/e2e/update-wallet-name-action.e2e.ts
@@ -137,3 +137,33 @@ test("Should error when name exceeds 42 characters", async (t) => {
 	await t.click(Selector('[data-testid="UpdateWalletName__submit"]'));
 	await t.expect(Selector("[data-testid=modal__inner]").exists).ok();
 });
+
+test("Should disallow empty spaces", async (t) => {
+	await t.click(Selector("p").withText("John Doe"));
+	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
+
+	// Navigate to wallet details page
+	await t.click(Selector("[data-testid=WalletCard__D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD]"));
+	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
+
+	// Click wallet update name option in dropdown menu
+	await scrollTop();
+	await t.click(Selector('[data-testid="WalletHeader__more-button"]'));
+	await t.click(
+		Selector('[data-testid="WalletHeader__more-button"] li').withText(
+			translations().WALLETS.PAGE_WALLET_DETAILS.OPTIONS.WALLET_NAME,
+		),
+	);
+
+	await t.expect(Selector("[data-testid=modal__inner]").exists).ok();
+	await t.expect(Selector('[data-testid="modal__close-btn"]').exists).ok();
+
+	const walletLabelNameInput = Selector('[data-testid="UpdateWalletName__input"]');
+	await t.selectText(walletLabelNameInput).pressKey("delete");
+	await t.typeText(walletLabelNameInput, "      ");
+
+	await t.expect(Selector("[data-testid=UpdateWalletName__submit]").hasAttribute("disabled")).ok();
+
+	await t.click(Selector('[data-testid="UpdateWalletName__submit"]'));
+	await t.expect(Selector("[data-testid=modal__inner]").exists).ok();
+});
