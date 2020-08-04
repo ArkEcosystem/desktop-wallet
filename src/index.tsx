7cd45b7848c8ec2843ebee3cb9d4c9b7f6668526
Brian Faust <hello@basecode.sh>
2020-08-04 06:58:11 +0300

    chore: disable react dev tools when not needed

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import "./styles/app.css";
 
+// @ts-ignore - Missing Types
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import { App } from "app";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
+if (process.env.NODE_ENV && ["development", "production"].includes(process.env.NODE_ENV)) {
+	disableReactDevTools();
+}
 
 ReactDOM.render(
 	<BrowserRouter>
