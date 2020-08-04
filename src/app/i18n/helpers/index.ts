4b7600924f2f48d102de180a1ea785ad788149aa
Edgar Goetzendorff <hello@dated.fun>
2020-08-04 12:21:16 +0200

    feat: add application error display (#2591)

--- src/app/i18n/helpers/index.ts
+++ src/app/i18n/helpers/index.ts
@@ -1,10 +1,10 @@
 // Translations
 import { translations as CONTACTS } from "../../../domains/contact/i18n";
 import { translations as DASHBOARD } from "../../../domains/dashboard/i18n";
+import { translations as ERROR } from "../../../domains/error/i18n";
 import { translations as EXCHANGE } from "../../../domains/exchange/i18n";
 import { translations as HELP } from "../../../domains/help/i18n";
 import { translations as NEWS } from "../../../domains/news/i18n";
-import { translations as OFFLINE } from "../../../domains/offline/i18n";
 import { translations as PLUGINS } from "../../../domains/plugin/i18n";
 import { translations as PROFILE } from "../../../domains/profile/i18n";
 import { translations as SETTINGS } from "../../../domains/setting/i18n";
@@ -20,7 +20,7 @@ export const buildTranslations = (): any => ({
 	EXCHANGE,
 	HELP,
 	NEWS,
-	OFFLINE,
+	ERROR,
 	PLUGINS,
 	PROFILE,
 	VOTE,
---
+++ src/domains/error/i18n.ts
@@ -0,0 +1,11 @@
+export const translations: { [key: string]: any } = {
+	APPLICATION: {
+		TITLE: "Something went wrong",
+		DESCRIPTION: "It looks like something went wrong. Try visiting another page or reloading.",
+		RELOAD: "Reload",
+	},
+	OFFLINE: {
+		TITLE: "Cannot Connect",
+		DESCRIPTION: "Check your internet connection if it doesn't work, check it again.",
+	},
+};
