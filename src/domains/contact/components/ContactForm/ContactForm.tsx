bdd1c37203ca382a6900a43666628e47eee73e75
goga-m <goga.mamoulasvili@gmail.com>
2020-08-04 17:42:55 +0300

    chore: fix exhaustive deps

--- src/domains/contact/components/ContactForm/ContactForm.tsx
+++ src/domains/contact/components/ContactForm/ContactForm.tsx
@@ -7,7 +7,7 @@ import { Icon } from "app/components/Icon";
 import { Input } from "app/components/Input";
 import { NetworkIcon } from "domains/network/components/NetworkIcon";
 import { SelectNetwork } from "domains/network/components/SelectNetwork";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo,useState } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
@@ -107,7 +107,7 @@ export const ContactForm = ({ contact, networks, onChange, onCancel, onDelete, o
 		for (const [field, message] of Object.entries(errors)) {
 			form.setError(field, message as string);
 		}
-	}, [errors]);
+	}, [form, errors]);
 
 	const handleAddAddress = () => {
 		setAddresses(
