cba0d01fc21aa7cbfd74ad7338a86c8a258b9f5e
brenopolanski <breno.polanski@gmail.com>
2020-08-04 22:30:02 -0300

    test: update tests

--- src/domains/profile/pages/CreateProfile/CreateProfile.test.tsx
+++ src/domains/profile/pages/CreateProfile/CreateProfile.test.tsx
@@ -75,6 +75,10 @@ describe("CreateProfile", () => {
 
 		expect(showOpenDialogMock).toHaveBeenCalledWith(showOpenDialogParams);
 
+		await act(async () => {
+			fireEvent.click(getByTestId("CreateProfile__submit-button"));
+		});
+
 		fireEvent.input(getByTestId("Input"), { target: { value: "test profile" } });
 		fireEvent.click(getAllByTestId("select-list__toggle-button")[0]);
 		fireEvent.click(getByTestId("select-list__toggle-option-0"));
