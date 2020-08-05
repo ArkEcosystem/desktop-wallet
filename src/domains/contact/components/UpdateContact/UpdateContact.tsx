541d7cf2efe273ecc270deefce28a40a1af5cad5
goga-m <goga.mamoulasvili@gmail.com>
2020-08-05 11:06:11 +0300

    refactor: implement contact update validations

--- src/domains/contact/components/UpdateContact/UpdateContact.tsx
+++ src/domains/contact/components/UpdateContact/UpdateContact.tsx
@@ -2,7 +2,7 @@ import { Contact, NetworkData, Profile } from "@arkecosystem/platform-sdk-profil
 import { Modal } from "app/components/Modal";
 import { useEnvironmentContext } from "app/contexts";
 import { ContactForm } from "domains/contact/components/ContactForm";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 type UpdateContactProps = {
@@ -26,27 +26,52 @@ export const UpdateContact = ({
 	onSave,
 	profile,
 }: UpdateContactProps) => {
+	const [errors, setErrors] = useState<any>({});
+
 	const { t } = useTranslation();
 	const { persist } = useEnvironmentContext();
 
-	const handleSave = async ({ name, addresses }: any) => {
-		await profile.contacts().update(contact.id(), {
-			name,
-			addresses,
-		});
-		await persist();
+	useEffect(() => setErrors({}), [isOpen]);
 
-		onSave?.(contact.id());
+	const formatError = (errorMessage: string, name: string) => {
+		switch (true) {
+			case errorMessage.includes("already exists"):
+				return {
+					name: t("CONTACTS.VALIDATION.CONTACT_NAME_EXISTS", {
+						name,
+					}),
+				};
+		}
+	};
+
+	const handleSave = async ({ name, addresses }: any) => {
+		try {
+			await profile.contacts().update(contact.id(), {
+				name,
+				addresses,
+			});
+			await persist();
+			onSave?.(contact.id());
+		} catch (e) {
+			setErrors(formatError(e.toString(), name));
+		}
+	};
+
+	const handleChange = (fieldName: string) => {
+		const { [fieldName]: _, ...restErrors } = errors;
+		setErrors(restErrors);
 	};
 
 	return (
 		<Modal title={t("CONTACTS.MODAL_UPDATE_CONTACT.TITLE")} isOpen={isOpen} onClose={onClose}>
 			<div className="mt-8">
 				<ContactForm
+					errors={errors}
 					contact={contact}
 					networks={networks}
 					onCancel={() => onCancel?.()}
 					onDelete={onDelete}
+					onChange={handleChange}
 					onSave={handleSave}
 				/>
 			</div>
