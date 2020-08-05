bf61b486cff90dbc2fe114259c2e456361f279c6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-08-05 14:38:56 +0100

    refactor: input fee to return value on change

--- src/domains/transaction/components/InputFee/InputFee.tsx
+++ src/domains/transaction/components/InputFee/InputFee.tsx
@@ -9,15 +9,23 @@ type InputFeeProps = {
 	max: number;
 	step: number;
 	selectionBarState: any;
+	onChange: (value: string) => void;
 };
 
-export const InputFee = ({ defaultValue, min, max, step, selectionBarState }: InputFeeProps) => {
+export const InputFee = ({ defaultValue, min, max, onChange, step, selectionBarState }: InputFeeProps) => {
 	const { t } = useTranslation();
 
 	return (
 		<div className="flex space-x-2">
 			<div className="flex-1">
-				<InputRange name="fee" defaultValue={defaultValue} min={min} max={max} step={step} />
+				<InputRange
+					name="fee"
+					defaultValue={defaultValue}
+					min={min}
+					max={max}
+					step={step}
+					onChange={onChange}
+				/>
 			</div>
 			<div>
 				<SelectionBar>
