1e9e0ef5fbdedf9eff46cc7df5a2d1d79ef778b2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-08-05 14:44:16 +0100

    refactor: remove form in send transaction component

--- src/domains/transaction/components/SendTransactionForm/SendTransactionForm.tsx
+++ src/domains/transaction/components/SendTransactionForm/SendTransactionForm.tsx
@@ -1,32 +1,26 @@
 import { NetworkData, Profile } from "@arkecosystem/platform-sdk-profiles";
 import { Button } from "app/components/Button";
-import { Form, FormField, FormLabel } from "app/components/Form";
+import {  FormField, FormLabel } from "app/components/Form";
 import { Input, InputAddonEnd, InputGroup } from "app/components/Input";
-import { InputRange } from "app/components/Input/InputRange";
-import { SelectionBar, SelectionBarOption } from "app/components/SelectionBar";
 import { useSelectionState } from "app/components/SelectionBar/useSelectionState";
 import { SelectNetwork } from "domains/network/components/SelectNetwork";
 import { SelectAddress } from "domains/profile/components/SelectAddress";
 import { AddRecipient } from "domains/transaction/components/AddRecipient";
+import { InputFee } from "domains/transaction/components/InputFee";
 import { RecipientListItem } from "domains/transaction/components/RecipientList/RecipientList.models";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
+import { useFormContext } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 import { styled } from "twin.macro";
 
 import { defaultStyle } from "./SendTransactionForm.styles";
 
 type SendTransactionFormProps = {
-	maxAvailableAmount: number;
 	formDefaultData: any;
-	feeRange: any;
-	defaultFee: number;
-	maxFee: number;
-	assetSymbol: string;
 	onSubmit?: (result: any) => void;
 	onBack?: () => void;
 	networks: NetworkData[];
 	profile: Profile;
-	wallets: any[];
 };
 
 const FormWrapper = styled.div`
@@ -34,123 +28,120 @@ const FormWrapper = styled.div`
 `;
 
 export const SendTransactionForm = ({
-	feeRange,
-	maxAvailableAmount,
 	formDefaultData,
-	maxFee,
 	onSubmit,
 	onBack,
-	assetSymbol,
 	networks,
 	profile,
-	wallets,
 }: SendTransactionFormProps) => {
-	const [addedRecipients] = useState([] as RecipientListItem[]);
+	const { t } = useTranslation();
+	const wallets = profile.wallets().values();
 
-	const form = useForm({ defaultValues: formDefaultData });
-	const { register } = form;
-	const { fee, sender, amount } = form.watch();
+	const form = useFormContext();
+	const { formState, getValues, register, setValue } = form;
+	const { network, recipients, senderAddress, smartbridge } = form.watch();
 	const feeRangeValue = useSelectionState(0);
 
+	// TODO: Get fees from SDK/API
+	const fee = getValues("fee") || 0;
+	const maxFee = 100;
+	const maxAvailableAmount = 80;
+	const feeRange = {
+		last: 10,
+		min: 1,
+		average: 14,
+	};
+
+	const onSelectNetwork = (network?: NetworkData | null) => {
+		setValue("network", network, true);
+	};
+
 	const onFormSubmit = () => {
-		const formResult = { ...form.getValues(), ...{ recipients: addedRecipients } };
-		onSubmit?.(formResult);
+		onSubmit?.(getValues());
 	};
 
 	return (
 		<FormWrapper>
-			<Form id="send-transaction__form" context={form} onSubmit={() => void 0}>
-				<FormField name="network" className="relative mt-1">
-					<div className="mb-2">
-						<FormLabel label="Network" />
-					</div>
-					<SelectNetwork id="SendTransactionForm__network" networks={networks} />
-				</FormField>
+			<FormField name="network" className="relative mt-1">
+				<div className="mb-2">
+					<FormLabel label="Network" />
+				</div>
+				<SelectNetwork
+					id="SendTransactionForm__network"
+					networks={networks}
+					value={network}
+					onSelect={onSelectNetwork}
+				/>
+			</FormField>
 
-				<FormField name="sender" className="relative mt-1">
-					<div className="mb-2">
-						<FormLabel label="Sender" />
-					</div>
+			<FormField name="senderAddress" className="relative mt-1">
+				<div className="mb-2">
+					<FormLabel label="Sender" />
+				</div>
 
-					<div data-testid="sender-address">
-						<SelectAddress address={sender} ref={register} wallets={wallets} />
-					</div>
-				</FormField>
-
-				<div data-testid="recipient-address">
-					<AddRecipient
-						assetSymbol={assetSymbol}
-						maxAvailableAmount={maxAvailableAmount}
-						availableAmount={amount}
-						profile={profile}
+				<div data-testid="sender-address">
+					<SelectAddress
+						address={senderAddress}
+						wallets={wallets}
+						onChange={(address: string) => setValue("senderAddress", address, true)}
 					/>
 				</div>
+			</FormField>
 
-				<FormField name="smartbridge" className="relative mt-1">
-					<div className="mb-2">
-						<FormLabel label="Smartbridge" />
-					</div>
-					<InputGroup>
-						<Input type="text" placeholder=" " className="pr-20" maxLength={255} />
-						<InputAddonEnd>
-							<button className="px-4 text-theme-neutral-light focus:outline-none">255 Max</button>
-						</InputAddonEnd>
-					</InputGroup>
-				</FormField>
+			<div data-testid="recipient-address">
+				<AddRecipient
+					maxAvailableAmount={maxAvailableAmount}
+					profile={profile}
+					onChange={(recipients: RecipientListItem[]) => setValue("recipients", recipients, true)}
+					recipients={recipients}
+				/>
+			</div>
 
-				<div className="flex items-center">
-					<div className="w-2/4">
-						<FormField name="fee" className="relative mt-1">
-							<div className="mb-2">
-								<FormLabel label="Fee ARK" />
-								<InputRange ref={register} defaultValue={fee} min={0} max={maxFee} step={0.01} />
-							</div>
-						</FormField>
-					</div>
-					<div className="w-2/4 mt-6 text-right">
-						<SelectionBar>
-							<SelectionBarOption value={feeRange.last} {...feeRangeValue}>
-								Last
-							</SelectionBarOption>
-							<SelectionBarOption value={feeRange.min} {...feeRangeValue}>
-								Min
-							</SelectionBarOption>
-							<SelectionBarOption value={feeRange.average} {...feeRangeValue}>
-								Average
-							</SelectionBarOption>
-						</SelectionBar>
-					</div>
+			<FormField name="smartbridge" className="relative mt-1">
+				<div className="mb-2">
+					<FormLabel label="Smartbridge" />
 				</div>
-				<div className="flex justify-end space-x-3">
-					<Button data-testid="send-transaction-click-back" variant="plain" onClick={onBack}>
-						Back
-					</Button>
-					<Button data-testid="send-transaction-click-submit" type="submit" onClick={onFormSubmit}>
-						Continue
-					</Button>
-				</div>
-			</Form>
+				<InputGroup>
+					<Input
+						type="text"
+						placeholder=" "
+						className="pr-20"
+						maxLength={255}
+						defaultValue={smartbridge}
+						onChange={(event: any) => setValue("smartbridge", event.target.value, true)}
+					/>
+					<InputAddonEnd>
+						<button type="button" className="px-4 text-theme-neutral-light focus:outline-none">
+							255 Max
+						</button>
+					</InputAddonEnd>
+				</InputGroup>
+			</FormField>
+
+			<FormField name="fee">
+				<FormLabel>{t("TRANSACTION.TRANSACTION_FEE")}</FormLabel>
+				<InputFee
+					selectionBarState={feeRangeValue}
+					defaultValue={fee}
+					min={0}
+					max={maxFee}
+					step={0.01}
+					onChange={(value: number) => setValue("fee", value, true)}
+				/>
+			</FormField>
+
+			<div className="flex justify-end space-x-3 hidden">
+				<Button data-testid="send-transaction-click-back" variant="plain" onClick={onBack}>
+					Back
+				</Button>
+				<Button data-testid="send-transaction-click-submit" onSubmit={onFormSubmit}>
+					Continue
+				</Button>
+			</div>
 		</FormWrapper>
 	);
 };
 
 SendTransactionForm.defaultProps = {
-	maxFee: 100,
-	maxAvailableAmount: 0,
-	assetSymbol: "ARK",
-	feeRange: {
-		last: 10,
-		min: 1,
-		average: 14,
-	},
-	networks: [],
-	defaultFee: 0,
-	formDefaultData: {
-		network: null,
-		sender: null,
-		amount: null,
-		smartbridge: null,
-		fee: 0,
-	},
-	wallets: [],
+	formDefaultData: {},
 };
