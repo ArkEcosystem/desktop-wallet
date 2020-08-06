ec681baea5ff44b033eb95feaeb56da8018219ce
goga-m <goga.mamoulasvili@gmail.com>
2020-08-06 10:37:10 +0300

    feat: create `Splash` page

---
+++ src/domains/splash/pages/Splash/Splash.tsx
@@ -0,0 +1,59 @@
+import { DateTime } from "@arkecosystem/platform-sdk-intl";
+import { images } from "app/assets/images";
+import { Divider } from "app/components/Divider";
+import { Icon } from "app/components/Icon";
+import { Page, Section } from "app/components/Layout";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import { version } from "../../../../../package.json";
+
+const { SplashBanner } = images.splash.pages.splash;
+const commonAssets = images.common;
+
+export const Splash = ({ year }: any) => {
+	const { t } = useTranslation();
+
+	const currentYear = year || DateTime.make().format("YYYY");
+
+	return (
+		<Page navbarStyle="logo-only">
+			<Section className="flex flex-col justify-center flex-1 text-center">
+				<div className="w-64 mx-auto lg:w-128">
+					<SplashBanner />
+				</div>
+
+				<div data-testid="Splash__text" className="mt-8">
+					<h1 className="text-4xl font-extrabold">{t("SPLASH.BRAND")}</h1>
+					<p className="text-theme-neutral-dark animate-pulse">{t("SPLASH.LOADING")}</p>
+					<div className="justify-center flex mt-4">
+						<div className="animate-spin text-theme-primary-400">
+							<Icon name="Notification" width={32} height={32} />
+						</div>
+					</div>
+				</div>
+				<div
+					data-testid="Splash__footer"
+					className="fixed bottom-4 left-0 right-0 flex justify-center items-center font-semibold text-sm text-theme-neutral-500"
+				>
+					<div>
+						{currentYear} {t("SPLASH.COPYRIGHT")}
+					</div>
+					<Divider type="vertical" />
+					<div>{t("SPLASH.RIGHTS")}</div>
+					<Divider type="vertical" />
+					<img
+						src={commonAssets.ARKLogo}
+						className="h-4 w-4 bg-theme-neutral-500 rounded-sm p-px mr-2"
+						alt="ARK Logo"
+					/>
+					<div>{t("SPLASH.PRODUCT")}</div>
+					<Divider type="vertical" />
+					<div>
+						{t("SPLASH.VERSION")} {version}
+					</div>
+				</div>
+			</Section>
+		</Page>
+	);
+};
