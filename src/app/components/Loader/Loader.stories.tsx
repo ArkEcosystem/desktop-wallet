453979b1bb26b199987fd0bb1e0954997e78997c
goga-m <goga-m@users.noreply.github.com>
2020-08-06 17:23:05 +0000

    style: resolve style guide violations

--- src/app/components/Loader/Loader.stories.tsx
+++ src/app/components/Loader/Loader.stories.tsx
@@ -5,7 +5,7 @@ import { Loader } from "./Loader";
 export default { title: "App / Components / Loader" };
 
 export const Default = () => (
-	<div className="space-y-5 ml-2">
+	<div className="ml-2 space-y-5">
 		<div className="my-4">Absolute position in relative container div</div>
 		<div className="relative border-1 border-theme-neutral-500" style={{ width: 600, height: 400 }}>
 			<Loader />
