d70a4cc0f52e9d1089e5747a432bf8dbaba11562
goga-m <goga.mamoulasvili@gmail.com>
2020-08-07 21:34:14 +0300

    test: add blockfolio fixture tests

--- src/domains/news/pages/News/News.test.tsx
+++ src/domains/news/pages/News/News.test.tsx
@@ -1,23 +1,45 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { Blockfolio, BlockfolioResponse } from "@arkecosystem/platform-sdk-news";
+import { httpClient } from "app/services";
 import { createMemoryHistory } from "history";
+import nock from "nock";
 import React from "react";
 import { Route } from "react-router-dom";
-import { getDefaultProfileId, renderWithRouter } from "testing-library";
+import { getDefaultProfileId, renderWithRouter, waitFor } from "testing-library";
+import blockfolioFixture from "tests/fixtures/news/blockfolio.json";
 
-import { news } from "../../data";
 import { News } from "./News";
 
 const history = createMemoryHistory();
 const newsURL = `/profiles/${getDefaultProfileId()}/news`;
 
+let subject: Blockfolio;
+
 describe("News", () => {
-	beforeAll(() => {
+	beforeAll(async () => {
 		history.push(newsURL);
+		nock.disableNetConnect();
+		nock("https://platform.ark.io/api")
+			.get("/coins/ark/signals")
+			.query(true)
+			.reply(200, blockfolioFixture)
+			.persist();
+
+		subject = new Blockfolio(httpClient);
 	});
 
-	it("should render", () => {
-		const { container } = renderWithRouter(
+	beforeAll(() => {
+		jest.useFakeTimers();
+	});
+
+	afterAll(() => {
+		jest.useRealTimers();
+	});
+
+	it("should render", async () => {
+		const { getAllByTestId } = renderWithRouter(
 			<Route path="/profiles/:profileId/news">
-				<News news={news} />
+				<News />
 			</Route>,
 			{
 				routes: [newsURL],
@@ -25,6 +47,15 @@ describe("News", () => {
 			},
 		);
 
-		expect(container).toMatchSnapshot();
+		await waitFor(() => {
+			expect(getAllByTestId("NewsCard")).toHaveLength(15);
+		});
+	});
+
+	it("should retrieve blockfolio data using findByCoin", async () => {
+		const result: BlockfolioResponse = await subject.findByCoin("ark");
+
+		expect(result.meta).toMatchObject(blockfolioFixture.meta);
+		expect(result.data).toMatchObject(blockfolioFixture.data);
 	});
 });
