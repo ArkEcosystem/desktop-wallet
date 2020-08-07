cd52ad1aecb9f33558c20b5c18f30059b3a172a4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-08-07 12:33:19 +0100

    test: improve & update input range based on changes

--- src/app/components/Input/InputRange.test.tsx
+++ src/app/components/Input/InputRange.test.tsx
@@ -1,35 +1,52 @@
+
 import React from "react";
 import { fireEvent, render } from "testing-library";
 
 import { InputRange } from "./InputRange";
 
+const properties = {
+	defaultValue: (5 * 1e8).toFixed(0),
+	min: 1,
+	max: 10,
+	step: 1,
+};
+
 describe("InputRange", () => {
 	it("should render with default value", () => {
-		const { asFragment, getByTestId } = render(<InputRange defaultValue={5} min={1} max={10} step={1} />);
+		const { asFragment, getByTestId } = render(<InputRange {...properties} />);
+
 		expect(getByTestId("InputCurrency")).toHaveValue("5");
 		expect(getByTestId("Range__thumb")).toHaveAttribute("aria-valuenow", "5");
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should update the range when changing the input", () => {
-		const { getByTestId } = render(<InputRange defaultValue={5} min={1} max={10} step={1} />);
+		const { getByTestId } = render(<InputRange {...properties} />);
 		const input = getByTestId("InputCurrency");
+
 		fireEvent.change(input, { target: { value: "9" } });
+
 		expect(getByTestId("Range__thumb")).toHaveAttribute("aria-valuenow", "9");
+		expect(input).toHaveValue("9");
 	});
 
 	it("should update the input when changing the range", () => {
-		const { getByTestId } = render(<InputRange defaultValue={5} min={1} max={10} step={1} />);
+		const { getByTestId } = render(<InputRange {...properties} />);
 		const thumb = getByTestId("Range__thumb");
+
 		fireEvent.keyDown(thumb, { key: "ArrowRight", code: "ArrowRight" });
-		expect(getByTestId("Range__thumb")).toHaveAttribute("aria-valuenow", "6");
+
+		expect(thumb).toHaveAttribute("aria-valuenow", "6");
 		expect(getByTestId("InputCurrency")).toHaveValue("6");
 	});
 
 	it("should not allow a value greater than the maximum", () => {
-		const { getByTestId } = render(<InputRange defaultValue={5} min={1} max={10} step={1} />);
+		const { getByTestId } = render(<InputRange {...properties} />);
 		const input = getByTestId("InputCurrency");
+
 		fireEvent.change(input, { target: { value: "11" } });
+
+		expect(input).toHaveValue("10");
 		expect(getByTestId("Range__thumb")).toHaveAttribute("aria-valuenow", "10");
 	});
 });
