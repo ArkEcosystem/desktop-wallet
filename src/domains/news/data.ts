6fbaa98086fd7dc8df29db35fdcde3b19d69058a
goga-m <goga.mamoulasvili@gmail.com>
2020-08-07 21:31:38 +0300

    refactor: drop unused news mock data

--- src/domains/news/data.ts
+++ src/domains/news/data.ts
@@ -1,37 +1,3 @@
-export const news = [
-	{
-		asset: {
-			icon: "Ark",
-			name: "ARK Ecosystem",
-			className: "text-theme-danger-400 border-theme-danger-light",
-		},
-		author: {
-			name: "Travis Walker",
-			role: "Co-Founder",
-		},
-		dateCreated: "2020-06-30T23:00:00.000Z",
-		category: "Technical",
-		content:
-			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, voluptate! Rem molestiae mollitia error quidem nostrum saepe explicabo quas velit ipsum facere, excepturi iste. Minus dolore alias modi recusandae ab.",
-	},
-	{
-		asset: {
-			icon: "Ark",
-			name: "ARK Ecosystem",
-			className: "text-theme-danger-400 border-theme-danger-light",
-		},
-		author: {
-			name: "Travis Walker",
-			role: "Co-Founder",
-		},
-		dateCreated: "2020-06-30T23:00:00.000Z",
-		category: "Technical",
-		content:
-			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, voluptate! Rem molestiae mollitia error quidem nostrum saepe explicabo quas velit ipsum facere, excepturi iste. Minus dolore alias modi recusandae ab.",
-		coverImage: "https://miro.medium.com/max/1000/1*vMM0Z7qfGgA8a8NRhB_ZQA.png",
-	},
-];
-
 export const categories = [
 	{
 		name: "All",
