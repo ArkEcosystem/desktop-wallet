cb7c6d17a8fa947dc609b5266d383b7797b3e486
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-08-07 00:00:22 -0300

    feat: paginate transactions table (#2622)

--- src/domains/dashboard/components/Transactions/Transactions.tsx
+++ src/domains/dashboard/components/Transactions/Transactions.tsx
@@ -7,10 +7,12 @@ import { useTranslation } from "react-i18next";
 type TransactionsProps = {
 	title: string;
 	transactions: Contracts.TransactionDataType[];
+	fetchMoreAction: Function;
+	moreAction?: any;
 	emptyText?: string;
 };
 
-export const Transactions = ({ transactions, title, emptyText }: TransactionsProps) => {
+export const Transactions = ({ transactions, title, emptyText, fetchMoreAction }: TransactionsProps) => {
 	const { t } = useTranslation();
 
 	return (
@@ -20,7 +22,12 @@ export const Transactions = ({ transactions, title, emptyText }: TransactionsPro
 				<div className="pt-8">
 					<TransactionTable transactions={transactions} currencyRate="2" />
 
-					<Button variant="plain" className="w-full mt-10 mb-5">
+					<Button
+						data-testid="transactions__fetch-more-button"
+						variant="plain"
+						className="w-full mt-10 mb-5"
+						onClick={() => fetchMoreAction()}
+					>
 						{t("COMMON.VIEW_MORE")}
 					</Button>
 				</div>
