2837bf010f5d995096a62fce2780ffd7ba62ea28
Brian Faust <hello@basecode.sh>
2020-08-08 13:21:49 +0300

    wip

--- config-overrides.js
+++ config-overrides.js
@@ -13,41 +13,7 @@ const injectTailwindCSS = () =>
 		require("postcss-import"),
 		require("tailwindcss")("./src/tailwind.config.js"),
 		require("autoprefixer"),
-<<<<<<< HEAD
-	]),
-	addWebpackPlugin(
-		new webpack.NormalModuleReplacementPlugin(
-			/node_modules\/@arkecosystem\/crypto\/dist\/index\.bundled\.js/,
-			"index.js",
-		),
-	),
-	(config) => {
-		config.node = {
-			fs: "empty",
-		};
-
-		config.externals = {
-			"node-hid": "commonjs node-hid",
-			"usb-detection": "commonjs usb-detection",
-		};
-
-		config.target = "electron-renderer";
-
-		config.resolve = {
-			extensions: [".ts", ".js", ".jsx", ".tsx", ".scss", ".json", ".node"],
-			alias: {
-				app: path.resolve(__dirname, "src/app/"),
-				data: path.resolve(__dirname, "src/data/"),
-				domains: path.resolve(__dirname, "src/domains"),
-				resources: path.resolve(__dirname, "src/resources"),
-				styles: path.resolve(__dirname, "src/styles"),
-				tests: path.resolve(__dirname, "src/tests"),
-				utils: path.resolve(__dirname, "src/utils"),
-			},
-		};
-=======
 	]);
->>>>>>> 3.0-react
 
 module.exports = override(
 	setWebpackTarget("electron-renderer"),
