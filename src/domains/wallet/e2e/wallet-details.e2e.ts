bf5255ba417fccb5218d1b8e7469b341c7bdc61a
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-10 16:37:57 -0300

    tests(e2e): add e2e journey

---
+++ src/domains/wallet/e2e/wallet-details.e2e.ts
@@ -0,0 +1,36 @@
+import { ClientFunction, Selector } from "testcafe";
+
+import { buildTranslations as translations } from "../../../app/i18n/helpers";
+import { getPageURL } from "../../../utils/e2e-utils";
+
+fixture`Wallet Details`.page(getPageURL());
+
+const scrollTop = ClientFunction(() => {
+	window.scrollTo({ top: 0 });
+});
+
+test("should open wallet details page", async (t) => {
+	await t.click(Selector("p").withText("John Doe"));
+	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
+
+	// Navigate to wallet details page
+	await t.click(Selector("[data-testid=WalletCard__D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD]"));
+	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
+});
+
+test("should load transactions with load more action", async (t) => {
+	await t.click(Selector("p").withText("John Doe"));
+	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
+
+	// Navigate to wallet details page
+	await t.click(Selector("[data-testid=WalletCard__D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD]"));
+	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
+
+	// Check transactions rows
+	await t.expect(Selector("[data-testid=transactions__fetch-more-button]").exists).ok();
+
+	await t.expect(Selector("[data-testid=TransactionRow]").count).eql(20);
+	await t.click(Selector("[data-testid=transactions__fetch-more-button]"));
+
+	await t.expect(Selector("[data-testid=TransactionRow]").count).eql(48);
+});
