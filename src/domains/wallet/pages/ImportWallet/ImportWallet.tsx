a4709bde8c49cd92b6e165aa9cf835568c72ef92
brenopolanski <breno.polanski@gmail.com>
2020-08-11 14:11:12 -0300

    feat: add completed step on the import wallet page

--- src/domains/wallet/pages/ImportWallet/ImportWallet.tsx
+++ src/domains/wallet/pages/ImportWallet/ImportWallet.tsx
@@ -1,15 +1,19 @@
 import { NetworkData, Profile, Wallet } from "@arkecosystem/platform-sdk-profiles";
+import { Avatar } from "app/components/Avatar";
 import { Button } from "app/components/Button";
+import { Divider } from "app/components/Divider";
 import { Form, FormField, FormHelperText, FormLabel } from "app/components/Form";
 import { Header } from "app/components/Header";
-import { InputAddress, InputPassword } from "app/components/Input";
+import { Input, InputAddress, InputPassword } from "app/components/Input";
 import { Page, Section } from "app/components/Layout";
 import { StepIndicator } from "app/components/StepIndicator";
 import { TabPanel, Tabs } from "app/components/Tabs";
 import { Toggle } from "app/components/Toggle";
 import { useEnvironmentContext } from "app/contexts";
 import { useActiveProfile } from "app/hooks/env";
+import { NetworkIcon } from "domains/network/components/NetworkIcon";
 import { SelectNetwork } from "domains/network/components/SelectNetwork";
+import { getNetworkExtendedData } from "domains/network/helpers";
 import React, { useEffect, useMemo, useState } from "react";
 import { useForm, useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
@@ -132,6 +136,56 @@ export const SecondStep = ({ profile }: { profile: Profile }) => {
 			<div className="mt-8" data-testid="ImportWallet__fields">
 				{renderImportInput()}
 			</div>
+		</section>
+	);
+};
+
+export const ThirdStep = () => {
+	const { getValues, register } = useFormContext();
+	const network: NetworkData = getValues("network");
+	const wallet: Wallet = getValues("wallet");
+	const networkConfig = getNetworkExtendedData({ coin: network.coin(), network: network.id() });
+	const { t } = useTranslation();
+
+	return (
+		<section data-testid="CreateWallet__fourth-step">
+			<div className="my-8">
+				<Header
+					title={t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.TITLE")}
+					subtitle={t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.SUBTITLE")}
+				/>
+			</div>
+
+			<ul>
+				<li className="flex justify-between">
+					<div>
+						<p className="text-sm font-semibold text-theme-neutral-dark">{t("COMMON.NETWORK")}</p>
+						<p data-testid="CreateWallet__network-name" className="text-lg font-medium">
+							{networkConfig?.displayName}
+						</p>
+					</div>
+					<NetworkIcon coin={network.coin()} network={network.id()} />
+				</li>
+				<li>
+					<Divider dashed />
+				</li>
+				<li className="flex justify-between">
+					<div>
+						<p className="text-sm font-semibold text-theme-neutral-dark">{t("COMMON.ADDRESS")}</p>
+						<p data-testid="CreateWallet__wallet-address" className="text-lg font-medium">
+							{wallet.address()}
+						</p>
+					</div>
+					<Avatar address={wallet.address()} />
+				</li>
+			</ul>
+
+			<Divider dashed />
+
+			<FormField name="name">
+				<FormLabel label={t("WALLETS.PAGE_CREATE_WALLET.WALLET_NAME")} />
+				<Input data-testid="CreateWallet__wallet-name" ref={register} />
+			</FormField>
 		</section>
 	);
 };
