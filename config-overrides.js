1d1a66552376e8b7a5ce54b71240f13710f20a87
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-08-12 15:33:49 +0100

    refactor: update webpack config externals

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,61 @@
 const {
 	override,
 	addPostcssPlugins,
+	addWebpackExternals,
 	addWebpackPlugin,
 	setWebpackTarget,
-	addWebpackExternals,
 } = require("customize-cra");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const { EnvironmentPlugin } = require("webpack");
+const { dependencies } = require("./package.json");
 
-const addNodeExternals = ({ allowlist = [] }) =>
-	addWebpackExternals([
-		nodeExternals({
-			allowlist,
-		}),
-	]);
+const whiteListedModules = [
+	"@arkecosystem/platform-sdk",
+	"@arkecosystem/platform-sdk-ada",
+	"@arkecosystem/platform-sdk-ark",
+	"@arkecosystem/platform-sdk-atom",
+	"@arkecosystem/platform-sdk-btc",
+	"@arkecosystem/platform-sdk-crypto",
+	"@arkecosystem/platform-sdk-eos",
+	"@arkecosystem/platform-sdk-eth",
+	"@arkecosystem/platform-sdk-intl",
+	"@arkecosystem/platform-sdk-lsk",
+	"@arkecosystem/platform-sdk-markets",
+	"@arkecosystem/platform-sdk-neo",
+	"@arkecosystem/platform-sdk-news",
+	"@arkecosystem/platform-sdk-profiles",
+	"@arkecosystem/platform-sdk-support",
+	"@arkecosystem/platform-sdk-trx",
+	"@arkecosystem/platform-sdk-xlm",
+	"@arkecosystem/platform-sdk-xmr",
+	"@arkecosystem/platform-sdk-xrp",
+	"@arkecosystem/utils",
+	"@tippyjs/react",
+	"downshift",
+	"framer-motion",
+	"got",
+	"hash-wasm",
+	"i18next",
+	"isomorphic-fetch",
+	"recharts",
+	"react",
+	"react-dom",
+	"react-error-boundary",
+	"react-hook-form",
+	"react-inlinesvg",
+	"react-router-config",
+	"react-scripts",
+	"react-i18next",
+	"react-range",
+	"react-router-dom",
+	"react-table",
+	"styled-components",
+	"swiper",
+];
+
+const addNodeExternals = () =>
+	addWebpackExternals([...Object.keys(dependencies || {}).filter((d) => !whiteListedModules.includes(d))]);
 
 const injectTailwindCSS = () =>
 	addPostcssPlugins([
@@ -35,13 +76,6 @@ const copyFiles = () =>
 		}),
 	);
 
-module.exports = override(
-	setWebpackTarget("electron-renderer"),
-	injectTailwindCSS(),
-	addNodeExternals({
-		allowlist: [/tippy/, /swipe/],
-	}),
-	copyFiles(),
-);
+module.exports = override(setWebpackTarget("electron-renderer"), injectTailwindCSS(), addNodeExternals(), copyFiles());
 
 module.exports.injectTailwindCSS = injectTailwindCSS;
