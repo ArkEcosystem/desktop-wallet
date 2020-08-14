675cdfd0f9b616ee4a7ec879aa8c71fa78985b58
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-14 18:02:23 -0300

    refactor: fill transfer detail with real transfer data

--- src/domains/transaction/components/TransferDetail/TransferDetail.tsx
+++ src/domains/transaction/components/TransferDetail/TransferDetail.tsx
@@ -1,10 +1,15 @@
+// import { Contracts } from "@arkecosystem/platform-sdk";
+import { ProfileSetting } from "@arkecosystem/platform-sdk-profiles";
+import { BigNumber } from "@arkecosystem/platform-sdk-support";
 import { Avatar } from "app/components/Avatar";
 import { Circle } from "app/components/Circle";
 import { Icon } from "app/components/Icon";
 import { Label } from "app/components/Label";
-// UI Elements
 import { Modal } from "app/components/Modal";
 import { TransactionDetail } from "app/components/TransactionDetail";
+import { TruncateMiddle } from "app/components/TruncateMiddle";
+// UI Elements
+import { useActiveProfile } from "app/hooks/env";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
@@ -12,17 +17,62 @@ type TransferDetailProps = {
 	isOpen: boolean;
 	onClose?: any;
 	onCancel?: any;
+	transaction: any;
+};
+
+const renderConfirmationStatus = (confirmations: BigNumber) => {
+	if (confirmations?.toNumber() < 51) {
+		return (
+			<div className="flex">
+				Not Confirmed
+				<div className="flex w-6 h-6 ml-2 rounded-full bg-theme-danger-200 text-theme-danger-500">
+					<div className="m-auto">
+						<Icon name="CrossSlim" width={12} height={12} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex">
+			Well Confirmed
+			<div className="flex w-6 h-6 ml-2 rounded-full bg-theme-success-200 text-theme-success-500">
+				<div className="m-auto">
+					<Icon name="Checkmark" width={15} height={15} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export const TransferDetail = (props: TransferDetailProps) => {
 	const { t } = useTranslation();
+	const activeProfile = useActiveProfile();
+	const ticker = activeProfile.settings().get<string>(ProfileSetting.ExchangeCurrency, "")!;
+	const walletAlias = activeProfile.wallets().findByAddress(props.transaction.recipient())?.alias();
 
-	return (
-		<Modal title={t("TRANSACTION.MODAL_TRANSFER_DETAIL.TITLE")} isOpen={props.isOpen} onClose={props.onClose}>
-			<TransactionDetail label={t("TRANSACTION.SENDER")} extra={<Avatar address="test" />} border={false}>
-				<div className="mt-2 font-semibold">ADDRESS</div>
-			</TransactionDetail>
+	const renderRecipient = () => {
+		if (walletAlias) {
+			return (
+				<TransactionDetail
+					label={t("TRANSACTION.RECIPIENT")}
+					extra={
+						<div className="flex items-center">
+							<Circle className="-mr-2 border-black">
+								<Icon name="Delegate" width={25} height={25} />
+							</Circle>
+							<Avatar address={props.transaction.recipient()} />
+						</div>
+					}
+				>
+					{walletAlias}
+					<TruncateMiddle text={props.transaction.recipient()} className="ml-2 text-theme-neutral" />
+				</TransactionDetail>
+			);
+		}
 
+		return (
 			<TransactionDetail
 				label={t("TRANSACTION.RECIPIENT")}
 				extra={
@@ -30,51 +80,82 @@ export const TransferDetail = (props: TransferDetailProps) => {
 						<Circle className="-mr-2 border-black">
 							<Icon name="Delegate" width={25} height={25} />
 						</Circle>
-						<Avatar address="test" />
+						<Avatar address={props.transaction.recipient()} />
 					</div>
 				}
 			>
-				ROBank
-				<span className="ml-2 text-theme-neutral">ADDR...ESSS</span>
+				<TruncateMiddle text={props.transaction.recipient()} className="text-theme-neutral" />
 			</TransactionDetail>
+		);
+	};
 
+	const renderAmount = () => {
+		if (props.transaction.isSent()) {
+			return (
+				<TransactionDetail
+					label={t("TRANSACTION.AMOUNT")}
+					extra={
+						<Circle className="border-theme-danger-200 text-theme-danger-dark">
+							<Icon name="Sent" width={25} height={25} />
+						</Circle>
+					}
+				>
+					<Label color="danger">{`${props.transaction.amount().toHuman()} ${ticker.toUpperCase()}`}</Label>
+
+					<span className="ml-2 text-theme-neutral">23,000.00 USD</span>
+				</TransactionDetail>
+			);
+		}
+
+		return (
 			<TransactionDetail
 				label={t("TRANSACTION.AMOUNT")}
 				extra={
 					<Circle className="border-theme-success-200 text-theme-success-dark">
-						<Icon name="Received" width={40} height={40} />
+						<Icon name="Received" width={25} height={25} />
 					</Circle>
 				}
 			>
-				<Label color="success">2,088.84557 ARK</Label>
+				<Label color="success">{`${props.transaction.amount().toHuman()} ${ticker.toUpperCase()}`}</Label>
 
 				<span className="ml-2 text-theme-neutral">23,000.00 USD</span>
 			</TransactionDetail>
+		);
+	};
 
-			<TransactionDetail label={t("TRANSACTION.TRANSACTION_FEE")}>0.09812015 ARK</TransactionDetail>
-
-			<TransactionDetail label={t("TRANSACTION.SMARTBRIDGE")}>
-				<div className="flex justify-between">
-					Hello!
-					<Icon name="Smartbridge" width={20} height={20} />
-				</div>
+	return (
+		<Modal title={t("TRANSACTION.MODAL_TRANSFER_DETAIL.TITLE")} isOpen={props.isOpen} onClose={props.onClose}>
+			<TransactionDetail label={t("TRANSACTION.SENDER")} extra={<Avatar address="test" />} border={false}>
+				<div className="mt-2 font-semibold">{props.transaction.sender()}</div>
 			</TransactionDetail>
 
-			<TransactionDetail label={t("TRANSACTION.TIMESTAMP")}>14.04.2020 21:42:40</TransactionDetail>
+			{renderRecipient()}
+
+			{renderAmount()}
+
+			<TransactionDetail
+				label={t("TRANSACTION.TRANSACTION_FEE")}
+			>{`${props.transaction.fee().toHuman()} ${ticker.toUpperCase()}`}</TransactionDetail>
+
+			{props.transaction.memo() && (
+				<TransactionDetail label={t("TRANSACTION.SMARTBRIDGE")}>
+					<div className="flex justify-between">
+						{props.transaction.memo()}
+						<Icon name="Smartbridge" width={20} height={20} />
+					</div>
+				</TransactionDetail>
+			)}
+
+			<TransactionDetail label={t("TRANSACTION.TIMESTAMP")}>
+				{props.transaction.timestamp()!.format("DD.MM.YYYY HH:mm:ss")}
+			</TransactionDetail>
 
 			<TransactionDetail label={t("TRANSACTION.CONFIRMATIONS")}>
-				<div className="flex">
-					Well Confirmed
-					<div className="flex w-6 h-6 ml-2 rounded-full bg-theme-success-200 text-theme-success-500">
-						<div className="m-auto">
-							<Icon name="Checkmark" width={15} height={15} />
-						</div>
-					</div>
-				</div>
+				{renderConfirmationStatus(props.transaction.confirmations())}
 			</TransactionDetail>
 
 			<TransactionDetail label={t("TRANSACTION.ID")}>
-				<span className="text-theme-primary-dark">1234678...12312313</span>
+				<TruncateMiddle text={props.transaction.id()} className="text-theme-primary-dark" />
 
 				<span className="inline-block ml-4 text-theme-primary-300">
 					<Icon name="Copy" />
@@ -82,7 +163,7 @@ export const TransferDetail = (props: TransferDetailProps) => {
 			</TransactionDetail>
 
 			<TransactionDetail label={t("TRANSACTION.BLOCK_ID")}>
-				<span className="text-theme-primary-dark">1234678...12312313</span>
+				<TruncateMiddle text={props.transaction.data.blockId} className="text-theme-primary-dark" />
 
 				<span className="inline-block ml-4 text-theme-primary-300">
 					<Icon name="Copy" />
