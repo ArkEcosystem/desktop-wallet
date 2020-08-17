c6a647db5a4eb874343945f3a46d2f6dc9da696e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-08-17 13:11:22 +0100

    refactor: generalise send transaction form component

--- src/domains/transaction/components/SendTransactionForm/SendTransactionForm.tsx
+++ src/domains/transaction/components/SendTransactionForm/SendTransactionForm.tsx
@@ -1,11 +1,8 @@
 import { NetworkData, Profile, Wallet } from "@arkecosystem/platform-sdk-profiles";
 import { FormField, FormLabel } from "app/components/Form";
-import { Input, InputAddonEnd, InputGroup } from "app/components/Input";
 import { SelectNetwork } from "domains/network/components/SelectNetwork";
 import { SelectAddress } from "domains/profile/components/SelectAddress";
-import { AddRecipient } from "domains/transaction/components/AddRecipient";
 import { InputFee } from "domains/transaction/components/InputFee";
-import { RecipientListItem } from "domains/transaction/components/RecipientList/RecipientList.models";
 import React, { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
@@ -15,9 +12,10 @@ type SendTransactionFormProps = {
 	networks: NetworkData[];
 	profile: Profile;
 	onFail?: any;
+	children?: React.ReactNode;
 };
 
-export const SendTransactionForm = ({ networks, profile, onFail }: SendTransactionFormProps) => {
+export const SendTransactionForm = ({ children, networks, profile, onFail }: SendTransactionFormProps) => {
 	const history = useHistory();
 	const { t } = useTranslation();
 	const [wallets, setWallets] = useState<Wallet[]>([]);
@@ -33,7 +31,6 @@ export const SendTransactionForm = ({ networks, profile, onFail }: SendTransacti
 	});
 
 	const fee = getValues("fee") || null;
-	const maxAvailableAmount = 80;
 
 	useEffect(() => {
 		const loadFees = async () => {
@@ -96,36 +93,7 @@ export const SendTransactionForm = ({ networks, profile, onFail }: SendTransacti
 				</div>
 			</FormField>
 
-			<div data-testid="recipient-address">
-				<AddRecipient
-					maxAvailableAmount={maxAvailableAmount}
-					profile={profile}
-					onChange={(recipients: RecipientListItem[]) => setValue("recipients", recipients, true)}
-					recipients={recipients}
-				/>
-			</div>
-
-			<FormField name="smartbridge" className="relative mt-1">
-				<div className="mb-2">
-					<FormLabel label="Smartbridge" />
-				</div>
-				<InputGroup>
-					<Input
-						data-testid="Input__smartbridge"
-						type="text"
-						placeholder=" "
-						className="pr-20"
-						maxLength={255}
-						defaultValue={smartbridge}
-						onChange={(event: any) => setValue("smartbridge", event.target.value, true)}
-					/>
-					<InputAddonEnd>
-						<button type="button" className="px-4 text-theme-neutral-light focus:outline-none">
-							255 Max
-						</button>
-					</InputAddonEnd>
-				</InputGroup>
-			</FormField>
+			{children}
 
 			<FormField name="fee">
 				<FormLabel>{t("TRANSACTION.TRANSACTION_FEE")}</FormLabel>
