5d7dbebf4995e7fb7237921af030f203835a3c14
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-08-20 21:40:09 +0100

    test: update based on registration changes

--- src/domains/transaction/pages/Registration/Registration.test.tsx
+++ src/domains/transaction/pages/Registration/Registration.test.tsx
@@ -1,189 +1,190 @@
 /* eslint-disable @typescript-eslint/require-await */
-
-import { availableNetworksMock } from "domains/network/data";
+import { Profile, Wallet } from "@arkecosystem/platform-sdk-profiles";
 import { createMemoryHistory } from "history";
+import nock from "nock";
 import React from "react";
 import { Route } from "react-router-dom";
-import { act, fireEvent, getDefaultProfileId, RenderResult, renderWithRouter, waitFor } from "testing-library";
+import {
+	act,
+	defaultNetMocks,
+	env,
+	fireEvent,
+	getDefaultProfileId,
+	RenderResult,
+	renderWithRouter,
+	waitFor,
+	within,
+} from "utils/testing-library";
 
 import { Registration } from "./Registration";
 
-let rendered: RenderResult;
+let profile: Profile;
+let wallet: Wallet;
 
-const defaultFormValues = {
-	networks: availableNetworksMock,
-	registrationTypes: [
-		{
-			value: "business",
-			label: "Business",
-		},
-	],
-	formDefaultData: {
-		network: null,
-		address: null,
-	},
-	onDownload: jest.fn(),
-};
+const renderPage = async () => {
+	const history = createMemoryHistory();
+	const registrationURL = `/profiles/${profile.id()}/transactions/${wallet.id()}/registration`;
+	history.push(registrationURL);
 
-describe("Registration", () => {
-	beforeEach(() => {
-		const history = createMemoryHistory();
-		const registrationURL = `/profiles/${getDefaultProfileId()}/transactions/registration`;
+	let rendered: RenderResult;
 
-		history.push(registrationURL);
-
+	await act(async () => {
 		rendered = renderWithRouter(
-			<Route path="/profiles/:profileId/transactions/registration">
-				<Registration {...defaultFormValues} />
+			<Route path="/profiles/:profileId/transactions/:walletId/registration">
+				<Registration />
 			</Route>,
 			{
 				routes: [registrationURL],
 				history,
 			},
 		);
+
+		await waitFor(() => expect(rendered.getByTestId("Registration__form")).toBeTruthy());
+	});
+
+	return {
+		...rendered!,
+		history,
+	};
+};
+
+describe("Registration", () => {
+	beforeAll(async () => {
+		profile = env.profiles().findById(getDefaultProfileId());
+		wallet = profile.wallets().first();
+
+		await profile.wallets().importByAddress("D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib", "ARK", "devnet");
+	});
+
+	beforeEach(() => {
+		nock.cleanAll();
+		defaultNetMocks();
 	});
 
 	it("should render 1st step", async () => {
-		const { asFragment, getByTestId } = rendered;
+		const { asFragment, getByTestId } = await renderPage();
 
 		expect(getByTestId("Registration__first-step")).toBeTruthy();
-		expect(defaultFormValues.onDownload).toHaveBeenCalledTimes(0);
+		expect(getByTestId("Registration__continue-button")).toHaveAttribute("disabled");
 		await waitFor(() => expect(asFragment()).toMatchSnapshot());
 	});
 
 	it("should should go back", async () => {
-		const { asFragment, getByTestId } = rendered;
-
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__back-button"));
-		});
+		const { asFragment, getByTestId } = await renderPage();
 
 		expect(getByTestId("Registration__first-step")).toBeTruthy();
-		expect(defaultFormValues.onDownload).toHaveBeenCalledTimes(0);
-		await waitFor(() => expect(asFragment()).toMatchSnapshot());
+
+		const typeSelectInput = within(getByTestId("Registration__type")).getByTestId("select-list__input");
+		expect(typeSelectInput).not.toHaveValue("delegateRegistration");
+
+		await act(async () => {
+			fireEvent.click(getByTestId("select-list__toggle-button"));
+
+			await waitFor(() => expect(getByTestId("select-list__toggle-option-1")).toBeTruthy());
+
+			fireEvent.click(getByTestId("select-list__toggle-option-1"));
+
+			await waitFor(() => expect(getByTestId("Registration__continue-button")).not.toHaveAttribute("disabled"));
+
+			fireEvent.click(getByTestId("Registration__continue-button"));
+
+			await waitFor(() => expect(getByTestId("SendDelegateRegistration__step--second")).toBeTruthy());
+			await waitFor(() => expect(getByTestId("Registration__back-button")).not.toHaveAttribute("disabled"));
+
+			fireEvent.click(getByTestId("Registration__back-button"));
+
+			await waitFor(() => expect(getByTestId("Registration__first-step")).toBeTruthy());
+			await waitFor(() => expect(typeSelectInput).toHaveValue("delegateRegistration"));
+			await waitFor(() => expect(asFragment()).toMatchSnapshot());
+		});
 	});
 
-	it("should render 2nd step", async () => {
-		const { asFragment, getByTestId } = rendered;
+	it("should select registration type & show form", async () => {
+		const { asFragment, getByTestId } = await renderPage();
+
+		expect(getByTestId("Registration__first-step")).toBeTruthy();
+
+		const typeSelectInput = within(getByTestId("Registration__type")).getByTestId("select-list__input");
+		expect(typeSelectInput).not.toHaveValue("delegateRegistration");
 
 		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
+			fireEvent.click(getByTestId("select-list__toggle-button"));
 
-		expect(getByTestId("Registration__second-step")).toBeTruthy();
-		expect(defaultFormValues.onDownload).toHaveBeenCalledTimes(0);
-		await waitFor(() => expect(asFragment()).toMatchSnapshot());
+			await waitFor(() => expect(getByTestId("select-list__toggle-option-1")).toBeTruthy());
+
+			fireEvent.click(getByTestId("select-list__toggle-option-1"));
+
+			await waitFor(() => expect(getByTestId("Registration__continue-button")).not.toHaveAttribute("disabled"));
+
+			fireEvent.click(getByTestId("Registration__continue-button"));
+
+			await waitFor(() => expect(getByTestId("SendDelegateRegistration__step--second")).toBeTruthy());
+			await waitFor(() => expect(typeSelectInput).toHaveValue("delegateRegistration"));
+			await waitFor(() => expect(asFragment()).toMatchSnapshot());
+		});
 	});
 
-	it("should render 3rd step", async () => {
-		const { asFragment, getByTestId } = rendered;
+	it("should change sender & route", async () => {
+		const { asFragment, getAllByTestId, getByTestId, history } = await renderPage();
+
+		expect(getByTestId("Registration__first-step")).toBeTruthy();
 
 		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
+			fireEvent.click(within(getByTestId("sender-address")).getByTestId("SelectAddress__wrapper"));
+			await waitFor(() => expect(getByTestId("modal__inner")).toBeTruthy());
 
-		expect(getByTestId("Registration__third-step")).toBeTruthy();
-		expect(defaultFormValues.onDownload).toHaveBeenCalledTimes(0);
-		await waitFor(() => expect(asFragment()).toMatchSnapshot());
+			const historySpy = jest.spyOn(history, "push");
+
+			const firstAddress = getByTestId("AddressListItem__select-1");
+			fireEvent.click(firstAddress);
+			expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+
+			const secondWallet = profile.wallets().values()[1];
+			await waitFor(() =>
+				expect(historySpy).toHaveBeenCalledWith(
+					`/profiles/${profile?.id()}/transactions/${secondWallet.id()}/registration`,
+				),
+			);
+
+			historySpy.mockRestore();
+
+			await waitFor(() => expect(asFragment()).toMatchSnapshot());
+		});
 	});
 
-	it("should render 4th step", async () => {
-		const { asFragment, getByTestId } = rendered;
+	it.skip("should not have delegate option if wallet is a delegate", async () => {
+		const { asFragment, getAllByTestId, getByTestId, history } = await renderPage();
+
+		expect(getByTestId("Registration__first-step")).toBeTruthy();
 
 		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
+			fireEvent.click(within(getByTestId("sender-address")).getByTestId("SelectAddress__wrapper"));
+			await waitFor(() => expect(getByTestId("modal__inner")).toBeTruthy());
 
-		expect(getByTestId("Registration__fourth-step")).toBeTruthy();
-		expect(defaultFormValues.onDownload).toHaveBeenCalledTimes(0);
-		await waitFor(() => expect(asFragment()).toMatchSnapshot());
-	});
+			const historySpy = jest.spyOn(history, "push");
 
-	it("should render 5th step", async () => {
-		const { asFragment, getByTestId } = rendered;
+			const firstAddress = getByTestId("AddressListItem__select-2");
+			fireEvent.click(firstAddress);
+			expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
 
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__send-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__send-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__send-button"));
-		});
+			const thirdWallet = profile.wallets().values()[2];
+			await waitFor(() =>
+				expect(historySpy).toHaveBeenCalledWith(
+					`/profiles/${profile?.id()}/transactions/${thirdWallet.id()}/registration`,
+				),
+			);
 
-		expect(getByTestId("TransactionSuccessful")).toBeTruthy();
-		expect(defaultFormValues.onDownload).toHaveBeenCalledTimes(0);
-		await waitFor(() => expect(asFragment()).toMatchSnapshot());
-	});
+			historySpy.mockRestore();
 
-	it("should submit", async () => {
-		const { asFragment, getByTestId } = rendered;
+			await waitFor(() => expect(getByTestId("Registration__first-step")).toBeTruthy());
 
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__continue-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__send-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__send-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__send-button"));
-		});
-		await act(async () => {
-			fireEvent.click(getByTestId("Registration__download-button"));
-		});
+			fireEvent.click(getByTestId("select-list__toggle-button"));
 
-		expect(defaultFormValues.onDownload).toHaveBeenCalledTimes(1);
-		await waitFor(() => expect(asFragment()).toMatchSnapshot());
-	});
-
-	it("should select registration type", async () => {
-		const { asFragment, getByTestId } = rendered;
-
-		const toggle = getByTestId("select-list__toggle-button");
-
-		act(() => {
-			fireEvent.click(toggle);
+			await waitFor(() => expect(getByTestId("select-list__toggle-option-0")).toBeTruthy());
+			await waitFor(() =>
+				expect(getByTestId("select-list__toggle-option-1")).toThrow(/Unable to find an element by/),
+			);
+			await waitFor(() => expect(asFragment()).toMatchSnapshot());
 		});
-
-		const firstOption = getByTestId("select-list__toggle-option-0");
-		expect(firstOption).toBeTruthy();
-
-		act(() => {
-			fireEvent.click(firstOption);
-		});
-
-		expect(getByTestId("select-list__input")).toHaveValue("business");
-		await waitFor(() => expect(asFragment()).toMatchSnapshot());
 	});
 });
