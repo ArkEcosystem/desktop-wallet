c57bcb0fd2131bed729c4cfb902f2e7599248738
brenopolanski <breno.polanski@gmail.com>
2020-08-19 22:31:43 -0300

    refactor: update delegate list stories

--- src/domains/vote/components/DelegateList/DelegateList.stories.tsx
+++ src/domains/vote/components/DelegateList/DelegateList.stories.tsx
@@ -1,9 +1,19 @@
-/* import React from "react";
+import React from "react";
+import { WalletsDecorator } from "utils/storybook";
 
-import { delegateListData } from "../../data";
 import { DelegateList } from "./DelegateList";
 
-export default { title: "Domains / Vote / Components / DelegateList" };
+export default {
+	title: "Domains / Vote / Components / DelegateList",
+	decorators: [
+		(storyFn: any) => (
+			<WalletsDecorator count={1} withDelegates={true}>
+				{storyFn}
+			</WalletsDecorator>
+		),
+	],
+};
 
-export const Default = () => <DelegateList data={delegateListData} />;
- */
+export const Default = ({ delegates }: { delegates: any }) => (
+	<DelegateList delegates={delegates.items().slice(0, 10)} />
+);
