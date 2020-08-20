0c026f0a001aed40faed9bf660640dbbe89a2112
Edgar Goetzendorff <hello@dated.fun>
2020-08-20 23:11:36 +0200

    refactor: change avatar button color on hover

--- src/domains/profile/pages/CreateProfile/CreateProfile.tsx
+++ src/domains/profile/pages/CreateProfile/CreateProfile.tsx
@@ -48,14 +48,16 @@ export const CreateProfile = () => {
 			content: (
 				<div className="flex flex-row mt-2 mb-8">
 					<div className="flex items-center justify-center w-24 h-24 mr-6 border-2 border-dashed rounded border-theme-neutral-300">
-						<button
-							type="button"
-							className="flex items-center justify-center w-20 h-20 rounded-full bg-theme-primary-contrast"
-							onClick={handleChangeAvatar}
-							data-testid="CreateProfile__upload-button"
-						>
-							<Icon name="Upload" />
-						</button>
+						<div className="w-20 h-20 rounded-full overflow-hidden">
+							<Button
+								className="h-20 w-20"
+								variant="plain"
+								onClick={handleChangeAvatar}
+								data-testid="CreateProfile__upload-button"
+							>
+								<Icon name="Upload" />
+							</Button>
+						</div>
 					</div>
 					{avatarImage && (
 						<div className="relative w-24 h-24 rounded bg-theme-neutral-contrast">
