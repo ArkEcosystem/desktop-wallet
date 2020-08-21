d15276a046f5ce145190a8d4df0e0ad3d28f9cfd
goga-m <goga.mamoulasvili@gmail.com>
2020-08-21 18:29:46 +0300

    feat: create `DelegateRowItemSkeleton` component

---
+++ src/domains/profile/pages/MyRegistrations/components/DelegateTable/DelegateRowItemSkeleton.tsx
@@ -0,0 +1,46 @@
+import { Circle } from "app/components/Circle";
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+
+export const DelegateRowItemSkeleton = () => (
+		<tr data-testid="delegate-table__row" className="border-b border-dashed border-theme-neutral-light">
+			<td className="w-24 py-6 pr-4">
+				<div className="flex items-center">
+					<Circle className="border-theme-neutral-300" size="lg" />
+					<Circle className="border-theme-neutral-300" size="lg" />
+				</div>
+			</td>
+			<td className="font-semibold">
+				<Skeleton height={6} width="60%" className="mt-4" />
+			</td>
+			<td className="font-semibold text-theme-neutral-dark">
+				<Skeleton height={6} width="80%" className="mt-4" />
+			</td>
+			<td className="font-semibold text-center text-theme-primary">
+				<span className="flex justify-center">
+					<Skeleton width={24} height={24} />
+				</span>
+			</td>
+			<td className="text-theme-neutral-light">
+				<span className="flex justify-center">
+					<Skeleton width={24} height={24} />
+				</span>
+			</td>
+			<td className="font-semibold text-theme-neutral-dark">
+				<div className="flex items-center justify-end">
+					<Skeleton height={6} width={100} className="mt-4" />
+				</div>
+			</td>
+			<td className="font-semibold text-theme-neutral-dark">
+				<div className="flex items-center justify-end">
+					<Skeleton height={6} width={40} className="mt-4" />
+					<Skeleton height={6} width={80} className="ml-4 mt-4" />
+				</div>
+			</td>
+			<td className="align-middle">
+				<span className="flex justify-end">
+					<Skeleton width={24} height={24} />
+				</span>
+			</td>
+		</tr>
+	);
