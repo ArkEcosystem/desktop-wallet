62aa2ef4e47abd9b3bd56cb544841f00f2dfae95
brenopolanski <breno.polanski@gmail.com>
2020-08-22 21:49:28 -0300

    test: add tests to the address table

--- src/domains/vote/components/AddressTable/AddressTable.test.tsx
+++ src/domains/vote/components/AddressTable/AddressTable.test.tsx
@@ -1,19 +1,27 @@
+import { Profile, Wallet } from "@arkecosystem/platform-sdk-profiles";
 import React from "react";
-import { render } from "testing-library";
+import { env, getDefaultProfileId, render } from "testing-library";
 
-import { addressListData } from "../../data";
-import { AddressList } from "./AddressList";
+import { AddressTable } from "./AddressTable";
 
-describe("AddressList", () => {
+let profile: Profile;
+let wallet: Wallet;
+
+describe("AddressTable", () => {
+	beforeAll(() => {
+		profile = env.profiles().findById(getDefaultProfileId());
+		wallet = profile.wallets().findById("ac38fe6d-4b67-4ef1-85be-17c5f6841129");
+	});
+
 	it("should render", () => {
-		const { container, asFragment } = render(<AddressList data={addressListData} />);
+		const { container, asFragment } = render(<AddressTable wallets={[wallet]} />);
 
 		expect(container).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should render with empty list", () => {
-		const { container, asFragment } = render(<AddressList data={[]} />);
+		const { container, asFragment } = render(<AddressTable wallets={[]} />);
 
 		expect(container).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
