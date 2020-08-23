28f5244b1987e955c07a517e0125fb9ba58119f6
goga-m <goga.mamoulasvili@gmail.com>
2020-08-23 20:21:16 +0300

    chore: update stories with loading states

--- src/domains/wallet/components/WalletVote/WalletVote.stories.tsx
+++ src/domains/wallet/components/WalletVote/WalletVote.stories.tsx
@@ -21,3 +21,5 @@ export const Default = ({ delegates }: { delegates: Coins.WalletDataCollection }
 );
 
 export const Empty = () => <WalletVote onUnvote={action("onUnvote")} />;
+
+export const Loading = () => <WalletVote isLoading={true} />;
