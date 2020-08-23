2f3057e13efe368fc147b3d7a955375d94487e01
goga-m <goga.mamoulasvili@gmail.com>
2020-08-23 20:33:42 +0300

    test: add e2e tests

--- src/domains/wallet/e2e/wallet-details.e2e.ts
+++ src/domains/wallet/e2e/wallet-details.e2e.ts
@@ -16,6 +16,18 @@ test("should open wallet details page", async (t) => {
 	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
 });
 
+test("should show initial loading state", async (t) => {
+	await t.click(Selector("p").withText("John Doe"));
+	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
+
+	// Navigate to wallet details page
+	await t.hover(Selector("[data-testid=WalletCard__D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD]"));
+	await t.click(Selector("[data-testid=WalletCard__D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD]"));
+	await t.expect(Selector("[data-testid=WalletRegistrations__skeleton]").exists).ok();
+	await t.expect(Selector("[data-testid=WalletVote__skeleton]").exists).ok();
+	await t.expect(Selector("[data-testid=TransactionRow__skeleton]").exists).ok();
+});
+
 test("should load transactions with load more action", async (t) => {
 	await t.click(Selector("p").withText("John Doe"));
 	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
