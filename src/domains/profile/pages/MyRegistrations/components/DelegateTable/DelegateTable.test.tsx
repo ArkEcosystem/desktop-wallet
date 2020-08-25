f88229fa82358154822293132f18c354a05163cd
goga-m <goga.mamoulasvili@gmail.com>
2020-08-25 14:26:52 +0300

    test: add tests for `DelegateTable`

--- src/domains/profile/pages/MyRegistrations/components/DelegateTable/DelegateTable.test.tsx
+++ src/domains/profile/pages/MyRegistrations/components/DelegateTable/DelegateTable.test.tsx
@@ -1,7 +1,7 @@
 import { Profile, ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
 import nock from "nock";
 import React from "react";
-import { env, getDefaultProfileId, render } from "testing-library";
+import { act, env, fireEvent, getDefaultProfileId, render, waitFor, within } from "testing-library";
 
 import { DelegateTable } from "./DelegateTable";
 
@@ -9,14 +9,14 @@ let profile: Profile;
 let delegates: ReadWriteWallet[];
 
 describe("Welcome", () => {
-	beforeAll(async () => {
+	beforeAll(() => {
+		nock("https://dwallets.ark.io")
+			.get("/delegates/D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb")
+			.reply(200, require("tests/fixtures/delegates/D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb.json"));
+
 		profile = env.profiles().findById(getDefaultProfileId());
 
 		const wallets = profile.wallets().values();
-
-		await env.persist();
-		nock.enableNetConnect();
-
 		delegates = wallets.filter((wallet: ReadWriteWallet) => wallet.isDelegate());
 	});
 
@@ -25,23 +25,50 @@ describe("Welcome", () => {
 		expect(asFragment()).toMatchSnapshot();
 	});
 
-	// it("should have a functional toggle", () => {
-	// 	const onAction = jest.fn();
-	// 	const { getAllByTestId, getByTestId } = render(<DelegateTable data={registrations} onAction={onAction} />);
-	//
-	// 	const toggle = getAllByTestId("dropdown__toggle");
-	//
-	// 	act(() => {
-	// 		fireEvent.click(toggle[0]);
-	// 	});
-	//
-	// 	const secondOption = getByTestId("dropdown__option--1");
-	// 	expect(secondOption).toBeTruthy();
-	//
-	// 	act(() => {
-	// 		fireEvent.click(secondOption);
-	// 	});
-	//
-	// 	expect(onAction).toHaveBeenCalled();
-	// });
+	it("should render delegates", async () => {
+		const { asFragment, getAllByTestId } = render(<DelegateTable wallets={delegates} />);
+
+		await waitFor(() => expect(getAllByTestId("DelegateRowItem").length).toEqual(1));
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should handle resign delegate dropdown action", async () => {
+		const onAction = jest.fn();
+		const { asFragment, getAllByTestId } = render(<DelegateTable wallets={delegates} onAction={onAction} />);
+		expect(asFragment()).toMatchSnapshot();
+
+		await waitFor(() => expect(getAllByTestId("DelegateRowItem").length).toEqual(1));
+
+		const dropdownToggle = within(getAllByTestId("DelegateRowItem")[0]).getByTestId("dropdown__toggle");
+		act(() => {
+			fireEvent.click(dropdownToggle);
+		});
+
+		const resignOption = within(getAllByTestId("DelegateRowItem")[0]).getByTestId("dropdown__option--1");
+		act(() => {
+			fireEvent.click(resignOption);
+		});
+
+		expect(onAction).toBeCalledWith({ walletId: delegates[0].id(), action: "resign" });
+	});
+
+	it("should handle update delegate dropdown action", async () => {
+		const onAction = jest.fn();
+		const { asFragment, getAllByTestId } = render(<DelegateTable wallets={delegates} onAction={onAction} />);
+		expect(asFragment()).toMatchSnapshot();
+
+		await waitFor(() => expect(getAllByTestId("DelegateRowItem").length).toEqual(1));
+
+		const dropdownToggle = within(getAllByTestId("DelegateRowItem")[0]).getByTestId("dropdown__toggle");
+		act(() => {
+			fireEvent.click(dropdownToggle);
+		});
+
+		const resignOption = within(getAllByTestId("DelegateRowItem")[0]).getByTestId("dropdown__option--0");
+		act(() => {
+			fireEvent.click(resignOption);
+		});
+
+		expect(onAction).toBeCalledWith({ walletId: delegates[0].id(), action: "update" });
+	});
 });
