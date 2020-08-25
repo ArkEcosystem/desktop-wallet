64cbea184eadebfeebeb72a188bcfb2534850ab6
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-25 11:54:45 -0300

    tests: add sync delegates test util

--- src/domains/transaction/components/VoteDetail/VoteDetail.test.tsx
+++ src/domains/transaction/components/VoteDetail/VoteDetail.test.tsx
@@ -4,7 +4,7 @@ import nock from "nock";
 import React from "react";
 import { Route } from "react-router-dom";
 import { TransactionFixture } from "tests/fixtures/transactions";
-import { getDefaultProfileId, renderWithRouter, waitFor } from "utils/testing-library";
+import { getDefaultProfileId, renderWithRouter, syncDelegates,waitFor } from "utils/testing-library";
 
 import { translations } from "../../i18n";
 import { VoteDetail } from "./VoteDetail";
@@ -15,13 +15,15 @@ const fixtureProfileId = getDefaultProfileId();
 let dashboardURL: string;
 
 describe("VoteDetail", () => {
-	beforeAll(() => {
+	beforeAll(async () => {
 		nock.disableNetConnect();
 		nock("https://dwallets.ark.io")
 			.get("/api/delegates")
 			.query({ page: "1" })
 			.reply(200, require("tests/fixtures/coins/ark/delegates.json"))
 			.persist();
+
+		await syncDelegates();
 	});
 
 	beforeEach(() => {
