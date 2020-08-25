795b97dc0ab7ae3bd36cbb39a973ac36e5043a0a
goga-m <goga.mamoulasvili@gmail.com>
2020-08-25 14:27:12 +0300

    test: add delegate mocks in `MyRegistrations` tests

--- src/domains/profile/pages/MyRegistrations/MyRegistrations.test.tsx
+++ src/domains/profile/pages/MyRegistrations/MyRegistrations.test.tsx
@@ -1,7 +1,8 @@
 import { createMemoryHistory } from "history";
+import nock from "nock";
 import React from "react";
 import { Route } from "react-router-dom";
-import { act, fireEvent, getDefaultProfileId, renderWithRouter, waitFor,within } from "testing-library";
+import { act, fireEvent, getDefaultProfileId, renderWithRouter, waitFor, within } from "testing-library";
 
 import { blockchainRegistrations, businessRegistrations } from "../../data";
 import { MyRegistrations } from "./MyRegistrations";
@@ -14,6 +15,12 @@ const emptyRegistrationsURL = `/profiles/cba050f1-880f-45f0-9af9-cfe48f406052/re
 const delegateWalletId = "d044a552-7a49-411c-ae16-8ff407acc430";
 
 describe("Welcome", () => {
+	beforeAll(() => {
+		nock("https://dwallets.ark.io")
+			.get("/delegates/D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb")
+			.reply(200, require("tests/fixtures/delegates/D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb.json"));
+	});
+
 	beforeEach(() => {
 		history.push(registrationsURL);
 	});
