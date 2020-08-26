bc3578b4fd65d40a1aef0ec6f191282e2ef55c0e
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-08-25 21:35:44 -0300

    test: fix e2e

--- src/domains/wallet/e2e/wallet-details.e2e.ts
+++ src/domains/wallet/e2e/wallet-details.e2e.ts
@@ -7,15 +7,6 @@ fixture`Wallet Details`.page(getPageURL());
 
 const scroll = ClientFunction((x, y) => window.scrollBy(x, y));
 
-test("should open wallet details page", async (t) => {
-	await t.click(Selector("p").withText("John Doe"));
-	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
-
-	// Navigate to wallet details page
-	await t.click(Selector("[data-testid=WalletCard__D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD]"));
-	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
-});
-
 test("should show initial loading state", async (t) => {
 	await t.click(Selector("p").withText("John Doe"));
 	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
@@ -29,6 +20,15 @@ test("should show initial loading state", async (t) => {
 	await t.expect(Selector("[data-testid=WalletRegistrations__skeleton]").exists).ok();
 	await t.expect(Selector("[data-testid=WalletVote__skeleton]").exists).ok();
 	await t.expect(Selector("[data-testid=TransactionRow__skeleton]").exists).ok();
+});
+
+test("should open wallet details page", async (t) => {
+	await t.click(Selector("p").withText("John Doe"));
+	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
+
+	// Navigate to wallet details page
+	await t.click(Selector("[data-testid=WalletCard__D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD]"));
+	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
 });
 
 test("should load transactions with load more action", async (t) => {
