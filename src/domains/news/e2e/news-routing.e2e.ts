2c21880780ae31da3846c76d969860fe3d92f35f
dated <hello@dated.fun>
2020-08-26 10:30:58 +0200

    refactor: news e2e tests

--- src/domains/news/e2e/news-routing.e2e.ts
+++ src/domains/news/e2e/news-routing.e2e.ts
@@ -1,6 +1,27 @@
+import { Selector } from "testcafe";
+
 import { getPageURL } from "../../../utils/e2e-utils";
 import { goToNews } from "./common";
 
-fixture`News page filtering`.page(getPageURL());
+fixture`News routing`.page(getPageURL());
 
-test("should navigate to news page", goToNews);
+test("should navigate to news page", async (t) => await goToNews(t));
+
+test("should navigate between pages", async (t) => {
+	await t.expect(Selector('[data-testid="NewsCard"]').count).eql(itemsPerPage);
+
+	await t.hover(Selector('[data-testid="CompactPagination__next"]'));
+
+	await t.expect(Selector('[data-testid="CompactPagination__previous"]').hasAttribute("disabled")).ok();
+
+	await t.click(Selector('[data-testid="CompactPagination__next"]'));
+	await t.expect(Selector('[data-testid="NewsCard"]').exists).ok();
+	await t.expect(Selector('[data-testid="NewsCard"]').count).eql(itemsPerPage);
+
+	await t.expect(Selector('[data-testid="CompactPagination__previous"]').hasAttribute("disabled")).notOk();
+	await t.hover(Selector('[data-testid="CompactPagination__previous"]'));
+	await t.click(Selector('[data-testid="CompactPagination__previous"]'));
+
+	await t.expect(Selector('[data-testid="NewsCard"]').exists).ok();
+	await t.expect(Selector('[data-testid="NewsCard"]').count).eql(itemsPerPage);
+});
