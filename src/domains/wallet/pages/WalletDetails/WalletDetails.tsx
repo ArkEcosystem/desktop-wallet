48355a556e7dec152e9b69ce512a534d1d3418c3
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-26 11:09:11 -0300

    refactor: remove sync delegates call

--- src/domains/wallet/pages/WalletDetails/WalletDetails.tsx
+++ src/domains/wallet/pages/WalletDetails/WalletDetails.tsx
@@ -60,9 +60,6 @@ export const WalletDetails = ({ txSkeletonRowsLimit }: WalletDetailsProps) => {
 
 	useEffect(() => {
 		const fetchAllData = async () => {
-			// TODO: move this to profile initialising and run it every X period
-			await env.coins().syncDelegates(activeWallet.coinId()!, activeWallet.networkId()!);
-
 			const transactions = (await activeWallet.transactions({ limit: 10 })).items();
 			const walletData = await activeWallet.client().wallet(activeWallet.address());
 
