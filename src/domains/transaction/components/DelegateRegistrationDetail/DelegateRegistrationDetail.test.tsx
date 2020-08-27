1941e14156fd538f19afc121ad4082562fd10754
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-08-27 15:41:27 -0300

    refactor: replace dummy data in delegate registration details

--- src/domains/transaction/components/DelegateRegistrationDetail/DelegateRegistrationDetail.test.tsx
+++ src/domains/transaction/components/DelegateRegistrationDetail/DelegateRegistrationDetail.test.tsx
@@ -1,5 +1,7 @@
+import { BigNumber } from "@arkecosystem/platform-sdk-support";
 import React from "react";
 import { render } from "testing-library";
+import { TransactionFixture } from "tests/fixtures/transactions";
 
 // i18n
 import { translations } from "../../i18n";
@@ -7,14 +9,40 @@ import { DelegateRegistrationDetail } from "./DelegateRegistrationDetail";
 
 describe("DelegateRegistrationDetail", () => {
 	it("should not render if not open", () => {
-		const { asFragment, getByTestId } = render(<DelegateRegistrationDetail isOpen={false} />);
+		const { asFragment, getByTestId } = render(
+			<DelegateRegistrationDetail
+				isOpen={false}
+				transaction={{ ...TransactionFixture, username: () => "Ark Wallet" }}
+			/>,
+		);
 
 		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should render a modal", () => {
-		const { asFragment, getByTestId } = render(<DelegateRegistrationDetail isOpen={true} />);
+		const { asFragment, getByTestId } = render(
+			<DelegateRegistrationDetail
+				isOpen={true}
+				transaction={{ ...TransactionFixture, username: () => "Ark Wallet" }}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_DELEGATE_REGISTRATION_DETAIL.TITLE);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render as confirmed", () => {
+		const { asFragment, getByTestId } = render(
+			<DelegateRegistrationDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					username: () => "Ark Wallet",
+					confirmations: () => BigNumber.make(52),
+				}}
+			/>,
+		);
 
 		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_DELEGATE_REGISTRATION_DETAIL.TITLE);
 		expect(asFragment()).toMatchSnapshot();
