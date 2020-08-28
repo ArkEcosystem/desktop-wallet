8b05fe9742785aaae77f842e0b7977acc468d47b
goga-m <goga-m@users.noreply.github.com>
2020-08-28 15:14:58 +0000

    style: resolve style guide violations

--- src/domains/transaction/e2e/delegate-resignation.e2e.ts
+++ src/domains/transaction/e2e/delegate-resignation.e2e.ts
@@ -1,4 +1,4 @@
-import {  Selector } from "testcafe";
+import { Selector } from "testcafe";
 
 import { buildTranslations } from "../../../app/i18n/helpers";
 import { getPageURL } from "../../../utils/e2e-utils";
