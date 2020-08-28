3343e561886bdbccf97ec6823faf0e87277bbe0e
Edgar Goetzendorff <hello@dated.fun>
2020-08-28 09:23:16 +0200

    refactor: explicitly align dropdown content and remove min width (#2745)

--- src/app/components/Dropdown/Dropdown.styles.ts
+++ src/app/components/Dropdown/Dropdown.styles.ts
@@ -1,7 +1,3 @@
-import tw, { css } from "twin.macro";
-
-export const defaultStyles = css`
-	min-width: 12rem;
-`;
+import tw from "twin.macro";
 
 export const defaultClasses = tw`mt-8 py-3 absolute z-10 bg-theme-background rounded-lg shadow-xl border-theme-neutral-contrast border-1`;
