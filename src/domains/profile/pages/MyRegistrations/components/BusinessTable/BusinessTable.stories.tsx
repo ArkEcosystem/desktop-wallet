1be8468a313de07c2fc0ba7d7def687968a04a31
goga-m <goga.mamoulasvili@gmail.com>
2020-08-31 18:00:11 +0300

    wip

--- src/domains/profile/pages/MyRegistrations/components/BusinessTable/BusinessTable.stories.tsx
+++ src/domains/profile/pages/MyRegistrations/components/BusinessTable/BusinessTable.stories.tsx
@@ -1,30 +1,30 @@
-import React from "react";
 
-import { BusinessTable } from "./BusinessTable";
+
+
 
 export default {
 	title: "Domains / Profile / Pages / MyRegistrations / Components / BusinessTable",
 };
 
-export const Default = () => {
-	const registrations = [
-		{
-			agent: "OLEBank",
-			businessName: "ARK Ecosystem",
-			history: [],
-			website: "",
-			msq: true,
-			repository: [],
-		},
-		{
-			agent: "OLEBank",
-			businessName: "ARK Ecosystem",
-			history: [],
-			website: "",
-			msq: true,
-			repository: [],
-		},
-	];
-
-	return <BusinessTable data={registrations} onAction={console.log} />;
-};
+// export const Default = () => {
+// 	const registrations = [
+// 		{
+// 			agent: "OLEBank",
+// 			businessName: "ARK Ecosystem",
+// 			history: [],
+// 			website: "",
+// 			msq: true,
+// 			repository: [],
+// 		},
+// 		{
+// 			agent: "OLEBank",
+// 			businessName: "ARK Ecosystem",
+// 			history: [],
+// 			website: "",
+// 			msq: true,
+// 			repository: [],
+// 		},
+// 	];
+//
+// 	return <BusinessTable data={registrations} onAction={console.log} />;
+// };
