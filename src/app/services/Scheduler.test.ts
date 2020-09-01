4c0819182240c7e8844804aa9d00885e029bb423
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-09-01 17:22:50 -0300

    tests: update typo error

--- src/app/services/Scheduler.test.ts
+++ src/app/services/Scheduler.test.ts
@@ -3,7 +3,7 @@ import { Scheduler } from "./Scheduler";
 describe("Scheduler test", () => {
 	jest.useFakeTimers();
 
-	it("should run an action after an determined interval", () => {
+	it("should run an job after an determined interval", () => {
 		const job = jest.fn();
 		const scheduler = new Scheduler(100);
 		scheduler.schedule([job]);
@@ -11,15 +11,15 @@ describe("Scheduler test", () => {
 		expect(job).toHaveBeenCalled();
 	});
 
-	it("should run an action using the default interval", () => {
+	it("should run an job using the default interval", () => {
 		const job = jest.fn();
 		const scheduler = new Scheduler();
-		scheduler.schedule([action]);
+		scheduler.schedule([job]);
 		jest.advanceTimersByTime(300000);
 		expect(job).toHaveBeenCalled();
 	});
 
-	it("should run actions and the done callback", () => {
+	it("should run jobs and the done callback", () => {
 		const job = jest.fn();
 		const doneCallback = jest.fn();
 		const scheduler = new Scheduler();
