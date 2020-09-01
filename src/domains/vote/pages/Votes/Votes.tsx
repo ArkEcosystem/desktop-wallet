2f26e568ef849c5da418e68f3377585e412532d8
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-09-01 11:28:06 -0300

    feat: sync wallet data (#2748)

--- src/domains/vote/pages/Votes/Votes.tsx
+++ src/domains/vote/pages/Votes/Votes.tsx
@@ -94,10 +94,7 @@ export const Votes = () => {
 	}, [activeProfile, network]);
 
 	const loadDelegates = useCallback(
-		async (wallet) => {
-			// TODO: move this to profile initialising and run it every X period
-			// eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-			await env.delegates().sync(wallet?.coinId()!, wallet?.networkId()!);
+		(wallet) => {
 			// eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
 			const delegates = env.delegates().all(wallet?.coinId()!, wallet?.networkId()!);
 			const readOnlyDelegates = DelegateMapper.execute(
@@ -119,10 +116,10 @@ export const Votes = () => {
 		setNetwork(network!);
 	};
 
-	const handleSelectAddress = async (address: string) => {
+	const handleSelectAddress = (address: string) => {
 		setAddress(address);
 		const wallet = activeProfile.wallets().findByAddress(address);
-		await loadDelegates(wallet);
+		loadDelegates(wallet);
 	};
 
 	return (
