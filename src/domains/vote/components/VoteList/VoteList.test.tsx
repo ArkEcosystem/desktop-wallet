ddb37b24ccef7b9fc57eab7097d4d1dbfdd7d328
brenopolanski <breno.polanski@gmail.com>
2020-09-02 11:49:40 -0300

    feat: add VoteList component

---
+++ src/domains/vote/components/VoteList/VoteList.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render } from "testing-library";
+
+import { VoteList } from "./VoteList";
+
+const votes = [
+	{
+		address: "FJKDSALJFKASLJFKSDAJD333FKFKDSAJFKSAJFKLASJKDFJ",
+		delegateName: "Delegate 1",
+	},
+	{
+		address: "AhFJKDSALJFKASLJFKSDEAJ333FKFKDSAJFKSAJFKLASJKDFJ",
+		delegateName: "Delegate 2",
+	},
+	{
+		address: "FAhFJKDSALJFKASLJFKSFDAJ333FKFKDSAJFKSAJFKLASJKDFJ",
+		delegateName: "Delegate 3",
+	},
+];
+
+describe("VoteList", () => {
+	it("should render", () => {
+		const { container, asFragment } = render(<VoteList votes={votes} />);
+
+		expect(container).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render with empty list", () => {
+		const { container, asFragment } = render(<VoteList votes={[]} />);
+
+		expect(container).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
