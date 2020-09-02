dce8a42f4fb8d70c27bda601a6e4db5a69e0b594
goga-m <goga.mamoulasvili@gmail.com>
2020-09-02 12:27:57 +0300

    test: add e2e tests

--- src/domains/profile/e2e/my-registrations-routing.e2e.ts
+++ src/domains/profile/e2e/my-registrations-routing.e2e.ts
@@ -2,30 +2,14 @@ import { Selector } from "testcafe";
 
 import { buildTranslations } from "../../../app/i18n/helpers";
 import { getPageURL } from "../../../utils/e2e-utils";
-import { goToProfile } from "./common";
+import { goToMyRegistrations,goToProfile } from "./common";
 
 const translations = buildTranslations();
 
-fixture`My Registrations`.page(getPageURL()).beforeEach(async (t) => await goToProfile(t));
-
-test("should navigate to my registrations from navigation bar", async (t) => {
-	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok();
-
-	await t.click(Selector('[data-testid="navbar__useractions"]'));
-	await t
-		.expect(Selector('[data-testid="dropdown__option--2"]').withText(translations.COMMON.REGISTRATIONS).exists)
-		.ok();
-	await t.click(Selector('[data-testid="dropdown__option--2"]').withText(translations.COMMON.REGISTRATIONS));
-
-	await t.expect(Selector("[data-testid=header__title]").exists).ok();
-	await t
-		.expect(
-			Selector("[data-testid=header__title]").withText(translations.PROFILE.PAGE_MY_REGISTRATIONS.TITLE).exists,
-		)
-		.ok();
-});
+fixture`My Registrations`.page(getPageURL());
 
 test("should navigate to my registrations page", async (t) => {
+	await goToProfile(t);
 	// Navigate to wallet details page
 	await t.hover(Selector("[data-testid=WalletCard__D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb]"));
 	await t.click(Selector("[data-testid=WalletCard__D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb]"));
@@ -38,6 +22,96 @@ test("should navigate to my registrations page", async (t) => {
 	await t
 		.expect(
 			Selector("[data-testid=header__title]").withText(translations.PROFILE.PAGE_MY_REGISTRATIONS.TITLE).exists,
+		)
+		.ok();
+});
+
+test("should navigate to my registrations from navigation bar", goToMyRegistrations);
+
+test("should render delegates list", async (t: any) => {
+	await goToMyRegistrations(t);
+
+	await t.expect(Selector("[data-testid=DelegateRowItem]").exists).ok();
+});
+
+test("should handle delegate update action", async (t: any) => {
+	await goToMyRegistrations(t);
+
+	await t.expect(Selector("[data-testid=DelegateRowItem]").exists).ok();
+	await t.expect(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__toggle]").exists).ok();
+
+	await t.hover(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__toggle]"));
+	await t.click(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__toggle]"));
+	// Update option
+	await t.click(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__option--0]"));
+
+	await t
+		.expect(
+			Selector("div").withText(translations.TRANSACTION.PAGE_UPDATE_REGISTRATION.FIRST_STEP.BUSINESS.TITLE)
+				.exists,
+		)
+		.ok();
+});
+
+test("should handle delegate resign action", async (t: any) => {
+	await goToMyRegistrations(t);
+
+	await t.expect(Selector("[data-testid=DelegateRowItem]").exists).ok();
+	await t.expect(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__toggle]").exists).ok();
+
+	await t.hover(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__toggle]"));
+	await t.click(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__toggle]"));
+	// Update option
+	await t.click(Selector("[data-testid=DelegateRowItem] [data-testid=dropdown__option--1]"));
+
+	await t
+		.expect(
+			Selector("div").withText(translations.TRANSACTION.PAGE_RESIGN_REGISTRATION.FIRST_STEP.DELEGATE.TITLE)
+				.exists,
+		)
+		.ok();
+});
+
+test("should render business registrations list", async (t: any) => {
+	await goToMyRegistrations(t);
+
+	await t.expect(Selector("[data-testid=BusinessRegistrationRowItem]").exists).ok();
+});
+
+test("should handle busines registration update action", async (t: any) => {
+	await goToMyRegistrations(t);
+
+	await t.expect(Selector("[data-testid=BusinessRegistrationRowItem]").exists).ok();
+	await t.expect(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__toggle]").exists).ok();
+
+	await t.hover(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__toggle]"));
+	await t.click(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__toggle]"));
+	// Update option
+	await t.click(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__option--0]"));
+
+	await t
+		.expect(
+			Selector("div").withText(translations.TRANSACTION.PAGE_UPDATE_REGISTRATION.FIRST_STEP.BUSINESS.TITLE)
+				.exists,
+		)
+		.ok();
+});
+
+test("should handle business registration resign action", async (t: any) => {
+	await goToMyRegistrations(t);
+
+	await t.expect(Selector("[data-testid=BusinessRegistrationRowItem]").exists).ok();
+	await t.expect(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__toggle]").exists).ok();
+
+	await t.hover(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__toggle]"));
+	await t.click(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__toggle]"));
+	// Update option
+	await t.click(Selector("[data-testid=BusinessRegistrationRowItem] [data-testid=dropdown__option--1]"));
+
+	await t
+		.expect(
+			Selector("div").withText(translations.TRANSACTION.PAGE_RESIGN_REGISTRATION.FIRST_STEP.DELEGATE.TITLE)
+				.exists,
 		)
 		.ok();
 });
