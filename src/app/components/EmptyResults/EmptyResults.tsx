0c5edaad29aee3c07b5de672c4f0725a95ab9997
goga-m <goga.mamoulasvili@gmail.com>
2020-09-07 17:55:34 +0300

    refactor: use `EmptyResults` component in `News`

--- src/app/components/EmptyResults/EmptyResults.tsx
+++ src/app/components/EmptyResults/EmptyResults.tsx
@@ -9,16 +9,16 @@ type EmptyResultsProps = {
 };
 
 export const EmptyResults = ({ className, title, subtitle }: EmptyResultsProps) => (
-		<div
-			className={`flex flex-col justify-center h-full text-center border-2 rounded-lg border-theme-primary-contrast bg-theme-background ${className}`}
-			data-testid="EmptyResults"
-		>
-			<div>
-				{title && <div className="mb-4 text-lg font-bold">{title}</div>}
-				{subtitle && <div className="mb-8 text-md">{subtitle}</div>}
-				<div className="mx-auto my-4 w-128">
-					<NoResultsBanner />
-				</div>
+	<div
+		className={`flex flex-col justify-center h-full text-center bg-theme-background ${className}`}
+		data-testid="EmptyResults"
+	>
+		<div>
+			{title && <div className="mb-4 text-lg font-bold">{title}</div>}
+			{subtitle && <div className="mb-8 text-md">{subtitle}</div>}
+			<div className="mx-auto my-4 w-128">
+				<NoResultsBanner />
 			</div>
 		</div>
-	);
+	</div>
+);
