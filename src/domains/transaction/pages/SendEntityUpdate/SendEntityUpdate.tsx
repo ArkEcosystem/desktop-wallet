adb4473ea43b90c82600a2c724587e20441478a8
goga-m <goga.mamoulasvili@gmail.com>
2020-09-13 21:07:07 +0300

    feat: integrate `AuthenticationStep` as step3

--- src/domains/transaction/pages/SendEntityUpdate/SendEntityUpdate.tsx
+++ src/domains/transaction/pages/SendEntityUpdate/SendEntityUpdate.tsx
@@ -8,6 +8,7 @@ import { TabPanel, Tabs } from "app/components/Tabs";
 import { useEnvironmentContext } from "app/contexts";
 import { useActiveProfile, useActiveWallet } from "app/hooks/env";
 import { toasts } from "app/services";
+import { AuthenticationStep as ThirdStep } from "domains/transaction/components/AuthenticationStep";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
@@ -15,7 +16,6 @@ import { useParams } from "react-router-dom";
 
 import { ReviewStep as SecondStep } from "../../components/EntityRegistrationForm/Step3";
 import { FirstStep } from "./Step1";
-import { ThirdStep } from "./Step3";
 import { FourthStep } from "./Step4";
 import { fetchTxIpfsData } from "./utils";
 
@@ -35,22 +35,6 @@ export const SendEntityUpdate = ({ formDefaultData, onDownload }: SendEntityUpda
 	const activeWallet = useActiveWallet();
 
 	const { t } = useTranslation();
-
-	const handleBack = () => {
-		setActiveTab(activeTab - 1);
-	};
-
-	const handleNext = async () => {
-		const isValid = await form.triggerValidation();
-
-		if (!isValid) {
-			const firstError = Object.values(form.errors)[0];
-			firstError?.ref?.scrollIntoView?.({ behavior: "smooth", block: "end" });
-			return;
-		}
-
-		setActiveTab(activeTab + 1);
-	};
 
 	const crumbs = [
 		{
@@ -95,12 +79,29 @@ export const SendEntityUpdate = ({ formDefaultData, onDownload }: SendEntityUpda
 		form.setValue("fee", fees.avg);
 	}, [env, activeWallet]);
 
+	const handleBack = () => {
+		setActiveTab(activeTab - 1);
+	};
+
+	const handleNext = async () => {
+		const isValid = await form.triggerValidation();
+
+		if (!isValid) {
+			const errors: object = form.errors.ipfsData?.meta;
+			const firstError = Object.values(errors)[0]?.ref;
+			firstError?.scrollIntoView?.({ behavior: "smooth", block: "end" });
+			return;
+		}
+
+		setActiveTab(activeTab + 1);
+	};
+
 	return (
 		<Page profile={activeProfile} crumbs={crumbs}>
 			<Section className="flex-1">
 				<Form className="max-w-xl mx-auto" context={form} onSubmit={(data: any) => onDownload(data)}>
 					<Tabs activeId={activeTab}>
-						<StepIndicator size={6} activeIndex={activeTab} />
+						<StepIndicator size={4} activeIndex={activeTab} />
 
 						<div className="mt-8">
 							<TabPanel tabId={1}>
@@ -110,20 +111,14 @@ export const SendEntityUpdate = ({ formDefaultData, onDownload }: SendEntityUpda
 								<SecondStep wallet={activeWallet} />
 							</TabPanel>
 							<TabPanel tabId={3}>
-								<ThirdStep form={form} passwordType="mnemonic" />
+								<ThirdStep wallet={activeWallet} />
 							</TabPanel>
 							<TabPanel tabId={4}>
-								<ThirdStep form={form} passwordType="password" />
-							</TabPanel>
-							<TabPanel tabId={5}>
-								<ThirdStep form={form} passwordType="ledger" />
-							</TabPanel>
-							<TabPanel tabId={6}>
 								<FourthStep />
 							</TabPanel>
 
 							<div className="flex justify-end mt-8 space-x-3">
-								{activeTab < 6 && activeTab > 1 && (
+								{activeTab < 3 && activeTab > 1 && (
 									<Button
 										disabled={activeTab === 1}
 										data-testid="SendEntityUpdate__back-button"
@@ -140,7 +135,7 @@ export const SendEntityUpdate = ({ formDefaultData, onDownload }: SendEntityUpda
 									</Button>
 								)}
 
-								{activeTab >= 3 && activeTab < 6 && (
+								{activeTab === 3 && (
 									<Button
 										data-testid="SendEntityUpdate__send-button"
 										onClick={handleNext}
@@ -151,7 +146,7 @@ export const SendEntityUpdate = ({ formDefaultData, onDownload }: SendEntityUpda
 									</Button>
 								)}
 
-								{activeTab === 6 && (
+								{activeTab === 4 && (
 									<div className="flex justify-end space-x-3">
 										<Button data-testid="SendEntityUpdate__wallet-button" variant="plain">
 											{t("COMMON.BACK_TO_WALLET")}
