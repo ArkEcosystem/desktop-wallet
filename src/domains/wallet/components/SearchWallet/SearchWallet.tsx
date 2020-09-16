639bd92db930623d66a1be22c4c70d4ecd688818
faustbrian <faustbrian@users.noreply.github.com>
2020-09-16 07:20:38 +0000

    style: resolve style guide violations

--- src/domains/wallet/components/SearchWallet/SearchWallet.tsx
+++ src/domains/wallet/components/SearchWallet/SearchWallet.tsx
@@ -13,155 +13,155 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 type SearchWalletListItemProps = {
-    index: number;
-    address: string;
-    balance: BigNumber;
-    convertedBalance: BigNumber;
-    coinName: string;
-    currency: string;
-    exchangeCurrency: string;
-    name?: string;
-    showNetwork: boolean;
-    onAction: any;
+	index: number;
+	address: string;
+	balance: BigNumber;
+	convertedBalance: BigNumber;
+	coinName: string;
+	currency: string;
+	exchangeCurrency: string;
+	name?: string;
+	showNetwork: boolean;
+	onAction: any;
 };
 
 const SearchWalletListItem = ({
-    index,
-    address,
-    balance,
-    convertedBalance,
-    coinName,
-    currency,
-    exchangeCurrency,
-    name,
-    showNetwork,
-    onAction,
+	index,
+	address,
+	balance,
+	convertedBalance,
+	coinName,
+	currency,
+	exchangeCurrency,
+	name,
+	showNetwork,
+	onAction,
 }: SearchWalletListItemProps) => {
-    const [shadowColor, setShadowColor] = React.useState<string>("--theme-background-color");
+	const [shadowColor, setShadowColor] = React.useState<string>("--theme-background-color");
 
-    const { t } = useTranslation();
+	const { t } = useTranslation();
 
-    return (
-        <TableRow
-            onMouseEnter={() => setShadowColor("--theme-color-neutral-100")}
-            onMouseLeave={() => setShadowColor("")}
-        >
-            <TableCell variant="start">
-                {showNetwork && (
-                    <Circle className="-mr-2" size="lg" shadowColor={shadowColor}>
-                        <Icon name={coinName} width={20} height={20} />
-                    </Circle>
-                )}
-                <Avatar size="lg" address={address} shadowColor={shadowColor} />
-            </TableCell>
+	return (
+		<TableRow
+			onMouseEnter={() => setShadowColor("--theme-color-neutral-100")}
+			onMouseLeave={() => setShadowColor("")}
+		>
+			<TableCell variant="start">
+				{showNetwork && (
+					<Circle className="-mr-2" size="lg" shadowColor={shadowColor}>
+						<Icon name={coinName} width={20} height={20} />
+					</Circle>
+				)}
+				<Avatar size="lg" address={address} shadowColor={shadowColor} />
+			</TableCell>
 
-            <TableCell>
-                <Address walletName={name} address={address} maxChars={22} />
-            </TableCell>
+			<TableCell>
+				<Address walletName={name} address={address} maxChars={22} />
+			</TableCell>
 
-            <TableCell innerClassName="font-semibold justify-end">
-                <Amount value={balance} ticker={currency} />
-            </TableCell>
+			<TableCell innerClassName="font-semibold justify-end">
+				<Amount value={balance} ticker={currency} />
+			</TableCell>
 
-            <TableCell innerClassName="text-theme-neutral-light justify-end">
-                <Amount value={convertedBalance} ticker={exchangeCurrency} />
-            </TableCell>
+			<TableCell innerClassName="text-theme-neutral-light justify-end">
+				<Amount value={convertedBalance} ticker={exchangeCurrency} />
+			</TableCell>
 
-            <TableCell variant="end" innerClassName="justify-end">
-                <Button
-                    data-testid={`SearchWalletListItem__select-${index}`}
-                    variant="plain"
-                    onClick={() => onAction({ address, coinName, name })}
-                >
-                    {t("COMMON.SELECT")}
-                </Button>
-            </TableCell>
-        </TableRow>
-    );
+			<TableCell variant="end" innerClassName="justify-end">
+				<Button
+					data-testid={`SearchWalletListItem__select-${index}`}
+					variant="plain"
+					onClick={() => onAction({ address, coinName, name })}
+				>
+					{t("COMMON.SELECT")}
+				</Button>
+			</TableCell>
+		</TableRow>
+	);
 };
 
 type SearchWalletProps = {
-    isOpen: boolean;
-    title: string;
-    description?: string;
-    searchBarExtra?: React.ReactNode;
-    wallets: ReadWriteWallet[];
-    showNetwork: boolean;
-    onClose?: any;
-    onSearch?: any;
-    onSelectWallet?: any;
+	isOpen: boolean;
+	title: string;
+	description?: string;
+	searchBarExtra?: React.ReactNode;
+	wallets: ReadWriteWallet[];
+	showNetwork: boolean;
+	onClose?: any;
+	onSearch?: any;
+	onSelectWallet?: any;
 };
 
 export const SearchWallet = ({
-    isOpen,
-    title,
-    description,
-    searchBarExtra,
-    wallets,
-    showNetwork,
-    onClose,
-    onSearch,
-    onSelectWallet,
+	isOpen,
+	title,
+	description,
+	searchBarExtra,
+	wallets,
+	showNetwork,
+	onClose,
+	onSearch,
+	onSelectWallet,
 }: SearchWalletProps) => {
-    const { t } = useTranslation();
+	const { t } = useTranslation();
 
-    const listColumns = [
-        {
-            Header: t("COMMON.ASSET_TYPE"),
-            className: !showNetwork ? "invisible w-0 text-0" : "",
-        },
-        {
-            Header: t("COMMON.ADDRESS"),
-            accessor: "address",
-        },
-        {
-            Header: t("COMMON.BALANCE"),
-            accessor: "balance",
-            className: "justify-end",
-        },
-        {
-            Header: t("COMMON.FIAT_VALUE"),
-            accessor: "fiat",
-            className: "justify-end",
-        },
-        {
-            Header: t("COMMON.ACTION"),
-            className: "invisible w-0 text-0",
-        },
-    ];
+	const listColumns = [
+		{
+			Header: t("COMMON.ASSET_TYPE"),
+			className: !showNetwork ? "invisible w-0 text-0" : "",
+		},
+		{
+			Header: t("COMMON.ADDRESS"),
+			accessor: "address",
+		},
+		{
+			Header: t("COMMON.BALANCE"),
+			accessor: "balance",
+			className: "justify-end",
+		},
+		{
+			Header: t("COMMON.FIAT_VALUE"),
+			accessor: "fiat",
+			className: "justify-end",
+		},
+		{
+			Header: t("COMMON.ACTION"),
+			className: "invisible w-0 text-0",
+		},
+	];
 
-    return (
-        <SearchResource
-            isOpen={isOpen}
-            title={title}
-            description={description}
-            searchBarExtra={searchBarExtra}
-            onClose={onClose}
-            onSearch={onSearch}
-        >
-            <div>
-                <Table columns={listColumns} data={wallets}>
-                    {(wallet: ReadWriteWallet, index: number) => (
-                        <SearchWalletListItem
-                            index={index}
-                            address={wallet.address()}
-                            balance={wallet.balance()}
-                            convertedBalance={wallet.convertedBalance()}
-                            coinName={wallet.coinId()}
-                            currency={wallet.currency()}
-                            exchangeCurrency={wallet.exchangeCurrency() || "BTC"} // @TODO get default from SDK
-                            name={wallet.alias()}
-                            showNetwork={showNetwork}
-                            onAction={onSelectWallet}
-                        />
-                    )}
-                </Table>
-            </div>
-        </SearchResource>
-    );
+	return (
+		<SearchResource
+			isOpen={isOpen}
+			title={title}
+			description={description}
+			searchBarExtra={searchBarExtra}
+			onClose={onClose}
+			onSearch={onSearch}
+		>
+			<div>
+				<Table columns={listColumns} data={wallets}>
+					{(wallet: ReadWriteWallet, index: number) => (
+						<SearchWalletListItem
+							index={index}
+							address={wallet.address()}
+							balance={wallet.balance()}
+							convertedBalance={wallet.convertedBalance()}
+							coinName={wallet.coinId()}
+							currency={wallet.currency()}
+							exchangeCurrency={wallet.exchangeCurrency() || "BTC"} // @TODO get default from SDK
+							name={wallet.alias()}
+							showNetwork={showNetwork}
+							onAction={onSelectWallet}
+						/>
+					)}
+				</Table>
+			</div>
+		</SearchResource>
+	);
 };
 
 SearchWallet.defaultProps = {
-    isOpen: false,
-    showNetwork: true,
+	isOpen: false,
+	showNetwork: true,
 };
