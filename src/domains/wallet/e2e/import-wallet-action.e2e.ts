eaa12bb3b808125000e141857dbb219175538b4a
Edgar Goetzendorff <hello@dated.fun>
2020-09-21 18:39:18 +0200

    test: adjust e2e tests

--- src/domains/wallet/e2e/import-wallet-action.e2e.ts
+++ src/domains/wallet/e2e/import-wallet-action.e2e.ts
@@ -1,61 +1,63 @@
 import { Selector } from "testcafe";
 
-import { buildTranslations as translations } from "../../../app/i18n/helpers";
+import { buildTranslations } from "../../../app/i18n/helpers";
 import { createFixture } from "../../../utils/e2e-utils";
+
+const translations = buildTranslations();
 
 createFixture(`Import Wallet action`);
 
 test("should import a wallet by mnemonic", async (t) => {
 	await t.click(Selector("p").withText("John Doe"));
-	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
+	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok();
 
 	// Navigate to import page
-	await t.click(Selector("button").withExactText(translations().COMMON.IMPORT));
+	await t.click(Selector("button").withExactText(translations.COMMON.IMPORT));
 	await t
-		.expect(Selector("div").withText(translations().WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
+		.expect(Selector("div").withText(translations.WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
 		.ok();
 
 	// Select a network and advance to second step
 	await t.click(Selector("#ImportWallet__network-item-1"));
 	await t
-		.expect(Selector("button").withText(translations().COMMON.CONTINUE).hasAttribute("disabled"))
+		.expect(Selector("button").withText(translations.COMMON.CONTINUE).hasAttribute("disabled"))
 		.notOk("Network selected", { timeout: 5000 });
-	await t.click(Selector("button").withExactText(translations().COMMON.CONTINUE));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 	await t
-		.expect(Selector("h1").withExactText(translations().WALLETS.PAGE_IMPORT_WALLET.METHOD_STEP.TITLE).exists)
+		.expect(Selector("h1").withExactText(translations.WALLETS.PAGE_IMPORT_WALLET.METHOD_STEP.TITLE).exists)
 		.ok();
 
 	// Fill a passphrase and advance to third step
 	const passphraseInput = Selector("input[name=passphrase]");
 
 	await t.typeText(passphraseInput, "buddy year cost vendor honey tonight viable nut female alarm duck symptom");
-	await t.click(Selector("button").withExactText(translations().COMMON.CONTINUE));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 
 	// Fill a wallet name
 	const walletNameInput = Selector("input[name=name]");
 
 	await t.typeText(walletNameInput, "Test Mnemonic");
-	await t.click(Selector("button").withExactText(translations().COMMON.GO_TO_WALLET));
+	await t.click(Selector("button").withExactText(translations.COMMON.GO_TO_WALLET));
 });
 
 test("should import a wallet by address", async (t) => {
 	await t.click(Selector("p").withText("John Doe"));
-	await t.expect(Selector("div").withText(translations().COMMON.WALLETS).exists).ok();
+	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok();
 
 	// Navigate to import page
-	await t.click(Selector("button").withExactText(translations().COMMON.IMPORT));
+	await t.click(Selector("button").withExactText(translations.COMMON.IMPORT));
 	await t
-		.expect(Selector("div").withText(translations().WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
+		.expect(Selector("div").withText(translations.WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
 		.ok();
 
 	// Select a network and advance to the step two
 	await t.click(Selector("#ImportWallet__network-item-1"));
 	await t
-		.expect(Selector("button").withText(translations().COMMON.CONTINUE).hasAttribute("disabled"))
+		.expect(Selector("button").withText(translations.COMMON.CONTINUE).hasAttribute("disabled"))
 		.notOk("Network selected", { timeout: 5000 });
-	await t.click(Selector("button").withExactText(translations().COMMON.CONTINUE));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 	await t
-		.expect(Selector("h1").withExactText(translations().WALLETS.PAGE_IMPORT_WALLET.METHOD_STEP.TITLE).exists)
+		.expect(Selector("h1").withExactText(translations.WALLETS.PAGE_IMPORT_WALLET.METHOD_STEP.TITLE).exists)
 		.ok();
 
 	// Use the address only
@@ -65,13 +67,13 @@ test("should import a wallet by address", async (t) => {
 	const addressInput = Selector("input[name=address]");
 
 	await t.typeText(addressInput, "DC8ghUdhS8w8d11K8cFQ37YsLBFhL3Dq2P");
-	await t.click(Selector("button").withExactText(translations().COMMON.CONTINUE));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 
 	// Fill a wallet name
 	const walletNameInput = Selector("input[name=name]");
 
 	await t.typeText(walletNameInput, "Test Address");
-	await t.click(Selector("button").withExactText(translations().COMMON.GO_TO_WALLET));
+	await t.click(Selector("button").withExactText(translations.COMMON.GO_TO_WALLET));
 });
 
 test("should show an error message for invalid address", async (t) => {
@@ -81,15 +83,15 @@ test("should show an error message for invalid address", async (t) => {
 	// Navigate to import page
 	await t.click(Selector("button").withExactText("Import"));
 	await t
-		.expect(Selector("div").withText(translations().WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
+		.expect(Selector("div").withText(translations.WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
 		.ok();
 
 	// Select a network and advance to step two
 	await t.click(Selector("#ImportWallet__network-item-1"));
 	await t
-		.expect(Selector("button").withText(translations().COMMON.CONTINUE).hasAttribute("disabled"))
+		.expect(Selector("button").withText(translations.COMMON.CONTINUE).hasAttribute("disabled"))
 		.notOk("Network selected", { timeout: 5000 });
-	await t.click(Selector("button").withExactText(translations().COMMON.CONTINUE));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 	await t.expect(Selector("h1").withExactText("Import Wallet").exists).ok();
 
 	// Use the address only
@@ -97,9 +99,9 @@ test("should show an error message for invalid address", async (t) => {
 
 	// Input address
 	const addressInput = Selector("input[name=address]");
-
 	await t.typeText(addressInput, "123");
-	await t.click(Selector("button").withExactText("Go to Wallet"));
+
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 
 	await t.expect(Selector("fieldset p").withText("The address is not valid").exists).ok({ timeout: 5000 });
 });
@@ -113,15 +115,15 @@ test("should show an error message for duplicate address", async (t) => {
 	// Navigate to import page
 	await t.click(Selector("button").withExactText("Import"));
 	await t
-		.expect(Selector("div").withText(translations().WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
+		.expect(Selector("div").withText(translations.WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
 		.ok();
 
 	// Select a network and advance to step two
 	await t.click(Selector("#ImportWallet__network-item-1"));
 	await t
-		.expect(Selector("button").withText(translations().COMMON.CONTINUE).hasAttribute("disabled"))
+		.expect(Selector("button").withText(translations.COMMON.CONTINUE).hasAttribute("disabled"))
 		.notOk("Network selected", { timeout: 5000 });
-	await t.click(Selector("button").withExactText(translations().COMMON.CONTINUE));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 	await t.expect(Selector("h1").withExactText("Import Wallet").exists).ok();
 
 	// Use the address only
@@ -131,7 +133,7 @@ test("should show an error message for duplicate address", async (t) => {
 	addressInput = Selector("input[name=address]");
 
 	await t.typeText(addressInput, "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib", { replace: true, speed: 1, paste: true });
-	await t.click(Selector("button").withExactText("Go to Wallet"));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 
 	// Try to import a duplicate wallet
 	await t.click(Selector("a").withExactText("Portfolio"));
@@ -139,15 +141,15 @@ test("should show an error message for duplicate address", async (t) => {
 	// Navigate to import page
 	await t.click(Selector("button").withExactText("Import"));
 	await t
-		.expect(Selector("div").withText(translations().WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
+		.expect(Selector("div").withText(translations.WALLETS.PAGE_IMPORT_WALLET.NETWORK_STEP.SUBTITLE).exists)
 		.ok();
 
 	// Select a network and advance to step two
 	await t.click(Selector("#ImportWallet__network-item-1"));
 	await t
-		.expect(Selector("button").withText(translations().COMMON.CONTINUE).hasAttribute("disabled"))
+		.expect(Selector("button").withText(translations.COMMON.CONTINUE).hasAttribute("disabled"))
 		.notOk("Network selected", { timeout: 5000 });
-	await t.click(Selector("button").withExactText(translations().COMMON.CONTINUE));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 	await t.expect(Selector("h1").withExactText("Import Wallet").exists).ok();
 
 	// Use the address only
@@ -157,7 +159,7 @@ test("should show an error message for duplicate address", async (t) => {
 	addressInput = Selector("input[name=address]");
 
 	await t.typeText(addressInput, "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib", { replace: true, speed: 1, paste: true });
-	await t.click(Selector("button").withExactText("Go to Wallet"));
+	await t.click(Selector("button").withExactText(translations.COMMON.CONTINUE));
 
 	await t
 		.expect(Selector("fieldset p").withText("Address D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib already exists").exists)
