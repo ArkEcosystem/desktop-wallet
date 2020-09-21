8a34ab8d62c47f9f99a3ff19c9e32013c3f2060c
goga-m <goga-m@users.noreply.github.com>
2020-09-21 10:56:55 +0000

    style: resolve style guide violations

--- src/domains/transaction/e2e/entity-update.e2e.ts
+++ src/domains/transaction/e2e/entity-update.e2e.ts
@@ -2,7 +2,7 @@ import { Selector } from "testcafe";
 
 import { buildTranslations } from "../../../app/i18n/helpers";
 import { createFixture, mockRequest } from "../../../utils/e2e-utils";
-import { goToMyRegistrations,goToProfile } from "../../profile/e2e/common";
+import { goToMyRegistrations, goToProfile } from "../../profile/e2e/common";
 import { goToImportWalletPage } from "./common";
 
 const IpfsFixture = require("../../../tests/fixtures/ipfs/QmRwgWaaEyYgGqp55196TsFDQLW4NZkyTnPwiSVhJ7NPRV.json");
