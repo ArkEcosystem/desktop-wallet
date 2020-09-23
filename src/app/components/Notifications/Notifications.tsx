9071fc470489bb0a42eb47fc2ee7e51348d183ea
goga-m <goga.mamoulasvili@gmail.com>
2020-09-23 17:39:58 +0300

    feat: mark as read when visible

--- src/app/components/Notifications/Notifications.tsx
+++ src/app/components/Notifications/Notifications.tsx
@@ -2,7 +2,7 @@ import { ExtendedTransactionData } from "@arkecosystem/platform-sdk-profiles";
 import { Table } from "app/components/Table";
 import { useEnvironmentContext } from "app/contexts";
 import { TransactionTable } from "domains/transaction/components/TransactionTable";
-import React, { useEffect, useMemo } from "react";
+import React, { useMemo, useRef } from "react";
 
 import { NotificationItem, NotificationsSkeleton } from "./";
 import { NotificationItemProps, NotificationsProps } from "./models";
@@ -19,39 +19,41 @@ export const Notifications = ({
 	const hiddenTableHeaders = [{ Header: "-", className: "hidden" }];
 	const { persist } = useEnvironmentContext();
 
-	const handleTransactionClick = (transaction: ExtendedTransactionData) => {
-		onAction?.("click", transaction);
-	};
-
-	useEffect(() => {
-		const addNotifications = async () => {
-			console.log("adding notification");
-			profile.notifications().push({
-				icon: "ArkLogo",
-				name: "ARK Explorer",
-				body: "- update v2.5.6",
-				action: "update",
-			});
-			await persist();
-		};
-		addNotifications();
-	}, []);
-
-	const plugins = useMemo(() => profile.notifications().values(), [profile]);
-	console.log("plugins 1", plugins);
+	// TODO: filter by type when types will be used
+	const plugins = useMemo(() => profile.notifications().values(), [profile, profile.notifications()]);
+	const wrapperRef = useRef();
 
 	if (!profile?.id() || (!transactions!.length && !plugins.length)) {
 		return <NotificationsSkeleton title={emptyText} />;
 	}
 
+	const markAsRead = async (isVisible: boolean, id: string) => {
+		if (!isVisible) return;
+
+		const notification = profile.notifications().get(id);
+		if (!notification || notification?.read_at) return;
+
+		profile.notifications().markAsRead(id);
+		await persist();
+	};
+
+	const handleTransactionClick = (transaction: ExtendedTransactionData) => {
+		onAction?.("click", transaction);
+	};
+
 	return (
-		<NotificationsWrapper>
+		<NotificationsWrapper ref={wrapperRef as React.MutableRefObject<any>}>
 			{plugins.length > 0 && (
 				<>
 					<div className="mb-2 text-sm font-bold text-theme-neutral">{pluginsHeader}</div>
 					<Table hideHeader columns={hiddenTableHeaders} data={plugins}>
 						{(plugin: NotificationItemProps) => (
-							<NotificationItem {...plugin} onAction={(name: string) => onAction?.(name, plugin)} />
+							<NotificationItem
+								{...plugin}
+								onAction={(name: string) => onAction?.(name, plugin)}
+								onVisibilityChange={(isVisible) => markAsRead(isVisible, plugin.id)}
+								containmentRef={wrapperRef}
+							/>
 						)}
 					</Table>
 				</>
