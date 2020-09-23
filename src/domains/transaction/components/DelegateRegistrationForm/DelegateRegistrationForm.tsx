17fecb940d1a88a5ca1294b1d6a049ed6dee72be
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-09-23 07:02:37 -0300

    chore: fix conflicts

--- src/domains/transaction/components/DelegateRegistrationForm/DelegateRegistrationForm.tsx
+++ src/domains/transaction/components/DelegateRegistrationForm/DelegateRegistrationForm.tsx
@@ -7,13 +7,11 @@ import { Avatar } from "app/components/Avatar";
 import { Circle } from "app/components/Circle";
 import { FormField, FormHelperText, FormLabel } from "app/components/Form";
 import { Icon } from "app/components/Icon";
-import { Input } from "app/components/Input";
+import { Input, InputGroup } from "app/components/Input";
 import { Label } from "app/components/Label";
 import { TabPanel, Tabs } from "app/components/Tabs";
 import { TransactionDetail } from "app/components/TransactionDetail";
 import { useEnvironmentContext } from "app/contexts";
-import { useValidation } from "app/hooks/validations";
-import { NetworkIcon } from "domains/network/components/NetworkIcon";
 import { InputFee } from "domains/transaction/components/InputFee";
 import { TotalAmountBox } from "domains/transaction/components/TotalAmountBox";
 import { SendEntityRegistrationForm } from "domains/transaction/pages/SendEntityRegistration/SendEntityRegistration.models";
@@ -24,8 +22,6 @@ import { useTranslation } from "react-i18next";
 const SecondStep = ({ fees, wallet }: any) => {
 	const { t } = useTranslation();
 	const { env } = useEnvironmentContext();
-
-	const { delegateRegistration } = useValidation();
 
 	const { getValues, register, setValue, watch } = useFormContext();
 	const username = getValues("username");
@@ -42,46 +38,63 @@ const SecondStep = ({ fees, wallet }: any) => {
 	useEffect(() => {
 		if (!username) {
 			register("username", {
-				...delegateRegistration.username(),
-				validate: (value) =>
-					!delegates.some((delegate: ReadOnlyWallet) => delegate.username() === value) ||
-					t("COMMON.VALIDATION.EXISTS", { field: t("COMMON.DELEGATE_NAME") }).toString(),
+				required: true,
+				validate: (value) => {
+					if (!value.match(/^[a-z0-9!@$&_.]+$/)) {
+						return t<string>("TRANSACTION.INVALID_DELEGATE_NAME");
+					}
+
+					if (value.length > 20) {
+						return t<string>("TRANSACTION.DELEGATE_NAME_TOO_LONG");
+					}
+
+					try {
+						env.delegates().findByUsername(wallet.coinId(), wallet.networkId(), value);
+
+						return t<string>("TRANSACTION.DELEGATE_NAME_EXISTS");
+					} catch {
+						// No Delegate found.
+					}
+
+					return true;
+				},
 			});
 		}
-	}, [delegateRegistration, delegates, register, username, t]);
+	}, [delegates, env, register, username, t, wallet]);
 
 	return (
 		<section data-testid="DelegateRegistrationForm__step--second">
 			<h1 className="mb-0">{t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.SECOND_STEP.TITLE")}</h1>
-
 			<div className="text-theme-neutral-dark">
 				{t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.SECOND_STEP.DESCRIPTION")}
 			</div>
 
-			<div className="mt-6">
+			<div className="mt-4">
 				<Alert>{t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.SECOND_STEP.WARNING")}</Alert>
 
 				<TransactionDetail
 					className="mt-2"
-					extra={<Avatar size="lg" address={wallet.address()} />}
+					extra={<Avatar size="lg" address="ABUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK" />}
 					borderPosition="bottom"
 				>
-					<div className="flex-1 space-y-2">
-						<div className="text-theme-neutral">
-							<span className="mr-1 text-sm">{t("TRANSACTION.SENDER")}</span>
-							<Label color="warning">
-								<span className="text-sm">{t("TRANSACTION.YOUR_ADDRESS")}</span>
-							</Label>
-						</div>
-						<Address address={wallet.address()} walletName={wallet.alias()} />
+					<div className="mb-2 font-semibold text-theme-neutral">
+						<span className="mr-1 text-sm">Sender</span>
+						<Label color="warning">
+							<span className="text-sm">{t("TRANSACTION.YOUR_ADDRESS")}</span>
+						</Label>
 					</div>
+					<Address address={wallet.address()} walletName={wallet.alias()} />
 				</TransactionDetail>
 
-				<div className="relative mt-8 space-y-8">
-					<FormField name="username">
+				<FormField name="username" className="relative mt-8">
+					<div className="mb-2">
 						<FormLabel label={t("TRANSACTION.DELEGATE_NAME")} />
+					</div>
+					<InputGroup>
 						<Input
 							data-testid="Input__username"
+							type="text"
+							placeholder=" "
 							className="pr-20"
 							defaultValue={username}
 							onChange={(event: any) =>
@@ -115,6 +128,7 @@ const ThirdStep = ({ wallet }: { wallet: ReadWriteWallet }) => {
 	const { t } = useTranslation();
 	const { getValues, unregister } = useFormContext();
 	const { fee, username } = getValues();
+	const coinName = wallet.coinId();
 
 	useEffect(() => {
 		unregister("mnemonic");
@@ -123,44 +137,52 @@ const ThirdStep = ({ wallet }: { wallet: ReadWriteWallet }) => {
 	return (
 		<section data-testid="DelegateRegistrationForm__step--third">
 			<h1 className="mb-0">{t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.SECOND_STEP.TITLE")}</h1>
-
 			<div className="text-theme-neutral-dark">
 				{t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.SECOND_STEP.DESCRIPTION")}
 			</div>
 
-			<TransactionDetail
-				border={false}
-				label={t("TRANSACTION.NETWORK")}
-				extra={<NetworkIcon size="lg" coin={wallet.network().coin()} network={wallet.network().id()} />}
-			>
-				{wallet.network().name()}
-			</TransactionDetail>
+			<div className="grid grid-flow-row gap-2 mt-4">
+				<TransactionDetail
+					border={false}
+					label={t("TRANSACTION.NETWORK")}
+					extra={
+						<div className="ml-1 text-theme-danger">
+							<Circle className="bg-theme-background border-theme-danger-light" size="lg">
+								{coinName && <Icon name={coinName} width={20} height={20} />}
+							</Circle>
+						</div>
+					}
+				>
+					<div className="flex-auto font-semibold truncate text-md text-theme-neutral-800 max-w-24">
+						{wallet.network().name()}
+					</div>
+				</TransactionDetail>
 
-			<TransactionDetail extra={<Avatar size="lg" address={wallet.address()} />}>
-				<div className="flex-1 space-y-2">
-					<div className="text-theme-neutral">
+				<TransactionDetail extra={<Avatar size="lg" address={wallet.address()} />}>
+					<div className="mb-2 font-semibold text-theme-neutral">
 						<span className="mr-1 text-sm">{t("TRANSACTION.SENDER")}</span>
 						<Label color="warning">
 							<span className="text-sm">{t("TRANSACTION.YOUR_ADDRESS")}</span>
 						</Label>
 					</div>
 					<Address address={wallet.address()} walletName={wallet.alias()} />
+				</TransactionDetail>
+
+				<TransactionDetail
+					label={t("TRANSACTION.DELEGATE_NAME")}
+					className="pt-6"
+					extra={
+						<div className="mx-2">
+							<Icon name="Delegate" width={32} height={32} />
+						</div>
+					}
+				>
+					{username}
+				</TransactionDetail>
+
+				<div className="mt-2">
+					<TotalAmountBox amount={BigNumber.ZERO} fee={BigNumber.make(fee)} />
 				</div>
-			</TransactionDetail>
-
-			<TransactionDetail
-				label={t("TRANSACTION.DELEGATE_NAME")}
-				extra={
-					<Circle className="bg-theme-background border-theme-text" size="lg">
-						<Icon name="Delegate" width={20} height={25} />
-					</Circle>
-				}
-			>
-				{username}
-			</TransactionDetail>
-
-			<div className="mt-2">
-				<TotalAmountBox amount={BigNumber.ZERO} fee={BigNumber.make(fee)} ticker={wallet.currency()} />
 			</div>
 		</section>
 	);
