bef00f82cf4506fc10b74c8ca57171aaa7dac44c
Brian Faust <hello@basecode.sh>
2020-09-28 12:26:03 +0300

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
 		"@typescript-eslint/no-unsafe-call": "off",
 		"@typescript-eslint/no-unsafe-member-access": "off",
 		"@typescript-eslint/no-unsafe-return": "off",
+		"@typescript-eslint/no-unused-expressions": "off",
 		"@typescript-eslint/no-unused-vars": "off",
 		"@typescript-eslint/no-var-requires": "off",
 		"@typescript-eslint/prefer-regexp-exec": "off",
