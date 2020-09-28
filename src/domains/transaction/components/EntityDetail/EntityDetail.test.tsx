891256d4c00d4d882d5cb6810834a9637f0d67b2
brenopolanski <breno.polanski@gmail.com>
2020-09-28 16:56:56 -0300

    feat: add entity detail modal

---
+++ src/domains/transaction/components/EntityDetail/EntityDetail.test.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { render } from "testing-library";
+import { TransactionFixture } from "tests/fixtures/transactions";
+
+import { translations } from "../../i18n";
+import { EntityDetail } from "./EntityDetail";
+
+describe("EntityDetail", () => {
+	const extraProps = {
+		ticker: "BTC",
+		walletAlias: "Wallet 1",
+	};
+
+	it("should not render if not open", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail isOpen={false} transaction={TransactionFixture} {...extraProps} />,
+		);
+
+		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail isOpen={true} transaction={TransactionFixture} {...extraProps} />,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_ENTITY_DETAIL.TITLE);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal without a wallet alias", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail isOpen={true} transaction={TransactionFixture} ticker="BTC" />,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_ENTITY_DETAIL.TITLE);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render as confirmed", () => {
+		const { asFragment, getByText, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isConfirmed: () => true,
+				}}
+				{...extraProps}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.MODAL_ENTITY_DETAIL.TITLE);
+		expect(getByText("Well confirmed")).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
