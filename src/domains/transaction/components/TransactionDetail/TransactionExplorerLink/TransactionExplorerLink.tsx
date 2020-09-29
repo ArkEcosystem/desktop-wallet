0879f103f068ab264e38f421256580e0499d78c1
dated <hello@dated.fun>
2020-09-29 11:39:26 +0200

    refactor: adjustments and fixes in `TransactionSuccessful` component

--- src/domains/transaction/components/TransactionDetail/TransactionExplorerLink/TransactionExplorerLink.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionExplorerLink/TransactionExplorerLink.tsx
@@ -5,21 +5,24 @@ import { TruncateMiddle } from "app/components/TruncateMiddle";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { TransactionDetail } from "../TransactionDetail";
+import { TransactionDetail, TransactionDetailProps } from "../TransactionDetail";
 
 type TransactionExplorerLinkProps = {
 	id: string;
 	link: string;
 	variant: "block" | "transaction";
-};
+} & TransactionDetailProps;
 
-export const TransactionExplorerLink = ({ id, link, variant }: TransactionExplorerLinkProps) => {
+export const TransactionExplorerLink = ({ id, link, variant, ...props }: TransactionExplorerLinkProps) => {
 	const { t } = useTranslation();
 
 	const isTransactionLink = () => variant === "transaction";
 
 	return (
-		<TransactionDetail label={isTransactionLink() ? t("TRANSACTION.ID") : t("TRANSACTION.BLOCK_ID")}>
+		<TransactionDetail
+			label={isTransactionLink() ? t("TRANSACTION.ID") : t("TRANSACTION.BLOCK_ID")}
+			{...props}
+		>
 			<div className="flex items-center">
 				<Link to={link} isExternal showExternalIcon={false}>
 					<TruncateMiddle text={id} maxChars={30} className="text-theme-primary-dark" />
@@ -37,4 +40,5 @@ export const TransactionExplorerLink = ({ id, link, variant }: TransactionExplor
 
 TransactionExplorerLink.defaultProps = {
 	variant: "transaction",
+	borderPosition: "top",
 };
