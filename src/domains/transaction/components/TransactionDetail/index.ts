2f0720e726a6ec52e738c4e56742cf44510e9e6b
Edgar Goetzendorff <hello@dated.fun>
2020-09-29 08:12:48 +0200

    refactor: adjustments and fixes in send vote steps (#2885)

--- src/domains/transaction/components/TransactionDetail/index.ts
+++ src/domains/transaction/components/TransactionDetail/index.ts
@@ -3,6 +3,7 @@ export * from "./TransactionConfirmations";
 export * from "./TransactionExplorerLink";
 export * from "./TransactionFee";
 export * from "./TransactionMemo";
+export * from "./TransactionNetwork";
 export * from "./TransactionRecipients";
 export * from "./TransactionSender";
 export * from "./TransactionTimestamp";
