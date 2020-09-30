793a088f0f69274090c1f4c2c9ce4ef62880c134
brenopolanski <breno.polanski@gmail.com>
2020-09-30 15:18:29 -0300

    test: increate test coverage

--- src/domains/transaction/components/EntityDetail/EntityDetail.test.tsx
+++ src/domains/transaction/components/EntityDetail/EntityDetail.test.tsx
@@ -72,4 +72,190 @@ describe("EntityDetail", () => {
 		expect(getByText(translations.WELL_CONFIRMED)).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
 	});
+
+	it("should render a modal business entity resignation", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isBusinessEntityResignation: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(
+			translations.TRANSACTION_TYPES.BUSINESS_ENTITY_RESIGNATION,
+		);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal business entity update", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isBusinessEntityUpdate: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.TRANSACTION_TYPES.BUSINESS_ENTITY_UPDATE);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal product entity registration", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isProductEntityRegistration: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(
+			translations.TRANSACTION_TYPES.PRODUCT_ENTITY_REGISTRATION,
+		);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal product entity resignation", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isProductEntityResignation: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(
+			translations.TRANSACTION_TYPES.PRODUCT_ENTITY_RESIGNATION,
+		);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal product entity update", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isProductEntityUpdate: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.TRANSACTION_TYPES.PRODUCT_ENTITY_UPDATE);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal plugin entity registration", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isPluginEntityRegistration: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(
+			translations.TRANSACTION_TYPES.PLUGIN_ENTITY_REGISTRATION,
+		);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal plugin entity resignation", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isPluginEntityResignation: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.TRANSACTION_TYPES.PLUGIN_ENTITY_RESIGNATION);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal plugin entity update", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isPluginEntityUpdate: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.TRANSACTION_TYPES.PLUGIN_ENTITY_UPDATE);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal module entity registration", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isModuleEntityRegistration: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(
+			translations.TRANSACTION_TYPES.MODULE_ENTITY_REGISTRATION,
+		);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal module entity resignation", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isModuleEntityResignation: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.TRANSACTION_TYPES.MODULE_ENTITY_RESIGNATION);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render a modal module entity update", () => {
+		const { asFragment, getByTestId } = render(
+			<EntityDetail
+				isOpen={true}
+				transaction={{
+					...TransactionFixture,
+					isTransfer: () => false,
+					isModuleEntityUpdate: () => true,
+				}}
+			/>,
+		);
+
+		expect(getByTestId("modal__inner")).toHaveTextContent(translations.TRANSACTION_TYPES.MODULE_ENTITY_UPDATE);
+		expect(asFragment()).toMatchSnapshot();
+	});
 });
