20c837841fae0e4db60486539318ee1d47b67b0a
Edgar Goetzendorff <hello@dated.fun>
2020-09-30 04:07:47 +0200

    feat: add delegate resignation details (#2897)

---
+++ src/domains/transaction/components/DelegateResignationDetail/DelegateResignationDetail.tsx
@@ -0,0 +1,71 @@
+import { Circle } from "app/components/Circle";
+import { Icon } from "app/components/Icon";
+import { Modal } from "app/components/Modal";
+import {
+	TransactionConfirmations,
+	TransactionDetail,
+	TransactionExplorerLink,
+	TransactionFee,
+	TransactionSender,
+	TransactionTimestamp,
+} from "domains/transaction/components/TransactionDetail";
+import React, { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+type DelegateResignationDetailProps = {
+	isOpen: boolean;
+	transaction: any;
+	onClose?: any;
+};
+
+export const DelegateResignationDetail = ({ isOpen, transaction, onClose }: DelegateResignationDetailProps) => {
+	const { t } = useTranslation();
+
+	const wallet = useMemo(() => transaction.wallet(), [transaction]);
+
+	return (
+		<Modal title={t("TRANSACTION.MODAL_DELEGATE_RESIGNATION_DETAIL.TITLE")} isOpen={isOpen} onClose={onClose}>
+			<TransactionSender
+				address={transaction.sender()}
+				alias={wallet.alias()}
+				labelExtra={t("TRANSACTION.YOUR_ADDRESS")}
+				border={false}
+			/>
+
+			<TransactionDetail
+				label={t("TRANSACTION.DELEGATE_NAME")}
+				extra={
+					<Circle className="border-theme-text" size="lg">
+						<Icon name="DelegateResigned" width={19} height={20} />
+					</Circle>
+				}
+			>
+				{wallet.username()}
+			</TransactionDetail>
+
+			<TransactionFee currency={wallet.currency()} value={transaction.fee()} />
+
+			<TransactionTimestamp timestamp={transaction.timestamp()} />
+
+			<TransactionConfirmations
+				isConfirmed={transaction.isConfirmed()}
+				confirmations={transaction.confirmations()}
+			/>
+
+			<TransactionExplorerLink id={transaction.id()} link={transaction.explorerLink()} />
+
+			{transaction.blockId() && (
+				<TransactionExplorerLink
+					id={transaction.blockId()}
+					link={transaction.explorerLinkForBlock()}
+					variant="block"
+				/>
+			)}
+		</Modal>
+	);
+};
+
+DelegateResignationDetail.defaultProps = {
+	isOpen: false,
+};
+
+DelegateResignationDetail.displayName = "DelegateResignationDetail";
