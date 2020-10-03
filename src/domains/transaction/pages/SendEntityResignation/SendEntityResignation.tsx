ad4fb340a8a6c1d755774c4a9eb0730ca82c9258
Edgar Goetzendorff <hello@dated.fun>
2020-10-03 23:11:53 +0200

    refactor: show loader while fetching transaction

--- src/domains/transaction/pages/SendEntityResignation/SendEntityResignation.tsx
+++ src/domains/transaction/pages/SendEntityResignation/SendEntityResignation.tsx
@@ -4,6 +4,7 @@ import { Button } from "app/components/Button";
 import { Form } from "app/components/Form";
 import { Icon } from "app/components/Icon";
 import { Page, Section } from "app/components/Layout";
+import { Loader } from "app/components/Loader";
 import { StepIndicator } from "app/components/StepIndicator";
 import { TabPanel, Tabs } from "app/components/Tabs";
 import { useEnvironmentContext } from "app/contexts";
@@ -31,6 +32,7 @@ export const SendEntityResignation = ({ formDefaultData, passwordType }: any) =>
 	const { isValid } = formState;
 
 	const [activeTab, setActiveTab] = useState(1);
+	const [isLoading, setIsLoading] = useState(true);
 
 	const [transaction, setTransaction] = useState((null as unknown) as Contracts.SignedTransactionData);
 
@@ -56,6 +58,12 @@ export const SendEntityResignation = ({ formDefaultData, passwordType }: any) =>
 			setState({ type: "delegate" });
 		}
 	}, [activeWallet, transactionId]);
+
+	useEffect(() => {
+		if (state.type) {
+			setIsLoading(false);
+		}
+	}, [state]);
 
 	const { entity, type } = state;
 
@@ -182,82 +190,86 @@ export const SendEntityResignation = ({ formDefaultData, passwordType }: any) =>
 	return (
 		<Page profile={activeProfile} crumbs={crumbs}>
 			<Section className="flex-1">
-				<Form className="max-w-xl mx-auto" context={form} onSubmit={handleSubmit}>
-					<Tabs activeId={activeTab}>
-						{fees && (
-							<div>
-								<StepIndicator size={4} activeIndex={activeTab} />
+				{isLoading && <Loader />}
 
-								<div className="mt-8">
-									<TabPanel tabId={1}>{getStepComponent()}</TabPanel>
-									<TabPanel tabId={2}>{getStepComponent()}</TabPanel>
-									<TabPanel tabId={3}>
-										<AuthenticationStep wallet={activeWallet} />
-									</TabPanel>
-									<TabPanel tabId={4}>{getStepComponent()}</TabPanel>
+				<div className={isLoading ? "hidden" : ""}>
+					<Form className="max-w-xl mx-auto" context={form} onSubmit={handleSubmit}>
+						<Tabs activeId={activeTab}>
+							{fees && (
+								<>
+									<StepIndicator size={4} activeIndex={activeTab} />
 
-									<div className="flex justify-end mt-10 space-x-3">
-										{activeTab < 4 && (
-											<Button
-												disabled={activeTab === 1}
-												data-testid="SendEntityResignation__back-button"
-												variant="plain"
-												onClick={handleBack}
-											>
-												{t("COMMON.BACK")}
-											</Button>
-										)}
+									<div className="mt-8">
+										<TabPanel tabId={1}>{getStepComponent()}</TabPanel>
+										<TabPanel tabId={2}>{getStepComponent()}</TabPanel>
+										<TabPanel tabId={3}>
+											<AuthenticationStep wallet={activeWallet} />
+										</TabPanel>
+										<TabPanel tabId={4}>{getStepComponent()}</TabPanel>
 
-										{activeTab < 3 && (
-											<Button
-												data-testid="SendEntityResignation__continue-button"
-												disabled={!isValid}
-												onClick={handleNext}
-											>
-												{t("COMMON.CONTINUE")}
-											</Button>
-										)}
-
-										{activeTab === 3 && (
-											<Button
-												type="submit"
-												data-testid="SendEntityResignation__send-button"
-												disabled={!isValid}
-												className="space-x-2"
-											>
-												<Icon name="Send" width={20} height={20} />
-												<span>{t("COMMON.SEND")}</span>
-											</Button>
-										)}
-
-										{activeTab === 4 && (
-											<div className="flex justify-end space-x-3">
+										<div className="flex justify-end mt-10 space-x-3">
+											{activeTab < 4 && (
 												<Button
-													data-testid="SendEntityResignation__wallet-button"
+													disabled={activeTab === 1}
+													data-testid="SendEntityResignation__back-button"
 													variant="plain"
-													onClick={() => {
-														history.push(`/profiles/${activeProfile.id()}/dashboard`);
-													}}
+													onClick={handleBack}
 												>
-													{t("COMMON.BACK_TO_WALLET")}
+													{t("COMMON.BACK")}
 												</Button>
+											)}
 
+											{activeTab < 3 && (
 												<Button
-													data-testid="SendEntityResignation__download-button"
-													variant="plain"
+													data-testid="SendEntityResignation__continue-button"
+													disabled={!isValid}
+													onClick={handleNext}
+												>
+													{t("COMMON.CONTINUE")}
+												</Button>
+											)}
+
+											{activeTab === 3 && (
+												<Button
+													type="submit"
+													data-testid="SendEntityResignation__send-button"
+													disabled={!isValid}
 													className="space-x-2"
 												>
-													<Icon name="Download" />
-													<span>{t("COMMON.DOWNLOAD")}</span>
+													<Icon name="Send" width={20} height={20} />
+													<span>{t("COMMON.SEND")}</span>
 												</Button>
-											</div>
-										)}
+											)}
+
+											{activeTab === 4 && (
+												<div className="flex justify-end space-x-3">
+													<Button
+														data-testid="SendEntityResignation__wallet-button"
+														variant="plain"
+														onClick={() => {
+															history.push(`/profiles/${activeProfile.id()}/dashboard`);
+														}}
+													>
+														{t("COMMON.BACK_TO_WALLET")}
+													</Button>
+
+													<Button
+														data-testid="SendEntityResignation__download-button"
+														variant="plain"
+														className="space-x-2"
+													>
+														<Icon name="Download" />
+														<span>{t("COMMON.DOWNLOAD")}</span>
+													</Button>
+												</div>
+											)}
+										</div>
 									</div>
-								</div>
-							</div>
-						)}
-					</Tabs>
-				</Form>
+								</>
+							)}
+						</Tabs>
+					</Form>
+				</div>
 			</Section>
 		</Page>
 	);
