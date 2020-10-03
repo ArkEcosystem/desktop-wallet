14064bf46ca67f91945dc12c5673c76d98e938f5
Edgar Goetzendorff <hello@dated.fun>
2020-10-04 00:47:31 +0200

    test: add entity mocks

--- src/utils/e2e-utils.ts
+++ src/utils/e2e-utils.ts
@@ -37,6 +37,38 @@ export const requestMocks = {
 		mockRequest("https://dwallets.ark.io/api/node/fees?days=7", "coins/ark/node-fees"),
 		mockRequest("https://dwallets.ark.io/api/transactions/fees", "coins/ark/transaction-fees"),
 		mockRequest("https://dwallets.ark.io/api/transactions/search?limit=10", "coins/ark/transactions"),
+		mockRequest(
+			(request: any) =>
+				request.url === "https://dwallets.ark.io/api/transactions/search" &&
+				request.method === "post" &&
+				request.body.toString() ===
+					'{"senderPublicKey":"03af2feb4fc97301e16d6a877d5b135417e8f284d40fac0f84c09ca37f82886c51","type":6,"typeGroup":2,"asset":{"type":0,"action":0}}',
+			"coins/ark/transactions/business-registrations",
+		),
+		mockRequest(
+			(request: any) =>
+				request.url === "https://dwallets.ark.io/api/transactions/search" &&
+				request.method === "post" &&
+				request.body.toString() ===
+					'{"senderPublicKey":"03af2feb4fc97301e16d6a877d5b135417e8f284d40fac0f84c09ca37f82886c51","type":6,"typeGroup":2,"asset":{"type":3,"action":0}}',
+			"coins/ark/transactions/plugin-registrations",
+		),
+		mockRequest(
+			(request: any) =>
+				request.url === "https://dwallets.ark.io/api/transactions/search" &&
+				request.method === "post" &&
+				request.body.toString() ===
+					'{"senderPublicKey":"03df6cd794a7d404db4f1b25816d8976d0e72c5177d17ac9b19a92703b62cdbbbc","type":6,"typeGroup":2,"asset":{"type":0,"action":0}}',
+			"coins/ark/transactions/empty-search",
+		),
+		mockRequest(
+			(request: any) =>
+				request.url === "https://dwallets.ark.io/api/transactions/search" &&
+				request.method === "post" &&
+				request.body.toString() ===
+					'{"senderPublicKey":"03df6cd794a7d404db4f1b25816d8976d0e72c5177d17ac9b19a92703b62cdbbbc","type":6,"typeGroup":2,"asset":{"type":3,"action":0}}',
+			"coins/ark/transactions/empty-search",
+		),
 	],
 	wallets: [
 		mockRequest(
