069c338984d827ed527c2d2ce61eb086b930d7e3
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-10-05 09:54:36 -0300

    refactor: move process url listener to router view

--- src/router/RouterView.tsx
+++ src/router/RouterView.tsx
@@ -1,5 +1,9 @@
+import { URIService } from "@arkecosystem/platform-sdk/dist/coins";
 import { useEnvironmentContext } from "app/contexts";
-import React from "react";
+import { getDeeplinkRoute, toasts } from "app/services";
+import { ipcRenderer } from "electron";
+import React, { useLayoutEffect, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { RouteConfig } from "react-router-config";
 import { Redirect, Route, Switch, useHistory, useLocation } from "react-router-dom";
 import { styled } from "twin.macro";
@@ -16,18 +20,40 @@ type Props = {
 
 export const RouterView = ({ routes, wrapper, middlewares }: Props) => {
 	const location = useLocation();
+	const { pathname } = location;
 	const history = useHistory();
+	const { t } = useTranslation();
 
 	const { env } = useEnvironmentContext();
-	const [redirectUrl, setRedirectUrl] = React.useState<string | undefined>(undefined);
+	const [redirectUrl, setRedirectUrl] = useState<string | undefined>(undefined);
 
-	const canActivate = React.useMemo(
+	const canActivate = useMemo(
 		() =>
 			middlewares!.every((middleware) =>
 				middleware.handler({ location, env, redirect: setRedirectUrl, history }),
 			),
 		[location, middlewares, env, history],
 	);
+
+	useLayoutEffect(() => {
+		ipcRenderer.on("process-url", (_, url) => {
+			const uriService = new URIService();
+			console.log({ pathname });
+			if (pathname === "/") return toasts.warning(t("COMMON.SELECT_A_PROFILE"));
+
+			if (pathname.includes("/dashboard")) return toasts.warning(t("COMMON.SELECT_A_WALLET"));
+
+			const urlParts = pathname.split("/");
+			const activeSession = {
+				profileId: urlParts[2],
+				walletId: urlParts[4],
+			};
+
+			const deeplinkSchema = uriService.deserialize(url);
+
+			return history.replace(getDeeplinkRoute(activeSession)[deeplinkSchema.method], deeplinkSchema);
+		});
+	}, [pathname, history, t]);
 
 	return (
 		<Switch>
