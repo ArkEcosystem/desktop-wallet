35a61f4f2f3b8a6463ce19a451ba1e4ec05723eb
goga-m <goga.mamoulasvili@gmail.com>
2020-10-06 16:22:12 +0300

    test: add e2e tests

--- src/domains/setting/e2e/settings-routing.e2e.ts
+++ src/domains/setting/e2e/settings-routing.e2e.ts
@@ -1,6 +1,37 @@
-import { createFixture } from "../../../utils/e2e-utils";
+import { Selector } from "testcafe";
+
+import { buildTranslations } from "../../../app/i18n/helpers";
+import { createFixture, getLocation } from "../../../utils/e2e-utils";
 import { goToSettings } from "./common";
+
+const translations = buildTranslations();
 
 createFixture(`Settings routing`);
 
 test("should navigate to settings page", async (t) => await goToSettings(t));
+
+test("should require password in settings page for protected profile", async (t) => {
+	await t.click(Selector("p").withText("Jane Doe"));
+	await t
+		.expect(Selector('[data-testid="modal__inner"]').withText(translations.PROFILE.MODAL_SIGN_IN.TITLE).exists)
+		.ok();
+
+	await t.typeText(Selector('[data-testid="SignIn__input--password"]'), "password");
+	await t.click(Selector('[data-testid="SignIn__submit-button"]'));
+
+	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok();
+	await t.expect(getLocation()).contains("/dashboard");
+
+	await t.click(Selector('[data-testid="navbar__useractions"]'));
+	await t.expect(Selector('[data-testid="dropdown__option--3"]').withText(translations.COMMON.SETTINGS).exists).ok();
+	await t.click(Selector('[data-testid="dropdown__option--3"]').withText(translations.COMMON.SETTINGS));
+
+	await t
+		.expect(Selector('[data-testid="modal__inner"]').withText(translations.PROFILE.MODAL_SIGN_IN.TITLE).exists)
+		.ok();
+
+	await t.typeText(Selector('[data-testid="SignIn__input--password"]'), "password");
+	await t.click(Selector('[data-testid="SignIn__submit-button"]'));
+
+	await t.expect(Selector("h1").withText(translations.SETTINGS.GENERAL.TITLE).exists).ok();
+});
