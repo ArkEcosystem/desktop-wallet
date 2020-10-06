256d96f55e55ce78dccb6d048bf14fa62af2d523
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-10-06 12:13:55 -0300

    refactor: remove ununsed service

--- src/app/services/Deeplinker.ts
+++
@@ -1,3 +0,0 @@
-export const getDeeplinkRoute: any = (activeSession: { profileId: string; walletId: string }) => ({
-	transfer: `/profiles/${activeSession.profileId}/wallets/${activeSession.walletId}/send-transfer`,
-});
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from "./HttpClient";
 import { ToastService } from "./ToastService";
-export * from "./Deeplinker";
 
 export const httpClient = new HttpClient(10);
 export const toasts = new ToastService();
