7c60b5d84019bcbad6b9573ac31dbc430340343f
goga-m <goga.mamoulasvili@gmail.com>
2020-10-09 15:37:48 +0300

    test: add coverage for `hasSufficientFunds` utility fn

--- src/domains/transaction/utils.test.ts
+++ src/domains/transaction/utils.test.ts
@@ -1,4 +1,13 @@
-import { evaluateFee } from "./utils";
+import { Profile, ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
+import { env,getDefaultProfileId } from "testing-library";
+
+import { evaluateFee, hasSufficientFunds } from "./utils";
+
+let profile: Profile;
+let wallet: ReadWriteWallet;
+let emptyWallet: ReadWriteWallet;
+
+const blankWalletPassphrase = "power return attend drink piece found tragic fire liar page disease combine";
 
 describe("Transaction utils", () => {
 	describe("Evalute Fee", () => {
@@ -10,6 +19,30 @@ describe("Transaction utils", () => {
 		it("should return the right fee when plain", () => {
 			const fee = evaluateFee("100000000");
 			expect(fee.toString()).toEqual("100000000");
+		});
+	});
+
+	describe("Check Sufficient Funds", () => {
+		beforeAll(async () => {
+			profile = env.profiles().findById(getDefaultProfileId());
+			emptyWallet = await profile.wallets().importByMnemonic(blankWalletPassphrase, "ARK", "ark.devnet");
+			wallet = profile.wallets().findById("ac38fe6d-4b67-4ef1-85be-17c5f6841129");
+		});
+
+		it("should have sufficient funds for given fee and amount", () => {
+			expect(hasSufficientFunds({ wallet, fee: 1, amount: 1 })).toBe(true);
+		});
+
+		it("should have sufficient funds for given fee", () => {
+			expect(hasSufficientFunds({ wallet, amount: 1 })).toBe(true);
+		});
+
+		it("should have sufficient funds for given amount", () => {
+			expect(hasSufficientFunds({ wallet, fee: 1 })).toBe(true);
+		});
+
+		it("should not have sufficient funds for given amount and fee", () => {
+			expect(hasSufficientFunds({ wallet: emptyWallet, fee: 1, amount: 1 })).toBe(false);
 		});
 	});
 });
