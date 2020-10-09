392acc37eab11eae50d72a4c3d6bb4b588807b90
goga-m <goga.mamoulasvili@gmail.com>
2020-10-09 13:56:06 +0300

    feat: add utility function to check for sufficient funds

--- src/domains/transaction/utils.ts
+++ src/domains/transaction/utils.ts
@@ -1,7 +1,21 @@
+import { ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
 import { BigNumber } from "@arkecosystem/platform-sdk-support";
+
+type SufficientFundsParams = {
+	wallet: ReadWriteWallet;
+	fee?: any;
+	amount?: any;
+};
 
 export const evaluateFee = (fee: any) => {
 	if (fee?.value) return BigNumber.make(fee.value);
 
 	return BigNumber.make(fee);
+};
+
+export const hasSufficientFunds = ({ wallet, fee = 0, amount = 0 }: SufficientFundsParams) => {
+	const feeValue = evaluateFee(fee);
+	const total = feeValue.plus(amount);
+
+	return wallet.balance().isGreaterThanOrEqualTo(total);
 };
