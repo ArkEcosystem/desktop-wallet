2a3b74f57b8464f2c9165a3aa4471fc7120eb95b
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-10-10 09:58:25 -0300

    test: increase ledger connection step

--- src/domains/wallet/pages/ImportWallet/Ledger/LedgerConnectionStep.test.tsx
+++ src/domains/wallet/pages/ImportWallet/Ledger/LedgerConnectionStep.test.tsx
@@ -68,4 +68,42 @@ describe("LedgerConnectionStep", () => {
 
 		getPublicKeySpy.mockReset();
 	});
+
+	it("should emit event on connect", async () => {
+		const publicKeyPaths = new Map([
+			["44'/111'/0'/0/0", "027716e659220085e41389efc7cf6a05f7f7c659cf3db9126caabce6cda9156582"],
+			["44'/111'/1'/0/0", wallet.publicKey()!],
+			["44'/111'/2'/0/0", "020aac4ec02d47d306b394b79d3351c56c1253cd67fe2c1a38ceba59b896d584d1"],
+		]);
+
+		const getPublicKeySpy = jest
+			.spyOn(wallet.coin().ledger(), "getPublicKey")
+			.mockResolvedValue(publicKeyPaths.values().next().value);
+
+		const onConnect = jest.fn();
+
+		const Component = () => {
+			const form = useForm({
+				defaultValues: {
+					network: wallet.network(),
+				},
+			});
+			return (
+				<FormProvider {...form}>
+					<LedgerProvider transport={transport}>
+						<LedgerConnectionStep onConnect={onConnect} />
+					</LedgerProvider>
+				</FormProvider>
+			);
+		};
+
+		const { container } = render(<Component />);
+
+		await waitFor(() => expect(screen.getByText("Successfully connected")).toBeInTheDocument());
+		await waitFor(() => expect(onConnect).toHaveBeenCalled());
+
+		expect(container).toMatchSnapshot();
+
+		getPublicKeySpy.mockReset();
+	});
 });
