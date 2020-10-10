8c49e5d81fc47897a55b0feed41894dc838a135d
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-10-10 04:03:25 +0300

    fix: smartbridge validation in `SendTransfer` (#2951)

---
+++ src/domains/transaction/validations/SendTransfer.ts
@@ -0,0 +1,37 @@
+export const sendTransfer = (t: any) => ({
+	fee: () => ({
+		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
+			field: t("COMMON.FEE"),
+		}),
+	}),
+	senderAddress: () => ({
+		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
+			field: t("COMMON.SENDER_ADDRESS"),
+		}),
+	}),
+	smartbridge: () => ({
+		maxLength: {
+			value: 255,
+			message: t("COMMON.VALIDATION.MAX_LENGTH", {
+				field: t("COMMON.SMARTBRIDGE"),
+				maxLength: 255,
+			}),
+		},
+	}),
+	network: () => ({
+		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
+			field: t("COMMON.NETWORK"),
+		}),
+	}),
+	recipients: () => ({
+		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
+			field: t("COMMON.RECIPIENTS"),
+		}),
+		validate: {
+			valid: (value: string) => {
+				if (Array.isArray(value) && value.length > 0) return true;
+				return t("COMMON.VALIDATION.MIN_RECIPIENTS");
+			},
+		},
+	}),
+});
