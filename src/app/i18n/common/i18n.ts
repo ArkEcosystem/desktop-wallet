1babb23cf4ee4456b3c0aad7c0b9706400f9eabd
brenopolanski <breno.polanski@gmail.com>
2020-10-13 14:41:05 -0300

    test: update snapshots

--- src/app/i18n/common/i18n.ts
+++ src/app/i18n/common/i18n.ts
@@ -89,7 +89,7 @@ export const translations: { [key: string]: any } = {
 	MOST_POPULAR: "Most popular",
 	MULTISIGNATURE: "Multisignature",
 	NAME: "Name",
-	NO_OPTIONS: "No Options",
+	NO_OPTIONS: "No options",
 	SENDER_ADDRESS: "Sender address",
 	SMARTBRIDGE: "Smartbridge",
 	FEE: "FEE",
