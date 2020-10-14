7e0bd8b6495a8d405d7c414624e6f9c1d31faf7b
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-10-14 12:00:20 -0300

    tests: add fee tests

--- src/domains/transaction/validations/Fee.test.ts
+++ src/domains/transaction/validations/Fee.test.ts
@@ -10,4 +10,8 @@ describe("Fee validation", () => {
 	it("should not return a warning message", () => {
 		expect(validateFee({ display: "0.1", value: "10000" }, "1000")).toEqual("");
 	});
+
+	it("should not return a warning message if no fee", () => {
+		expect(validateFee(undefined, "1000")).toEqual("");
+	});
 });
