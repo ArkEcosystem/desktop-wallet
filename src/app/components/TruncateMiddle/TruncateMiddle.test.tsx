93598ea13026faa7c7380bc45917edd35fbd16e7
Edgar Goetzendorff <hello@dated.fun>
2020-10-17 02:41:33 +0200

    wip

--- src/app/components/TruncateMiddle/TruncateMiddle.test.tsx
+++ src/app/components/TruncateMiddle/TruncateMiddle.test.tsx
@@ -6,12 +6,12 @@ import { TruncateMiddle } from "./TruncateMiddle";
 describe("TruncateMiddle", () => {
 	it("should truncate with default maxChars", () => {
 		const { container } = render(<TruncateMiddle text="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT" />);
-		expect(container).toHaveTextContent("ASuusX…GQ3kqT");
+		expect(container).toHaveTextContent("ASuusX … GQ3kqT");
 	});
 
 	it("should truncate with custom maxChars", () => {
 		const { container } = render(<TruncateMiddle text="ASuusXSW9kfWnicScSgUTjttP6T9GQ3kqT" maxChars={28} />);
-		expect(container).toHaveTextContent("ASuusXSW9kfW…ttP6T9GQ3kqT");
+		expect(container).toHaveTextContent("ASuusXSW9kfW … ttP6T9GQ3kqT");
 	});
 
 	it("should not truncate if string is less than maxChars", () => {
