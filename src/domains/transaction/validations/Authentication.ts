4fdc7e7dd7c166549f492379f4dd5533a06b356c
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-10-21 11:46:55 -0300

    wip

--- src/domains/transaction/validations/Authentication.ts
+++ src/domains/transaction/validations/Authentication.ts
@@ -1,8 +1,16 @@
+import { ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
+
 export const authentication = (t: any) => ({
-	mnemonic: () => ({
+	mnemonic: (wallet: ReadWriteWallet) => ({
 		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
 			field: t("COMMON.MNEMONIC"),
 		}),
+		validate: {
+			matchWalletAddress: async (value: string) => {
+				const generatedAddress = await wallet.coin().identity().address().fromMnemonic(value);
+				return generatedAddress === wallet.address();
+			},
+		},
 	}),
 	secondMnemonic: () => ({
 		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
