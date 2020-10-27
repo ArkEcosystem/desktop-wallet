9f4ced77f6fc9ddf22d2538e920f4de5de522f7e
dated <dated@users.noreply.github.com>
2020-10-27 23:15:39 +0000

    style: resolve style guide violations

--- src/domains/wallet/pages/WalletDetails/components/WalletRegistrations/WalletRegistrations.tsx
+++ src/domains/wallet/pages/WalletDetails/components/WalletRegistrations/WalletRegistrations.tsx
@@ -64,7 +64,7 @@ export const WalletRegistrations = ({
 					</div>
 
 					<div className="flex justify-between flex-1">
-						<div className="flex flex-col mr-4 font-semibold text-theme-text leading-snug">
+						<div className="flex flex-col mr-4 font-semibold leading-snug text-theme-text">
 							<span className="mr-2">
 								{t("WALLETS.PAGE_WALLET_DETAILS.REGISTRATIONS.EMPTY_DESCRIPTION")}
 							</span>
