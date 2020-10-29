58d12a2afe8d9d7a18db3a909784c7253ac8e73c
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-10-29 09:37:02 -0300

    wip

--- src/app/contexts/Ledger/hooks/scanner.test.tsx
+++ src/app/contexts/Ledger/hooks/scanner.test.tsx
@@ -35,7 +35,12 @@ describe("Use Ledger Scanner", () => {
 					},
 				],
 			})
-			.persist();
+			.get("/wallets")
+			.query((params) => !!params.address)
+			.reply(200, {
+				meta: {},
+				data: [],
+			});
 	});
 
 	beforeEach(() => {
@@ -49,6 +54,11 @@ describe("Use Ledger Scanner", () => {
 			["44'/1'/2'/0/0", "020aac4ec02d47d306b394b79d3351c56c1253cd67fe2c1a38ceba59b896d584d1"],
 			["44'/1'/3'/0/0", "033a5474f68f92f254691e93c06a2f22efaf7d66b543a53efcece021819653a200"],
 			["44'/1'/4'/0/0", "03d3c6889608074b44155ad2e6577c3368e27e6e129c457418eb3e5ed029544e8d"],
+			["44'/1'/5'/0/0", "02ac8d84d81648154f79ba64fbf64cd6ee60f385b2aa52e8eb72bc1374bf55a68c"],
+			["44'/1'/6'/0/0", "032cfbb18f4e49952c6d6475e8adc6d0cba00b81ef6606cc4927b78c6c50558beb"],
+			["44'/1'/7'/0/0", "0242555e90957de10e3912ce8831bcc985a40a645447dbfe8a0913ee6d89914707"],
+			["44'/1'/8'/0/0", "02677f73453da6073f5cf76db8f65fabc1a3b7aadc7b06027e0df709f14e097790"],
+			["44'/1'/9'/0/0", "03f3512aa9717b2ca83d371ed3bb2d3ff922969f3ceef92f65c060afa2bc2244fb"],
 		]);
 	});
 
@@ -96,5 +106,13 @@ describe("Use Ledger Scanner", () => {
 		await waitFor(() => expect(screen.queryAllByText("Balance: Loading")).toHaveLength(0));
 
 		expect(container).toMatchSnapshot();
+
+		// Fetch more
+		act(() => {
+			fireEvent.click(screen.getByRole("button"));
+		});
+
+		await waitFor(() => expect(screen.queryAllByRole("listitem")).toHaveLength(4));
+		await waitFor(() => expect(screen.queryAllByText("Balance: Loading")).toHaveLength(0));
 	});
 });
