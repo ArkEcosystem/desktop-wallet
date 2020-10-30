c428b43ec987484d3c3f21ab7d3aa9cdaebfcdcc
goga-m <goga.mamoulasvili@gmail.com>
2020-10-30 19:31:23 +0200

    feat: implement download progress in `WalletUpdate` download step

--- src/domains/wallet/components/WalletUpdate/Step2.tsx
+++ src/domains/wallet/components/WalletUpdate/Step2.tsx
@@ -1,24 +1,38 @@
 import { CircularProgressBar } from "app/components/CircularProgressBar";
+import { DownloadProgress } from "app/hooks/use-updater";
+import prettyBytes from "pretty-bytes";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export const SecondStep = () => {
+export const SecondStep = ({ transferred, total, percent }: DownloadProgress) => {
 	const { t } = useTranslation();
 
 	return (
 		<section data-testid="WalletUpdate__second-step">
-			{/* TODO: Remove the class name `mb-8` in the functional code */}
-			<div className="flex w-2/5 mx-auto mb-8">
+			<div className="flex w-2/5 mx-auto">
 				<div className="flex-1">
 					<p className="text-sm font-semibold text-theme-neutral-light">{t("COMMON.DOWNLOADED")}</p>
-					<p className="text-sm font-bold text-theme-secondary-text">154 KB / 154 KB</p>
+					<p className="text-sm font-bold text-theme-secondary-text">
+						{prettyBytes(total)} / {prettyBytes(transferred)}
+					</p>
 				</div>
-				<div className="flex-1">
+				<div>
 					<div className="flex justify-center">
-						<CircularProgressBar value={78} size={50} strokeWidth={5} fontSize={0.8} />
+						<CircularProgressBar
+							value={parseInt(percent.toFixed(0))}
+							size={50}
+							strokeWidth={5}
+							fontSize={0.8}
+						/>
 					</div>
 				</div>
 			</div>
 		</section>
 	);
+};
+
+SecondStep.defaultProps = {
+	transferred: 0,
+	total: 0,
+	percent: 0,
 };
