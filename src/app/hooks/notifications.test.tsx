5473e0b94d48445aa80cb264bce8aa28d8461e8d
goga-m <goga.mamoulasvili@gmail.com>
2020-11-02 17:28:56 +0200

    test(unit): add wallet update notification tests

--- src/app/hooks/notifications.test.tsx
+++ src/app/hooks/notifications.test.tsx
@@ -38,4 +38,36 @@ describe("Notifications Hook", () => {
 			.get(transactionNotifications[0].id);
 		expect(savedNotification).toBeTruthy();
 	});
+
+	it("should push wallet update notification for all profiles", () => {
+		const wrapper = ({ children }: any) => <EnvironmentProvider env={env}> {children} </EnvironmentProvider>;
+		const { result } = renderHook(() => useNotifications(), { wrapper });
+
+		const { notifications } = result.current;
+
+		notifications.notifyWalletUpdate({ version: "3.0.0" });
+		const newNotification = env
+			.profiles()
+			.values()[0]
+			.notifications()
+			.values()
+			.find((n) => n?.meta?.version === "3.0.0");
+
+		expect(newNotification).toBeDefined();
+		expect(newNotification?.action).toEqual("update");
+		expect(newNotification?.type).toEqual("wallet");
+		expect(newNotification?.meta?.version).toEqual("3.0.0");
+	});
+
+	it("should not push wallet update notification if exists", () => {
+		const wrapper = ({ children }: any) => <EnvironmentProvider env={env}> {children} </EnvironmentProvider>;
+		const { result } = renderHook(() => useNotifications(), { wrapper });
+
+		const { notifications } = result.current;
+
+		const initialNotificationsCount = env.profiles().values()[0].notifications().count();
+		notifications.notifyWalletUpdate({ version: "2.5.6" });
+		const notificationsCount = env.profiles().values()[0].notifications().count();
+		expect(notificationsCount).toEqual(initialNotificationsCount);
+	});
 });
