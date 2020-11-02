39e47a5f7f95172a13a02347026de41b4a6fe66c
goga-m <goga.mamoulasvili@gmail.com>
2020-11-02 10:23:46 +0200

    test: fix notifications tests

--- src/app/hooks/notifications.test.tsx
+++ src/app/hooks/notifications.test.tsx
@@ -1,11 +1,14 @@
 import { renderHook } from "@testing-library/react-hooks";
 import nock from "nock";
+import React from "react";
 import { env, getDefaultProfileId } from "utils/testing-library";
 
 import { useNotifications } from "./notifications";
 
 const NotificationsTransactionsFixture = require("tests/fixtures/coins/ark/devnet/notification-transactions.json");
 const TransactionsFixture = require("tests/fixtures/coins/ark/devnet/transactions.json");
+
+import { EnvironmentProvider } from "app/contexts";
 
 describe("Notifications Hook", () => {
 	beforeAll(() => {
@@ -20,7 +23,9 @@ describe("Notifications Hook", () => {
 	});
 
 	it("should create and save notifications from received transactions", async () => {
-		const { result } = renderHook(() => useNotifications(env));
+		const wrapper = ({ children }: any) => <EnvironmentProvider env={env}> {children} </EnvironmentProvider>;
+		const { result } = renderHook(() => useNotifications(), { wrapper });
+
 		const { notifications } = result.current;
 
 		const transactionNotifications = await notifications.syncReceivedTransactions();
