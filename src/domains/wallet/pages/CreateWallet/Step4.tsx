e3c6216cc69db571a8e0656b5ea5206f4c1c9b10
Edgar Goetzendorff <hello@dated.fun>
2020-11-10 01:46:00 +0100

    refactor: disallow duplicate wallet names on create

--- src/domains/wallet/pages/CreateWallet/Step4.tsx
+++ src/domains/wallet/pages/CreateWallet/Step4.tsx
@@ -1,5 +1,5 @@
 import { Coins } from "@arkecosystem/platform-sdk";
-import { ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
+import { Profile, ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
 import { Address } from "app/components/Address";
 import { Avatar } from "app/components/Avatar";
 import { FormField, FormHelperText, FormLabel } from "app/components/Form";
@@ -12,7 +12,7 @@ import React, { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
-export const FourthStep = ({ nameMaxLength }: { nameMaxLength: number }) => {
+export const FourthStep = ({ nameMaxLength, profile }: { nameMaxLength: number; profile: Profile }) => {
 	const { getValues, register, watch } = useFormContext();
 
 	// getValues does not get the value of `defaultValues` on first render
@@ -27,31 +27,31 @@ export const FourthStep = ({ nameMaxLength }: { nameMaxLength: number }) => {
 	const { t } = useTranslation();
 
 	return (
-		<section data-testid="CreateWallet__fourth-step">
-			<div className="my-8">
-				<Header
-					title={t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.TITLE")}
-					subtitle={t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.SUBTITLE")}
-				/>
+		<section data-testid="CreateWallet__fourth-step" className="space-y-8">
+			<Header
+				title={t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.TITLE")}
+				subtitle={t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.SUBTITLE")}
+			/>
+
+			<div>
+				<TransactionDetail
+					label={t("COMMON.CRYPTOASSET")}
+					borderPosition="bottom"
+					extra={<NetworkIcon size="lg" coin={network.coin()} network={network.id()} />}
+				>
+					{networkConfig?.displayName}
+				</TransactionDetail>
+
+				<TransactionDetail
+					label={t("COMMON.ADDRESS")}
+					borderPosition="bottom"
+					extra={<Avatar size="lg" address={wallet.address()} />}
+				>
+					<Address address={wallet.address()} maxChars={0} />
+				</TransactionDetail>
 			</div>
 
-			<TransactionDetail
-				label={t("COMMON.CRYPTOASSET")}
-				borderPosition="bottom"
-				extra={<NetworkIcon size="lg" coin={network.coin()} network={network.id()} />}
-			>
-				{networkConfig?.displayName}
-			</TransactionDetail>
-
-			<TransactionDetail
-				label={t("COMMON.ADDRESS")}
-				borderPosition="bottom"
-				extra={<Avatar size="lg" address={wallet.address()} />}
-			>
-				<Address address={wallet.address()} maxChars={0} />
-			</TransactionDetail>
-
-			<FormField name="name" className="mt-8">
+			<FormField name="name">
 				<FormLabel label={t("WALLETS.PAGE_CREATE_WALLET.WALLET_NAME")} required={false} optional />
 				<Input
 					data-testid="CreateWallet__wallet-name"
@@ -61,6 +61,14 @@ export const FourthStep = ({ nameMaxLength }: { nameMaxLength: number }) => {
 							message: t("WALLETS.PAGE_CREATE_WALLET.VALIDATION.MAXLENGTH_ERROR", {
 								maxLength: nameMaxLength,
 							}),
+						},
+						validate: {
+							duplicateAlias: (alias) =>
+								!alias ||
+								!profile.wallets().findByAlias(alias) ||
+								t("WALLETS.PAGE_CREATE_WALLET.VALIDATION.ALIAS_EXISTS", {
+									alias,
+								}).toString(),
 						},
 					})}
 				/>
