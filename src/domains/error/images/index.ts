5c815ab075102e6a16b91e8593006f46c2bd2441
Edgar Goetzendorff <hello@dated.fun>
2020-11-10 13:08:38 +0100

    refactor: catch boot error (#3072)

--- src/domains/error/images/index.ts
+++ src/domains/error/images/index.ts
@@ -1,13 +1,9 @@
 import { ReactComponent as CannotConnectBanner } from "./cannot-connect-banner.svg";
+import { ImagesDark } from "./dark";
 import { ReactComponent as ErrorBanner } from "./error-banner.svg";
 
 export const error = {
-	pages: {
-		ApplicationError: {
-			ErrorBanner,
-		},
-		Offline: {
-			CannotConnectBanner,
-		},
-	},
+	ErrorBanner,
+	CannotConnectBanner,
+	...ImagesDark,
 };
