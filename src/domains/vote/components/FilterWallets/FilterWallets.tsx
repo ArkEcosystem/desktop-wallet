7771339cf0b6f9b686a1a3ff1782068e2981d4e3
brenopolanski <breno.polanski@gmail.com>
2020-11-13 10:26:11 -0300

    feat: add FilterWallets component

---
+++ src/domains/vote/components/FilterWallets/FilterWallets.tsx
@@ -0,0 +1,80 @@
+import { Dropdown, DropdownOption } from "app/components/Dropdown";
+import { FilterNetwork } from "app/components/FilterNetwork";
+import { Icon } from "app/components/Icon";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+export type FilterWalletsProps = {
+	networks?: any;
+	walletsDisplayType?: string;
+	onNetworkChange?: any;
+	onViewAllNetworks?: any;
+	onWalletsDisplayType?: any;
+};
+
+export const FilterWallets = ({
+	networks,
+	walletsDisplayType,
+	onNetworkChange,
+	onViewAllNetworks,
+	onWalletsDisplayType,
+}: FilterWalletsProps) => {
+	const { t } = useTranslation();
+
+	const walletDisplayOptions = [
+		{ label: t("COMMON.ALL"), value: "all" },
+		{ label: t("COMMON.FAVORITES"), value: "favorites" },
+		{ label: t("COMMON.LEDGER"), value: "ledger" },
+	];
+
+	return (
+		<div className="flex flex-col text-left" data-testid="FilterWallets">
+			<div className="mb-8">
+				<div className="font-semibold text-theme-secondary-text">
+					{t("DASHBOARD.FILTER_WALLETS.CRYPTOASSET.TITLE")}
+				</div>
+				<div className="mt-1 text-sm text-theme-neutral">
+					{t("DASHBOARD.FILTER_WALLETS.CRYPTOASSET.DESCRIPTION")}
+				</div>
+			</div>
+
+			<FilterNetwork networks={networks} onChange={onNetworkChange} onViewAll={onViewAllNetworks} />
+
+			<div className="my-8 border-t border-dotted border-theme-neutral-300 dark:border-theme-neutral-800" />
+
+			<div className="flex flex-col">
+				<div className="flex items-center justify-between">
+					<div className="font-semibold text-theme-secondary-text">
+						{t("DASHBOARD.FILTER_WALLETS.WALLETS.TITLE")}
+					</div>
+
+					<Dropdown
+						toggleIcon="ChevronDown"
+						options={walletDisplayOptions}
+						onSelect={(option: DropdownOption) => onWalletsDisplayType?.(option)}
+						toggleContent={
+							<div
+								data-testid="filter-wallets__wallets"
+								className="flex items-center justify-end cursor-pointer text-theme-secondary-text"
+							>
+								<span className="inline-block mr-2 font-semibold">
+									{walletDisplayOptions.find((option) => option.value === walletsDisplayType)?.label}
+								</span>
+								<Icon name="ChevronDown" width={12} height={12} />
+							</div>
+						}
+					/>
+				</div>
+
+				<div className="pr-12 mt-1 text-sm text-theme-neutral">
+					{t("DASHBOARD.FILTER_WALLETS.WALLETS.DESCRIPTION")}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+FilterWallets.defaultProps = {
+	networks: [],
+	walletsDisplayType: "all",
+};
