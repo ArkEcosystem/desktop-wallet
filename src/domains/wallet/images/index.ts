d857fd43c91c9a069be960e23145e72dddef6390
Edgar Goetzendorff <hello@dated.fun>
2020-11-16 12:33:16 +0100

    refactor: show skeleton on initial ledger wallet fetching (#3088)

--- src/domains/wallet/images/index.ts
+++ src/domains/wallet/images/index.ts
@@ -1,12 +1,12 @@
+import { ImagesDark } from "./dark";
 import { ReactComponent as WaitingLedgerDevice } from "./waiting-ledger-device.svg";
 import { ReactComponent as WalletUpdateReadyBanner } from "./wallet-update-ready.svg";
 import { ReactComponent as WalletUpdateBanner } from "./wallet-update.svg";
 
 export const wallet = {
+	WaitingLedgerDevice,
+	...ImagesDark,
 	components: {
-		Ledger: {
-			WaitingLedgerDevice,
-		},
 		walletUpdate: {
 			WalletUpdateBanner,
 			WalletUpdateReadyBanner,
