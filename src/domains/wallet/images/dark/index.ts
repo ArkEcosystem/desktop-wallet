8bf6b6b5b3c2ab1b7bcd5c02d853e0303d64f9c6
Edgar Goetzendorff <hello@dated.fun>
2020-11-17 00:40:08 +0100

    chore: update dark wallet images

--- src/domains/wallet/images/dark/index.ts
+++ src/domains/wallet/images/dark/index.ts
@@ -1,5 +1,9 @@
 import { ReactComponent as WaitingLedgerDeviceDark } from "./waiting-ledger-device.svg";
+import { ReactComponent as WalletUpdateReadyBannerDark } from "./wallet-update-ready.svg";
+import { ReactComponent as WalletUpdateBannerDark } from "./wallet-update.svg";
 
 export const ImagesDark: any = {
 	WaitingLedgerDeviceDark,
+	WalletUpdateBannerDark,
+	WalletUpdateReadyBannerDark,
 };
