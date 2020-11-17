50b5120d552701ac167c524fb4044d5ccc3fb48b
Edgar Goetzendorff <hello@dated.fun>
2020-11-17 01:38:50 +0100

    test: fix notification e2e tests

--- src/domains/profile/e2e/notifications.e2e.ts
+++ src/domains/profile/e2e/notifications.e2e.ts
@@ -1,12 +1,21 @@
 import { Selector } from "testcafe";
 
 import { buildTranslations } from "../../../app/i18n/helpers";
-import { createFixture } from "../../../utils/e2e-utils";
+import { createFixture, mockRequest } from "../../../utils/e2e-utils";
 import { goToProfile } from "./common";
 
 const translations = buildTranslations();
 
-createFixture(`Profile notifications`).beforeEach(async (t) => await goToProfile(t));
+createFixture(`Profile notifications`, [
+	mockRequest(
+		"https://dwallets.ark.io/api/transactions/ea63bf9a4b3eaf75a1dfff721967c45dce64eb7facf1aef29461868681b5c79b",
+		"coins/ark/devnet/transactions/transfer",
+	),
+	mockRequest(
+		"https://dwallets.ark.io/api/transactions/1a767ebc0cc53246b9105a9f09b6c2ffa7baedcc7e632c8c1bac58f8f17389f6",
+		"coins/ark/devnet/transactions/transfer",
+	),
+]).beforeEach(async (t) => await goToProfile(t));
 
 test("should show red dot for unread notifications", async (t) => {
 	await t.expect(Selector("[data-testid=navbar__buttons--notifications] .rounded-full").exists).ok();
