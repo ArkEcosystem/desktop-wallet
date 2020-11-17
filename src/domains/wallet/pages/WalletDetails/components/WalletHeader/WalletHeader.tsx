2071539c8000a1433086ac85547d4ceeb77b477b
dated <hello@dated.fun>
2020-11-17 11:57:46 +0100

    wip

--- src/domains/wallet/pages/WalletDetails/components/WalletHeader/WalletHeader.tsx
+++ src/domains/wallet/pages/WalletDetails/components/WalletHeader/WalletHeader.tsx
@@ -55,6 +55,8 @@ export const WalletHeader = ({
 	network,
 	publicKey,
 	ticker,
+	showMultiSignatureOption,
+	showSecondSignatureOption,
 	showSignMessageOption,
 	showStoreHashOption,
 	showVerifyMessageOption,
