54d7c59de9f17a2a2d2796ef8da412c9d2e26665
Edgar Goetzendorff <hello@dated.fun>
2020-11-17 14:25:01 +0100

    wip

--- src/app/components/Dropdown/Dropdown.tsx
+++ src/app/components/Dropdown/Dropdown.tsx
@@ -89,7 +89,7 @@ const renderOptions = (options: DropdownOption[] | DropdownOptionGroup[], onSele
 						<span>
 							{option.label}
 							{option.secondaryLabel && (
-								<span className="ml-1 text-theme-secondary">{option.secondaryLabel}</span>
+								<span className="ml-1 text-theme-secondary-text">{option.secondaryLabel}</span>
 							)}
 						</span>
 						{option?.icon && option?.iconPosition !== "start" && <Icon name={option.icon} />}
