696675e101e0bbdd4e85b0966aa2e021c1c58073
goga-m <goga.mamoulasvili@gmail.com>
2020-11-18 12:55:23 +0200

    test: add coverage

--- src/domains/profile/pages/CreateProfile/CreateProfile.test.tsx
+++ src/domains/profile/pages/CreateProfile/CreateProfile.test.tsx
@@ -56,14 +56,17 @@ const baseSettings = {
 	USE_TEST_NETWORKS: false,
 };
 
-const renderComponent = () =>
-	renderWithRouter(
+const renderComponent = async () => {
+	const result = renderWithRouter(
 		<EnvironmentProvider env={env}>
 			<ThemeProvider>
 				<CreateProfile />
 			</ThemeProvider>
 		</EnvironmentProvider>,
 	);
+	await waitFor(() => expect(result.getByTestId("CreateProfile__submit-button")).toHaveAttribute("disabled"));
+	return result;
+};
 
 describe("CreateProfile", () => {
 	beforeAll(() => {
@@ -76,8 +79,8 @@ describe("CreateProfile", () => {
 		}));
 	});
 
-	it("should render", () => {
-		const { container, getByText, asFragment } = renderComponent();
+	it("should render", async () => {
+		const { container, getByText, asFragment } = await renderComponent();
 
 		expect(container).toBeTruthy();
 		fireEvent.click(getByText("Back"));
@@ -86,7 +89,7 @@ describe("CreateProfile", () => {
 	});
 
 	it("should store profile", async () => {
-		const { asFragment, container, getAllByTestId, getByTestId } = renderComponent();
+		const { asFragment, container, getAllByTestId, getByTestId } = await renderComponent();
 
 		// Upload avatar image
 		await act(async () => {
@@ -110,6 +113,7 @@ describe("CreateProfile", () => {
 
 		fireEvent.click(getByTestId("select-list__toggle-option-0"));
 
+		await waitFor(() => expect(getByTestId("CreateProfile__submit-button")).toHaveAttribute("disabled"));
 		await act(async () => {
 			fireEvent.click(getByTestId("CreateProfile__submit-button"));
 		});
@@ -147,7 +151,7 @@ describe("CreateProfile", () => {
 	});
 
 	it("should not create new profile if profile name exists", async () => {
-		const { asFragment, getAllByTestId, getByTestId } = renderComponent();
+		const { asFragment, getAllByTestId, getByTestId } = await renderComponent();
 
 		fireEvent.input(getAllByTestId("Input")[0], { target: { value: "t" } });
 
@@ -171,6 +175,7 @@ describe("CreateProfile", () => {
 
 		fireEvent.click(getByTestId("select-list__toggle-option-0"));
 
+		await waitFor(() => expect(getByTestId("CreateProfile__submit-button")).not.toHaveAttribute("disabled"));
 		await act(async () => {
 			fireEvent.click(getByTestId("CreateProfile__submit-button"));
 		});
@@ -179,7 +184,7 @@ describe("CreateProfile", () => {
 	});
 
 	it("should store profile with password", async () => {
-		const { asFragment, getAllByTestId, getByTestId } = renderComponent();
+		const { asFragment, getAllByTestId, getByTestId } = await renderComponent();
 
 		fireEvent.input(getAllByTestId("Input")[0], { target: { value: "test profile 3" } });
 		fireEvent.input(getAllByTestId("Input")[1], { target: { value: "test password" } });
@@ -193,6 +198,7 @@ describe("CreateProfile", () => {
 
 		fireEvent.click(getByTestId("select-list__toggle-option-0"));
 
+		await waitFor(() => expect(getByTestId("CreateProfile__submit-button")).not.toHaveAttribute("disabled"));
 		await act(async () => {
 			fireEvent.click(getByTestId("CreateProfile__submit-button"));
 		});
@@ -203,7 +209,7 @@ describe("CreateProfile", () => {
 	});
 
 	it("should fail password confirmation", async () => {
-		const { asFragment, getAllByTestId, getByTestId } = renderComponent();
+		const { asFragment, getAllByTestId, getByTestId } = await renderComponent();
 
 		fireEvent.input(getAllByTestId("Input")[0], { target: { value: "test profile 3" } });
 		fireEvent.input(getAllByTestId("Input")[1], { target: { value: "test password" } });
@@ -217,17 +223,28 @@ describe("CreateProfile", () => {
 
 		fireEvent.click(getByTestId("select-list__toggle-option-0"));
 
+		await waitFor(() => expect(getByTestId("CreateProfile__submit-button")).toHaveAttribute("disabled"));
 		await act(async () => {
 			fireEvent.click(getByTestId("CreateProfile__submit-button"));
 		});
+		await waitFor(() => expect(getAllByTestId("Input")[2]).toHaveAttribute("aria-invalid"));
 
+		fireEvent.input(getAllByTestId("Input")[2], { target: { value: "" } });
+		fireEvent.input(getAllByTestId("Input")[1], { target: { value: "password" } });
+
+		await waitFor(() => expect(getAllByTestId("Input")[2]).toHaveValue(""));
+		await waitFor(() => expect(getAllByTestId("Input")[1]).toHaveValue("password"));
+
+		await act(async () => {
+			fireEvent.click(getByTestId("CreateProfile__submit-button"));
+		});
 		await waitFor(() => expect(getAllByTestId("Input")[2]).toHaveAttribute("aria-invalid"));
 
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should upload and remove avatar image", async () => {
-		const { asFragment, getAllByTestId, getByTestId } = renderComponent();
+		const { asFragment, getAllByTestId, getByTestId } = await renderComponent();
 
 		// Upload avatar image
 		await act(async () => {
@@ -250,6 +267,8 @@ describe("CreateProfile", () => {
 
 		fireEvent.click(getByTestId("select-list__toggle-option-0"));
 
+		await waitFor(() => expect(getByTestId("CreateProfile__submit-button")).not.toHaveAttribute("disabled"));
+
 		await act(async () => {
 			fireEvent.click(getByTestId("CreateProfile__submit-button"));
 		});
@@ -258,7 +277,7 @@ describe("CreateProfile", () => {
 	});
 
 	it("should not upload avatar image", async () => {
-		const { asFragment, getAllByTestId, getByTestId } = renderComponent();
+		const { asFragment, getAllByTestId, getByTestId } = await renderComponent();
 
 		// Not upload avatar image
 		showOpenDialogMock = jest.spyOn(electron.remote.dialog, "showOpenDialog").mockImplementation(() => ({
