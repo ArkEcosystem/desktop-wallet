4f9c48d04987c847c4ac19f36055239af389ce4f
goga-m <goga.mamoulasvili@gmail.com>
2020-11-18 15:23:40 +0200

    fix: show external icon in `WalletDetails` learn more links

--- src/domains/wallet/pages/WalletDetails/components/WalletRegistrations/WalletRegistrations.tsx
+++ src/domains/wallet/pages/WalletDetails/components/WalletRegistrations/WalletRegistrations.tsx
@@ -72,8 +72,8 @@ export const WalletRegistrations = ({
 								{t("WALLETS.PAGE_WALLET_DETAILS.REGISTRATIONS.EMPTY_DESCRIPTION")}
 							</span>
 
-							<div className="mr-auto">
-								<Link to="@TODO" isExternal showExternalIcon={false}>
+							<div className="mr-auto mt-1">
+								<Link to="@TODO" isExternal>
 									{t("COMMON.LEARN_MORE")}
 								</Link>
 							</div>
--- src/domains/wallet/pages/WalletDetails/components/WalletVote/WalletVote.tsx
+++ src/domains/wallet/pages/WalletDetails/components/WalletVote/WalletVote.tsx
@@ -54,12 +54,8 @@ export const WalletVote = ({ votes, maxVotes, isLoading, onButtonClick }: Wallet
 						<div className="flex flex-col mr-4 font-semibold leading-snug text-theme-text">
 							<span className="mr-2">{t("WALLETS.PAGE_WALLET_DETAILS.VOTES.EMPTY_DESCRIPTION")}</span>
 
-							<div className="mr-auto">
-								<Link
-									to="https://guides.ark.dev/usage-guides/desktop-wallet-voting"
-									isExternal
-									showExternalIcon={false}
-								>
+							<div className="mr-auto mt-1">
+								<Link to="https://guides.ark.dev/usage-guides/desktop-wallet-voting" isExternal>
 									{t("COMMON.LEARN_MORE")}
 								</Link>
 							</div>
