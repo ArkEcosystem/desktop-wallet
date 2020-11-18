c4fa80fa1633c1193c0d7627fcc221235683e5df
goga-m <goga.mamoulasvili@gmail.com>
2020-11-18 14:17:38 +0200

    chore: update snapshots

--- src/domains/dashboard/utils.ts
+++ src/domains/dashboard/utils.ts
@@ -1,8 +1,8 @@
-import { Profile,ProfileSetting } from "@arkecosystem/platform-sdk-profiles";
+import { Profile, ProfileSetting } from "@arkecosystem/platform-sdk-profiles";
 import { uniq } from "@arkecosystem/utils";
 
 export const enableNetworkInDashboardFilters = (profile: Profile, networkId: string) => {
-	const config: any = profile.settings().get(ProfileSetting.DashboardConfiguration);
+	const config: any = profile.settings().get(ProfileSetting.DashboardConfiguration) || { selectedNetworkIds: [] };
 	config.selectedNetworkIds.push(networkId);
 	profile.settings().set(ProfileSetting.DashboardConfiguration, {
 		...config,
