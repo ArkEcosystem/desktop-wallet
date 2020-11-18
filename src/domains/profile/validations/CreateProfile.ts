546307f29d5f34326d0a746bf2052b5b920378a5
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2020-11-18 19:07:56 +0200

    feat: add validations in profile create (#3097)

---
+++ src/domains/profile/validations/CreateProfile.ts
@@ -0,0 +1,32 @@
+export const createProfile = (t: any) => ({
+	name: () => ({
+		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
+			field: t("SETTINGS.GENERAL.PERSONAL.NAME"),
+		}).toString(),
+		maxLength: {
+			value: 42,
+			message: t("COMMON.VALIDATION.MAX_LENGTH", {
+				field: t("SETTINGS.GENERAL.PERSONAL.NAME"),
+				maxLength: 42,
+			}),
+		},
+	}),
+	password: () => ({
+		minLength: {
+			value: 6,
+			message: t("COMMON.VALIDATION.MIN_LENGTH", {
+				field: t("SETTINGS.GENERAL.PERSONAL.PASSWORD"),
+				minLength: 6,
+			}),
+		},
+	}),
+	confirmPassword: (password: string) => ({
+		validate: (confirmPassword: string) =>
+			!!password && password !== confirmPassword ? t("COMMON.VALIDATION.PASSWORD_MISMATCH") : true,
+	}),
+	currency: () => ({
+		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
+			field: t("SETTINGS.GENERAL.PERSONAL.CURRENCY"),
+		}).toString(),
+	}),
+});
