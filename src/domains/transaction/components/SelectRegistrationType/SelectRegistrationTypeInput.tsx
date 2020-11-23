949113c05cc4244c5cff08048ee488aeeb6d68e4
brenopolanski <breno.polanski@gmail.com>
2020-11-23 08:56:46 -0300

    chore: rename RegistrationType to SelectRegistrationType

--- src/domains/transaction/components/SelectRegistrationType/SelectRegistrationTypeInput.tsx
+++ src/domains/transaction/components/SelectRegistrationType/SelectRegistrationTypeInput.tsx
@@ -9,24 +9,24 @@ type Props = {
 const TypeAhead = ({ value }: { value?: string }) => (
 	<InputAddon
 		as="span"
-		data-testid="RegistrationTypeInput__typeahead"
+		data-testid="SelectRegistrationTypeInput__typeahead"
 		className="py-3 font-normal border border-transparent opacity-50 pointer-events-none pl-15"
 	>
 		{value}
 	</InputAddon>
 );
 
-export const RegistrationTypeInput = React.forwardRef<HTMLInputElement, Props>(
+export const SelectRegistrationTypeInput = React.forwardRef<HTMLInputElement, Props>(
 	({ suggestion, ...props }: Props, ref) => (
-		<InputGroup data-testid="RegistrationTypeInput">
+		<InputGroup data-testid="SelectRegistrationTypeInput">
 			<InputAddonStart className="px-4">
 				{/* TODO: add type name */}
 				<Icon name="Product" width={22} height={22} />
 			</InputAddonStart>
 			{suggestion && <TypeAhead value={suggestion} />}
-			<Input data-testid="RegistrationTypeInput__input" ref={ref} className="pl-15" {...props} />
+			<Input data-testid="SelectRegistrationTypeInput__input" ref={ref} className="pl-15" {...props} />
 		</InputGroup>
 	),
 );
 
-RegistrationTypeInput.displayName = "RegistrationTypeInput";
+SelectRegistrationTypeInput.displayName = "SelectRegistrationTypeInput";
