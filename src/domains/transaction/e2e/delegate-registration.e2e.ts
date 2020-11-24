71b3c73bbf929ae540770dcb84d0a1e63ce5445a
brenopolanski <breno.polanski@gmail.com>
2020-11-23 22:50:32 -0300

    test: fix e2e tests

--- src/domains/transaction/e2e/delegate-registration.e2e.ts
+++ src/domains/transaction/e2e/delegate-registration.e2e.ts
@@ -44,8 +44,8 @@ test("should successfully submit delegate registration", async (t) => {
 	await t.click(Selector("[data-testid=SearchWalletListItem__select-2]"));
 
 	// Choose registration type & go to step 2
-	await t.click('[data-testid="SelectDropdownInput__input"]');
-	await t.click('[data-testid="select-list__toggle-option-3"]');
+	await t.click('[data-testid="SelectRegistrationTypeInput__input"]');
+	await t.click(Selector("#SendTransactionForm__registrationType-item-3"));
 	await t.click(Selector("button").withText(translations.COMMON.CONTINUE));
 
 	// Choose username
