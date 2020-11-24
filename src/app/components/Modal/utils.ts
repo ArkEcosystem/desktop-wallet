9954d5984c89f319610f168eaf7a92fe84d7f10e
Edgar Goetzendorff <hello@dated.fun>
2020-11-24 16:26:38 +0100

    refactor: adjust treshhold

--- src/app/components/Modal/utils.ts
+++ src/app/components/Modal/utils.ts
@@ -1,5 +1,5 @@
 export const modalOffsetClass = (itemHeight: number, windowHeight: number) => {
 	const padding = 80;
 
-	return itemHeight + padding > windowHeight ? "top-0 my-20" : "";
+	return itemHeight + 2 * padding > windowHeight ? "top-0 my-20" : "";
 };
