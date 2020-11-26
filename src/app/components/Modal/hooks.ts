5eebe34206cd9c871305af31a82646962ae54fd1
Edgar Goetzendorff <hello@dated.fun>
2020-11-26 01:12:32 +0100

    refactor: add custom scrollbars

--- src/app/components/Modal/hooks.ts
+++ src/app/components/Modal/hooks.ts
@@ -2,18 +2,14 @@ import { useCallback, useEffect } from "react";
 
 export const useModal = ({ isOpen, onClose }: { isOpen: boolean; onClose?: any }) => {
 	useEffect(() => {
-		document.body.style.width = "100vw";
-		document.body.style.overflowX = "hidden";
-		document.body.style.overflowY = "auto";
+		document.body.style.overflow = "overlay";
 
 		if (isOpen) {
 			document.body.style.overflow = "hidden";
-			document.body.style.paddingRight = "calc(100vw - 100%)";
 		}
 
 		return () => {
-			document.body.style.overflowY = "auto";
-			document.body.style.paddingRight = "0";
+			document.body.style.overflow = "overlay";
 			return;
 		};
 	}, [isOpen]);
