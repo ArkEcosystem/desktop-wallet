4df62d74c212efdac00946ef5dd7664bfe5c24fa
Brian Faust <hello@basecode.sh>
2020-11-30 11:49:18 +0200

    wip

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "./HttpClient";
 import { ToastService } from "./ToastService";
 
-export const httpClient = new HttpClient(10);
+export const httpClient = new HttpClient(30);
 export const toasts = new ToastService();
