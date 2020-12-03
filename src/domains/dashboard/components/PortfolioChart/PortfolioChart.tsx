4ed0fe1e54c88d663c7ab6319ec4219c0a4f07cd
goga-m <goga.mamoulasvili@gmail.com>
2020-12-03 11:11:56 +0200

    refactor: memoize `PortfolioChart` and move logic in hook

--- src/domains/dashboard/components/PortfolioChart/PortfolioChart.tsx
+++ src/domains/dashboard/components/PortfolioChart/PortfolioChart.tsx
@@ -1,73 +1,25 @@
 import { Profile } from "@arkecosystem/platform-sdk-profiles";
-import { sortByDesc } from "@arkecosystem/utils";
 import { Section } from "app/components/Layout";
 import { LineChart } from "app/components/LineChart";
-import { BarItem, PercentageBar } from "app/components/PercentageBar";
-import { useEnvironmentContext } from "app/contexts";
-import { useDashboardConfig } from "domains/dashboard/pages";
-import { getNetworkExtendedData } from "domains/network/helpers";
-import React, { useMemo } from "react";
+import {  PercentageBar } from "app/components/PercentageBar";
+import React, { memo } from "react";
 import { useTranslation } from "react-i18next";
 
-import { balances } from "../../data";
+import { usePortfolioData } from "./hooks";
 
-export const PortfolioChart = ({ profile }: { profile: Profile }) => {
-	const { env } = useEnvironmentContext();
+type PortfolioChartProps = {
+	profile: Profile;
+	isVisible?: boolean;
+	showChartAnimation?: boolean;
+};
+
+export const PortfolioChart = memo(({ profile, isVisible, showChartAnimation }: PortfolioChartProps) => {
 	const { t } = useTranslation();
+	const { percentages, balances, chartLines } = usePortfolioData({ profile });
 
-	const { getConfiguration } = useDashboardConfig({ profile });
-	const { showChartAnimation, showPortfolio } = getConfiguration();
-	console.log({ showChartAnimation, showPortfolio });
+	// TODO: Set dashboard config showChartAnimation to false when unmount
 
-	const chartLines = [
-		{
-			dataKey: "btc",
-			label: "BTC",
-			color: "warning-600",
-		},
-		{
-			dataKey: "usd",
-			label: "USD",
-			color: "success-600",
-		},
-	];
-
-	const availableNetworks = useMemo(() => env
-			.availableNetworks()
-			.filter((network) => network.isLive())
-			.map((network) => {
-				const extended = getNetworkExtendedData({ coin: network.coin(), network: network.id() });
-				return Object.assign(network, { extra: extended });
-			}), [env]);
-
-	const balancePerCoin = useMemo(() => profile.walletAggregate().balancePerCoin(), [profile]);
-
-	const portfolioPercentages = useMemo(() => {
-		const data: BarItem[] = [];
-
-		for (const coin of Object.keys(balancePerCoin)) {
-			for (const network of availableNetworks) {
-				if (network.extra && network.ticker() === coin) {
-					data.push({
-						color: network.extra.textClass.replace("text-theme-", ""),
-						label: coin,
-						percentage: Number(balancePerCoin[coin].percentage),
-					});
-				}
-			}
-		}
-
-		return sortByDesc([...data], "percentage");
-	}, [availableNetworks, balancePerCoin]);
-
-	if (!showPortfolio || !balances.length) return <></>;
-
-	// TODO: adjust
-	// useEffect(() => {
-	// 	if (!showChartAnimation) return;
-	//
-	// 	setDashboardConfiguration({ showChartAnimation: false });
-	// }, [showChartAnimation]);
+	if (!isVisible || !balances.length) return <></>;
 
 	return (
 		<Section>
@@ -84,14 +36,9 @@ export const PortfolioChart = ({ profile }: { profile: Profile }) => {
 				<>
 					<div className="pt-6 mb-2 border-b border-dashed border-theme-neutral-300 dark:border-theme-neutral-800" />
 
-					<PercentageBar
-						title={t("DASHBOARD.DASHBOARD_PAGE.CHART.PERCENTAGES_LABEL")}
-						data={portfolioPercentages}
-					/>
+					<PercentageBar title={t("DASHBOARD.DASHBOARD_PAGE.CHART.PERCENTAGES_LABEL")} data={percentages} />
 				</>
 			)}
 		</Section>
 	);
-};
-
-PortfolioChart.defaultProps = {};
+});
