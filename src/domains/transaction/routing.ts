8b72b987d5791fdd256e442b75a88e56808bf769
Brian Faust <hello@basecode.sh>
2020-12-17 07:07:46 +0200

    wip

--- src/domains/transaction/routing.ts
+++ src/domains/transaction/routing.ts
@@ -1,48 +1,10 @@
 import {
-	SendEntityRegistration,
-	SendEntityResignation,
-	SendEntityUpdate,
 	SendIpfs,
 	SendTransfer,
 	SendVote,
 } from "./pages";
 
 export const TransactionRoutes = [
-	{
-		path: "/profiles/:profileId/wallets/:walletId/send-entity-registration",
-		exact: true,
-		component: SendEntityRegistration,
-	},
-	{
-		path: "/profiles/:profileId/send-entity-registration",
-		exact: true,
-		component: SendEntityRegistration,
-	},
-	{
-		path: "/profiles/:profileId/wallets/:walletId/:registrationType/send-entity-registration",
-		exact: true,
-		component: SendEntityRegistration,
-	},
-	{
-		path: "/profiles/:profileId/wallets/:walletId/send-entity-update",
-		exact: true,
-		component: SendEntityUpdate,
-	},
-	{
-		path: "/profiles/:profileId/wallets/:walletId/send-entity-resignation",
-		exact: true,
-		component: SendEntityResignation,
-	},
-	{
-		path: "/profiles/:profileId/wallets/:walletId/transactions/:transactionId/send-entity-update",
-		exact: true,
-		component: SendEntityUpdate,
-	},
-	{
-		path: "/profiles/:profileId/wallets/:walletId/transactions/:transactionId/send-entity-resignation",
-		exact: true,
-		component: SendEntityResignation,
-	},
 	{
 		path: "/profiles/:profileId/wallets/:walletId/send-transfer",
 		exact: true,
