250db9ac90bac862ee499766551bfc9d8594ca03
Lucio Rubens <luciorubeens@users.noreply.github.com>
2021-01-12 15:29:26 -0300

    feat: implement plugin configuration

---
+++ src/plugins/configuration/plugin-configuration.test.ts
@@ -0,0 +1,136 @@
+import path from "path";
+
+import { PluginConfigurationData } from "./plugin-configuration";
+
+describe("Plugin Configuration", () => {
+	it("should validate successful", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test" });
+		expect(subject.validate()).toBeTruthy();
+	});
+
+	it("should fail to validate", () => {
+		const subject = PluginConfigurationData.make({ version: 1 });
+		expect(() => subject.validate()).toThrowError();
+	});
+
+	it("should format title with scope", () => {
+		const subject = PluginConfigurationData.make({ name: "@arkecosystem/plugin-test" });
+		expect(subject.title()).toBe("Plugin Test");
+	});
+
+	it("should format title", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test" });
+		expect(subject.title()).toBe("Plugin Test");
+	});
+
+	it("should format a custom title", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test", "desktop-wallet": { title: "My Plugin" } });
+		expect(subject.title()).toBe("My Plugin");
+	});
+
+	it("should format id", () => {
+		const subject1 = PluginConfigurationData.make({ name: "@arkecosystem/plugin-test" });
+		expect(subject1.id()).toBe(2969335244);
+
+		const subject2 = PluginConfigurationData.make({ name: "@arkecosystem/plugin-explorer" });
+		expect(subject2.id()).toBe(3073367889);
+	});
+
+	it("should return author if official scope", () => {
+		const subject = PluginConfigurationData.make({ name: "@arkecosystem/plugin-test" });
+		expect(subject.author()).toBe("ARK Ecosystem");
+	});
+
+	it("should return unknown author if not defined", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test" });
+		expect(subject.author()).toBe("Unknown");
+	});
+
+	it("should return author", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test", author: "Jhon" });
+		expect(subject.author()).toBe("Jhon");
+	});
+
+	it("should return author from contributors", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test", contributors: [{ name: "Jhon" }] });
+		expect(subject.author()).toBe("Jhon");
+	});
+
+	it("should return default version", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test" });
+		expect(subject.version()).toBe("0.0.0");
+	});
+
+	it("should return keywords", () => {
+		const subject = PluginConfigurationData.make({
+			name: "plugin-test",
+			keywords: ["desktop", "plugin", "desktop-wallet"],
+		});
+		expect(subject.keywords()).toEqual(["Desktop", "Plugin", "Desktop Wallet"]);
+	});
+
+	it("should return permissions", () => {
+		const subject = PluginConfigurationData.make({
+			name: "plugin-test",
+			"desktop-wallet": { permissions: ["LAUNCH", "HTTP"] },
+		});
+		expect(subject.permissions()).toEqual(["LAUNCH", "HTTP"]);
+	});
+
+	it("should return urls", () => {
+		const subject = PluginConfigurationData.make({
+			name: "plugin-test",
+			"desktop-wallet": { urls: ["http://github.com"] },
+		});
+		expect(subject.urls()).toEqual(["http://github.com"]);
+	});
+
+	it("should return default category if not defined", () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test" });
+		expect(subject.categories()).toEqual(["other"]);
+	});
+
+	it("should return valid categories", () => {
+		const subject = PluginConfigurationData.make({
+			name: "plugin-test",
+			"desktop-wallet": { categories: ["exchange", "finance"] },
+		});
+		expect(subject.categories()).toEqual(["exchange"]);
+	});
+
+	it("should return plugin size from fs", async () => {
+		const subject = PluginConfigurationData.make(
+			{ name: "plugin-test" },
+			path.resolve("src/tests/fixtures/plugins/plugin-test-custom-buttom"),
+		);
+		await new Promise((r) => setTimeout(r, 100));
+		expect(subject.size()).toBe("0 B");
+	});
+
+	it("should return plugin size from package", async () => {
+		const subject = PluginConfigurationData.make({ name: "plugin-test", dist: { unpackedSize: 15000 } });
+		await subject.syncSize();
+		expect(subject.size()).toBe("15 kB");
+	});
+
+	it("should return logo", () => {
+		const subject = PluginConfigurationData.make({
+			name: "plugin-test",
+			"desktop-wallet": {
+				logo:
+					"https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/sound-notifications/master/logo.png",
+			},
+		});
+		expect(subject.logo()).toBe(
+			"https://raw.githubusercontent.com/ark-ecosystem-desktop-plugins/sound-notifications/master/logo.png",
+		);
+	});
+
+	it("should not return invalid logo", () => {
+		const subject = PluginConfigurationData.make({
+			name: "plugin-test",
+			"desktop-wallet": { logo: "https://google.com" },
+		});
+		expect(subject.logo()).toBeUndefined();
+	});
+});
