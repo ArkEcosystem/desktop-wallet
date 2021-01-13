f6124cab45d45dd6993e1c7d99a4e2eb24e5aafa
Edgar Goetzendorff <hello@dated.fun>
2021-01-13 22:19:49 +0100

    refactor: adjust styles of BlockfolioAd

--- src/domains/news/components/BlockfolioAd/BlockfolioAd.tsx
+++ src/domains/news/components/BlockfolioAd/BlockfolioAd.tsx
@@ -12,14 +12,16 @@ export const BlockfolioAd = () => {
 
 	return (
 		<div className="relative">
-			<div className="flex absolute top-0 right-4 text-center rounded-b-xl bg-theme-primary-contrast">
+			<div className="flex absolute top-0 right-4 text-center rounded-b-xl bg-theme-primary-100 dark:bg-theme-neutral-800">
 				<span className="px-2 pt-1 pb-2 text-xs font-semibold text-theme-primary">{t("NEWS.AD")}</span>
 			</div>
+
 			<img
 				src={BlockfolioBanner}
-				className="w-full rounded-lg border-2 border-theme-primary-contrast"
+				className="w-full rounded-lg border-2 border-theme-primary-100 dark:border-theme-neutral-800"
 				alt="Blockfolio Banner"
 			/>
+
 			<div className="absolute left-10 top-24">
 				<div className="flex flex-col space-y-5 text-theme-background">
 					<Link to="https://blockfolio.com/" showExternalIcon={false} isExternal>
@@ -29,6 +31,7 @@ export const BlockfolioAd = () => {
 						{"The world's most popular Bitcoin & cryptocurrency portfolio tracker. 100% free."}
 					</p>
 				</div>
+
 				<div className="flex mt-12 space-x-2">
 					<Link
 						to="https://itunes.apple.com/us/app/blockfolio-bitcoin-altcoin/id1095564685?mt=8"
@@ -37,6 +40,7 @@ export const BlockfolioAd = () => {
 					>
 						<DownloadAppStoreButton className="w-32 h-10 b-0" />
 					</Link>
+
 					<Link
 						to="https://play.google.com/store/apps/details?id=com.blockfolio.blockfolio&amp;hl=en_US"
 						showExternalIcon={false}
