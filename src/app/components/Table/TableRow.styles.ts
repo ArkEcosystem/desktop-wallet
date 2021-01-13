99a8143fcbe879d7e468063cd2918823bce19b9f
Edgar Goetzendorff <hello@dated.fun>
2021-01-13 20:08:56 +0100

    refactor: move props from TableCell to TableRow

--- src/app/components/Table/TableRow.styles.ts
+++ src/app/components/Table/TableRow.styles.ts
@@ -1,16 +1,29 @@
-import tw from "twin.macro";
+import tw, { css } from "twin.macro";
 
 import { TableRowFunction } from "./TableRow";
 
-const baseStyle = [tw`transition-colors duration-100`];
+const baseStyle = tw`transition-colors duration-100`;
 
 const getCursorStyles = (onClick?: TableRowFunction): any => (onClick ? tw`cursor-pointer` : "");
 
 const getBorderStyles = (border?: boolean): any =>
 	border ? tw`border-b last:border-b-0 border-dashed border-theme-neutral-300 dark:border-theme-neutral-800` : "";
 
-export const getStyles = ({ border, onClick }: any) => [
-	...baseStyle,
-	getBorderStyles(border),
-	getCursorStyles(onClick),
-];
+const getHoverStyles = (isSelected?: boolean): any =>
+	css`
+		&:hover td > div {
+			${isSelected
+				? tw`bg-theme-success-100 dark:bg-theme-success-900`
+				: tw`bg-theme-neutral-100 dark:bg-theme-neutral-800`}
+		}
+	`;
+
+export const getStyles = ({ onClick, border, isSelected }: any) => {
+	const styles = [baseStyle, getBorderStyles(border), getCursorStyles(onClick)];
+
+	if (onClick) {
+		styles.push(getHoverStyles(isSelected));
+	}
+
+	return styles;
+};
