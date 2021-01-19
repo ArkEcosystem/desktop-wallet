69d57cd686d9720b26d1f56d00be17ac6af1455d
goga-m <goga.mamoulasvili@gmail.com>
2021-01-19 13:25:27 +0200

    fix: scroll to top on pagination change in delegates table

--- src/domains/vote/components/DelegateTable/DelegateTable.tsx
+++ src/domains/vote/components/DelegateTable/DelegateTable.tsx
@@ -164,6 +164,7 @@ export const DelegateTable = ({
 
 	const handleSelectPage = (page: number) => {
 		setCurrentPage(page);
+		window.scrollTo({ top: 0, behavior: "smooth" });
 	};
 
 	const paginator = (items: ReadOnlyWallet[], currentPage: number, itemsPerPage: number) => {
