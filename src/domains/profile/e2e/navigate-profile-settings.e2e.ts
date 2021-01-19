84971af339bd8a2dbd5b870136622e15b408e564
dated <dated@users.noreply.github.com>
2021-01-19 10:24:08 +0000

    style: resolve style guide violations

--- src/domains/profile/e2e/navigate-profile-settings.e2e.ts
+++ src/domains/profile/e2e/navigate-profile-settings.e2e.ts
@@ -10,9 +10,7 @@ createFixture(`Welcome Page Profile Card`);
 test("should navigate to profile setting from profile card menu", async (t) => {
 	await t.click(Selector('[data-testid="Card"] [data-testid="dropdown__toggle"]').child(0));
 	await t.click(
-		Selector('[data-testid="Card"] [data-testid="dropdown__option--0"]').withText(
-			translations.COMMON.SETTINGS,
-		),
+		Selector('[data-testid="Card"] [data-testid="dropdown__option--0"]').withText(translations.COMMON.SETTINGS),
 	);
 	await t.expect(Selector("h1").withText(translations.SETTINGS.GENERAL.TITLE).exists).ok();
 });
