975131b4a104a84d821f0e58e58a4e7dd75cac6f
Edgar Goetzendorff <hello@dated.fun>
2021-01-22 05:04:11 +0100

    feat: reposition and resize dropdowns based on window size (#3204)

--- src/app/components/Dropdown/Dropdown.test.tsx
+++ src/app/components/Dropdown/Dropdown.test.tsx
@@ -47,6 +47,7 @@ describe("Dropdown", () => {
 		act(() => {
 			fireEvent.click(toggle);
 		});
+
 		expect(getByTestId("dropdown__content")).toBeTruthy();
 	});
 
@@ -183,6 +184,88 @@ describe("ClickOutside Hook", () => {
 
 		act(() => {
 			fireEvent.mouseDown(document);
+		});
+	});
+
+	describe("positioning", () => {
+		it("it should render content below toggle", () => {
+			const documentClientHeightSpy = jest.spyOn(document.body, "clientHeight", "get").mockReturnValue(50);
+			const getComputedStyleSpy = jest
+				.spyOn(window, "getComputedStyle")
+				.mockReturnValueOnce({ marginTop: "10px" });
+
+			const { getByTestId } = render(
+				<Dropdown>
+					<span>hello</span>
+				</Dropdown>,
+			);
+			const toggle = getByTestId("dropdown__toggle");
+
+			act(() => {
+				fireEvent.click(toggle);
+			});
+
+			expect(getByTestId("dropdown__content")).toHaveAttribute("style", "opacity: 100;");
+
+			documentClientHeightSpy.mockRestore();
+			getComputedStyleSpy.mockRestore();
+		});
+
+		it("should render content below toggle and reduce its height", () => {
+			const getBoundingClientRectSpy = jest
+				.spyOn(Element.prototype, "getBoundingClientRect")
+				.mockReturnValue({ height: 90, top: 0 });
+			const toggleHeightSpy = jest.spyOn(HTMLElement.prototype, "offsetHeight", "get").mockReturnValueOnce(10);
+			const dropdownHeightSpy = jest.spyOn(HTMLElement.prototype, "offsetHeight", "get").mockReturnValue(100);
+			const documentClientHeightSpy = jest.spyOn(document.body, "clientHeight", "get").mockReturnValue(100);
+			const elementClientHeightSpy = jest.spyOn(Element.prototype, "clientHeight", "get").mockReturnValue(100);
+
+			const { getByTestId } = render(
+				<Dropdown>
+					<span>hello</span>
+				</Dropdown>,
+			);
+			const toggle = getByTestId("dropdown__toggle");
+
+			act(() => {
+				fireEvent.click(toggle);
+			});
+
+			expect(getByTestId("dropdown__content")).toHaveAttribute(
+				"style",
+				"opacity: 100; height: 60px; overflow-y: scroll;",
+			);
+
+			getBoundingClientRectSpy.mockRestore();
+			toggleHeightSpy.mockRestore();
+			dropdownHeightSpy.mockRestore();
+			documentClientHeightSpy.mockRestore();
+			elementClientHeightSpy.mockRestore();
+		});
+
+		it("should render content above toggle and apply a negative margin", () => {
+			const getBoundingClientRectSpy = jest
+				.spyOn(Element.prototype, "getBoundingClientRect")
+				.mockReturnValue({ height: 50, top: 100 });
+			const offsetHeightSpy = jest.spyOn(HTMLElement.prototype, "offsetHeight", "get").mockReturnValue(50);
+			const documentClientHeightSpy = jest.spyOn(document.body, "clientHeight", "get").mockReturnValue(150);
+
+			const { getByTestId } = render(
+				<Dropdown>
+					<span>hello</span>
+				</Dropdown>,
+			);
+			const toggle = getByTestId("dropdown__toggle");
+
+			act(() => {
+				fireEvent.click(toggle);
+			});
+
+			expect(getByTestId("dropdown__content")).toHaveAttribute("style", "opacity: 100; margin-top: -100px;");
+
+			getBoundingClientRectSpy.mockRestore();
+			offsetHeightSpy.mockRestore();
+			documentClientHeightSpy.mockRestore();
 		});
 	});
 });
