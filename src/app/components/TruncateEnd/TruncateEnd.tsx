448887a9776b3193b7b5e4e20160f9b16a95beae
goga-m <goga.mamoulasvili@gmail.com>
2021-01-25 17:31:06 +0200

    feat: add `TruncateEnd` component

---
+++ src/app/components/TruncateEnd/TruncateEnd.tsx
@@ -0,0 +1,37 @@
+import { Tooltip } from "app/components/Tooltip";
+import React from "react";
+import { styled } from "twin.macro";
+
+type Props = {
+	text: string;
+	maxChars?: number;
+	as?: React.ElementType;
+	showTooltip?: boolean;
+} & React.HTMLProps<any>;
+
+const Wrapper = styled.span``;
+
+export const TruncateEnd = ({ text, maxChars, showTooltip, ...props }: Props) => {
+	const result = React.useMemo(() => {
+		if (!maxChars || text.length <= maxChars) {
+			return text;
+		}
+
+		const start = text.substr(0, maxChars);
+
+		return `${start} …`;
+	}, [maxChars, text]);
+
+	return (
+		<Tooltip content={text} disabled={!showTooltip}>
+			<Wrapper data-testid="TruncateEnd" {...props}>
+				{result}
+			</Wrapper>
+		</Tooltip>
+	);
+};
+
+TruncateEnd.defaultProps = {
+	maxChars: 16,
+	showTooltip: true,
+};
