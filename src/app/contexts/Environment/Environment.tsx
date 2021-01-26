dbe3fedf970c2c0c5442be13e2b2dc7402fbda9d
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-01-26 17:39:22 +0200

    refactor: save active profile before persist (#3247)

--- src/app/contexts/Environment/Environment.tsx
+++ src/app/contexts/Environment/Environment.tsx
@@ -1,5 +1,7 @@
 import { Environment } from "@arkecosystem/platform-sdk-profiles";
+import { useProfileUtils } from "app/hooks/use-profile-synchronizer";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 type Context = { env: Environment; state?: Record<string, unknown>; persist: () => Promise<void> };
 
@@ -14,15 +16,27 @@ export const EnvironmentProvider = ({ children, env }: Props) => {
 	const isDemo = process.env.REACT_APP_BUILD_MODE === "demo";
 	const [state, setState] = React.useState<any>(undefined);
 
+	const history = useHistory();
+	const { getProfileFromUrl } = useProfileUtils(env);
+
 	const persist = React.useCallback(async () => {
-		// e2e ci tests hang when persist is called
-		if (!isDemo) {
-			await env.persist();
+		if (isDemo) {
+			// prevent from persisting in demo. e2e tests hang.
+			setState({});
+			return;
 		}
+
+		const activeProfile = getProfileFromUrl(history?.location?.pathname);
+
+		if (activeProfile) {
+			activeProfile.save();
+		}
+
+		await env.persist();
 
 		// Force update
 		setState({});
-	}, [env, isDemo]);
+	}, [env, isDemo, getProfileFromUrl, history]);
 
 	return (
 		<EnvironmentContext.Provider value={{ env, state, persist } as Context}>{children}</EnvironmentContext.Provider>
