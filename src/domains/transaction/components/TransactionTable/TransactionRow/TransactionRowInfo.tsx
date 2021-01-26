78fdf9c2606b690cd5b4ef6a1c9ee5e99a85e91b
Edgar Goetzendorff <hello@dated.fun>
2021-01-26 03:04:25 +0100

    chore: use i18n keys in `TransactionRowInfo` (#3244)

--- src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowInfo.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowInfo.tsx
@@ -2,6 +2,7 @@ import { ExtendedTransactionData } from "@arkecosystem/platform-sdk-profiles";
 import { Icon } from "app/components/Icon";
 import { Tooltip } from "app/components/Tooltip";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 type Props = {
 	memo?: string;
@@ -9,39 +10,37 @@ type Props = {
 	isLedger?: boolean;
 };
 
-const VendorField = ({ vendorField }: { vendorField: string | undefined }) => (
-	<Tooltip className="break-all" content={vendorField}>
-		<span className="p-1">
-			<Icon data-testid="TransactionRowInfo__vendorField" name="Smartbridge" width={17} height={16} />
-		</span>
-	</Tooltip>
-);
+export const BaseTransactionRowInfo = ({ memo, isMultiSignature, isLedger }: Props) => {
+	const { t } = useTranslation();
 
-const Ledger = () => (
-	// TODO: i18n
-	<Tooltip content="Ledger">
-		<span className="p-1">
-			<Icon data-testid="TransactionRowInfo__ledger" name="Ledger" />
-		</span>
-	</Tooltip>
-);
+	return (
+		<div data-testid="TransactionRowInfo" className="inline-flex space-x-1 align-middle">
+			{isLedger && (
+				<Tooltip content={t("COMMON.LEDGER")}>
+					<span className="p-1">
+						<Icon data-testid="TransactionRowInfo__ledger" name="Ledger" />
+					</span>
+				</Tooltip>
+			)}
 
-const MultiSignature = () => (
-	// TODO: i18n
-	<Tooltip content="MultiSignature">
-		<span className="p-1">
-			<Icon data-testid="TransactionRowInfo__multiSignature" name="Multisig" width={22} height={14} />
-		</span>
-	</Tooltip>
-);
+			{isMultiSignature && (
+				<Tooltip content={t("COMMON.MULTISIGNATURE")}>
+					<span className="p-1">
+						<Icon data-testid="TransactionRowInfo__multiSignature" name="Multisig" width={22} height={14} />
+					</span>
+				</Tooltip>
+			)}
 
-export const BaseTransactionRowInfo = ({ memo, isMultiSignature, isLedger }: Props) => (
-	<div data-testid="TransactionRowInfo" className="inline-flex space-x-1 align-middle">
-		{isLedger && <Ledger />}
-		{isMultiSignature && <MultiSignature />}
-		{memo && <VendorField vendorField={memo} />}
-	</div>
-);
+			{memo && (
+				<Tooltip className="break-all" content={memo}>
+					<span className="p-1">
+						<Icon data-testid="TransactionRowInfo__vendorField" name="Smartbridge" width={17} height={16} />
+					</span>
+				</Tooltip>
+			)}
+		</div>
+	);
+};
 
 export const TransactionRowInfo = ({ transaction }: { transaction: ExtendedTransactionData }) => (
 	<BaseTransactionRowInfo
