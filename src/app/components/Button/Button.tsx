1cdff408942adc117c478d384f4e42a1a68f82e4
goga-m <goga.mamoulasvili@gmail.com>
2021-01-27 16:35:04 +0200

    refactor: add icon and isLoading props in Button

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "app/components//Icon";
+import { Spinner } from "app/components/Spinner";
 import { pluginManager } from "app/PluginProviders";
 import { withThemeDecorator } from "plugins";
 import React from "react";
@@ -9,13 +11,33 @@ import { getStyles } from "./Button.styles";
 type ButtonProps = {
 	variant?: ButtonVariant;
 	size?: Size;
+	isLoading?: boolean;
+	icon?: string;
+	iconWidth?: number | string;
+	iconHeight?: number | string;
 } & React.ButtonHTMLAttributes<any>;
 
 const StyledButton = styled.button<ButtonProps>(getStyles);
 
-export const OriginalButton = React.forwardRef<HTMLButtonElement, ButtonProps>((props: ButtonProps, ref) => (
-	<StyledButton {...props} ref={ref} />
-));
+export const OriginalButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+	({ children, icon, isLoading, iconWidth = 14, iconHeight = 14, ...props }: ButtonProps, ref) => (
+		<StyledButton {...props} ref={ref}>
+			<div className="flex items-center space-x-2 relative">
+				{icon && <Icon name={icon} width={iconWidth} height={iconHeight} />}
+
+				{!isLoading && <>{children}</>}
+
+				{isLoading && (
+					<>
+						<span className="invisible">{children}</span>
+
+						<Spinner size="sm" className="absolute right-0 top-0 bottom-0" />
+					</>
+				)}
+			</div>
+		</StyledButton>
+	),
+);
 
 OriginalButton.defaultProps = {
 	type: "button",
