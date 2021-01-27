537cc09cb726d4c86d4d1383038c59ce4b58b93c
goga-m <goga.mamoulasvili@gmail.com>
2021-01-27 18:01:30 +0200

    fix: adjust spacing for spinner

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -31,7 +31,9 @@ export const OriginalButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
 					<>
 						<span className="invisible">{children}</span>
 
-						<Spinner size="sm" className="absolute right-0 top-0 bottom-0" />
+						<div className="absolute top-0 right-0 bottom-0 left-0">
+							<Spinner size="sm" className="m-auto" />
+						</div>
 					</>
 				)}
 			</div>
