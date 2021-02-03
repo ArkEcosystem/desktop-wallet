66238013ae87346924ef47abc1c5cbe95a7e899f
goga-m <goga.mamoulasvili@gmail.com>
2021-02-03 17:21:00 +0200

    refactor: move loading state in `Wallets`

--- src/domains/dashboard/pages/Dashboard/Dashboard.tsx
+++ src/domains/dashboard/pages/Dashboard/Dashboard.tsx
@@ -25,7 +25,7 @@ export const Dashboard = () => {
 				<Wallets
 					title={t("COMMON.WALLETS")}
 					walletsCount={profileWalletsCount}
-					isLoading={profileIsSyncing && profileWalletsCount === 0}
+					isLoading={profileIsSyncing}
 					onCreateWallet={() => history.push(`/profiles/${activeProfile.id()}/wallets/create`)}
 					onImportWallet={() => history.push(`/profiles/${activeProfile.id()}/wallets/import`)}
 					onImportLedgerWallet={() =>
