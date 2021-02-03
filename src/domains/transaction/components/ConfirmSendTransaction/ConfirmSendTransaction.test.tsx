bd5395c97ff67964e9abf457ad9e63966f0ca009
goga-m <goga-m@users.noreply.github.com>
2021-02-03 17:31:33 +0000

    style: resolve style guide violations

--- src/domains/transaction/components/ConfirmSendTransaction/ConfirmSendTransaction.test.tsx
+++ src/domains/transaction/components/ConfirmSendTransaction/ConfirmSendTransaction.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { act,fireEvent, render } from "utils/testing-library";
+import { act, fireEvent, render } from "utils/testing-library";
 
 import { ConfirmSendTransaction } from "./ConfirmSendTransaction";
 
