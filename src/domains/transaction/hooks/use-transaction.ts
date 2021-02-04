bcad14a50a7de415cc049a4a0beb3be89c8c5b07
goga-m <goga.mamoulasvili@gmail.com>
2021-02-04 15:22:18 +0200

    feat: create `useTransaction` hook

---
+++ src/domains/transaction/hooks/use-transaction.ts
@@ -0,0 +1,16 @@
+import { ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
+import { useCallback, useMemo } from "react";
+
+export const useTransaction = () => {
+	const fetchWalletUnconfirmedTransactions = useCallback(async (wallet: ReadWriteWallet) => {
+		const sent = await wallet.sentTransactions({ limit: 20, cursor: 1 });
+		return sent.items().filter((transaction) => !transaction.isConfirmed());
+	}, []);
+
+	return useMemo(
+		() => ({
+			fetchWalletUnconfirmedTransactions,
+		}),
+		[fetchWalletUnconfirmedTransactions],
+	);
+};
