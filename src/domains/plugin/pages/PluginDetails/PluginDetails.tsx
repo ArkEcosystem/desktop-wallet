821fd3727bab9a9dfd5e7fbfd0b95b1e37e0fe2e
Brian Faust <faustbrian@users.noreply.github.com>
2021-02-04 12:13:59 +0200

    chore: remove plugin review section (#3318)

--- src/domains/plugin/pages/PluginDetails/PluginDetails.tsx
+++ src/domains/plugin/pages/PluginDetails/PluginDetails.tsx
@@ -1,28 +1,14 @@
-import { Button } from "app/components/Button";
 import { Page, Section } from "app/components/Layout";
 import { useActiveProfile, useQueryParams } from "app/hooks";
-import { Comments } from "domains/plugin/components/Comments";
 import { PluginHeader } from "domains/plugin/components/PluginHeader";
 import { PluginInfo } from "domains/plugin/components/PluginInfo";
 import { PluginUninstallConfirmation } from "domains/plugin/components/PluginUninstallConfirmation/PluginUninstallConfirmation";
-import { ReviewBox } from "domains/plugin/components/ReviewBox";
 import { usePluginManagerContext } from "plugins";
 import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 
-import { reviewData } from "../../data";
-
-const commentsSortOptions = {
-	type: "Best",
-	direction: "asc",
-};
-
-type PluginDetailsProps = {
-	reviewData?: any;
-};
-
-export const PluginDetails = ({ reviewData }: PluginDetailsProps) => {
+export const PluginDetails = () => {
 	const activeProfile = useActiveProfile();
 	const queryParams = useQueryParams();
 	const history = useHistory();
@@ -55,7 +41,6 @@ export const PluginDetails = ({ reviewData }: PluginDetailsProps) => {
 	}, [isInstalled, packageConfiguration, latestConfiguration]);
 
 	const pluginData = plugin?.toObject() || ({} as any);
-	const { comments, ratings, totalAvaliations } = reviewData;
 
 	const crumbs = [
 		{
@@ -81,30 +66,6 @@ export const PluginDetails = ({ reviewData }: PluginDetailsProps) => {
 				<PluginInfo {...pluginData} isInstalled={isInstalled} />
 			</Section>
 
-			<Section>
-				<div className="flex">
-					<div className="flex flex-col">
-						<div className="flex justify-between items-center">
-							<h2 className="mb-0">Reviews ARK Explorer</h2>
-							<Button data-testid="ReviewBox__button--comment" variant="secondary">
-								Leave a comment
-							</Button>
-						</div>
-
-						<div className="col-span-2" data-testid="plugin-details__comments">
-							<Comments comments={comments} sortOptions={commentsSortOptions} />
-						</div>
-					</div>
-
-					<div
-						className="p-8 mb-auto ml-32 rounded-xl border-2 border-theme-secondary-300 dark:border-theme-secondary-800"
-						data-testid="plugin-details__review-box"
-					>
-						<ReviewBox averageScore={""} ratings={ratings} totalAvaliations={totalAvaliations} />
-					</div>
-				</div>
-			</Section>
-
 			{pluginCtrl && (
 				<PluginUninstallConfirmation
 					isOpen={isUninstallOpen}
@@ -116,8 +77,4 @@ export const PluginDetails = ({ reviewData }: PluginDetailsProps) => {
 			)}
 		</Page>
 	);
-};
-
-PluginDetails.defaultProps = {
-	reviewData,
 };
