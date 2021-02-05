a825fbb3d20b5a2d46f78d79e25141f9baadf234
goga-m <goga.mamoulasvili@gmail.com>
2021-02-05 16:03:09 +0200

    feat: create new `UnconfirmedTransactionTable`

---
+++ src/domains/transaction/components/TransactionTable/UnconfirmedTransactionTable/UnconfirmedTransactionRow.tsx
@@ -0,0 +1,44 @@
+import { ExtendedTransactionData } from "@arkecosystem/platform-sdk-profiles";
+import { TableCell, TableRow } from "app/components/Table";
+import { TimeAgo } from "app/components/TimeAgo";
+import React from "react";
+import { Size } from "types";
+import { shouldUseDarkColors } from "utils/electron-utils";
+
+import { TransactionRowAmount } from "../TransactionRow/TransactionRowAmount";
+import { TransactionRowMode } from "../TransactionRow/TransactionRowMode";
+import { TransactionRowRecipientLabel } from "../TransactionRow/TransactionRowRecipientLabel";
+
+type Props = {
+	transaction: ExtendedTransactionData;
+	walletName?: string;
+	iconSize?: Size;
+} & React.HTMLProps<any>;
+
+export const UnconfirmedTransactionRow = ({ transaction, walletName, iconSize, ...props }: Props) => {
+	const [shadowColor, setShadowColor] = React.useState<string>("--theme-background-color");
+
+	return (
+		<TableRow
+			onMouseEnter={() =>
+				setShadowColor(shouldUseDarkColors() ? "--theme-color-secondary-800" : "--theme-color-secondary-100")
+			}
+			onMouseLeave={() => setShadowColor("")}
+			{...props}
+		>
+			<TableCell variant="start" innerClassName="space-x-3" isCompact>
+				<TimeAgo date={transaction.timestamp()?.toString() as string} />
+				<TransactionRowRecipientLabel transaction={transaction} walletName={walletName} />
+			</TableCell>
+
+			<TableCell variant="start" innerClassName="space-x-3" isCompact>
+				<TransactionRowMode transaction={transaction} circleShadowColor={shadowColor} iconSize="sm" />
+				<TransactionRowRecipientLabel transaction={transaction} walletName={walletName} />
+			</TableCell>
+
+			<TableCell variant="end" innerClassName="justify-end" isCompact>
+				<TransactionRowAmount transaction={transaction} />
+			</TableCell>
+		</TableRow>
+	);
+};
---
+++ src/domains/transaction/components/TransactionTable/UnconfirmedTransactionTable/UnconfirmedTransactionTable.tsx
@@ -0,0 +1,42 @@
+import { ExtendedTransactionData } from "@arkecosystem/platform-sdk-profiles";
+import { Table } from "app/components/Table";
+import React, { memo } from "react";
+import { useTranslation } from "react-i18next";
+
+import { UnconfirmedTransactionRow } from "./UnconfirmedTransactionRow";
+
+type Props = {
+	transactions: ExtendedTransactionData[];
+};
+
+export const UnconfirmedTransactionTable = memo(({ transactions }: Props) => {
+	const { t } = useTranslation();
+
+	const columns: any = [
+		{
+			Header: t("COMMON.DATE"),
+			accessor: (transaction: ExtendedTransactionData) => transaction.timestamp?.()?.toUNIX(),
+			sortDescFirst: true,
+			cellWidth: "w-50",
+		},
+		{
+			Header: t("COMMON.RECIPIENT"),
+			cellWidth: "w-96",
+		},
+		{
+			Header: t("COMMON.AMOUNT"),
+			accessor: (transaction: ExtendedTransactionData) => transaction.total?.().toHuman(),
+			className: "justify-end",
+		},
+	];
+
+	return (
+		<div data-testid="TransactionTable" className="relative">
+			<Table columns={columns} data={transactions}>
+				{(row: ExtendedTransactionData) => (
+					<UnconfirmedTransactionRow onClick={console.log} transaction={row} />
+				)}
+			</Table>
+		</div>
+	);
+});
