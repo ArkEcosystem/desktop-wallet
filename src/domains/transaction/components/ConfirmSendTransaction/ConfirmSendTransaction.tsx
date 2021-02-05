581242188f7aaf88e217160fde150fc1254985e1
goga-m <goga.mamoulasvili@gmail.com>
2021-02-05 16:03:45 +0200

    refactor: integrate `UnconfirmedTransactionTable` in confirm modal

--- src/domains/transaction/components/ConfirmSendTransaction/ConfirmSendTransaction.tsx
+++ src/domains/transaction/components/ConfirmSendTransaction/ConfirmSendTransaction.tsx
@@ -1,34 +1,42 @@
+import { ExtendedTransactionData } from "@arkecosystem/platform-sdk-profiles";
 import { Button } from "app/components/Button";
-import { Image } from "app/components/Image";
 import { Modal } from "app/components/Modal";
 import React from "react";
 import { useTranslation } from "react-i18next";
+
+import { UnconfirmedTransactionTable } from "../TransactionTable/UnconfirmedTransactionTable/UnconfirmedTransactionTable";
 
 type ConfirmSendTransactionProps = {
 	isOpen: boolean;
 	onClose?: any;
 	onConfirm?: any;
+	unconfirmedTransactions: ExtendedTransactionData[];
 };
 
-// TODO: Update modal inner content when designs are ready
-export const ConfirmSendTransaction = ({ isOpen, onClose, onConfirm }: ConfirmSendTransactionProps) => {
+export const ConfirmSendTransaction = ({
+	isOpen,
+	onClose,
+	onConfirm,
+	unconfirmedTransactions,
+}: ConfirmSendTransactionProps) => {
 	const { t } = useTranslation();
 
 	return (
 		<Modal
 			title={t("TRANSACTION.MODAL_CONFIRM_SEND_TRANSACTION.TITLE")}
-			image={<Image name="WalletUpdateBanner" domain="wallet" className="my-8" />}
 			description={t("TRANSACTION.MODAL_CONFIRM_SEND_TRANSACTION.DESCRIPTION")}
 			isOpen={isOpen}
 			onClose={onClose}
 		>
+			<UnconfirmedTransactionTable transactions={unconfirmedTransactions} />
+
 			<div className="flex justify-end mt-8 space-x-3">
 				<Button variant="secondary" onClick={onClose} data-testid="ConfirmSendTransaction__cancel">
-					{t("COMMON.CANCEL")}
+					{t("COMMON.BACK")}
 				</Button>
 
 				<Button type="submit" data-testid="ConfirmSendTransaction__confirm" onClick={onConfirm}>
-					{t("COMMON.CONFIRM")}
+					{t("COMMON.CONTINUE")}
 				</Button>
 			</div>
 		</Modal>
