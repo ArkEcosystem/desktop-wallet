7541c46e6c57c1ba719c5783fac82406642b9834
goga-m <goga-m@users.noreply.github.com>
2021-02-08 11:15:17 +0000

    style: resolve style guide violations

--- src/domains/wallet/components/SearchWallet/SearchWallet.tsx
+++ src/domains/wallet/components/SearchWallet/SearchWallet.tsx
@@ -1,4 +1,4 @@
-import { ProfileSetting,ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
+import { ProfileSetting, ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
 import { BigNumber } from "@arkecosystem/platform-sdk-support";
 import { Address } from "app/components/Address";
 import { Amount } from "app/components/Amount";
