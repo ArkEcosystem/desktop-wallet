b8b34c17a03cf1bbdaab74603ce01fa459e11d16
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-02-09 13:57:54 +0100

    refactor: use card for create button on welcome screen to match profiles (#3328)

--- src/domains/profile/e2e/create-profile-routing.e2e.ts
+++ src/domains/profile/e2e/create-profile-routing.e2e.ts
@@ -10,7 +10,7 @@ createFixture(`Welcome -> Create Profile routing`);
 const getLocation = ClientFunction(() => document.location.href);
 
 test("should navigate to create profile and back to welcome screen", async (t) => {
-	await t.click(Selector("button").withExactText(translations.PROFILE.CREATE_PROFILE));
+	await t.click(Selector('[data-testid="Card"]').withExactText(translations.PROFILE.CREATE_PROFILE));
 	await t.expect(getLocation()).contains("/profiles/create");
 	await t.click(Selector("h1").withExactText(translations.PROFILE.PAGE_CREATE_PROFILE.TITLE));
 
