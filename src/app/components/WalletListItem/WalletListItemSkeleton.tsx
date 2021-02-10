07677cc980c87c745b76e9f0c41cbce785856be8
goga-m <goga-m@users.noreply.github.com>
2021-02-10 16:53:52 +0000

    style: resolve style guide violations

--- src/app/components/WalletListItem/WalletListItemSkeleton.tsx
+++ src/app/components/WalletListItem/WalletListItemSkeleton.tsx
@@ -4,31 +4,31 @@ import React from "react";
 import Skeleton from "react-loading-skeleton";
 
 export const WalletListItemSkeleton = () => (
-		<TableRow data-testid="WalletListItemSkeleton">
-			<TableCell variant="start" innerClassName="space-x-4">
-				<div className="-space-x-2">
-					<div className="flex items-center mr-2 -space-x-1">
-						<Circle className="border-transparent" size="lg">
-							<Skeleton circle height={44} width={44} />
-						</Circle>
-						<Circle className="border-transparent" size="lg">
-							<Skeleton circle height={44} width={44} />
-						</Circle>
-					</div>
+	<TableRow data-testid="WalletListItemSkeleton">
+		<TableCell variant="start" innerClassName="space-x-4">
+			<div className="-space-x-2">
+				<div className="flex items-center mr-2 -space-x-1">
+					<Circle className="border-transparent" size="lg">
+						<Skeleton circle height={44} width={44} />
+					</Circle>
+					<Circle className="border-transparent" size="lg">
+						<Skeleton circle height={44} width={44} />
+					</Circle>
 				</div>
-				<Skeleton height={16} width={260} />
-			</TableCell>
+			</div>
+			<Skeleton height={16} width={260} />
+		</TableCell>
 
-			<TableCell innerClassName="justify-center text-sm font-bold text-center align-middle">
-				<Skeleton height={16} width={100} />
-			</TableCell>
+		<TableCell innerClassName="justify-center text-sm font-bold text-center align-middle">
+			<Skeleton height={16} width={100} />
+		</TableCell>
 
-			<TableCell innerClassName="font-semibold justify-end">
-				<Skeleton height={16} width={229} />
-			</TableCell>
+		<TableCell innerClassName="font-semibold justify-end">
+			<Skeleton height={16} width={229} />
+		</TableCell>
 
-			<TableCell variant="end" innerClassName="justify-end text-theme-secondary-400">
-				<Skeleton height={16} width={100} />
-			</TableCell>
-		</TableRow>
-	);
+		<TableCell variant="end" innerClassName="justify-end text-theme-secondary-400">
+			<Skeleton height={16} width={100} />
+		</TableCell>
+	</TableRow>
+);
