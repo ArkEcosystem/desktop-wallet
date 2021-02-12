ee99cfea2172e15e9ed05bc3f668178d5de17ae7
Lúcio Rubens <4539235+luciorubeens@users.noreply.github.com>
2021-02-12 15:04:17 -0300

    feat: show plugin permissions on install (#3336)

--- src/domains/plugin/components/InstallPlugin/InstallPlugin.test.tsx
+++ src/domains/plugin/components/InstallPlugin/InstallPlugin.test.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/require-await */
 
-import { act } from "@testing-library/react-hooks";
+import { PluginManager, PluginManagerProvider } from "plugins";
 import React from "react";
-import { fireEvent, render, RenderResult, waitFor } from "testing-library";
+import { render } from "utils/testing-library";
 
 import { InstallPlugin } from "./InstallPlugin";
 import { FirstStep } from "./Step1";
@@ -11,14 +11,20 @@ import { ThirdStep } from "./Step3";
 
 describe("InstallPlugin", () => {
 	it("should not render if not open", () => {
-		const { asFragment, getByTestId } = render(<InstallPlugin isOpen={false} />);
+		const { asFragment, getByTestId } = render(
+			<PluginManagerProvider manager={new PluginManager()} services={[]}>
+				<InstallPlugin isOpen={false} />
+			</PluginManagerProvider>,
+		);
 
 		expect(() => getByTestId("modal__inner")).toThrow(/Unable to find an element by/);
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should render 1st step", async () => {
-		const { getByTestId, asFragment } = render(<FirstStep />);
+		const { getByTestId, asFragment } = render(
+			<FirstStep plugin={{ permissions: ["PROFILE", "EVENTS", "HTTP"] }} />,
+		);
 
 		expect(getByTestId("InstallPlugin__step--first")).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
@@ -36,35 +42,5 @@ describe("InstallPlugin", () => {
 
 		expect(getByTestId("InstallPlugin__step--third")).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
-	});
-
-	it("should render", async () => {
-		let rendered: RenderResult;
-
-		await act(async () => {
-			rendered = render(<InstallPlugin isOpen={true} />);
-			await waitFor(() => expect(rendered.getByTestId(`InstallPlugin__step--first`)).toBeTruthy());
-		});
-
-		const { getByTestId, asFragment } = rendered!;
-
-		expect(asFragment()).toMatchSnapshot();
-
-		await act(async () => {
-			const downloadButton = getByTestId(`InstallPlugin__download-button`);
-
-			// Navigation between steps
-			fireEvent.click(downloadButton);
-			expect(getByTestId(`InstallPlugin__step--second`)).toBeTruthy();
-
-			const continueButton = getByTestId(`InstallPlugin__continue-button`);
-
-			fireEvent.click(continueButton);
-			expect(getByTestId(`InstallPlugin__step--third`)).toBeTruthy();
-
-			// Back
-			fireEvent.click(getByTestId(`InstallPlugin__cancel-button`));
-			expect(getByTestId(`InstallPlugin__step--second`)).toBeTruthy();
-		});
 	});
 });
--- src/domains/plugin/components/InstallPlugin/Step1.tsx
+++ src/domains/plugin/components/InstallPlugin/Step1.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export const FirstStep = () => {
+type Props = {
+	plugin: any;
+};
+
+export const FirstStep = ({ plugin }: Props) => {
 	const { t } = useTranslation();
 
 	return (
@@ -11,9 +15,9 @@ export const FirstStep = () => {
 			</p>
 			<div className="max-w-sm">
 				<ul className="mt-2 ml-5 leading-8 list-outside list-circle text-theme-secondary-text">
-					<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_1")}</li>
-					<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_2")}</li>
-					<li>{t("PLUGINS.MODAL_INSTALL_PLUGIN.ITEM_3")}</li>
+					{plugin.permissions?.map((permission: string) => (
+						<li key={permission}>{t(`PLUGINS.PERMISSIONS.${permission}`)}</li>
+					))}
 				</ul>
 			</div>
 		</section>
