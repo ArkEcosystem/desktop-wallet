fa4872033bf5d1a4e9fcd3843e97571d8ef17752
Roman Likhovich <romanlikhovich@ester.co>
2021-02-12 12:25:44 +0300

    fix: cover src/app/components/Tooltip 100%

--- src/app/components/Tooltip/Tooltip.test.tsx
+++ src/app/components/Tooltip/Tooltip.test.tsx
@@ -4,8 +4,13 @@ import { render } from "utils/testing-library";
 import { Tooltip } from "./Tooltip";
 
 describe("Tooltip", () => {
-	it("should render", () => {
-		const { getByTestId, asFragment } = render(<Tooltip />);
+	it("should render default", () => {
+		const { asFragment } = render(<Tooltip />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render small", () => {
+		const { asFragment } = render(<Tooltip content="Test small tooltip" variant="sm" />);
 		expect(asFragment()).toMatchSnapshot();
 	});
 });
