9a99cb791456ef9390f92e60f1ecac48673d210e
Roman Likhovich <romanlikhovich@ester.co>
2021-02-17 11:29:37 +0300

    fix: coverd src/app/hooks/debounce 100

--- src/app/hooks/use-previous.test.tsx
+++ src/app/hooks/use-previous.test.tsx
@@ -2,11 +2,8 @@ import { renderHook } from "@testing-library/react-hooks";
 
 import { usePrevious } from "./use-previous";
 
-const NotificationsTransactionsFixture = require("tests/fixtures/coins/ark/devnet/notification-transactions.json");
-const TransactionsFixture = require("tests/fixtures/coins/ark/devnet/transactions.json");
-
 describe("Previous Hook", () => {
-	it("should create and save notifications from received transactions", () => {
+	it("Should render the usePrevious hook", () => {
 		const { result } = renderHook(() => usePrevious({ result: { current: true } }));
 		expect(result).toBeTruthy();
 	});
