9f3b4817f61b30f3e82e4321281983659487cbc6
Roman Likhovich <romanlikhovich@ester.co>
2021-02-17 13:00:22 +0300

    fix: covered: src/app/hooks/click-outside 100

--- src/app/hooks/click-outside.test.tsx
+++ src/app/hooks/click-outside.test.tsx
@@ -2,6 +2,7 @@ import { act } from "@testing-library/react-hooks";
 import { clickOutsideHandler } from "app/hooks/click-outside";
 import { fireEvent } from "utils/testing-library";
 
+
 describe("ClickOutside Hook", () => {
 	it("should not call callback if clicked on target element", () => {
 		const el = document;
@@ -37,5 +38,12 @@ describe("ClickOutside Hook", () => {
 		act(() => {
 			fireEvent.mouseDown(document);
 		});
+	});
+
+	it("should cover the removeEvent", () => {
+		const div = document.createElement("div");
+		const ref = { current: div };
+		const handler = clickOutsideHandler(ref, () => "test")();
+		expect(handler).toBeUndefined();
 	});
 });
