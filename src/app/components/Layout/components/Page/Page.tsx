a345a62ce61f3ff8207139d18208fd05d752ef96
goga-m <goga.mamoulasvili@gmail.com>
2021-02-24 21:51:12 +0200

    refactor: remove gray background from general `Page` component

--- src/app/components/Layout/components/Page/Page.tsx
+++ src/app/components/Layout/components/Page/Page.tsx
@@ -14,14 +14,14 @@ type PageProps = {
 };
 
 export const Page = ({ navbarVariant, title, profile, crumbs, sidebar, children }: PageProps) => (
-	<div className="flex relative flex-col min-h-screen bg-theme-secondary-background">
+	<div className="flex relative flex-col min-h-screen">
 		{<NavigationBar variant={navbarVariant} title={title} profile={profile} />}
 
 		{crumbs?.length && <Breadcrumbs crumbs={crumbs} className="container py-5 px-14 mx-auto font-semibold" />}
 
-		<div className={`flex flex-col flex-1 ${navbarVariant === "full" && !crumbs?.length ? "mt-5" : ""}`}>
+		<div className="flex flex-col flex-1">
 			{sidebar ? (
-				<div className="flex flex-1 bg-theme-background">
+				<div className="flex flex-1">
 					<div className="container flex mx-auto">
 						<div className="px-10 my-16 border-r border-theme-primary-100 dark:border-theme-secondary-800">
 							{sidebar}
