15fccce5193ab7c3eb9b5878d16e5bede96b797c
Lucio Rubens <luciorubeens@users.noreply.github.com>
2021-02-24 15:54:19 -0300

    refactor: display only title in the plugin view page

--- src/domains/plugin/pages/PluginView/PluginView.tsx
+++ src/domains/plugin/pages/PluginView/PluginView.tsx
@@ -1,19 +1,11 @@
-import { Button } from "app/components/Button";
-import { Icon } from "app/components/Icon";
 import { Image } from "app/components/Image";
-import { Page, Section } from "app/components/Layout";
-import { Tooltip } from "app/components/Tooltip";
+import { Page } from "app/components/Layout";
 import { useActiveProfile, useQueryParams } from "app/hooks";
-import { PluginUninstallConfirmation } from "domains/plugin/components/PluginUninstallConfirmation/PluginUninstallConfirmation";
 import { LaunchRender, usePluginManagerContext } from "plugins";
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router-dom";
+import React from "react";
 
 export const PluginView = () => {
 	const queryParams = useQueryParams();
-	const { t } = useTranslation();
-	const history = useHistory();
 
 	const profile = useActiveProfile();
 	const { pluginManager, reportPlugin } = usePluginManagerContext();
@@ -21,29 +13,9 @@ export const PluginView = () => {
 	const pluginId = queryParams.get("pluginId")!;
 	const plugin = pluginManager.plugins().findById(pluginId);
 
-	const [isUninstallOpen, setIsUninstallOpen] = useState(false);
-
-	const crumbs = [
-		{
-			route: `/profiles/${profile.id()}/plugins`,
-			label: t("PLUGINS.PAGE_PLUGIN_MANAGER.TITLE"),
-		},
-		{
-			label: plugin?.config().title() as string,
-		},
-	];
-
-	const handleReportPlugin = () => {
-		reportPlugin(plugin!.config());
-	};
-
-	const handleOnDelete = () => {
-		history.push(`/profiles/${profile.id()}/plugins`);
-	};
-
 	return (
-		<Page profile={profile} crumbs={crumbs}>
-			<Section>
+		<Page profile={profile}>
+			<div className="bg-theme-background px-4 sm:px-6 lg:px-10 py-4 -mt-5">
 				<div className="flex justify-between items-center">
 					<div className="flex items-center space-x-3">
 						{plugin?.config().logo() ? (
@@ -51,62 +23,22 @@ export const PluginView = () => {
 								data-testid="PluginView__logo"
 								src={plugin.config().logo()}
 								alt="Logo"
-								className="overflow-hidden w-12 h-12 rounded"
+								className="overflow-hidden w-6 h-6 rounded"
 							/>
 						) : (
-							<Image name="PluginLogoPlaceholder" domain="plugin" className="w-12 h-12" />
+							<Image name="PluginLogoPlaceholder" domain="plugin" className="w-6 h-6" />
 						)}
 
 						<div className="flex space-x-10 divide-x divide-theme-secondary-300 dark:divide-theme-secondary-700">
-							<dl className="pl-6 first:pl-0">
-								<dt className="text-sm font-bold text-theme-secondary-500">{t("COMMON.PLUGIN")}</dt>
-								<dd className="font-bold text-theme-secondary-text dark:text-theme-text">
+							<dl>
+								<dd className="font-semibold text-theme-secondary-text dark:text-theme-text">
 									{plugin?.config().title()}
-								</dd>
-							</dl>
-
-							<dl className="pl-6 first:pl-0">
-								<dt className="text-sm font-bold text-theme-secondary-500">{t("COMMON.CATEGORY")}</dt>
-								<dd className="font-bold capitalize text-theme-secondary-text dark:text-theme-text">
-									{plugin?.config().categories()?.[0]}
-								</dd>
-							</dl>
-
-							<dl className="pl-6 first:pl-0">
-								<dt className="text-sm font-bold text-theme-secondary-500">{t("COMMON.VERSION")}</dt>
-								<dd className="font-bold text-theme-secondary-text dark:text-theme-text">
-									v{plugin?.config().version()}
-								</dd>
-							</dl>
-
-							<dl className="pl-6 first:pl-0">
-								<dt className="text-sm font-bold text-theme-secondary-500">{t("COMMON.SIZE")}</dt>
-								<dd className="font-bold text-theme-secondary-text dark:text-theme-text">
-									{plugin?.config().size()}
 								</dd>
 							</dl>
 						</div>
 					</div>
-
-					<div className="flex items-center space-x-2">
-						<Tooltip content="Report this plugin">
-							<Button data-testid="PluginView__report" size="icon" onClick={handleReportPlugin}>
-								<Icon name="AlertWarning" width="1.5rem" height="1.25rem" />
-							</Button>
-						</Tooltip>
-
-						<Tooltip content="Uninstall">
-							<Button
-								data-testid="PluginView__uninstall"
-								size="icon"
-								onClick={() => setIsUninstallOpen(true)}
-							>
-								<Icon name="Trash" width="1.5rem" height="1.25rem" />
-							</Button>
-						</Tooltip>
-					</div>
 				</div>
-			</Section>
+			</div>
 
 			<div className="flex relative flex-1 w-full h-full">
 				<LaunchRender
@@ -115,16 +47,6 @@ export const PluginView = () => {
 					fallback={<h1>Plugin not loaded or enabled</h1>}
 				/>
 			</div>
-
-			{plugin && (
-				<PluginUninstallConfirmation
-					isOpen={isUninstallOpen}
-					plugin={plugin}
-					profile={profile}
-					onClose={() => setIsUninstallOpen(false)}
-					onDelete={handleOnDelete}
-				/>
-			)}
 		</Page>
 	);
 };
