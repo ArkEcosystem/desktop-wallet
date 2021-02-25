5d3694cbf4e897084e824c85ee35c6ca654d3ced
Edgar Goetzendorff <hello@dated.fun>
2021-02-25 22:16:06 +0100

    chore: update domains/transaction images

--- src/domains/transaction/components/ErrorStep/ErrorStep.tsx
+++ src/domains/transaction/components/ErrorStep/ErrorStep.tsx
@@ -1,30 +1,27 @@
-import { images } from "app/assets/images";
 import { Button } from "app/components/Button";
+import { Header } from "app/components/Header";
+import { Image } from "app/components/Image";
 import React from "react";
 import { useTranslation } from "react-i18next";
-const { ErrorBanner } = images.common;
 
 type ErroStepProps = {
 	title?: string;
-	subtitle?: string;
+	description?: string;
 	isRepeatDisabled?: boolean;
 	onBack?: () => void;
 	onRepeat?: () => void;
 };
 
-export const ErrorStep = ({ title, subtitle, onBack, onRepeat, isRepeatDisabled = false }: ErroStepProps) => {
+export const ErrorStep = ({ title, description, onBack, onRepeat, isRepeatDisabled = false }: ErroStepProps) => {
 	const { t } = useTranslation();
+
 	return (
-		<div data-testid="ErrorStep">
-			<div>
-				<h1 className="mb-8 text-lg font-bold md:text-4xl">{title || t("TRANSACTION.ERROR.TITLE")}</h1>
-				<div className="my-4 mx-auto w-128">
-					<ErrorBanner />
-				</div>
-				<div className="my-8 text-md text-theme-secondary-text">
-					{subtitle || t("TRANSACTION.ERROR.SUBTITLE")}
-				</div>
-			</div>
+		<div data-testid="ErrorStep" className="space-y-8">
+			<Header title={title || t("TRANSACTION.ERROR.TITLE")} />
+
+			<Image name="TransactionErrorBanner" domain="transaction" className="w-full" />
+
+			<p className="text-theme-secondary-text">{t("TRANSACTION.ERROR.DESCRIPTION")}</p>
 
 			<div className="flex justify-end space-x-3">
 				<Button onClick={() => onBack?.()} data-testid="ErrorStep__wallet-button" variant="secondary">
