af063793d1b78ff467b3ba85aba70762c41d6736
Edgar Goetzendorff <hello@dated.fun>
2021-03-03 22:08:03 +0100

    fix: remove unused variable

--- src/domains/transaction/pages/SendVote/SendVote.tsx
+++ src/domains/transaction/pages/SendVote/SendVote.tsx
@@ -40,7 +40,7 @@ export const SendVote = () => {
 	const form = useForm({ mode: "onChange" });
 
 	const { hasDeviceAvailable, isConnected } = useLedgerContext();
-	const { clearErrors, formState, getValues, handleSubmit, register, setError, setValue, watch } = form;
+	const { clearErrors, formState, getValues, handleSubmit, register, setValue, watch } = form;
 	const { isValid, isSubmitting } = formState;
 
 	const { fee, fees } = watch();
