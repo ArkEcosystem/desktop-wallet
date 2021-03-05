4175a58ca08a920a28fb7b23d2991ea618b1d8a5
goga-m <goga.mamoulasvili@gmail.com>
2021-03-05 11:03:22 +0200

    feat: add changed state in `ControlButton` using dot

--- src/app/components/LayoutControls/LayoutControls.tsx
+++ src/app/components/LayoutControls/LayoutControls.tsx
@@ -8,7 +8,7 @@ type LayoutControlsProps = {
 	selectedViewType: string;
 };
 
-export const ControlButton = styled.div<{ isActive?: boolean }>`
+export const ControlButton = styled.div<{ isActive?: boolean; isChanged?: boolean }>`
 	${tw`flex items-center justify-center transition-colors duration-200 relative w-11 h-9 cursor-pointer`}
 
 	&:after {
@@ -29,6 +29,18 @@ export const ControlButton = styled.div<{ isActive?: boolean }>`
 					`,
 			  ]
 			: tw`text-theme-primary-300 dark:text-theme-secondary-600 hover:text-theme-danger-400`};
+
+	${({ isChanged }) =>
+		isChanged && [
+			tw`text-theme-danger-400 dark:text-theme-danger-400`,
+			css`
+				&:before {
+					${tw`transition-opacity duration-200 absolute inset-x-0 bg-theme-danger-400 dark:bg-theme-danger-700 rounded opacity-0 group-hover:opacity-100 w-2 h-2 rounded-full block right-0 top-0 mt-1 mr-1 left-auto`}
+					content: "";
+					opacity: 100;
+				}
+			`,
+		]};
 `;
 
 export const LayoutControls = ({ onSelectGridView, onSelectListView, selectedViewType }: LayoutControlsProps) => (
