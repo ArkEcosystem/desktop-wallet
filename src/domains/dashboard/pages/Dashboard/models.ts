43f28324575f2d7cf4e105beb8c15be623b45bcf
Edgar Goetzendorff <hello@dated.fun>
2021-03-05 14:23:04 +0100

    refactor: move transaction history toggle to settings page (#3403)

--- src/domains/dashboard/pages/Dashboard/models.ts
+++ src/domains/dashboard/pages/Dashboard/models.ts
@@ -1,5 +1,4 @@
 export type DashboardConfiguration = {
-	showTransactions: boolean;
 	viewType: "list" | "grid";
 	walletsDisplayType: "all" | "favorites" | "ledger";
 	selectedNetworkIds: string[];
--- src/domains/vote/pages/Votes/Votes.tsx
+++ src/domains/vote/pages/Votes/Votes.tsx
@@ -263,7 +263,7 @@ export const Votes = () => {
 										}
 									>
 										<div className="py-7 px-10 w-128">
-											<FilterWallets {...filterProperties} showToggleViews={false} />
+											<FilterWallets {...filterProperties} />
 										</div>
 									</Dropdown>
 								</div>
