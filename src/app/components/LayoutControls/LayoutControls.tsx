89f070f18c658861950c5661c6390e4aafeaf4bd
goga-m <goga.mamoulasvili@gmail.com>
2021-03-05 11:16:44 +0200

    chore: update snapshots

--- src/app/components/LayoutControls/LayoutControls.tsx
+++ src/app/components/LayoutControls/LayoutControls.tsx
@@ -35,7 +35,7 @@ export const ControlButton = styled.div<{ isActive?: boolean; isChanged?: boolea
 			tw`text-theme-danger-400 dark:text-theme-danger-400`,
 			css`
 				&:before {
-					${tw`transition-opacity duration-200 absolute inset-x-0 bg-theme-danger-400 dark:bg-theme-danger-700 rounded opacity-0 group-hover:opacity-100 w-2 h-2 rounded-full block right-0 top-0 mt-1 mr-1 left-auto`}
+					${tw`transition-opacity duration-200 absolute inset-x-0 bg-theme-danger-400 dark:bg-theme-danger-700 rounded opacity-0 group-hover:opacity-100 w-2 h-2 rounded-full block right-0 top-0 mt-1 mr-2 left-auto`}
 					content: "";
 					opacity: 100;
 				}
