755590ae145fbbd6b39e9be88cc7d1943c5a9c09
Lucio Rubens <luciorubeens@users.noreply.github.com>
2021-03-08 11:17:25 -0300

    fix: description text

--- src/domains/wallet/i18n.ts
+++ src/domains/wallet/i18n.ts
@@ -130,8 +130,7 @@ export const translations: { [key: string]: any } = {
 
 		METHOD_STEP: {
 			TITLE: "Import Wallet",
-			SUBTITLE:
-				"Enter your wallet password in order to get full access to your money. Or you can choose an address for viewing only.",
+			SUBTITLE: "Select the import method by which you want to import the address to your Profile",
 			TYPE: "Import Type",
 		},
 
