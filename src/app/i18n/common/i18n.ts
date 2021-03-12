2ce8aee8be192fd263b57f6e2fc4a72803ddd5c7
Edgar Goetzendorff <hello@dated.fun>
2021-03-12 23:39:00 +0100

    refactor: adjust mnemonic tooltip and disabled 2nd mnemonic input

--- src/app/i18n/common/i18n.ts
+++ src/app/i18n/common/i18n.ts
@@ -277,8 +277,7 @@ export const translations: { [key: string]: any } = {
 	INPUT_PASSPHRASE: {
 		VALIDATION: {
 			ADDRESS_ALREADY_EXISTS: "Address {{address}} already exists",
-			MNEMONIC_NOT_MATCH_ADDRESS: "The mnemonic does not match the sender address",
-			MNEMONIC_NOT_MATCH_PUBLIC_KEY: "The mnemonic does not match the second public key",
+			MNEMONIC_NOT_MATCH_WALLET: "This mnemonic does not correspond to your wallet",
 		},
 	},
 
