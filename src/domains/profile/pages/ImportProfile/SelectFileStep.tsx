2ab430b100cdbad63440f7c6201f70282be305a4
goga-m <goga.mamoulasvili@gmail.com>
2021-03-12 14:24:11 +0200

    refactor: use `SelectFile` component in `SelectFileStep`

--- src/domains/profile/pages/ImportProfile/SelectFileStep.tsx
+++ src/domains/profile/pages/ImportProfile/SelectFileStep.tsx
@@ -1,50 +1,20 @@
 import { Alert } from "app/components/Alert";
 import { Button } from "app/components/Button";
 import { Header } from "app/components/Header";
-import { Icon } from "app/components/Icon";
-import { useFiles } from "app/hooks/use-files";
-import path from "path";
+import { SelectFile } from "app/components/SelectFile";
+import { ReadableFile } from "app/hooks/use-files";
 import React from "react";
 import { useTranslation } from "react-i18next";
-
-import { ImportFile } from "./models";
 
 type SelectFileStepProps = {
 	fileFormat: string;
 	onFileFormatChange?: (fileFormat: string) => void;
-	onFileSelected?: (file: ImportFile) => void;
+	onSelect?: (file: ReadableFile) => void;
 	onBack?: () => void;
 };
 
-export const SelectFileStep = ({ onBack, onFileSelected, onFileFormatChange, fileFormat }: SelectFileStepProps) => {
+export const SelectFileStep = ({ onBack, onSelect, onFileFormatChange, fileFormat }: SelectFileStepProps) => {
 	const { t } = useTranslation();
-	const { openFile, readFileContents } = useFiles();
-
-	const handleBrowseFiles = async () => {
-		const file = await openFile({ extensions: [fileFormat.replace(/\./g, "")] });
-
-		if (!file) {
-			return;
-		}
-
-		onFileSelected?.(file);
-	};
-
-	const handleFileDrop = (event: any) => {
-		event.preventDefault();
-		event.stopPropagation();
-
-		const firstAcceptedFileByExtension = [...event.dataTransfer.files].find(
-			({ name }) => path.extname(name) === fileFormat,
-		);
-
-		if (!firstAcceptedFileByExtension) {
-			return;
-		}
-
-		const file = readFileContents(firstAcceptedFileByExtension.path);
-		onFileSelected?.(file);
-	};
 
 	const handleBack = () => {
 		if (fileFormat === ".json") {
@@ -60,30 +30,8 @@ export const SelectFileStep = ({ onBack, onFileSelected, onFileFormatChange, fil
 				title={t("PROFILE.IMPORT.TITLE")}
 				subtitle={t("PROFILE.IMPORT.SELECT_FILE_STEP.DESCRIPTION", { fileFormat })}
 			/>
-			<div
-				onDragOver={(event: any) => event.preventDefault()}
-				onDragEnter={(event: any) => event.preventDefault()}
-				onDrop={handleFileDrop}
-				className="h-52 border-2 border-dashed rounded-lg mt-8 border-theme-secondary-300 dark:border-theme-secondary-800 flex flex-col items-center justify-center"
-			>
-				{fileFormat === ".dwe" && <Icon name="DweFile" width={40} height={40} />}
-				{fileFormat === ".json" && <Icon name="JsonFile" width={40} height={40} />}
 
-				<div className="mt-8">
-					<span className="font-semibold">{t("PROFILE.IMPORT.SELECT_FILE_STEP.DRAG_AND_DROP")} </span>
-					<span
-						onClick={handleBrowseFiles}
-						title={t("PROFILE.IMPORT.SELECT_FILE_STEP.UPLOAD_TITLE")}
-						data-testid="PluginManager__home__featured__view-more"
-						className="font-semibold cursor-pointer link"
-					>
-						{t("PROFILE.IMPORT.SELECT_FILE_STEP.BROWSE_FILES")}
-					</span>
-				</div>
-				<div className="font-semibold text-theme-secondary-500 text-sm mt-2">
-					{t("PROFILE.IMPORT.SELECT_FILE_STEP.SUPPORTED_FORMAT", { fileFormat })}
-				</div>
-			</div>
+			<SelectFile fileFormat={fileFormat} onSelect={onSelect} />
 
 			{fileFormat === ".dwe" && (
 				<p className="text-sm text-theme-secondary-text md:text-base mt-6 text-center">
