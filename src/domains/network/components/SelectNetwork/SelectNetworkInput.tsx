eb478f09c8ea7851710b8ca7cdd709e5b2d41f28
Edgar Goetzendorff <hello@dated.fun>
2021-03-17 16:15:39 +0100

    wip

--- src/domains/network/components/SelectNetwork/SelectNetworkInput.tsx
+++ src/domains/network/components/SelectNetwork/SelectNetworkInput.tsx
@@ -1,5 +1,4 @@
 import { Coins } from "@arkecosystem/platform-sdk";
-import { useFormField } from "app/components/Form/useFormField";
 import { Input } from "app/components/Input";
 import { NetworkIcon } from "domains/network/components/NetworkIcon";
 import React from "react";
@@ -10,30 +9,26 @@ type Props = {
 } & React.InputHTMLAttributes<any>;
 
 export const SelectNetworkInput = React.forwardRef<HTMLInputElement, Props>(
-	({ network, suggestion, ...props }: Props, ref) => {
-		const fieldContext = useFormField();
-
-		return (
-			<Input
-				data-testid="SelectNetworkInput__input"
-				ref={ref}
-				suggestion={suggestion}
-				addons={{
-					start: (
-						<NetworkIcon
-							data-testid="SelectNetworkInput__network"
-							coin={network?.coin()}
-							network={network?.id()}
-							size="sm"
-							showTooltip={false}
-							noShadow
-						/>
-					),
-				}}
-				{...props}
-			/>
-		);
-	},
+	({ network, suggestion, ...props }: Props, ref) => (
+		<Input
+			data-testid="SelectNetworkInput__input"
+			ref={ref}
+			suggestion={suggestion}
+			addons={{
+				start: (
+					<NetworkIcon
+						data-testid="SelectNetworkInput__network"
+						coin={network?.coin()}
+						network={network?.id()}
+						size="sm"
+						showTooltip={false}
+						noShadow
+					/>
+				),
+			}}
+			{...props}
+		/>
+	),
 );
 
 SelectNetworkInput.displayName = "SelectNetworkInput";
