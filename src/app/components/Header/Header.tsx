dbc4bbd92bf3401f3b9eb83db8424fa74585d66f
goga-m <goga.mamoulasvili@gmail.com>
2021-03-17 13:56:56 +0200

    feat: add optional title suffix in `Header` component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 type Props = {
 	title: string;
+	titleSuffix?: string | React.ReactNode;
 	subtitle?: string | React.ReactNode;
 	extra?: React.ReactNode;
 };
 
-export const Header = ({ title, subtitle, extra }: Props) => (
+export const Header = ({ title, titleSuffix, subtitle, extra }: Props) => (
 	<div className="flex justify-between items-end bg-theme-background">
 		<div className="space-y-4">
 			<h1 className="mb-0 text-4xl" data-testid="header__title">
-				{title}
+				{title} {titleSuffix && titleSuffix}
 			</h1>
 			{subtitle && (
 				<div className="text-theme-secondary-text" data-testid="header__subtitle">
