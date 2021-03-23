b359e012f0e5d8b072f346b48cb47bb7bd592f30
Edgar Goetzendorff <hello@dated.fun>
2021-03-23 17:57:24 +0100

    test: fix failing

--- src/domains/transaction/pages/SendDelegateResignation/FormStep.tsx
+++ src/domains/transaction/pages/SendDelegateResignation/FormStep.tsx
@@ -51,11 +51,11 @@ export const FormStep = ({ senderWallet }: { senderWallet: ReadWriteWallet }) =>
 	return (
 		<section data-testid="SendDelegateResignation__form-step" className="space-y-8">
 			<Header
-				title={t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.FORM_STEP.TITLE")}
-				subtitle={t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.FORM_STEP.DESCRIPTION")}
+				title={t("TRANSACTION.PAGE_DELEGATE_RESIGNATION.FORM_STEP.TITLE")}
+				subtitle={t("TRANSACTION.PAGE_DELEGATE_RESIGNATION.FORM_STEP.DESCRIPTION")}
 			/>
 
-			<Alert>{t("TRANSACTION.PAGE_DELEGATE_REGISTRATION.FORM_STEP.WARNING")}</Alert>
+			<Alert>{t("TRANSACTION.PAGE_DELEGATE_RESIGNATION.FORM_STEP.WARNING")}</Alert>
 
 			<div>
 				<TransactionSender
