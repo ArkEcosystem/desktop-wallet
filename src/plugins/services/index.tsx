1a67069a515dce164bc5d8dbcb9e1844193e7776
Lucio Rubens <luciorubeens@users.noreply.github.com>
2021-03-25 07:03:46 -0300

    wip

--- src/plugins/services/index.tsx
+++ src/plugins/services/index.tsx
@@ -2,6 +2,7 @@ export * from "./events";
 export * from "./filesystem";
 export * from "./http";
 export * from "./launch";
+export * from "./message";
 export * from "./profile";
 export * from "./store";
 export * from "./theme";
