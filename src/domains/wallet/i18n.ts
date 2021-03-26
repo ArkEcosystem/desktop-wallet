01a9c6d2cf9e1104c93a0dcf17ffb7424e0974b3
Edgar Goetzendorff <hello@dated.fun>
2021-03-26 17:28:45 +0100

    chore: adjust empty transactions text on wallets (#3562)

--- src/domains/wallet/i18n.ts
+++ src/domains/wallet/i18n.ts
@@ -193,12 +193,7 @@ export const translations: { [key: string]: any } = {
 		TRANSACTION_HISTORY: {
 			TITLE: "Transaction History",
 			EMPTY_MESSAGE:
-				"This will display the history of your transactions. But you don't have more than one transaction at the moment.",
-		},
-
-		REGISTRATIONS: {
-			TITLE: "Registrations",
-			EMPTY_DESCRIPTION: "You have no active registrations",
+				"You don't have any transactions yet. Once transactions have been made they will show up in your history.",
 		},
 
 		VOTES: {
