66ea0b08388b5a7e90afbceb7885139dace7ff3d
Edgar Goetzendorff <hello@dated.fun>
2021-03-31 21:32:59 +0200

    chore: remove unused env

--- src/domains/wallet/pages/CreateWallet/CreateWallet.tsx
+++ src/domains/wallet/pages/CreateWallet/CreateWallet.tsx
@@ -19,7 +19,7 @@ import { SuccessStep } from "./SuccessStep";
 import { WalletOverviewStep } from "./WalletOverviewStep";
 
 export const CreateWallet = () => {
-	const { env, persist } = useEnvironmentContext();
+	const { persist } = useEnvironmentContext();
 	const history = useHistory();
 	const { t } = useTranslation();
 
