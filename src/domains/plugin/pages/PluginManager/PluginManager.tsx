b7ce699b3cc8ecca5678b99368371a7628796c59
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2021-04-01 10:39:56 +0300

    refactor: optimize sync jobs (#3586)

--- src/domains/plugin/pages/PluginManager/PluginManager.tsx
+++ src/domains/plugin/pages/PluginManager/PluginManager.tsx
@@ -18,7 +18,7 @@ import { PluginUninstallConfirmation } from "domains/plugin/components/PluginUni
 import { PluginUpdatesConfirmation } from "domains/plugin/components/PluginUpdatesConfirmation";
 import { usePluginUpdateQueue } from "domains/plugin/hooks/use-plugin-update-queue";
 import { PluginController, usePluginManagerContext } from "plugins";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 
@@ -151,7 +151,15 @@ const UpdateAllBanner = ({
 
 export const PluginManager = () => {
 	const { t } = useTranslation();
-	const { allPlugins, isFetchingPackages, trigger, updatingStats, filters, filterBy } = usePluginManagerContext();
+	const {
+		allPlugins,
+		isFetchingPackages,
+		trigger,
+		updatingStats,
+		filters,
+		filterBy,
+		fetchPluginPackages,
+	} = usePluginManagerContext();
 
 	const activeProfile = useActiveProfile();
 	const history = useHistory();
@@ -186,6 +194,10 @@ export const PluginManager = () => {
 
 		return result;
 	}, [plugins]);
+
+	useEffect(() => {
+		fetchPluginPackages();
+	}, [fetchPluginPackages]);
 
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	const filteredPackages = useMemo(() => pluginsByCategory[currentView] || [], [
