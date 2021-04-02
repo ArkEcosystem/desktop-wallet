3d73c85e6d569c3471ff1d04a4a328ddd53511af
Edgar Goetzendorff <hello@dated.fun>
2021-04-02 03:18:38 +0200

    refactor: adjust interpolation of empty text on votes page (#3595)

--- src/domains/vote/i18n.ts
+++ src/domains/vote/i18n.ts
@@ -3,7 +3,7 @@ export const translations: { [key: string]: any } = {
 		TITLE: "My Votes",
 		SUBTITLE: "Manage your cryptoasset staking",
 		EMPTY_MESSAGE:
-			"Your must first <1>{{create}}</1> or <2>{{import}}</2> an address to view your current voting status",
+			"Your must first <bold>{{create}}</bold> or <bold>{{import}}</bold> an address to view your current voting status.",
 		SEARCH_PLACEHOLDER: "Enter the delegate’s name or address for a quick search",
 		SELECT_CRYPTOASSET_MESSAGE: "Select one of the proposed cryptoassets above to vote for a delegate.",
 	},
