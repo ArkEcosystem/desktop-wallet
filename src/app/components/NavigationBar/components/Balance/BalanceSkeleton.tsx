7446012662f9bfc59d018e881652305f90d2ba10
goga-m <goga.mamoulasvili@gmail.com>
2021-04-13 16:19:02 +0300

    chore: adjust balance skeleton height

--- src/app/components/NavigationBar/components/Balance/BalanceSkeleton.tsx
+++ src/app/components/NavigationBar/components/Balance/BalanceSkeleton.tsx
@@ -9,7 +9,7 @@ export const BalanceSkeleton = () => {
 		<div className="text-right">
 			<div className="text-xs font-semibold text-theme-secondary-700">{t("COMMON.YOUR_BALANCE")}</div>
 			<div className="text-sm font-bold text-theme-secondary-text dark:text-theme-text flex justify-end mt-1">
-				<Skeleton height={14} width={60} />
+				<Skeleton height={16} width={60} />
 			</div>
 		</div>
 	);
