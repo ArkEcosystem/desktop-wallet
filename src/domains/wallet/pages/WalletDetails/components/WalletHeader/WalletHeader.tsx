8dbb732ff94df9e07c6c919cf77b453a38a53a54
Edgar Goetzendorff <hello@dated.fun>
2021-04-13 05:14:01 +0200

    refactor: adjust wallet details dropdown position (#3620)

--- src/domains/wallet/pages/WalletDetails/components/WalletHeader/WalletHeader.tsx
+++ src/domains/wallet/pages/WalletDetails/components/WalletHeader/WalletHeader.tsx
@@ -248,7 +248,7 @@ export const WalletHeader = ({ profile, wallet, currencyDelta, onSend }: WalletH
 					</div>
 				</div>
 
-				<div className="flex items-center w-1/2 pl-12 space-x-2 h-13">
+				<div className="flex items-center w-1/2 pl-12 h-13">
 					<div className="flex flex-col mr-auto">
 						<div className="flex items-center text-sm font-semibold text-theme-secondary-text">
 							<span>{t("COMMON.BALANCE")}:</span>
@@ -316,13 +316,13 @@ export const WalletHeader = ({ profile, wallet, currencyDelta, onSend }: WalletH
 							!wallet.hasBeenFullyRestored() ||
 							!wallet.hasSyncedWithNetwork()
 						}
-						className="my-auto"
+						className="my-auto ml-1"
 						onClick={onSend}
 					>
 						{t("COMMON.SEND")}
 					</Button>
 
-					<div data-testid="WalletHeader__more-button" className="my-auto">
+					<div data-testid="WalletHeader__more-button" className="my-auto ml-3">
 						<Dropdown
 							toggleContent={
 								<Button variant="secondary" size="icon" className="text-left">
@@ -331,7 +331,6 @@ export const WalletHeader = ({ profile, wallet, currencyDelta, onSend }: WalletH
 							}
 							onSelect={handleSelect}
 							options={[primaryOptions, registrationOptions, additionalOptions, secondaryOptions]}
-							dropdownClass="top-5 right-3 text-left"
 						/>
 					</div>
 				</div>
