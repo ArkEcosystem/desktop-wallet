2fdaec28d8fcf1228219d4ab68b714e212ed530c
Ehsan <me@ehsanabtahi.com>
2021-04-21 22:04:01 +0430

    fix: adjust styles of settings page

--- src/app/components/Layout/components/Page/Page.tsx
+++ src/app/components/Layout/components/Page/Page.tsx
@@ -29,11 +29,11 @@ export const Page = ({ navbarVariant, title, backToUrl, isBackDisabled, profile,
 			{sidebar ? (
 				<div className="flex flex-1">
 					<div className="container flex mx-auto">
-						<div className="px-10 my-16 border-r border-theme-primary-100 dark:border-theme-secondary-800">
+						<div className="px-12 my-8 border-r border-theme-primary-100 dark:border-theme-secondary-800">
 							{sidebar}
 						</div>
 
-						<div className="ml-16 w-full">{children}</div>
+						<div className="w-full">{children}</div>
 					</div>
 				</div>
 			) : (
