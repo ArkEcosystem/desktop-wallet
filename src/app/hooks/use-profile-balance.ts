d581a0690750058a13685a20c6c8c30396551f62
goga-m <goga.mamoulasvili@gmail.com>
2021-04-22 12:44:19 +0300

    feat: create profile balance hook

---
+++ src/app/hooks/use-profile-balance.ts
@@ -0,0 +1,32 @@
+import { Contracts } from "@arkecosystem/platform-sdk-profiles";
+import { BigNumber } from "@arkecosystem/platform-sdk-support";
+import { useEffect, useState, useCallback } from "react";
+
+type BalanceProps = {
+	profile?: Contracts.IProfile;
+	isLoading?: boolean;
+};
+
+export const useProfileBalance = ({ profile, isLoading = false }: BalanceProps) => {
+	const [convertedBalance, setConvertedBalance] = useState(BigNumber.ZERO);
+
+	const updateBalance = useCallback(() => {
+		try {
+			const balance = profile?.convertedBalance() as BigNumber;
+			if (balance) {
+				setConvertedBalance(balance);
+			}
+		} catch {}
+	}, [profile]);
+
+	useEffect(() => {
+		if (isLoading) {
+			return;
+		}
+		updateBalance();
+	}, [profile, isLoading]);
+
+	return {
+		convertedBalance,
+	};
+};
