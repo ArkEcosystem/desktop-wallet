e51b9d9bf424526833206770a578e27d5161c20d
goga-m <goga.mamoulasvili@gmail.com>
2021-04-28 13:58:38 +0300

    test: fix failing in profile domain

--- src/domains/profile/hooks/use-profile-addresses.test.tsx
+++ src/domains/profile/hooks/use-profile-addresses.test.tsx
@@ -10,7 +10,8 @@ let profile: Contracts.IProfile;
 describe("useProfileAddresses", () => {
 	beforeAll(async () => {
 		profile = env.profiles().findById(getDefaultProfileId());
-		await profile.restore();
+		await env.profiles().restore(profile);
+
 		profile
 			.contacts()
 			.last()
@@ -33,11 +34,17 @@ describe("useProfileAddresses", () => {
 	});
 
 	it("should filter address by selected network", async () => {
-		const wallet = await profile.wallets().importByMnemonic("test", "ARK", "ark.mainnet");
+		const wallet = await profile.walletFactory().fromMnemonic({
+			mnemonic: "test",
+			coin: "ARK",
+			network: "ark.devnet",
+		});
+		profile.wallets().push(wallet);
+
 		const { result } = renderHook(() => useProfileAddresses({ profile, network: wallet.network() }));
 
-		expect(result.current.allAddresses).toHaveLength(1);
-		expect(result.current.contactAddresses).toHaveLength(0);
-		expect(result.current.profileAddresses).toHaveLength(1);
+		expect(result.current.allAddresses).toHaveLength(4);
+		expect(result.current.contactAddresses).toHaveLength(1);
+		expect(result.current.profileAddresses).toHaveLength(3);
 	});
 });
