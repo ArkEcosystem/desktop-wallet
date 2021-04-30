f093d552696697afcd5353b341415afbe1bb4ac4
goga-m <goga.mamoulasvili@gmail.com>
2021-04-30 12:31:39 +0300

    wip

--- src/domains/dashboard/e2e/dashboard-routing.e2e.ts
+++ src/domains/dashboard/e2e/dashboard-routing.e2e.ts
@@ -8,14 +8,14 @@ const translations = buildTranslations();
 createFixture(`Dashboard routing`);
 
 test("should navigate to dashboard", async (t) => {
-	await t.expect(Selector("span").withText("John Doe").exists).ok({ timeout: 30000 });
+	await t.expect(Selector("span").withText("John Doe").exists).ok({ timeout: 60000 });
 	await t.click(Selector("span").withText("John Doe"));
 	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok();
 	await t.expect(getLocation()).contains("/dashboard");
 });
 
 test("should navigate to dashboard of password protected profile", async (t) => {
-	await t.expect(Selector("span").withText("Jane Doe").exists).ok({ timeout: 30000 });
+	await t.expect(Selector("span").withText("Jane Doe").exists).ok({ timeout: 60000 });
 	await t.click(Selector("span").withText("Jane Doe"));
 	await t
 		.expect(Selector('[data-testid="modal__inner"]').withText(translations.PROFILE.MODAL_SIGN_IN.TITLE).exists)
@@ -24,14 +24,14 @@ test("should navigate to dashboard of password protected profile", async (t) => 
 	await t.typeText(Selector('[data-testid="SignIn__input--password"]'), "password");
 	await t.click(Selector('[data-testid="SignIn__submit-button"]'));
 
-	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok({ timeout: 30000 });
+	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok({ timeout: 60000 });
 	await t.expect(getLocation()).contains("/dashboard");
 });
 
 test("should show ledger waiting device modal", async (t) => {
-	await t.expect(Selector("span").withText("John Doe").exists).ok({ timeout: 30000 });
+	await t.expect(Selector("span").withText("John Doe").exists).ok({ timeout: 60000 });
 	await t.click(Selector("span").withText("John Doe"));
-	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok({ timeout: 30000 });
+	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok({ timeout: 60000 });
 
 	await t.click(Selector("button").withExactText(translations.DASHBOARD.WALLET_CONTROLS.IMPORT_LEDGER));
 	await t.expect(Selector("span").withText(translations.WALLETS.MODAL_LEDGER_WALLET.WAITING_DEVICE).exists).ok();
