36eaf9a9ce80647cab2d9eac8aae4d77f7e8975a
goga-m <goga.mamoulasvili@gmail.com>
2021-05-02 11:37:43 +0300

    test: increase coverage in wallet hooks

---
+++ src/domains/wallet/hooks/use-wallet-import.test.tsx
@@ -0,0 +1,41 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import { useWalletImport } from "./use-wallet-import";
+import { Contracts } from "@arkecosystem/platform-sdk-profiles";
+import { env } from "utils/testing-library";
+
+let profile: Contracts.IProfile;
+
+describe("useWalletImport", () => {
+	beforeAll(async () => {
+		profile = env.profiles().first();
+		await env.profiles().restore(profile);
+		await profile.sync();
+	});
+
+	it("#encryptedWif", async () => {
+		const {
+			result: { current },
+		} = renderHook(() => useWalletImport({ profile }));
+		const wallet = profile.wallets().first();
+		const network = wallet.network();
+
+		const mockEncryptedWif = jest.spyOn(profile.walletFactory(), "fromWIFWithEncryption").mockImplementation(() => {
+			throw new Error("error");
+		});
+
+		await act(async () => {
+			expect(
+				current.importWalletByType({ network, type: "encryptedWif", value: "password", encryptedWif: "wif" }),
+			).rejects.toBeTruthy();
+		});
+
+		mockEncryptedWif.mockRestore();
+
+		await act(async () => {
+			expect(
+				current.importWalletByType({ network, type: "uknown", value: "password", encryptedWif: "wif" }),
+			).resolves.toBeUndefined();
+		});
+	});
+});
---
+++ src/domains/wallet/hooks/use-wallet-sync.test.tsx
@@ -0,0 +1,32 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import { useWalletSync } from "./use-wallet-sync";
+import { Contracts } from "@arkecosystem/platform-sdk-profiles";
+import { env } from "utils/testing-library";
+
+let profile: Contracts.IProfile;
+
+describe("useWalletSync", () => {
+	beforeAll(async () => {
+		profile = env.profiles().first();
+		await env.profiles().restore(profile);
+		await profile.sync();
+	});
+
+	it("#syncAll", async () => {
+		const {
+			result: { current },
+		} = renderHook(() => useWalletSync({ profile, env }));
+
+		const wallet = profile.wallets().first();
+		const network = wallet.network();
+
+		const mockAllowVoting = jest.spyOn(network, "allowsVoting").mockReturnValue(false);
+
+		await act(async () => {
+			expect(current.syncAll(wallet)).resolves.toBeTruthy();
+		});
+
+		mockAllowVoting.mockRestore();
+	});
+});
