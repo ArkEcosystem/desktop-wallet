9c26f56abb8556ec81fba6c7762bae502accd94f
Edgar Goetzendorff <hello@dated.fun>
2021-05-04 18:21:13 +0200

    test: coverage

---
+++ src/app/hooks/use-network-options.test.tsx
@@ -0,0 +1,40 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { EnvironmentProvider } from "app/contexts";
+import React from "react";
+import { env } from "utils/testing-library";
+
+import { useNetworkOptions } from "./use-network-options";
+
+describe("useNetworkOptions hook", () => {
+	it("should return network options", () => {
+		const wrapper = ({ children }: any) => <EnvironmentProvider env={env}>{children}</EnvironmentProvider>;
+		const { result } = renderHook(() => useNetworkOptions(), { wrapper });
+
+		const networks = result.current.networkOptions;
+
+		expect(networks).toEqual([
+			{
+				label: "ARK Mainnet",
+				value: "ark.mainnet",
+			},
+			{
+				label: "ARK Devnet",
+				value: "ark.devnet",
+			},
+			{
+				label: "Compendia - Mainnet",
+				value: "compendia.mainnet",
+			},
+		]);
+	});
+
+	it("should get a network by its id", () => {
+		const id = "ark.mainnet";
+
+		const wrapper = ({ children }: any) => <EnvironmentProvider env={env}> {children} </EnvironmentProvider>;
+		const { result } = renderHook(() => useNetworkOptions(), { wrapper });
+
+		const network = result.current.networkById(id);
+		expect(network?.id()).toEqual(id);
+	});
+});
