e9d1f7f44943ac019d3ea76699e9f987e4b99c14
goga-m <goga.mamoulasvili@gmail.com>
2021-05-07 13:04:13 +0300

    test(e2e): increase timeouts in wallet transactions

--- src/domains/profile/e2e/common.ts
+++ src/domains/profile/e2e/common.ts
@@ -9,5 +9,5 @@ export const goToProfile = async (t: any) => {
 	await t.click(Selector("span").withText("John Doe"));
 	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok();
 	const transactionsCount = Selector('[data-testid="TableRow"]').count;
-	await t.expect(transactionsCount).gte(10, { timeout: 60000 });
+	await t.expect(transactionsCount).gte(10, { timeout: 120000 });
 };
