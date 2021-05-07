d110a4f13c2c8a35064d85c4996cbd340fe3a746
goga-m <goga.mamoulasvili@gmail.com>
2021-05-07 15:02:32 +0300

    wip

--- src/domains/wallet/e2e/common.ts
+++ src/domains/wallet/e2e/common.ts
@@ -4,11 +4,16 @@ import { buildTranslations } from "../../../app/i18n/helpers";
 
 const translations = buildTranslations();
 
-export const goToWallet = async (t: any, wallet = "D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD") => {
+export const goToWalletAndWaitTransactions = async (t: any, wallet = "D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD") => {
 	await t.click(Selector(`[data-testid=WalletCard__${wallet}]`));
 	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
 	const transactionsCount = Selector('[data-testid="TableRow"]').count;
 	await t.expect(transactionsCount).gte(12, { timeout: 300000 });
+};
+
+export const goToWallet = async (t: any, wallet = "D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD") => {
+	await t.click(Selector(`[data-testid=WalletCard__${wallet}]`));
+	await t.expect(Selector("[data-testid=WalletHeader]").exists).ok();
 };
 
 export const importWallet = async (t: any, passphrase = "passphrase", alias = "Test Wallet") => {
