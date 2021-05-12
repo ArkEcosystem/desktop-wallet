e4611ec976ce514a1f17067adad6ea4a829113db
Brian Faust <hello@basecode.sh>
2021-05-12 12:47:36 +0300

    wip

--- src/domains/profile/pages/CreateProfile/CreateProfile.test.tsx
+++ src/domains/profile/pages/CreateProfile/CreateProfile.test.tsx
@@ -107,7 +107,7 @@ describe("CreateProfile", () => {
 			...baseSettings,
 			AVATAR: "data:image/png;base64,avatarImage",
 			NAME: "test profile 1",
-			THEME: "light",
+			THEME: "dark",
 		});
 		expect(profiles[0].usesPassword()).toBe(false);
 
