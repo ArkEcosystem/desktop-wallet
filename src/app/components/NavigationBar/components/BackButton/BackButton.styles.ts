e071b7a31d22ca82d6e3aa8e40d415e2e9051e3b
Ehsan Abtahi <me@ehsanabtahi.com>
2021-05-14 08:29:21 +0200

    chore: focus styles of back button in navigation bar (#3743)

--- src/app/components/NavigationBar/components/BackButton/BackButton.styles.ts
+++ src/app/components/NavigationBar/components/BackButton/BackButton.styles.ts
@@ -2,9 +2,17 @@ import tw from "twin.macro";
 
 const baseStyles = tw`flex items-center w-12 transition-colors duration-200 bg-theme-secondary-100 dark:bg-black`;
 
-const getVariant = (disabled?: boolean) =>
-	disabled
-		? tw`text-theme-secondary-500 dark:text-theme-secondary-800 cursor-not-allowed`
-		: tw`text-theme-primary-600 dark:text-theme-secondary-200 hover:bg-theme-primary-100 hover:dark:bg-theme-secondary-800`;
+const getVariant = (disabled?: boolean) => {
+	if (disabled) {
+		return tw`text-theme-secondary-500 dark:text-theme-secondary-800 cursor-not-allowed`;
+	}
+
+	return tw`
+		text-theme-primary-600
+		dark:text-theme-secondary-200
+		hover:(bg-theme-primary-100 dark:bg-theme-secondary-800)
+		focus:(outline-none ring-2 ring-inset ring-theme-primary-400 rounded)
+	`;
+};
 
 export const getStyles = ({ disabled }: { disabled?: boolean }) => [baseStyles, getVariant(disabled)];
