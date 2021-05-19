fb7b62b991e22a4e4d0825a0d11569bef1fb1e07
Edgar Goetzendorff <hello@dated.fun>
2021-05-19 13:20:01 +0200

    refactor: catch error during unconfirmed transaction lookup (#3776)

--- src/domains/transaction/hooks/use-transaction.ts
+++ src/domains/transaction/hooks/use-transaction.ts
@@ -3,13 +3,17 @@ import { useCallback, useMemo } from "react";
 
 export const useTransaction = () => {
 	const fetchWalletUnconfirmedTransactions = useCallback(async (wallet: Contracts.IReadWriteWallet) => {
-		const sent = await wallet.transactionIndex().sent({ limit: 20, cursor: 1 });
-		return sent
-			.items()
-			.filter(
-				(transaction) =>
-					!transaction.isConfirmed() && (transaction.isMultiPayment() || transaction.isTransfer()),
-			);
+		try {
+			const sent = await wallet.transactionIndex().sent({ limit: 20, cursor: 1 });
+			return sent
+				.items()
+				.filter(
+					(transaction) =>
+						!transaction.isConfirmed() && (transaction.isMultiPayment() || transaction.isTransfer()),
+				);
+		} catch {
+			return [];
+		}
 	}, []);
 
 	return useMemo(
