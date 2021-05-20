bf1e5da41855d12225ca6c72daa95ef2e8f4c39e
goga-m <goga.mamoulasvili@gmail.com>
2021-05-20 15:36:39 +0300

    feat: implement automatic signout hook

---
+++ src/app/hooks/use-automatic-signout.ts
@@ -0,0 +1,59 @@
+import { Contracts } from "@arkecosystem/platform-sdk-profiles";
+import { useRef } from "react";
+import { isIdle } from "utils/electron-utils";
+
+type ActivityState = {
+	intervalId?: number;
+	threshold?: number;
+};
+
+type HandlerParams = {
+	profile?: Contracts.IProfile;
+	onTimeout?: CallbackFunction;
+};
+
+type CallbackFunction = () => void;
+
+export const useAutomaticSignout = () => {
+	const activityState = useRef<ActivityState>({});
+
+	const setActivityState = (callback: CallbackFunction, interval: number, threshold: number) => {
+		clearActivityState();
+
+		const intervalId = +setInterval(callback, interval);
+		activityState.current = { intervalId, threshold };
+	};
+
+	const clearActivityState = () => {
+		clearInterval(activityState.current.intervalId);
+		activityState.current = {};
+	};
+
+	const monitorIdleTime = ({ profile, onTimeout }: HandlerParams) => {
+		if (!profile || !profile.status().isRestored()) {
+			clearActivityState();
+			return;
+		}
+
+		// Already counting
+		if (activityState.current.intervalId) {
+			return;
+		}
+
+		const idleThreshold =
+			(profile.settings().get(Contracts.ProfileSetting.AutomaticSignOutPeriod, 1) as number) * 60;
+
+		setActivityState(
+			() => {
+				if (isIdle(10)) {
+					onTimeout?.();
+					clearActivityState();
+				}
+			},
+			15 * 1000,
+			idleThreshold,
+		);
+	};
+
+	return { monitorIdleTime, resetAutomatiSignout: clearActivityState };
+};
