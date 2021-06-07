cbd9430eb6153b4e5bc60b81827f572328568cc2
Brian Faust <faustbrian@users.noreply.github.com>
2021-06-07 14:36:39 +0300

    Restore use-fees.ts

--- src/app/hooks/use-fees.ts
+++ src/app/hooks/use-fees.ts
@@ -1,7 +1,11 @@
-import { Services } from "@arkecosystem/platform-sdk";
+import { Coins, Services } from "@arkecosystem/platform-sdk";
 import { Contracts as ProfileContracts } from "@arkecosystem/platform-sdk-profiles";
+import { BigNumber } from "@arkecosystem/platform-sdk-support";
 import { useEnvironmentContext } from "app/contexts";
 import { useCallback } from "react";
+
+const normalizeValue = (value: string, decimals: number): string =>
+	BigNumber.make(value).denominated(decimals).toHuman();
 
 export const useFees = ({ profile, normalize = true }: { profile: ProfileContracts.IProfile; normalize?: boolean }) => {
 	const { env } = useEnvironmentContext();
@@ -21,11 +25,14 @@ export const useFees = ({ profile, normalize = true }: { profile: ProfileContrac
 				return transactionFees;
 			}
 
+			const config = profile.coins().get(coin, network).config();
+			const decimals = config.get<number>(Coins.ConfigKey.CurrencyDecimals);
+
 			return {
-				static: transactionFees.static.toHuman(),
-				avg: transactionFees.avg.toHuman(),
-				min: transactionFees.min.toHuman(),
-				max: transactionFees.max.toHuman(),
+				static: normalizeValue(transactionFees.static, decimals),
+				avg: normalizeValue(transactionFees.avg, decimals),
+				min: normalizeValue(transactionFees.min, decimals),
+				max: normalizeValue(transactionFees.max, decimals),
 			};
 		},
 		[env, normalize, profile],
