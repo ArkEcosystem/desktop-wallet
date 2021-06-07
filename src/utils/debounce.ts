9996e7323b86cf06b994fad14d8c48b1aeb2986c
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-06-07 12:13:46 +0200

    remove log leftover

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,6 +1,5 @@
 export const debounceAsync = (callback: Function, delay: number) => {
 	let timeout: any;
-	console.log("hello dudes i am real");
 
 	return async function (...args: any) {
 		return new Promise((resolve) => {
