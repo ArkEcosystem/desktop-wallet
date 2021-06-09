f9fbde6c532d7e3246eb7d23749091b065e3fa83
Edgar Goetzendorff <hello@dated.fun>
2021-06-10 01:54:40 +0200

    wip

--- src/app/components/NavigationBar/components/Balance/Balance.tsx
+++ src/app/components/NavigationBar/components/Balance/Balance.tsx
@@ -1,7 +1,7 @@
 import { Contracts } from "@arkecosystem/platform-sdk-profiles";
 import { Amount } from "app/components/Amount";
 import { useProfileBalance } from "app/hooks/use-profile-balance";
-import React, { useCallback, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { BalanceSkeleton } from "./BalanceSkeleton";
@@ -14,17 +14,12 @@ interface BalanceProps {
 export const Balance = ({ profile, isLoading }: BalanceProps) => {
 	const [width, setWidth] = useState<number | undefined>();
 
+	const ref = useRef<HTMLDivElement>(null);
+
 	const { t } = useTranslation();
 	const { convertedBalance } = useProfileBalance({ profile, isLoading });
 
-	const callbackRef = useCallback(
-		(node) => {
-			if (node) {
-				setWidth(node.clientWidth);
-			}
-		},
-		[convertedBalance],
-	);
+	useEffect(() => setWidth((width) => ref?.current?.clientWidth || width), [convertedBalance]);
 
 	if (isLoading) {
 		return <BalanceSkeleton width={width} />;
@@ -34,7 +29,7 @@ export const Balance = ({ profile, isLoading }: BalanceProps) => {
 		<div className="text-right">
 			<div className="text-xs font-semibold text-theme-secondary-700">{t("COMMON.YOUR_BALANCE")}</div>
 			<div
-				ref={callbackRef}
+				ref={ref}
 				className="text-sm font-bold text-theme-secondary-text dark:text-theme-text"
 				data-testid="Balance__value"
 			>
