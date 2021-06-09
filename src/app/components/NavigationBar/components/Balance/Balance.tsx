5a1b79e40a9a9f1808867c6b5fa1aa8ed78cc8af
Edgar Goetzendorff <hello@dated.fun>
2021-06-10 01:44:11 +0200

    refactor: dynamic balance width during sync

--- src/app/components/NavigationBar/components/Balance/Balance.tsx
+++ src/app/components/NavigationBar/components/Balance/Balance.tsx
@@ -1,7 +1,7 @@
 import { Contracts } from "@arkecosystem/platform-sdk-profiles";
 import { Amount } from "app/components/Amount";
 import { useProfileBalance } from "app/hooks/use-profile-balance";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { BalanceSkeleton } from "./BalanceSkeleton";
@@ -12,17 +12,29 @@ interface BalanceProps {
 }
 
 export const Balance = ({ profile, isLoading }: BalanceProps) => {
+	const [width, setWidth] = useState<number | undefined>();
+
 	const { t } = useTranslation();
 	const { convertedBalance } = useProfileBalance({ profile, isLoading });
 
+	const callbackRef = useCallback(
+		(node) => {
+			if (node) {
+				setWidth(node.clientWidth);
+			}
+		},
+		[convertedBalance],
+	);
+
 	if (isLoading) {
-		return <BalanceSkeleton />;
+		return <BalanceSkeleton width={width} />;
 	}
 
 	return (
 		<div className="text-right">
 			<div className="text-xs font-semibold text-theme-secondary-700">{t("COMMON.YOUR_BALANCE")}</div>
 			<div
+				ref={callbackRef}
 				className="text-sm font-bold text-theme-secondary-text dark:text-theme-text"
 				data-testid="Balance__value"
 			>
