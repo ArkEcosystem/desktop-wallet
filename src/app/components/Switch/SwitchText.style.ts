70c25b307c3c4f1ea4cf156773841a9a938027c2
Ehsan <me@ehsanabtahi.com>
2021-06-09 16:49:10 +0430

    refactor: create SwitchText.style

---
+++ src/app/components/Switch/SwitchText.style.ts
@@ -0,0 +1,39 @@
+import tw from "twin.macro";
+import { Size } from "types";
+
+const baseStyle = [tw`font-semibold select-none text-theme-secondary-500 dark:text-theme-secondary-700`];
+
+const getSelected = (selected: boolean) => {
+	if (selected) {
+		return tw`text-theme-secondary-700 dark:text-theme-secondary-200`;
+	}
+};
+
+const getVariant = (disabled: boolean) => {
+	if (disabled) {
+		return tw`cursor-default`;
+	}
+};
+
+const getSize = (size?: Size) => {
+	switch (size) {
+		case "sm":
+			return tw`text-sm`;
+		case "lg":
+			return tw`text-lg`;
+		default:
+			return tw`text-base`;
+	}
+};
+
+export interface SwitchTextType {
+	size?: Size;
+	selected?: boolean;
+	disabled?: boolean;
+}
+export const getSwitchTextStyles = ({ size, selected, disabled }: SwitchTextType) => [
+	...baseStyle,
+	getSize(size),
+	getSelected(selected!),
+	getVariant(disabled!),
+];
