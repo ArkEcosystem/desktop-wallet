69512f18c142d9857c31eac5d6dedb46a8df7835
Edgar Goetzendorff <hello@dated.fun>
2021-06-09 23:39:41 +0200

    fix: avoid memoing corrupted time format

--- src/app/hooks/use-time-format.tsx
+++ src/app/hooks/use-time-format.tsx
@@ -1,6 +1,5 @@
 import { Contracts } from "@arkecosystem/platform-sdk-profiles";
 import { useEnvironmentContext } from "app/contexts";
-import { useMemo } from "react";
 import { useHistory } from "react-router-dom";
 
 import { useProfileUtils } from "./use-profile-utils";
@@ -14,15 +13,11 @@ export const useTimeFormat = () => {
 
 	const profile = getProfileFromUrl(history?.location.pathname);
 
-	const result = useMemo(() => {
-		if (!profile) {
-			return defaultFormat;
-		}
+	if (!profile) {
+		return defaultFormat;
+	}
 
-		const timeFormat = profile.settings().get<string>(Contracts.ProfileSetting.TimeFormat);
+	const timeFormat = profile.settings().get<string>(Contracts.ProfileSetting.TimeFormat);
 
-		return defaultFormat.replace("HH:mm", timeFormat!);
-	}, [profile]);
-
-	return result;
+	return timeFormat ? defaultFormat.replace("HH:mm", timeFormat) : defaultFormat;
 };
