603a03e6d499cdb971293e3113a5f240d1a05aca
Brian Faust <hello@basecode.sh>
2021-06-10 11:04:24 +0300

    wip

--- src/domains/vote/components/AddressTable/AddressRow/AddressRow.test.tsx
+++ src/domains/vote/components/AddressTable/AddressRow/AddressRow.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { Contracts } from "@arkecosystem/platform-sdk-profiles";
 // @README: This import is fine in tests but should be avoided in production code.
-import { ReadOnlyWallet } from "@arkecosystem/platform-sdk-profiles/dist/drivers/memory/wallets/read-only-wallet";
+import { ReadOnlyWallet } from "@arkecosystem/platform-sdk-profiles/distribution/drivers/memory/wallets/read-only-wallet";
 import nock from "nock";
 import React from "react";
 import { act, env, fireEvent, getDefaultProfileId, render, syncDelegates, waitFor } from "testing-library";
