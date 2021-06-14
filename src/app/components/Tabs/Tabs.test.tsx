a3f29c693ab2b20e166b2ccff1dea17c5fbeba8d
Edgar Goetzendorff <hello@dated.fun>
2021-06-14 16:35:56 +0200

    wip

--- src/app/components/Tabs/Tabs.test.tsx
+++ src/app/components/Tabs/Tabs.test.tsx
@@ -24,6 +24,26 @@ describe("Tabs", () => {
 		expect(asFragment()).toMatchSnapshot();
 	});
 
+	it("should render without background", () => {
+		const { container, asFragment } = render(
+			<Tabs noBackground>
+				<TabList>
+					<Tab tabId={1}>First</Tab>
+					<Tab tabId={2}>Second</Tab>
+					<Tab tabId={3}>Third</Tab>
+				</TabList>
+				<div className="mt-5">
+					<TabPanel tabId={1}>1</TabPanel>
+					<TabPanel tabId={2}>2</TabPanel>
+					<TabPanel tabId={3}>3</TabPanel>
+				</div>
+			</Tabs>,
+		);
+
+		expect(container).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
 	it("should react to use effect call", () => {
 		const { container, asFragment, getByTestId } = render(
 			<Tabs activeId={2}>
@@ -48,6 +68,7 @@ describe("Tabs", () => {
 			<Tabs activeId={1}>
 				<TabList>
 					<Tab tabId={1}>First</Tab>
+					<div>separator</div>
 					<Tab tabId={2}>Second</Tab>
 				</TabList>
 				<TabPanel tabId={1}>1</TabPanel>
