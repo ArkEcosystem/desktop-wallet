87e5d919d77f7df10705e0f2f56ab415e4572a88
goga-m <goga.mamoulasvili@gmail.com>
2021-06-18 17:44:21 +0300

    test(e2e): add missing mocks

--- src/utils/e2e-utils.ts
+++ src/utils/e2e-utils.ts
@@ -196,6 +196,10 @@ export const requestMocks = {
 			"coins/ark/devnet/transactions",
 		),
 		mockRequest(
+			"https://dwallets.ark.io/api/transactions?page=1&limit=20&senderId=D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD",
+			"coins/ark/devnet/transactions",
+		),
+		mockRequest(
 			"https://dwallets.ark.io/api/transactions?limit=30&address=D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD%2CD5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb",
 			"coins/ark/devnet/transactions",
 		),
