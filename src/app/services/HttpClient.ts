8f16c8b6253b80a766d94d6d2587ab81a1a7ea45
Brian Faust <hello@basecode.sh>
2021-06-25 07:15:15 +0300

    wip

--- src/app/services/HttpClient.ts
+++ src/app/services/HttpClient.ts
@@ -10,71 +10,69 @@ import { Cache } from "./Cache";
 const fetch = process.env.REACT_APP_IS_E2E ? window.fetch : crossFetch;
 
 export class HttpClient extends AbstractRequest {
-	private readonly cache: Cache;
+    private readonly cache: Cache;
 
-	public constructor(ttl: number) {
-		super();
+    public constructor(ttl: number) {
+        super();
 
-		this.cache = new Cache(ttl);
+        this.cache = new Cache(ttl);
 
-		this.withHeaders({
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		});
-	}
+        this.withHeaders({
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        });
+    }
 
-	/* istanbul ignore next */
-	public withSocksProxy(host: string): HttpClient {
-		this._options.agent = new SocksProxyAgent(host);
+    /* istanbul ignore next */
+    public withSocksProxy(host: string): HttpClient {
+        this._options.agent = new SocksProxyAgent(host);
 
-		return this;
-	}
+        return this;
+    }
 
-	protected async send(
-		method: string,
-		url: string,
-		data?: {
-			query?: object;
-			data?: any;
-		},
-	): Promise<HttpResponse> {
-		if (data?.query && Object.keys(data?.query).length > 0) {
-			url = `${url}?${new URLSearchParams(data.query as any)}`;
-		}
+    protected async send(
+        method: string,
+        url: string,
+        data?: {
+            query?: object;
+            data?: any;
+        },
+    ): Promise<HttpResponse> {
+        if (data?.query && Object.keys(data?.query).length > 0) {
+            url = `${url}?${new URLSearchParams(data.query as any)}`;
+        }
 
-		const cacheKey: string = hash(`${method}.${url}.${JSON.stringify(data)}`).toString();
-		// console.time(url);
+        const cacheKey: string = hash(`${method}.${url}.${JSON.stringify(data)}`).toString();
+        // console.time(url);
 
-		return this.cache.remember(cacheKey, async () => {
-			let response;
+        return this.cache.remember(cacheKey, async () => {
+            let response;
 
-			if (method === "GET") {
-				response = await fetch(url, this._options);
-			}
+            if (method === "GET") {
+                response = await fetch(url, this._options);
+            }
 
-			if (method === "POST") {
-				response = await fetch(url, {
-					...this._options,
-					method: "POST",
-					body: JSON.stringify(data?.data),
-				});
-			}
+            if (method === "POST") {
+                response = await fetch(url, {
+                    ...this._options,
+                    method: "POST",
+                    body: JSON.stringify(data?.data),
+                });
+            }
 
-			if (!response) {
-				throw new Error("Received no response. This looks like a bug.");
-			}
+            if (!response) {
+                throw new Error("Received no response. This looks like a bug.");
+            }
 
-			console.log({ method, url, data });
+            return new Response({
+                body: await response.text(),
+                headers: (response.headers as unknown) as Record<string, Primitive>,
+                statusCode: response.status,
+            });
+        });
+    }
 
-			return new Response({
-				body: await response.text(),
-				headers: (response.headers as unknown) as Record<string, Primitive>,
-				statusCode: response.status,
-			});
-		});
-	}
-
-	public clearCache() {
-		this.cache.flush();
-	}
+    public clearCache() {
+        this.cache.flush();
+    }
 }
