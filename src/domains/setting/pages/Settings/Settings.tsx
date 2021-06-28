3c4347c4fc30846febc7d1ab7d07aca377a88cb6
goga-m <goga.mamoulasvili@gmail.com>
2021-06-28 19:41:27 +0300

    refactor: cleanup settings page state

--- src/domains/setting/pages/Settings/Settings.tsx
+++ src/domains/setting/pages/Settings/Settings.tsx
@@ -1,22 +1,13 @@
 import { Page, Section } from "app/components/Layout";
 import { SideBar } from "app/components/SideBar";
-import { useActiveProfile, useReloadPath } from "app/hooks";
-import { toasts } from "app/services";
+import { useActiveProfile } from "app/hooks";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { matchPath, Prompt } from "react-router-dom";
 
 import { availableSettings } from "./available-settings";
 
 export const Settings = () => {
 	const [activeSettings, setActiveSettings] = useState("General");
-
-	const reloadPath = useReloadPath();
-
-	const form = useForm({ mode: "onChange" });
-	const { register, errors, formState, reset } = form;
-	const { isDirty } = formState;
 
 	const activeProfile = useActiveProfile();
 
@@ -40,48 +31,10 @@ export const Settings = () => {
 		},
 	];
 
-	const handleSuccess = (message?: string) => {
-		reloadPath();
-		reset();
-
-		toasts.success(message || t("SETTINGS.GENERAL.SUCCESS"));
-	};
-
-	const handleError = (errorMessage: string, message?: string) => {
-		toasts.error(`${message || t("COMMON.ERROR")}: ${errorMessage}`);
-	};
-
-	const getPromptMessage = (location: any) => {
-		/* istanbul ignore next */
-		const pathname = location.pathname || location.location?.pathname;
-
-		const matchCurrent = matchPath(pathname, {
-			path: "/profiles/:profileId/settings",
-		});
-
-		const isReload = matchCurrent !== null;
-
-		if (isReload) {
-			return true;
-		}
-
-		if (isDirty) {
-			return "block";
-		}
-
-		return true;
-	};
-
 	const renderSettings = () => {
 		const ActiveSettings = availableSettings[activeSettings];
 
-		return (
-			<ActiveSettings
-				formConfig={{ context: form, register, errors }}
-				onSuccess={handleSuccess}
-				onError={handleError}
-			/>
-		);
+		return <ActiveSettings />;
 	};
 
 	return (
@@ -90,7 +43,6 @@ export const Settings = () => {
 			sidebar={<SideBar items={settingsItems} activeItem={activeSettings} handleActiveItem={setActiveSettings} />}
 		>
 			<Section innerClassName="px-12">{renderSettings()}</Section>
-			<Prompt message={getPromptMessage} />
 		</Page>
 	);
 };
