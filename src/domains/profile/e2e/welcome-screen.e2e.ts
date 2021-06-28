9c13b20df2939fe17006569e6a3b4f45caeb9312
Edgar Goetzendorff <hello@dated.fun>
2021-06-28 13:48:23 +0200

    wip

--- src/domains/profile/e2e/welcome-screen.e2e.ts
+++ src/domains/profile/e2e/welcome-screen.e2e.ts
@@ -2,6 +2,8 @@ import { Selector } from "testcafe";
 
 import { buildTranslations } from "../../../app/i18n/helpers";
 import { createFixture } from "../../../utils/e2e-utils";
+import { goToSettings } from "../../settings/e2e/common";
+import { goToProfile } from "./common";
 
 const translations = buildTranslations();
 
@@ -12,12 +14,13 @@ test("should load profiles welcome page", async (t) => {
 });
 
 test("should return to welcome page when application is idle", async (t) => {
-	await t.expect(Selector("span").withText(translations.COMMON.DESKTOP_WALLET).exists).ok();
+	await goToProfile(t);
+	await goToSettings(t);
 
-	await t.click(Selector("span").withText("John Doe"));
+	await t.click(Selector("[data-testid=Input__suggestion]").withText("15 minutes"));
+	await t.click('[data-testid="SelectDropdown__option--0"]');
 
-	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).ok();
-	await t.expect(Selector('[data-testid="transactions__fetch-more-button"]').exists).ok();
+	await t.click(Selector("button").withText(translations.COMMON.SAVE));
 
 	await t.expect(Selector("div").withText(translations.COMMON.WALLETS).exists).notOk({ timeout: 100_000 });
 });
