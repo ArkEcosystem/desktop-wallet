5598d7986119277736f17c4ac8b30dedcdc288ad
goga-m <goga.mamoulasvili@gmail.com>
2021-06-29 11:19:28 +0300

    refactor: cleanup settings page component

--- src/domains/setting/pages/Settings/Settings.tsx
+++ src/domains/setting/pages/Settings/Settings.tsx
@@ -1,48 +1,24 @@
 import { Page, Section } from "app/components/Layout";
 import { SideBar } from "app/components/SideBar";
 import { useActiveProfile } from "app/hooks";
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
-
-import { availableSettings } from "./available-settings";
+import { useSettingsMenu } from "domains/setting/hooks/use-settings-menu";
+import { Export,General, PasswordSettings } from "domains/setting/pages/Settings";
+import React from "react";
 
 export const Settings = () => {
-	const [activeSettings, setActiveSettings] = useState("General");
-
 	const activeProfile = useActiveProfile();
-
-	const { t } = useTranslation();
-
-	const settingsItems = [
-		{
-			itemKey: "General",
-			label: t("SETTINGS.GENERAL.MENU_ITEM"),
-			icon: "SettingsHorizontal",
-		},
-		{
-			itemKey: "Password",
-			label: t("SETTINGS.PASSWORD.MENU_ITEM"),
-			icon: "Lock",
-		},
-		{
-			itemKey: "Export",
-			label: t("SETTINGS.EXPORT.MENU_ITEM"),
-			icon: "Export",
-		},
-	];
-
-	const renderSettings = () => {
-		const ActiveSettings = availableSettings[activeSettings];
-
-		return <ActiveSettings />;
-	};
+	const { menuItems, activeSettings, setActiveSettings } = useSettingsMenu();
 
 	return (
 		<Page
 			profile={activeProfile}
-			sidebar={<SideBar items={settingsItems} activeItem={activeSettings} handleActiveItem={setActiveSettings} />}
+			sidebar={<SideBar items={menuItems} activeItem={activeSettings} handleActiveItem={setActiveSettings} />}
 		>
-			<Section innerClassName="px-12">{renderSettings()}</Section>
+			<Section innerClassName="px-12">
+				{activeSettings === "General" && <General />}
+				{activeSettings === "Password" && <PasswordSettings />}
+				{activeSettings === "Export" && <Export />}
+			</Section>
 		</Page>
 	);
 };
