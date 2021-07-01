b4567fe65248f80e7c20316aa84445d452efc324
goga-m <goga.mamoulasvili@gmail.com>
2021-07-01 18:13:05 +0300

    feat: implement `SettingsPageWrapper` component

---
+++ src/domains/setting/components/SettingsPageWrapper/SettingsPageWrapper.tsx
@@ -0,0 +1,38 @@
+import { Page, Section } from "app/components/Layout";
+import { SideBar } from "app/components/SideBar";
+import { useSettingsMenu } from "domains/setting/hooks/use-settings-menu";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Contracts } from "@arkecosystem/platform-sdk-profiles";
+
+type ActiveSettings = "general" | "export" | "password";
+
+export const SettingsWrapper = ({
+	children,
+	profile,
+	activeSettings,
+}: {
+	profile: Contracts.IProfile;
+	children: React.ReactNode;
+	activeSettings: ActiveSettings;
+}) => {
+	const { menuItems } = useSettingsMenu();
+	const history = useHistory();
+
+	return (
+		<Page
+			profile={profile}
+			sidebar={
+				<SideBar
+					items={menuItems}
+					activeItem={activeSettings}
+					handleActiveItem={(activeSetting: string) => {
+						history.push(`/profiles/${profile.id()}/settings/${activeSetting}`);
+					}}
+				/>
+			}
+		>
+			<Section innerClassName="px-12">{children}</Section>
+		</Page>
+	);
+};
