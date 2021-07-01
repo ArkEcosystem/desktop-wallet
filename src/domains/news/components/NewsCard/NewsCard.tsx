bcd8e04d6c98a44601c2f95131ebc60e1b1169dc
Ehsan Abtahi <me@ehsanabtahi.com>
2021-07-01 03:05:55 +0200

    fix: adjust styles of the news card (#3993)

--- src/domains/news/components/NewsCard/NewsCard.tsx
+++ src/domains/news/components/NewsCard/NewsCard.tsx
@@ -23,7 +23,7 @@ export const NewsCard = ({ text, category, author, created_at: createdAt, coverI
 	return (
 		<div data-testid="NewsCard">
 			<Card className="bg-theme-background">
-				<div className="flex flex-col p-4 space-y-8 bg-theme-background">
+				<div className="flex flex-col p-4 space-y-6 bg-theme-background">
 					<div className="flex justify-between w-full">
 						<div className="flex items-center space-x-4">
 							<NetworkIcon size="lg" coin={asset?.coin} network={asset?.network} noShadow />
