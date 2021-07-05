69c52ec59df4261bb8e8dd10413495fe2ff5048e
Brian Faust <hello@basecode.sh>
2021-07-05 09:53:12 +0300

    wip

--- src/app/assets/images/index.ts
+++ src/app/assets/images/index.ts
@@ -6,11 +6,11 @@ import { transaction } from "domains/transaction/images";
 import { wallet } from "domains/wallet/images";
 
 import { ReactComponent as ARKLogo } from "./ark-logo.svg";
-import { ReactComponent as ChangeNowLogo } from "./changenow-logo.svg";
 import { ImagesDark } from "./dark";
 import { ReactComponent as DeleteBanner } from "./delete-banner.svg";
 import { ReactComponent as EmptyNotifications } from "./empty-notifications.svg";
 import { ReactComponent as ErrorBanner } from "./error-banner.svg";
+import { ReactComponent as Exchanges } from "./exchanges.svg";
 import { ReactComponent as GenericWarning } from "./generic-warning.svg";
 import { ReactComponent as NoResultsBanner } from "./no-results-banner.svg";
 import { ReactComponent as SuccessBanner } from "./success-banner.svg";
@@ -20,10 +20,10 @@ import { ReactComponent as WelcomeBanner } from "./welcome-banner.svg";
 export const images = {
 	common: {
 		ARKLogo,
-		ChangeNowLogo,
 		DeleteBanner,
 		EmptyNotifications,
 		ErrorBanner,
+		Exchanges,
 		GenericWarning,
 		NoResultsBanner,
 		SuccessBanner,
