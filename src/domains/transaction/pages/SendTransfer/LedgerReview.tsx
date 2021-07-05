6f0ed053eeea7b493e1e35c0ba74601df05f87a2
Edgar Goetzendorff <hello@dated.fun>
2021-07-05 22:56:37 +0200

    refactor: show skeleton while loading transaction expiration

--- src/domains/transaction/pages/SendTransfer/LedgerReview.tsx
+++ src/domains/transaction/pages/SendTransfer/LedgerReview.tsx
@@ -1,5 +1,6 @@
 import { Contracts } from "@arkecosystem/platform-sdk-profiles";
 import { Icon } from "app/components/Icon";
+import { Skeleton } from "app/components/Skeleton";
 import { Tooltip } from "app/components/Tooltip";
 import { TotalAmountBox } from "domains/transaction/components/TotalAmountBox";
 import {
@@ -35,29 +36,39 @@ export const TransferLedgerReview = ({
 		timestamp: t("TRANSACTION.EXPIRATION.TIMESTAMP"),
 	};
 
+	const renderExpiration = () => {
+		if (estimatedExpiration) {
+			return estimatedExpiration;
+		}
+		
+		return (
+			<span data-testid="TransferLedgerReview__expiration-skeleton" className="flex my-0.5">
+				<Skeleton height={16} width={80} />
+			</span>
+		);
+	};
+
 	return (
 		<>
 			<TransactionRecipients currency={wallet.currency()} recipients={recipients} border={false} />
 
 			{memo && <TransactionMemo memo={memo} />}
 
-			{estimatedExpiration && (
-				<TransactionDetail
-					label={
-						<div data-testid="LedgerReview__expiration" className="flex items-center space-x-2">
-							<span>{t("COMMON.EXPIRATION")}</span>
+			<TransactionDetail
+				label={
+					<div data-testid="LedgerReview__expiration" className="flex items-center space-x-2">
+						<span>{t("COMMON.EXPIRATION")}</span>
 
-							<Tooltip content={expirationTypeTranslations[expirationType]}>
-								<div className="flex justify-center items-center w-5 h-5 rounded-full cursor-pointer questionmark bg-theme-primary-100 hover:bg-theme-primary-700 dark:bg-theme-secondary-800 text-theme-primary-600 dark:text-theme-secondary-200 hover:text-white">
-									<Icon width={10} height={10} name="QuestionMark" />
-								</div>
-							</Tooltip>
-						</div>
-					}
-				>
-					{estimatedExpiration}
-				</TransactionDetail>
-			)}
+						<Tooltip content={expirationTypeTranslations[expirationType]}>
+							<div className="flex justify-center items-center w-5 h-5 rounded-full cursor-pointer questionmark bg-theme-primary-100 hover:bg-theme-primary-700 dark:bg-theme-secondary-800 text-theme-primary-600 dark:text-theme-secondary-200 hover:text-white">
+								<Icon width={10} height={10} name="QuestionMark" />
+							</div>
+						</Tooltip>
+					</div>
+				}
+			>
+				{renderExpiration()}
+			</TransactionDetail>
 
 			<div className="mt-2">
 				<TotalAmountBox amount={amount} fee={fee} ticker={wallet.currency()} />
