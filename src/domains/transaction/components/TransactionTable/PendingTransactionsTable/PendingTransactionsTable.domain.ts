65f392cc032726bf7d666c01604c7b2b69c5c00f
goga-m <goga.mamoulasvili@gmail.com>
2021-07-05 16:46:41 +0300

    feat: add domain transaction contracts and domain

---
+++ src/domains/transaction/components/TransactionTable/PendingTransactionsTable/PendingTransactionsTable.domain.ts
@@ -0,0 +1,48 @@
+import { TFunction } from "i18next";
+
+const createTableColumns = (
+	t: TFunction,
+): {
+	accessor?: string;
+	cellWidth?: string;
+	className?: string;
+	Header: string;
+	minimumWidth?: boolean;
+	sortDescFirst?: boolean;
+}[] => [
+	{
+		Header: t("COMMON.ID"),
+		minimumWidth: true,
+	},
+	{
+		Header: t("COMMON.DATE"),
+		accessor: "timestamp",
+		sortDescFirst: true,
+		cellWidth: "w-50",
+	},
+	{
+		Header: t("COMMON.RECIPIENT"),
+		cellWidth: "w-96",
+	},
+	{
+		Header: t("COMMON.INFO"),
+		className: "justify-center",
+	},
+	{
+		Header: t("COMMON.STATUS"),
+		className: "justify-center",
+		minimumWidth: true,
+	},
+	{
+		Header: t("COMMON.AMOUNT"),
+		accessor: "amount",
+		className: "justify-end no-border",
+	},
+	{
+		Header: "Sign",
+		className: "hidden",
+		cellWidth: "w-24",
+	},
+];
+
+export { createTableColumns };
