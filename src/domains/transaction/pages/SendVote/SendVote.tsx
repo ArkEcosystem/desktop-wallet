70e720703c5a561898dfef7b36dc8752303fc378
Edgar Goetzendorff <hello@dated.fun>
2021-07-06 09:26:48 +0200

    refactor: remove mnemonic error check from `SendVote`

--- src/domains/transaction/pages/SendVote/SendVote.tsx
+++ src/domains/transaction/pages/SendVote/SendVote.tsx
@@ -11,7 +11,7 @@ import { AuthenticationStep } from "domains/transaction/components/Authenticatio
 import { ErrorStep } from "domains/transaction/components/ErrorStep";
 import { FeeWarning } from "domains/transaction/components/FeeWarning";
 import { useFeeConfirmation, useTransactionBuilder, useWalletSignatory } from "domains/transaction/hooks";
-import { handleBroadcastError, isMnemonicError } from "domains/transaction/utils";
+import { handleBroadcastError } from "domains/transaction/utils";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
@@ -311,14 +311,6 @@ export const SendVote = () => {
 				await confirmSendVote(isUnvote ? "unvote" : "vote");
 			}
 		} catch (error) {
-			/* istanbul ignore next */
-			if (isMnemonicError(error)) {
-				/* istanbul ignore next */
-				setValue("mnemonic", "");
-				/* istanbul ignore next */
-				return setError("mnemonic", { message: t("TRANSACTION.INVALID_MNEMONIC"), type: "manual" });
-			}
-
 			setErrorMessage(JSON.stringify({ message: error.message, type: error.name }));
 			setActiveTab(5);
 		}
